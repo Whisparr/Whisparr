winterqt/Radarr

New: Notifiarr moved from webhook to API

src/NzbDrone.Core/Notifications/Notifiarr/NotifiarrProxy.cs
@@ -78,8 +78,7 @@ namespace NzbDrone.Core.Notifications.Notifiarr
             try
             {
                 var url = settings.Environment == (int)NotifiarrEnvironment.Development ? "https://dev.notifiarr.com" : "https://notifiarr.com";
-                var requestBuilder = new HttpRequestBuilder(url + "/notifier.php").Post();
-                requestBuilder.AddFormParameter("api", settings.APIKey).Build();
+                var requestBuilder = new HttpRequestBuilder(url + "/api/v1/notification/radarr/" + settings.APIKey).Post();
                 requestBuilder.AddFormParameter("instanceName", settings.InstanceName).Build();
 
                 foreach (string key in message.Keys)

Whisparr/Whisparr

src/NzbDrone.Core/Notifications/Notifiarr/NotifiarrProxy.cs
@@ -79,8 +79,7 @@ namespace NzbDrone.Core.Notifications.Notifiarr
             try
             {
                 var url = settings.Environment == (int)NotifiarrEnvironment.Development ? "https://dev.notifiarr.com" : "https://notifiarr.com";
-                var requestBuilder = new HttpRequestBuilder(url + "/notifier.php").Post();
-                requestBuilder.AddFormParameter("api", settings.APIKey).Build();
+                var requestBuilder = new HttpRequestBuilder(url + "/api/v1/notification/whisparr/" + settings.APIKey).Post();
                 requestBuilder.AddFormParameter("instanceName", settings.InstanceName).Build();
 
                 foreach (string key in message.Keys)
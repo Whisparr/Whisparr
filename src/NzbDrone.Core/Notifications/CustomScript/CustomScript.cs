Sonarr/Sonarr

New: Add custom format info to episode import notifications

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -134,6 +134,8 @@ namespace NzbDrone.Core.Notifications.CustomScript
             environmentVariables.Add("Sonarr_EpisodeFile_MediaInfo_Subtitles", episodeFile.MediaInfo.Subtitles.ConcatToString(" / "));
             environmentVariables.Add("Sonarr_EpisodeFile_MediaInfo_VideoCodec", MediaInfoFormatter.FormatVideoCodec(episodeFile.MediaInfo, null));
             environmentVariables.Add("Sonarr_EpisodeFile_MediaInfo_VideoDynamicRangeType", MediaInfoFormatter.FormatVideoDynamicRangeType(episodeFile.MediaInfo));
+            environmentVariables.Add("Sonarr_EpisodeFile_CustomFormat", string.Join("|", message.EpisodeInfo.CustomFormats));
+            environmentVariables.Add("Sonarr_EpisodeFile_CustomFormatScore", message.EpisodeInfo.CustomFormatScore.ToString());
 
             if (message.OldFiles.Any())
             {

src/NzbDrone.Core/Notifications/Notifiarr/Notifiarr.cs
@@ -104,6 +104,8 @@ namespace NzbDrone.Core.Notifications.Notifiarr
             variables.Add("Sonarr_EpisodeFile_MediaInfo_Subtitles", episodeFile.MediaInfo.Subtitles.ConcatToString(" / "));
             variables.Add("Sonarr_EpisodeFile_MediaInfo_VideoCodec", MediaInfoFormatter.FormatVideoCodec(episodeFile.MediaInfo, null));
             variables.Add("Sonarr_EpisodeFile_MediaInfo_VideoDynamicRangeType", MediaInfoFormatter.FormatVideoDynamicRangeType(episodeFile.MediaInfo));
+            variables.Add("Sonarr_EpisodeFile_CustomFormat", string.Join("|", message.EpisodeInfo.CustomFormats));
+            variables.Add("Sonarr_EpisodeFile_CustomFormatScore", message.EpisodeInfo.CustomFormatScore.ToString());
 
             if (message.OldFiles.Any())
             {

Whisparr/Whisparr

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -130,6 +130,8 @@ namespace NzbDrone.Core.Notifications.CustomScript
             environmentVariables.Add("Whisparr_EpisodeFile_MediaInfo_Subtitles", episodeFile.MediaInfo.Subtitles.ConcatToString(" / "));
             environmentVariables.Add("Whisparr_EpisodeFile_MediaInfo_VideoCodec", MediaInfoFormatter.FormatVideoCodec(episodeFile.MediaInfo, null));
             environmentVariables.Add("Whisparr_EpisodeFile_MediaInfo_VideoDynamicRangeType", MediaInfoFormatter.FormatVideoDynamicRangeType(episodeFile.MediaInfo));
+            environmentVariables.Add("Whisparr_EpisodeFile_CustomFormat", string.Join("|", message.EpisodeInfo.CustomFormats));
+            environmentVariables.Add("Whisparr_EpisodeFile_CustomFormatScore", message.EpisodeInfo.CustomFormatScore.ToString());
 
             if (message.OldFiles.Any())
             {

src/NzbDrone.Core/Notifications/Notifiarr/Notifiarr.cs
@@ -100,6 +100,8 @@ namespace NzbDrone.Core.Notifications.Notifiarr
             variables.Add("Whisparr_EpisodeFile_MediaInfo_Subtitles", episodeFile.MediaInfo.Subtitles.ConcatToString(" / "));
             variables.Add("Whisparr_EpisodeFile_MediaInfo_VideoCodec", MediaInfoFormatter.FormatVideoCodec(episodeFile.MediaInfo, null));
             variables.Add("Whisparr_EpisodeFile_MediaInfo_VideoDynamicRangeType", MediaInfoFormatter.FormatVideoDynamicRangeType(episodeFile.MediaInfo));
+            variables.Add("Whisparr_EpisodeFile_CustomFormat", string.Join("|", message.EpisodeInfo.CustomFormats));
+            variables.Add("Whisparr_EpisodeFile_CustomFormatScore", message.EpisodeInfo.CustomFormatScore.ToString());
 
             if (message.OldFiles.Any())
             {
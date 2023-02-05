Sonarr/Sonarr

New: Added new series title rename tokens without year
Closes #5369

frontend/src/Settings/MediaManagement/Naming/NamingModal.js
@@ -46,9 +46,12 @@ const seriesTokens = [
   { token: '{Series Title}', example: 'Series Title\'s' },
   { token: '{Series CleanTitle}', example: 'Series Titles' },
   { token: '{Series CleanTitleYear}', example: 'Series Titles! 2010' },
+  { token: '{Series CleanTitleWithoutYear}', example: 'Series Titles!' },
   { token: '{Series TitleThe}', example: 'Series Title\'s, The' },
   { token: '{Series TitleTheYear}', example: 'Series Title\'s, The (2010)' },
+  { token: '{Series TitleTheWithoutYear}', example: 'Series Title\'s, The' },
   { token: '{Series TitleYear}', example: 'Series Title\'s (2010)' },
+  { token: '{Series TitleWithoutYear}', example: 'Series Title\'s' },
   { token: '{Series TitleFirstCharacter}', example: 'S' },
   { token: '{Series Year}', example: '2010' }
 ];

src/NzbDrone.Core/Organizer/FileNameBuilder.cs
@@ -66,7 +66,7 @@ namespace NzbDrone.Core.Organizer
 
         public static readonly Regex AirDateRegex = new Regex(@"\{Air(\s|\W|_)Date\}", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-        public static readonly Regex SeriesTitleRegex = new Regex(@"(?<token>\{(?:Series)(?<separator>[- ._])(Clean)?Title(The)?(Year)?\})",
+        public static readonly Regex SeriesTitleRegex = new Regex(@"(?<token>\{(?:Series)(?<separator>[- ._])(Clean)?Title(The)?(Without)?(Year)?\})",
                                                                             RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         private static readonly Regex FileNameCleanupRegex = new Regex(@"([- ._])(\1)+", RegexOptions.Compiled);
@@ -370,6 +370,13 @@ namespace NzbDrone.Core.Organizer
             return $"{title} ({year})";
         }
 
+        public static string TitleWithoutYear(string title)
+        {
+            title = YearRegex.Replace(title, "");
+
+            return title;
+        }
+
         public static string CleanFileName(string name, bool replace = true)
         {
             string result = name;
@@ -453,9 +460,12 @@ namespace NzbDrone.Core.Organizer
             tokenHandlers["{Series Title}"] = m => series.Title;
             tokenHandlers["{Series CleanTitle}"] = m => CleanTitle(series.Title);
             tokenHandlers["{Series CleanTitleYear}"] = m => CleanTitle(TitleYear(series.Title, series.Year));
+            tokenHandlers["{Series CleanTitleWithoutYear}"] = m => CleanTitle(TitleWithoutYear(series.Title));
             tokenHandlers["{Series TitleThe}"] = m => TitleThe(series.Title);
             tokenHandlers["{Series TitleYear}"] = m => TitleYear(series.Title, series.Year);
+            tokenHandlers["{Series TitleWithoutYear}"] = m => TitleWithoutYear(series.Title);
             tokenHandlers["{Series TitleTheYear}"] = m => TitleYear(TitleThe(series.Title), series.Year);
+            tokenHandlers["{Series TitleTheWithoutYear}"] = m => TitleWithoutYear(TitleThe(series.Title));
             tokenHandlers["{Series TitleFirstCharacter}"] = m => TitleThe(series.Title).Substring(0, 1).FirstCharToUpper();
             tokenHandlers["{Series Year}"] = m => series.Year.ToString();
         }

Whisparr/Whisparr

frontend/src/Settings/MediaManagement/Naming/NamingModal.js
@@ -46,9 +46,12 @@ const seriesTokens = [
   { token: '{Site Title}', example: 'Site Title\'s' },
   { token: '{Site CleanTitle}', example: 'Site Titles' },
   { token: '{Site CleanTitleYear}', example: 'Site Titles! 2010' },
+  { token: '{Site CleanTitleWithoutYear}', example: 'Site Titles!' },
   { token: '{Site TitleThe}', example: 'Site Title\'s, The' },
   { token: '{Site TitleTheYear}', example: 'Site Title\'s, The (2010)' },
+  { token: '{Site TitleTheWithoutYear}', example: 'Site Title\'s, The' },
   { token: '{Site TitleYear}', example: 'Site Title\'s (2010)' },
+  { token: '{Site TitleWithoutYear}', example: 'Site Title\'s' },
   { token: '{Site TitleFirstCharacter}', example: 'S' },
   { token: '{Site Year}', example: '2010' }
 ];

src/NzbDrone.Core/Organizer/FileNameBuilder.cs
@@ -57,7 +57,7 @@ namespace NzbDrone.Core.Organizer
 
         public static readonly Regex AirDateRegex = new Regex(@"\{Release(\s|\W|_)Date\}", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-        public static readonly Regex SeriesTitleRegex = new Regex(@"(?<token>\{(?:Site)(?<separator>[- ._])(Clean)?Title(The)?(Year)?\})",
+        public static readonly Regex SeriesTitleRegex = new Regex(@"(?<token>\{(?:Site)(?<separator>[- ._])(Clean)?Title(The)?(Without)?(Year)?\})",
                                                                             RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         private static readonly Regex FileNameCleanupRegex = new Regex(@"([- ._])(\1)+", RegexOptions.Compiled);
@@ -336,6 +336,13 @@ namespace NzbDrone.Core.Organizer
             return $"{title} ({year})";
         }
 
+        public static string TitleWithoutYear(string title)
+        {
+            title = YearRegex.Replace(title, "");
+
+            return title;
+        }
+
         public static string CleanFileName(string name, bool replace = true)
         {
             string result = name;
@@ -396,9 +403,12 @@ namespace NzbDrone.Core.Organizer
             tokenHandlers["{Site Title}"] = m => series.Title;
             tokenHandlers["{Site CleanTitle}"] = m => CleanTitle(series.Title);
             tokenHandlers["{Site CleanTitleYear}"] = m => CleanTitle(TitleYear(series.Title, series.Year));
+            tokenHandlers["{Site CleanTitleWithoutYear}"] = m => CleanTitle(TitleWithoutYear(series.Title));
             tokenHandlers["{Site TitleThe}"] = m => TitleThe(series.Title);
             tokenHandlers["{Site TitleYear}"] = m => TitleYear(series.Title, series.Year);
+            tokenHandlers["{Site TitleWithoutYear}"] = m => TitleWithoutYear(series.Title);
             tokenHandlers["{Site TitleTheYear}"] = m => TitleYear(TitleThe(series.Title), series.Year);
+            tokenHandlers["{Site TitleTheWithoutYear}"] = m => TitleWithoutYear(TitleThe(series.Title));
             tokenHandlers["{Site TitleFirstCharacter}"] = m => TitleThe(series.Title).Substring(0, 1).FirstCharToUpper();
             tokenHandlers["{Site Year}"] = m => series.Year.ToString();
         }
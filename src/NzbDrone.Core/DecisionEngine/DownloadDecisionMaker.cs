dbb1d82b7746711f7a143fbb47c9de38179eed2f
Mark McDowall <mark@mcdowall.ca>
2023-03-22 17:24:37 -0700

    Fixed: Pushed releases should be stored as pushed release
    (cherry picked from commit c41aec5f77dce256877d508abeac0d5cc12b4f48)

--- src/NzbDrone.Core/DecisionEngine/DownloadDecisionMaker.cs
+++ src/NzbDrone.Core/DecisionEngine/DownloadDecisionMaker.cs
@@ -43,7 +43,7 @@ namespace NzbDrone.Core.DecisionEngine
 
         public List<DownloadDecision> GetRssDecision(List<ReleaseInfo> reports, bool pushedRelease = false)
         {
-            return GetDecisions(reports).ToList();
+            return GetDecisions(reports, pushedRelease).ToList();
         }
 
         public List<DownloadDecision> GetSearchDecision(List<ReleaseInfo> reports, SearchCriteriaBase searchCriteriaBase)
@@ -51,7 +51,7 @@ namespace NzbDrone.Core.DecisionEngine
             return GetDecisions(reports, false, searchCriteriaBase).ToList();
         }
 
-        private IEnumerable<DownloadDecision> GetDecisions(List<ReleaseInfo> reports, bool pushedRelease = false, SearchCriteriaBase searchCriteria = null)
+        private IEnumerable<DownloadDecision> GetDecisions(List<ReleaseInfo> reports, bool pushedRelease, SearchCriteriaBase searchCriteria = null)
         {
             if (reports.Any())
             {

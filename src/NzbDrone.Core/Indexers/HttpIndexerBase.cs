a7b8a92965ff3cb1069728a55efab4bad824bbe0
Stepan Goremykin <goremukin@gmail.com>
2023-03-26 20:40:51 -0700

    Use Array.Empty and fix a few multiple enumerations
    (cherry picked from commit 11d91faaada0e70910c832ce405ddeed52a24172)

--- src/NzbDrone.Core/Indexers/HttpIndexerBase.cs
+++ src/NzbDrone.Core/Indexers/HttpIndexerBase.cs
@@ -43,7 +43,7 @@ namespace NzbDrone.Core.Indexers
         {
             if (!SupportsRss)
             {
-                return new List<ReleaseInfo>();
+                return Array.Empty<ReleaseInfo>();
             }
 
             return FetchReleases(g => g.GetRecentRequests(), true);
@@ -53,7 +53,7 @@ namespace NzbDrone.Core.Indexers
         {
             if (!SupportsSearch)
             {
-                return new List<ReleaseInfo>();
+                return Array.Empty<ReleaseInfo>();
             }
 
             return FetchReleases(g => g.GetSearchRequests(searchCriteria));
@@ -63,7 +63,7 @@ namespace NzbDrone.Core.Indexers
         {
             if (!SupportsSearch)
             {
-                return new List<ReleaseInfo>();
+                return Array.Empty<ReleaseInfo>();
             }
 
             return FetchReleases(g => g.GetSearchRequests(searchCriteria));

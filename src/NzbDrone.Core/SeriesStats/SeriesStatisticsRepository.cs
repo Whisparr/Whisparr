82e64b03f42d17a1f7fd156bd55a642a15a7616d
Qstick <qstick@gmail.com>
2023-04-13 17:23:50 -0500

    Fixed: Restore old Sqlite version compatibility

--- src/NzbDrone.Core/SeriesStats/SeriesStatisticsRepository.cs
+++ src/NzbDrone.Core/SeriesStats/SeriesStatisticsRepository.cs
@@ -70,6 +70,7 @@ namespace NzbDrone.Core.SeriesStats
             parameters.Add("currentDate", currentDate, null);
 
             var trueIndicator = _database.DatabaseType == DatabaseType.PostgreSQL ? "true" : "1";
+            var falseIndicator = _database.DatabaseType == DatabaseType.PostgreSQL ? "false" : "0";
 
             return new SqlBuilder(_database.DatabaseType)
             .Select($@"""Episodes"".""SeriesId"" AS SeriesId,
@@ -78,8 +79,8 @@ namespace NzbDrone.Core.SeriesStats
                              SUM(CASE WHEN ""AirDateUtc"" <= @currentDate OR ""EpisodeFileId"" > 0 THEN 1 ELSE 0 END) AS AvailableEpisodeCount,
                              SUM(CASE WHEN (""Monitored"" = {trueIndicator} AND ""AirDateUtc"" <= @currentDate) OR ""EpisodeFileId"" > 0 THEN 1 ELSE 0 END) AS EpisodeCount,
                              SUM(CASE WHEN ""EpisodeFileId"" > 0 THEN 1 ELSE 0 END) AS EpisodeFileCount,
-                             MIN(CASE WHEN ""AirDateUtc"" < @currentDate OR ""EpisodeFileId"" > 0 OR ""Monitored"" = false THEN NULL ELSE ""AirDateUtc"" END) AS NextAiringString,
-                             MAX(CASE WHEN ""AirDateUtc"" >= @currentDate OR ""EpisodeFileId"" = 0 AND ""Monitored"" = false THEN NULL ELSE ""AirDateUtc"" END) AS PreviousAiringString", parameters)
+                             MIN(CASE WHEN ""AirDateUtc"" < @currentDate OR ""EpisodeFileId"" > 0 OR ""Monitored"" = {falseIndicator} THEN NULL ELSE ""AirDateUtc"" END) AS NextAiringString,
+                             MAX(CASE WHEN ""AirDateUtc"" >= @currentDate OR ""EpisodeFileId"" = 0 AND ""Monitored"" = {falseIndicator} THEN NULL ELSE ""AirDateUtc"" END) AS PreviousAiringString", parameters)
             .GroupBy<Episode>(x => x.SeriesId)
             .GroupBy<Episode>(x => x.SeasonNumber);
         }

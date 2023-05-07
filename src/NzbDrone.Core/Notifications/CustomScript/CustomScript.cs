Sonarr/Sonarr

New: Add release info to webhook/custom script import events
Closes #5503

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -138,6 +138,9 @@ namespace NzbDrone.Core.Notifications.CustomScript
             environmentVariables.Add("Sonarr_EpisodeFile_MediaInfo_VideoDynamicRangeType", MediaInfoFormatter.FormatVideoDynamicRangeType(episodeFile.MediaInfo));
             environmentVariables.Add("Sonarr_EpisodeFile_CustomFormat", string.Join("|", message.EpisodeInfo.CustomFormats));
             environmentVariables.Add("Sonarr_EpisodeFile_CustomFormatScore", message.EpisodeInfo.CustomFormatScore.ToString());
+            environmentVariables.Add("Sonarr_Release_Indexer", message.Release?.Indexer);
+            environmentVariables.Add("Sonarr_Release_Size", message.Release?.Size.ToString());
+            environmentVariables.Add("Sonarr_Release_Title", message.Release?.Title);
 
             if (message.OldFiles.Any())
             {

Whisparr/Whisparr

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -130,6 +130,9 @@ namespace NzbDrone.Core.Notifications.CustomScript
             environmentVariables.Add("Whisparr_EpisodeFile_MediaInfo_VideoDynamicRangeType", MediaInfoFormatter.FormatVideoDynamicRangeType(episodeFile.MediaInfo));
             environmentVariables.Add("Whisparr_EpisodeFile_CustomFormat", string.Join("|", message.EpisodeInfo.CustomFormats));
             environmentVariables.Add("Whisparr_EpisodeFile_CustomFormatScore", message.EpisodeInfo.CustomFormatScore.ToString());
+            environmentVariables.Add("Whisparr_Release_Indexer", message.Release?.Indexer);
+            environmentVariables.Add("Whisparr_Release_Size", message.Release?.Size.ToString());
+            environmentVariables.Add("Whisparr_Release_Title", message.Release?.Title);
 
             if (message.OldFiles.Any())
             {
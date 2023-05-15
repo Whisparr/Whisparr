Readarr/Readarr

Fixed: Handle missing category when getting Qbittorrent download path
[common]

Fixes READARR-2VR
Fixes READARR-1GP

src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrent.cs
@@ -366,9 +366,7 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
             if (Settings.MusicCategory.IsNotNullOrWhiteSpace() && version >= Version.Parse("2.0"))
             {
-                var label = Proxy.GetLabels(Settings)[Settings.MusicCategory];
-
-                if (label.SavePath.IsNotNullOrWhiteSpace())
+                if (Proxy.GetLabels(Settings).TryGetValue(Settings.MusicCategory, out var label) && label.SavePath.IsNotNullOrWhiteSpace())
                 {
                     var labelDir = new OsPath(label.SavePath);
 

Whisparr/Whisparr

(cherry picked from commit 6f97ca9a55471386454457ca52b93733e18e85e4)
(cherry picked from commit e7a8f6332c43f179d4c95b3a8a0253a235bb6eec)
(cherry picked from commit 8c68dfb8ceb5d151b71fbd1af6ee0c2e404aa083)

src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrent.cs
@@ -367,9 +367,7 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
 
             if (Settings.TvCategory.IsNotNullOrWhiteSpace() && version >= Version.Parse("2.0"))
             {
-                var label = Proxy.GetLabels(Settings)[Settings.TvCategory];
-
-                if (label.SavePath.IsNotNullOrWhiteSpace())
+                if (Proxy.GetLabels(Settings).TryGetValue(Settings.TvCategory, out var label) && label.SavePath.IsNotNullOrWhiteSpace())
                 {
                     var labelDir = new OsPath(label.SavePath);
 
@@ -474,9 +472,9 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
                 _logger.Error(ex, "Unable to test qBittorrent");
 
                 return new NzbDroneValidationFailure("Host", "Unable to connect to qBittorrent")
-                       {
-                           DetailedDescription = ex.Message
-                       };
+                {
+                    DetailedDescription = ex.Message
+                };
             }
 
             return null;
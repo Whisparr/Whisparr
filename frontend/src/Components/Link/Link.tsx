nicksen/Sonarr

Convert Manual Import to Typescript

frontend/src/Components/Link/Link.tsx
@@ -1,0 +1,89 @@
+import classNames from 'classnames';
+import React, { ComponentClass, FunctionComponent, useCallback } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import styles from './Link.css';
+
+interface ReactRouterLinkProps {
+  to?: string;
+}
+
+export interface LinkProps extends React.HTMLProps<HTMLAnchorElement> {
+  className?: string;
+  component?:
+    | string
+    | FunctionComponent<LinkProps>
+    | ComponentClass<LinkProps, unknown>;
+  to?: string;
+  target?: string;
+  isDisabled?: boolean;
+  noRouter?: boolean;
+  onPress?(event: Event): void;
+}
+function Link(props: LinkProps) {
+  const {
+    className,
+    component = 'button',
+    to,
+    target,
+    type,
+    isDisabled,
+    noRouter = false,
+    onPress,
+    ...otherProps
+  } = props;
+
+  const onClick = useCallback(
+    (event) => {
+      if (!isDisabled && onPress) {
+        onPress(event);
+      }
+    },
+    [isDisabled, onPress]
+  );
+
+  const linkProps: React.HTMLProps<HTMLAnchorElement> & ReactRouterLinkProps = {
+    target,
+  };
+  let el = component;
+
+  if (to) {
+    if (/\w+?:\/\//.test(to)) {
+      el = 'a';
+      linkProps.href = to;
+      linkProps.target = target || '_blank';
+      linkProps.rel = 'noreferrer';
+    } else if (noRouter) {
+      el = 'a';
+      linkProps.href = to;
+      linkProps.target = target || '_self';
+    } else {
+      el = RouterLink;
+      linkProps.to = `${window.Sonarr.urlBase}/${to.replace(/^\//, '')}`;
+      linkProps.target = target;
+    }
+  }
+
+  if (el === 'button' || el === 'input') {
+    linkProps.type = type || 'button';
+    linkProps.disabled = isDisabled;
+  }
+
+  linkProps.className = classNames(
+    className,
+    styles.link,
+    to && styles.to,
+    isDisabled && 'isDisabled'
+  );
+
+  const elementProps = {
+    ...otherProps,
+    type,
+    ...linkProps,
+  };
+
+  elementProps.onClick = onClick;
+
+  return React.createElement(el, elementProps);
+}
+
+export default Link;

frontend/src/InteractiveImport/Series/SelectSeriesModalContent.tsx
@@ -1,0 +1,92 @@
+import React, { useCallback, useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
+import TextInput from 'Components/Form/TextInput';
+import Button from 'Components/Link/Button';
+import ModalBody from 'Components/Modal/ModalBody';
+import ModalContent from 'Components/Modal/ModalContent';
+import ModalFooter from 'Components/Modal/ModalFooter';
+import ModalHeader from 'Components/Modal/ModalHeader';
+import Scroller from 'Components/Scroller/Scroller';
+import { scrollDirections } from 'Helpers/Props';
+import Series from 'Series/Series';
+import createAllSeriesSelector from 'Store/Selectors/createAllSeriesSelector';
+import SelectSeriesRow from './SelectSeriesRow';
+import styles from './SelectSeriesModalContent.css';
+
+interface SelectSeriesModalContentProps {
+  modalTitle: string;
+  onSeriesSelect(series: Series): void;
+  onModalClose(): void;
+}
+
+function SelectSeriesModalContent(props: SelectSeriesModalContentProps) {
+  const { modalTitle, onSeriesSelect, onModalClose } = props;
+
+  const allSeries = useSelector(createAllSeriesSelector());
+  const [filter, setFilter] = useState('');
+
+  const onFilterChange = useCallback(
+    ({ value }) => {
+      setFilter(value);
+    },
+    [setFilter]
+  );
+
+  const onSeriesSelectWrapper = useCallback(
+    (seriesId: number) => {
+      const series = allSeries.find((s) => s.id === seriesId);
+
+      onSeriesSelect(series);
+    },
+    [allSeries, onSeriesSelect]
+  );
+
+  const items = useMemo(() => {
+    const sorted = [...allSeries].sort((a, b) =>
+      a.sortTitle.localeCompare(b.sortTitle)
+    );
+
+    return sorted.filter((item) =>
+      item.title.toLowerCase().includes(filter.toLowerCase())
+    );
+  }, [allSeries, filter]);
+
+  return (
+    <ModalContent onModalClose={onModalClose}>
+      <ModalHeader>{modalTitle} - Select Series</ModalHeader>
+
+      <ModalBody
+        className={styles.modalBody}
+        scrollDirection={scrollDirections.NONE}
+      >
+        <TextInput
+          className={styles.filterInput}
+          placeholder="Filter series"
+          name="filter"
+          value={filter}
+          autoFocus={true}
+          onChange={onFilterChange}
+        />
+
+        <Scroller className={styles.scroller} autoFocus={false}>
+          {items.map((item) => {
+            return (
+              <SelectSeriesRow
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                onSeriesSelect={onSeriesSelectWrapper}
+              />
+            );
+          })}
+        </Scroller>
+      </ModalBody>
+
+      <ModalFooter>
+        <Button onPress={onModalClose}>Cancel</Button>
+      </ModalFooter>
+    </ModalContent>
+  );
+}
+
+export default SelectSeriesModalContent;

Whisparr/Whisparr

(cherry picked from commit defdc84b7ee1dae0ecbfec7590534684afe893b0)

frontend/src/Components/Link/Link.tsx
@@ -1,0 +1,89 @@
+import classNames from 'classnames';
+import React, { ComponentClass, FunctionComponent, useCallback } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import styles from './Link.css';
+
+interface ReactRouterLinkProps {
+  to?: string;
+}
+
+export interface LinkProps extends React.HTMLProps<HTMLAnchorElement> {
+  className?: string;
+  component?:
+    | string
+    | FunctionComponent<LinkProps>
+    | ComponentClass<LinkProps, unknown>;
+  to?: string;
+  target?: string;
+  isDisabled?: boolean;
+  noRouter?: boolean;
+  onPress?(event: Event): void;
+}
+function Link(props: LinkProps) {
+  const {
+    className,
+    component = 'button',
+    to,
+    target,
+    type,
+    isDisabled,
+    noRouter = false,
+    onPress,
+    ...otherProps
+  } = props;
+
+  const onClick = useCallback(
+    (event) => {
+      if (!isDisabled && onPress) {
+        onPress(event);
+      }
+    },
+    [isDisabled, onPress]
+  );
+
+  const linkProps: React.HTMLProps<HTMLAnchorElement> & ReactRouterLinkProps = {
+    target,
+  };
+  let el = component;
+
+  if (to) {
+    if (/\w+?:\/\//.test(to)) {
+      el = 'a';
+      linkProps.href = to;
+      linkProps.target = target || '_blank';
+      linkProps.rel = 'noreferrer';
+    } else if (noRouter) {
+      el = 'a';
+      linkProps.href = to;
+      linkProps.target = target || '_self';
+    } else {
+      el = RouterLink;
+      linkProps.to = `${window.Whisparr.urlBase}/${to.replace(/^\//, '')}`;
+      linkProps.target = target;
+    }
+  }
+
+  if (el === 'button' || el === 'input') {
+    linkProps.type = type || 'button';
+    linkProps.disabled = isDisabled;
+  }
+
+  linkProps.className = classNames(
+    className,
+    styles.link,
+    to && styles.to,
+    isDisabled && 'isDisabled'
+  );
+
+  const elementProps = {
+    ...otherProps,
+    type,
+    ...linkProps,
+  };
+
+  elementProps.onClick = onClick;
+
+  return React.createElement(el, elementProps);
+}
+
+export default Link;

frontend/src/InteractiveImport/Series/SelectSeriesModalContent.tsx
@@ -1,0 +1,92 @@
+import React, { useCallback, useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
+import TextInput from 'Components/Form/TextInput';
+import Button from 'Components/Link/Button';
+import ModalBody from 'Components/Modal/ModalBody';
+import ModalContent from 'Components/Modal/ModalContent';
+import ModalFooter from 'Components/Modal/ModalFooter';
+import ModalHeader from 'Components/Modal/ModalHeader';
+import Scroller from 'Components/Scroller/Scroller';
+import { scrollDirections } from 'Helpers/Props';
+import Series from 'Series/Series';
+import createAllSeriesSelector from 'Store/Selectors/createAllSeriesSelector';
+import SelectSeriesRow from './SelectSeriesRow';
+import styles from './SelectSeriesModalContent.css';
+
+interface SelectSeriesModalContentProps {
+  modalTitle: string;
+  onSeriesSelect(series: Series): void;
+  onModalClose(): void;
+}
+
+function SelectSeriesModalContent(props: SelectSeriesModalContentProps) {
+  const { modalTitle, onSeriesSelect, onModalClose } = props;
+
+  const allSeries = useSelector(createAllSeriesSelector());
+  const [filter, setFilter] = useState('');
+
+  const onFilterChange = useCallback(
+    ({ value }) => {
+      setFilter(value);
+    },
+    [setFilter]
+  );
+
+  const onSeriesSelectWrapper = useCallback(
+    (seriesId: number) => {
+      const series = allSeries.find((s) => s.id === seriesId);
+
+      onSeriesSelect(series);
+    },
+    [allSeries, onSeriesSelect]
+  );
+
+  const items = useMemo(() => {
+    const sorted = [...allSeries].sort((a, b) =>
+      a.sortTitle.localeCompare(b.sortTitle)
+    );
+
+    return sorted.filter((item) =>
+      item.title.toLowerCase().includes(filter.toLowerCase())
+    );
+  }, [allSeries, filter]);
+
+  return (
+    <ModalContent onModalClose={onModalClose}>
+      <ModalHeader>{modalTitle} - Select Site</ModalHeader>
+
+      <ModalBody
+        className={styles.modalBody}
+        scrollDirection={scrollDirections.NONE}
+      >
+        <TextInput
+          className={styles.filterInput}
+          placeholder="Filter series"
+          name="filter"
+          value={filter}
+          autoFocus={true}
+          onChange={onFilterChange}
+        />
+
+        <Scroller className={styles.scroller} autoFocus={false}>
+          {items.map((item) => {
+            return (
+              <SelectSeriesRow
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                onSeriesSelect={onSeriesSelectWrapper}
+              />
+            );
+          })}
+        </Scroller>
+      </ModalBody>
+
+      <ModalFooter>
+        <Button onPress={onModalClose}>Cancel</Button>
+      </ModalFooter>
+    </ModalContent>
+  );
+}
+
+export default SelectSeriesModalContent;
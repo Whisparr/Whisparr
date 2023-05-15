Sonarr/Sonarr

New: More information on on why hardlinks should be used over copying
Closes #5542

frontend/src/Settings/MediaManagement/MediaManagement.js
@@ -193,7 +193,7 @@ class MediaManagement extends Component {
                         <FormInputGroup
                           type={inputTypes.CHECK}
                           name="copyUsingHardlinks"
-                          helpText="Use Hardlinks when trying to copy files from torrents that are still being seeded"
+                          helpText="Hardlinks allow Sonarr to import seeding torrents to the the series folder without taking extra disk space or copying the entire contents of the file. Hardlinks will only work if the source and destination are on the same volume"
                           helpTextWarning="Occasionally, file locks may prevent renaming files that are being seeded. You may temporarily disable seeding and use Sonarr's rename function as a work around."
                           onChange={onInputChange}
                           {...settings.copyUsingHardlinks}

Whisparr/Whisparr

(cherry picked from commit 83a9d15ff8721c8effdc2c8055e37bfb757022d4)

frontend/src/Settings/MediaManagement/MediaManagement.js
@@ -193,7 +193,7 @@ class MediaManagement extends Component {
                         <FormInputGroup
                           type={inputTypes.CHECK}
                           name="copyUsingHardlinks"
-                          helpText="Use Hardlinks when trying to copy files from torrents that are still being seeded"
+                          helpText="Hardlinks allow Whisparr to import seeding torrents to the the site folder without taking extra disk space or copying the entire contents of the file. Hardlinks will only work if the source and destination are on the same volume"
                           helpTextWarning="Occasionally, file locks may prevent renaming files that are being seeded. You may temporarily disable seeding and use Whisparr's rename function as a work around."
                           onChange={onInputChange}
                           {...settings.copyUsingHardlinks}
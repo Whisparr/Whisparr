Sonarr/Sonarr

New: Notifications when Manual Interaction is required for importing
Closes #5193

src/NzbDrone.Core/Notifications/NotificationBase.cs
@@ -18,6 +18,7 @@ namespace NzbDrone.Core.Notifications
         protected const string HEALTH_ISSUE_TITLE = "Health Check Failure";
         protected const string HEALTH_RESTORED_TITLE = "Health Check Restored";
         protected const string APPLICATION_UPDATE_TITLE = "Application Updated";
+        protected const string MANUAL_INTERACTION_REQUIRED_TITLE = "Manual Interaction";
 
         protected const string EPISODE_GRABBED_TITLE_BRANDED = "Sonarr - " + EPISODE_GRABBED_TITLE;
         protected const string EPISODE_DOWNLOADED_TITLE_BRANDED = "Sonarr - " + EPISODE_DOWNLOADED_TITLE;
@@ -27,6 +28,7 @@ namespace NzbDrone.Core.Notifications
         protected const string HEALTH_ISSUE_TITLE_BRANDED = "Sonarr - " + HEALTH_ISSUE_TITLE;
         protected const string HEALTH_RESTORED_TITLE_BRANDED = "Sonarr - " + HEALTH_RESTORED_TITLE;
         protected const string APPLICATION_UPDATE_TITLE_BRANDED = "Sonarr - " + APPLICATION_UPDATE_TITLE;
+        protected const string MANUAL_INTERACTION_REQUIRED_TITLE_BRANDED = "Sonarr - " + MANUAL_INTERACTION_REQUIRED_TITLE;
 
         public abstract string Name { get; }
 
@@ -77,6 +79,10 @@ namespace NzbDrone.Core.Notifications
         {
         }
 
+        public virtual void OnManualInteractionRequired(ManualInteractionRequiredMessage message)
+        {
+        }
+
         public virtual void ProcessQueue()
         {
         }
@@ -92,6 +98,7 @@ namespace NzbDrone.Core.Notifications
         public bool SupportsOnHealthIssue => HasConcreteImplementation("OnHealthIssue");
         public bool SupportsOnHealthRestored => HasConcreteImplementation("OnHealthRestored");
         public bool SupportsOnApplicationUpdate => HasConcreteImplementation("OnApplicationUpdate");
+        public bool SupportsOnManualInteractionRequired => HasConcreteImplementation("OnManualInteractionRequired");
 
         protected TSettings Settings => (TSettings)Definition.Settings;
 

Whisparr/Whisparr

(cherry picked from commit 5ec282750bdea16f770282b80fdaec56a65749e2)

src/NzbDrone.Core/Notifications/NotificationBase.cs
@@ -18,6 +18,7 @@ namespace NzbDrone.Core.Notifications
         protected const string HEALTH_ISSUE_TITLE = "Health Check Failure";
         protected const string HEALTH_RESTORED_TITLE = "Health Check Restored";
         protected const string APPLICATION_UPDATE_TITLE = "Application Updated";
+        protected const string MANUAL_INTERACTION_REQUIRED_TITLE = "Manual Interaction";
 
         protected const string EPISODE_GRABBED_TITLE_BRANDED = "Whisparr - " + EPISODE_GRABBED_TITLE;
         protected const string EPISODE_DOWNLOADED_TITLE_BRANDED = "Whisparr - " + EPISODE_DOWNLOADED_TITLE;
@@ -27,6 +28,7 @@ namespace NzbDrone.Core.Notifications
         protected const string HEALTH_ISSUE_TITLE_BRANDED = "Whisparr - " + HEALTH_ISSUE_TITLE;
         protected const string HEALTH_RESTORED_TITLE_BRANDED = "Whisparr - " + HEALTH_RESTORED_TITLE;
         protected const string APPLICATION_UPDATE_TITLE_BRANDED = "Whisparr - " + APPLICATION_UPDATE_TITLE;
+        protected const string MANUAL_INTERACTION_REQUIRED_TITLE_BRANDED = "Whisparr - " + MANUAL_INTERACTION_REQUIRED_TITLE;
 
         public abstract string Name { get; }
 
@@ -77,6 +79,10 @@ namespace NzbDrone.Core.Notifications
         {
         }
 
+        public virtual void OnManualInteractionRequired(ManualInteractionRequiredMessage message)
+        {
+        }
+
         public virtual void ProcessQueue()
         {
         }
@@ -92,6 +98,7 @@ namespace NzbDrone.Core.Notifications
         public bool SupportsOnHealthIssue => HasConcreteImplementation("OnHealthIssue");
         public bool SupportsOnHealthRestored => HasConcreteImplementation("OnHealthRestored");
         public bool SupportsOnApplicationUpdate => HasConcreteImplementation("OnApplicationUpdate");
+        public bool SupportsOnManualInteractionRequired => HasConcreteImplementation("OnManualInteractionRequired");
 
         protected TSettings Settings => (TSettings)Definition.Settings;
 
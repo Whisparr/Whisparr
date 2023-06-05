Sonarr/Sonarr

New: On Health Restored notification
Closes #4262

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -274,6 +274,21 @@ namespace NzbDrone.Core.Notifications.CustomScript
             ExecuteScript(environmentVariables);
         }
 
+        public override void OnHealthRestored(HealthCheck.HealthCheck previousCheck)
+        {
+            var environmentVariables = new StringDictionary();
+
+            environmentVariables.Add("Sonarr_EventType", "HealthRestored");
+            environmentVariables.Add("Sonarr_InstanceName", _configFileProvider.InstanceName);
+            environmentVariables.Add("Sonarr_ApplicationUrl", _configService.ApplicationUrl);
+            environmentVariables.Add("Sonarr_Health_Restored_Level", Enum.GetName(typeof(HealthCheckResult), previousCheck.Type));
+            environmentVariables.Add("Sonarr_Health_Restored_Message", previousCheck.Message);
+            environmentVariables.Add("Sonarr_Health_Restored_Type", previousCheck.Source.Name);
+            environmentVariables.Add("Sonarr_Health_Restored_Wiki", previousCheck.WikiUrl.ToString() ?? string.Empty);
+
+            ExecuteScript(environmentVariables);
+        }
+
         public override void OnApplicationUpdate(ApplicationUpdateMessage updateMessage)
         {
             var environmentVariables = new StringDictionary();

src/NzbDrone.Core/Notifications/NotificationBase.cs
@@ -16,6 +16,7 @@ namespace NzbDrone.Core.Notifications
         protected const string SERIES_ADDED_TITLE = "Series Added";
         protected const string SERIES_DELETED_TITLE = "Series Deleted";
         protected const string HEALTH_ISSUE_TITLE = "Health Check Failure";
+        protected const string HEALTH_RESTORED_TITLE = "Health Check Restored";
         protected const string APPLICATION_UPDATE_TITLE = "Application Updated";
 
         protected const string EPISODE_GRABBED_TITLE_BRANDED = "Sonarr - " + EPISODE_GRABBED_TITLE;
@@ -24,6 +25,7 @@ namespace NzbDrone.Core.Notifications
         protected const string SERIES_ADDED_TITLE_BRANDED = "Sonarr - " + SERIES_DELETED_TITLE;
         protected const string SERIES_DELETED_TITLE_BRANDED = "Sonarr - " + SERIES_DELETED_TITLE;
         protected const string HEALTH_ISSUE_TITLE_BRANDED = "Sonarr - " + HEALTH_ISSUE_TITLE;
+        protected const string HEALTH_RESTORED_TITLE_BRANDED = "Sonarr - " + HEALTH_RESTORED_TITLE;
         protected const string APPLICATION_UPDATE_TITLE_BRANDED = "Sonarr - " + APPLICATION_UPDATE_TITLE;
 
         public abstract string Name { get; }
@@ -67,6 +69,10 @@ namespace NzbDrone.Core.Notifications
         {
         }
 
+        public virtual void OnHealthRestored(HealthCheck.HealthCheck previousCheck)
+        {
+        }
+
         public virtual void OnApplicationUpdate(ApplicationUpdateMessage updateMessage)
         {
         }
@@ -84,6 +90,7 @@ namespace NzbDrone.Core.Notifications
         public bool SupportsOnEpisodeFileDelete => HasConcreteImplementation("OnEpisodeFileDelete");
         public bool SupportsOnEpisodeFileDeleteForUpgrade => SupportsOnEpisodeFileDelete;
         public bool SupportsOnHealthIssue => HasConcreteImplementation("OnHealthIssue");
+        public bool SupportsOnHealthRestored => HasConcreteImplementation("OnHealthRestored");
         public bool SupportsOnApplicationUpdate => HasConcreteImplementation("OnApplicationUpdate");
 
         protected TSettings Settings => (TSettings)Definition.Settings;

Whisparr/Whisparr

(cherry picked from commit 5fdc8514da7c7ad98192f2ecb2415b3a7b5d0d05)

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -251,6 +251,21 @@ namespace NzbDrone.Core.Notifications.CustomScript
             ExecuteScript(environmentVariables);
         }
 
+        public override void OnHealthRestored(HealthCheck.HealthCheck previousCheck)
+        {
+            var environmentVariables = new StringDictionary();
+
+            environmentVariables.Add("Whisparr_EventType", "HealthRestored");
+            environmentVariables.Add("Whisparr_InstanceName", _configFileProvider.InstanceName);
+            environmentVariables.Add("Whisparr_ApplicationUrl", _configService.ApplicationUrl);
+            environmentVariables.Add("Whisparr_Health_Restored_Level", Enum.GetName(typeof(HealthCheckResult), previousCheck.Type));
+            environmentVariables.Add("Whisparr_Health_Restored_Message", previousCheck.Message);
+            environmentVariables.Add("Whisparr_Health_Restored_Type", previousCheck.Source.Name);
+            environmentVariables.Add("Whisparr_Health_Restored_Wiki", previousCheck.WikiUrl.ToString() ?? string.Empty);
+
+            ExecuteScript(environmentVariables);
+        }
+
         public override void OnApplicationUpdate(ApplicationUpdateMessage updateMessage)
         {
             var environmentVariables = new StringDictionary();

src/NzbDrone.Core/Notifications/NotificationBase.cs
@@ -16,6 +16,7 @@ namespace NzbDrone.Core.Notifications
         protected const string SERIES_ADDED_TITLE = "Series Added";
         protected const string SERIES_DELETED_TITLE = "Series Deleted";
         protected const string HEALTH_ISSUE_TITLE = "Health Check Failure";
+        protected const string HEALTH_RESTORED_TITLE = "Health Check Restored";
         protected const string APPLICATION_UPDATE_TITLE = "Application Updated";
 
         protected const string EPISODE_GRABBED_TITLE_BRANDED = "Whisparr - " + EPISODE_GRABBED_TITLE;
@@ -24,6 +25,7 @@ namespace NzbDrone.Core.Notifications
         protected const string SERIES_ADDED_TITLE_BRANDED = "Whisparr - " + SERIES_DELETED_TITLE;
         protected const string SERIES_DELETED_TITLE_BRANDED = "Whisparr - " + SERIES_DELETED_TITLE;
         protected const string HEALTH_ISSUE_TITLE_BRANDED = "Whisparr - " + HEALTH_ISSUE_TITLE;
+        protected const string HEALTH_RESTORED_TITLE_BRANDED = "Whisparr - " + HEALTH_RESTORED_TITLE;
         protected const string APPLICATION_UPDATE_TITLE_BRANDED = "Whisparr - " + APPLICATION_UPDATE_TITLE;
 
         public abstract string Name { get; }
@@ -67,6 +69,10 @@ namespace NzbDrone.Core.Notifications
         {
         }
 
+        public virtual void OnHealthRestored(HealthCheck.HealthCheck previousCheck)
+        {
+        }
+
         public virtual void OnApplicationUpdate(ApplicationUpdateMessage updateMessage)
         {
         }
@@ -84,6 +90,7 @@ namespace NzbDrone.Core.Notifications
         public bool SupportsOnEpisodeFileDelete => HasConcreteImplementation("OnEpisodeFileDelete");
         public bool SupportsOnEpisodeFileDeleteForUpgrade => SupportsOnEpisodeFileDelete;
         public bool SupportsOnHealthIssue => HasConcreteImplementation("OnHealthIssue");
+        public bool SupportsOnHealthRestored => HasConcreteImplementation("OnHealthRestored");
         public bool SupportsOnApplicationUpdate => HasConcreteImplementation("OnApplicationUpdate");
 
         protected TSettings Settings => (TSettings)Definition.Settings;
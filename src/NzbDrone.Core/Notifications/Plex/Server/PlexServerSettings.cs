Sonarr/Sonarr

New: Add path mapping for partial library updates in Plex Server Notifications
(cherry picked from commit 24206ad0a3095c4bf5c860af516d8543bd6525d6)

src/NzbDrone.Core/Notifications/Plex/Server/PlexServerSettings.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Annotations;
 using NzbDrone.Core.ThingiProvider;
 using NzbDrone.Core.Validation;
@@ -11,6 +12,8 @@ namespace NzbDrone.Core.Notifications.Plex.Server
         {
             RuleFor(c => c.Host).ValidHost();
             RuleFor(c => c.Port).InclusiveBetween(1, 65535);
+            RuleFor(c => c.MapFrom).NotEmpty().Unless(c => c.MapTo.IsNullOrWhiteSpace());
+            RuleFor(c => c.MapTo).NotEmpty().Unless(c => c.MapFrom.IsNullOrWhiteSpace());
         }
     }
 
@@ -42,6 +45,12 @@ namespace NzbDrone.Core.Notifications.Plex.Server
 
         [FieldDefinition(5, Label = "Update Library", Type = FieldType.Checkbox)]
         public bool UpdateLibrary { get; set; }
+
+        [FieldDefinition(6, Label = "Map Paths From", Type = FieldType.Textbox, Advanced = true, HelpText = "Sonarr path, used to modify series paths when Plex sees library path location differently from Sonarr")]
+        public string MapFrom { get; set; }
+
+        [FieldDefinition(7, Label = "Map Paths To", Type = FieldType.Textbox, Advanced = true, HelpText = "Plex path, used to modify series paths when Plex sees library path location differently from Sonarr")]
+        public string MapTo { get; set; }
 
         public bool IsValid => !string.IsNullOrWhiteSpace(Host);
 

Whisparr/Whisparr

(cherry picked from commit de08d372677052f5dcf9ee689cd48282746bfbec)

src/NzbDrone.Core/Notifications/Plex/Server/PlexServerSettings.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Annotations;
 using NzbDrone.Core.ThingiProvider;
 using NzbDrone.Core.Validation;
@@ -11,6 +12,8 @@ namespace NzbDrone.Core.Notifications.Plex.Server
         {
             RuleFor(c => c.Host).ValidHost();
             RuleFor(c => c.Port).InclusiveBetween(1, 65535);
+            RuleFor(c => c.MapFrom).NotEmpty().Unless(c => c.MapTo.IsNullOrWhiteSpace());
+            RuleFor(c => c.MapTo).NotEmpty().Unless(c => c.MapFrom.IsNullOrWhiteSpace());
         }
     }
 
@@ -42,6 +45,12 @@ namespace NzbDrone.Core.Notifications.Plex.Server
 
         [FieldDefinition(5, Label = "Update Library", Type = FieldType.Checkbox)]
         public bool UpdateLibrary { get; set; }
+
+        [FieldDefinition(6, Label = "Map Paths From", Type = FieldType.Textbox, Advanced = true, HelpText = "Whisparr path, used to modify series paths when Plex sees library path location differently from Whisparr")]
+        public string MapFrom { get; set; }
+
+        [FieldDefinition(7, Label = "Map Paths To", Type = FieldType.Textbox, Advanced = true, HelpText = "Plex path, used to modify series paths when Plex sees library path location differently from Whisparr")]
+        public string MapTo { get; set; }
 
         public bool IsValid => !string.IsNullOrWhiteSpace(Host);
 
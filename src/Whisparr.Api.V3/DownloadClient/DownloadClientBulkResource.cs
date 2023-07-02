Sonarr/Sonarr

New: Bulk Manage Import Lists, Indexers, Clients

src/Sonarr.Api.V3/DownloadClient/DownloadClientBulkResource.cs
@@ -1,0 +1,34 @@
+using System.Collections.Generic;
+using NzbDrone.Core.Download;
+
+namespace Sonarr.Api.V3.DownloadClient
+{
+    public class DownloadClientBulkResource : ProviderBulkResource<DownloadClientBulkResource>
+    {
+        public bool? Enable { get; set; }
+        public int? Priority { get; set; }
+        public bool? RemoveCompletedDownloads { get; set; }
+        public bool? RemoveFailedDownloads { get; set; }
+    }
+
+    public class DownloadClientBulkResourceMapper : ProviderBulkResourceMapper<DownloadClientBulkResource, DownloadClientDefinition>
+    {
+        public override List<DownloadClientDefinition> UpdateModel(DownloadClientBulkResource resource, List<DownloadClientDefinition> existingDefinitions)
+        {
+            if (resource == null)
+            {
+                return new List<DownloadClientDefinition>();
+            }
+
+            existingDefinitions.ForEach(existing =>
+            {
+                existing.Enable = resource.Enable ?? existing.Enable;
+                existing.Priority = resource.Priority ?? existing.Priority;
+                existing.RemoveCompletedDownloads = resource.RemoveCompletedDownloads ?? existing.RemoveCompletedDownloads;
+                existing.RemoveFailedDownloads = resource.RemoveFailedDownloads ?? existing.RemoveFailedDownloads;
+            });
+
+            return existingDefinitions;
+        }
+    }
+}

src/Sonarr.Api.V3/Indexers/IndexerBulkResource.cs
@@ -1,0 +1,34 @@
+using System.Collections.Generic;
+using NzbDrone.Core.Indexers;
+
+namespace Sonarr.Api.V3.Indexers
+{
+    public class IndexerBulkResource : ProviderBulkResource<IndexerBulkResource>
+    {
+        public bool? EnableRss { get; set; }
+        public bool? EnableAutomaticSearch { get; set; }
+        public bool? EnableInteractiveSearch { get; set; }
+        public int? Priority { get; set; }
+    }
+
+    public class IndexerBulkResourceMapper : ProviderBulkResourceMapper<IndexerBulkResource, IndexerDefinition>
+    {
+        public override List<IndexerDefinition> UpdateModel(IndexerBulkResource resource, List<IndexerDefinition> existingDefinitions)
+        {
+            if (resource == null)
+            {
+                return new List<IndexerDefinition>();
+            }
+
+            existingDefinitions.ForEach(existing =>
+            {
+                existing.EnableRss = resource.EnableRss ?? existing.EnableRss;
+                existing.EnableAutomaticSearch = resource.EnableAutomaticSearch ?? existing.EnableAutomaticSearch;
+                existing.EnableInteractiveSearch = resource.EnableInteractiveSearch ?? existing.EnableInteractiveSearch;
+                existing.Priority = resource.Priority ?? existing.Priority;
+            });
+
+            return existingDefinitions;
+        }
+    }
+}

src/Sonarr.Api.V3/Notifications/NotificationBulkResource.cs
@@ -1,0 +1,12 @@
+using NzbDrone.Core.Notifications;
+
+namespace Sonarr.Api.V3.Notifications
+{
+    public class NotificationBulkResource : ProviderBulkResource<NotificationBulkResource>
+    {
+    }
+
+    public class NotificationBulkResourceMapper : ProviderBulkResourceMapper<NotificationBulkResource, NotificationDefinition>
+    {
+    }
+}

src/Sonarr.Api.V3/ProviderControllerBase.cs
@@ -6,23 +6,31 @@ using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Common.Serializer;
 using NzbDrone.Core.ThingiProvider;
 using NzbDrone.Core.Validation;
+using Sonarr.Api.V3.Series;
 using Sonarr.Http.REST;
 using Sonarr.Http.REST.Attributes;
 
 namespace Sonarr.Api.V3
 {
-    public abstract class ProviderControllerBase<TProviderResource, TProvider, TProviderDefinition> : RestController<TProviderResource>
+    public abstract class ProviderControllerBase<TProviderResource, TBulkProviderResource, TProvider, TProviderDefinition> : RestController<TProviderResource>
         where TProviderDefinition : ProviderDefinition, new()
         where TProvider : IProvider
         where TProviderResource : ProviderResource<TProviderResource>, new()
+        where TBulkProviderResource : ProviderBulkResource<TBulkProviderResource>, new()
     {
         private readonly IProviderFactory<TProvider, TProviderDefinition> _providerFactory;
         private readonly ProviderResourceMapper<TProviderResource, TProviderDefinition> _resourceMapper;
+        private readonly ProviderBulkResourceMapper<TBulkProviderResource, TProviderDefinition> _bulkResourceMapper;
 
-        protected ProviderControllerBase(IProviderFactory<TProvider, TProviderDefinition> providerFactory, string resource, ProviderResourceMapper<TProviderResource, TProviderDefinition> resourceMapper)
+        protected ProviderControllerBase(IProviderFactory<TProvider,
+            TProviderDefinition> providerFactory,
+            string resource,
+            ProviderResourceMapper<TProviderResource, TProviderDefinition> resourceMapper,
+            ProviderBulkResourceMapper<TBulkProviderResource, TProviderDefinition> bulkResourceMapper)
         {
             _providerFactory = providerFactory;
             _resourceMapper = resourceMapper;
+            _bulkResourceMapper = bulkResourceMapper;
 
             SharedValidator.RuleFor(c => c.Name).NotEmpty();
             SharedValidator.RuleFor(c => c.Name).Must((v, c) => !_providerFactory.All().Any(p => p.Name == c && p.Id != v.Id)).WithMessage("Should be unique");
@@ -91,6 +99,39 @@ namespace Sonarr.Api.V3
             return Accepted(providerResource.Id);
         }
 
+        [HttpPut("bulk")]
+        [Consumes("application/json")]
+        public ActionResult<TProviderResource> UpdateProvider([FromBody] TBulkProviderResource providerResource)
+        {
+            var definitionsToUpdate = _providerFactory.Get(providerResource.Ids).ToList();
+
+            foreach (var definition in definitionsToUpdate)
+            {
+                if (providerResource.Tags != null)
+                {
+                    var newTags = providerResource.Tags;
+                    var applyTags = providerResource.ApplyTags;
+
+                    switch (applyTags)
+                    {
+                        case ApplyTags.Add:
+                            newTags.ForEach(t => definition.Tags.Add(t));
+                            break;
+                        case ApplyTags.Remove:
+                            newTags.ForEach(t => definition.Tags.Remove(t));
+                            break;
+                        case ApplyTags.Replace:
+                            definition.Tags = new HashSet<int>(newTags);
+                            break;
+                    }
+                }
+            }
+
+            _bulkResourceMapper.UpdateModel(providerResource, definitionsToUpdate);
+
+            return Accepted(_providerFactory.Update(definitionsToUpdate).Select(x => _resourceMapper.ToResource(x)));
+        }
+
         private TProviderDefinition GetDefinition(TProviderResource providerResource, bool validate, bool includeWarnings, bool forceValidate)
         {
             var existingDefinition = providerResource.Id > 0 ? _providerFactory.Find(providerResource.Id) : null;
@@ -108,6 +149,15 @@ namespace Sonarr.Api.V3
         public object DeleteProvider(int id)
         {
             _providerFactory.Delete(id);
+
+            return new { };
+        }
+
+        [HttpDelete("bulk")]
+        [Consumes("application/json")]
+        public object DeleteProviders([FromBody] TBulkProviderResource resource)
+        {
+            _providerFactory.Delete(resource.Ids);
 
             return new { };
         }

Whisparr/Whisparr

(cherry picked from commit 73ccab53d5194282de4b983354c9afa5a6d678fb)

src/Whisparr.Api.V3/DownloadClient/DownloadClientBulkResource.cs
@@ -1,0 +1,34 @@
+using System.Collections.Generic;
+using NzbDrone.Core.Download;
+
+namespace Whisparr.Api.V3.DownloadClient
+{
+    public class DownloadClientBulkResource : ProviderBulkResource<DownloadClientBulkResource>
+    {
+        public bool? Enable { get; set; }
+        public int? Priority { get; set; }
+        public bool? RemoveCompletedDownloads { get; set; }
+        public bool? RemoveFailedDownloads { get; set; }
+    }
+
+    public class DownloadClientBulkResourceMapper : ProviderBulkResourceMapper<DownloadClientBulkResource, DownloadClientDefinition>
+    {
+        public override List<DownloadClientDefinition> UpdateModel(DownloadClientBulkResource resource, List<DownloadClientDefinition> existingDefinitions)
+        {
+            if (resource == null)
+            {
+                return new List<DownloadClientDefinition>();
+            }
+
+            existingDefinitions.ForEach(existing =>
+            {
+                existing.Enable = resource.Enable ?? existing.Enable;
+                existing.Priority = resource.Priority ?? existing.Priority;
+                existing.RemoveCompletedDownloads = resource.RemoveCompletedDownloads ?? existing.RemoveCompletedDownloads;
+                existing.RemoveFailedDownloads = resource.RemoveFailedDownloads ?? existing.RemoveFailedDownloads;
+            });
+
+            return existingDefinitions;
+        }
+    }
+}

src/Whisparr.Api.V3/Indexers/IndexerBulkResource.cs
@@ -1,0 +1,34 @@
+using System.Collections.Generic;
+using NzbDrone.Core.Indexers;
+
+namespace Whisparr.Api.V3.Indexers
+{
+    public class IndexerBulkResource : ProviderBulkResource<IndexerBulkResource>
+    {
+        public bool? EnableRss { get; set; }
+        public bool? EnableAutomaticSearch { get; set; }
+        public bool? EnableInteractiveSearch { get; set; }
+        public int? Priority { get; set; }
+    }
+
+    public class IndexerBulkResourceMapper : ProviderBulkResourceMapper<IndexerBulkResource, IndexerDefinition>
+    {
+        public override List<IndexerDefinition> UpdateModel(IndexerBulkResource resource, List<IndexerDefinition> existingDefinitions)
+        {
+            if (resource == null)
+            {
+                return new List<IndexerDefinition>();
+            }
+
+            existingDefinitions.ForEach(existing =>
+            {
+                existing.EnableRss = resource.EnableRss ?? existing.EnableRss;
+                existing.EnableAutomaticSearch = resource.EnableAutomaticSearch ?? existing.EnableAutomaticSearch;
+                existing.EnableInteractiveSearch = resource.EnableInteractiveSearch ?? existing.EnableInteractiveSearch;
+                existing.Priority = resource.Priority ?? existing.Priority;
+            });
+
+            return existingDefinitions;
+        }
+    }
+}

src/Whisparr.Api.V3/Notifications/NotificationBulkResource.cs
@@ -1,0 +1,12 @@
+using NzbDrone.Core.Notifications;
+
+namespace Whisparr.Api.V3.Notifications
+{
+    public class NotificationBulkResource : ProviderBulkResource<NotificationBulkResource>
+    {
+    }
+
+    public class NotificationBulkResourceMapper : ProviderBulkResourceMapper<NotificationBulkResource, NotificationDefinition>
+    {
+    }
+}

src/Whisparr.Api.V3/ProviderControllerBase.cs
@@ -6,23 +6,31 @@ using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Common.Serializer;
 using NzbDrone.Core.ThingiProvider;
 using NzbDrone.Core.Validation;
+using Whisparr.Api.V3.Series;
 using Whisparr.Http.REST;
 using Whisparr.Http.REST.Attributes;
 
 namespace Whisparr.Api.V3
 {
-    public abstract class ProviderControllerBase<TProviderResource, TProvider, TProviderDefinition> : RestController<TProviderResource>
+    public abstract class ProviderControllerBase<TProviderResource, TBulkProviderResource, TProvider, TProviderDefinition> : RestController<TProviderResource>
         where TProviderDefinition : ProviderDefinition, new()
         where TProvider : IProvider
         where TProviderResource : ProviderResource<TProviderResource>, new()
+        where TBulkProviderResource : ProviderBulkResource<TBulkProviderResource>, new()
     {
         private readonly IProviderFactory<TProvider, TProviderDefinition> _providerFactory;
         private readonly ProviderResourceMapper<TProviderResource, TProviderDefinition> _resourceMapper;
+        private readonly ProviderBulkResourceMapper<TBulkProviderResource, TProviderDefinition> _bulkResourceMapper;
 
-        protected ProviderControllerBase(IProviderFactory<TProvider, TProviderDefinition> providerFactory, string resource, ProviderResourceMapper<TProviderResource, TProviderDefinition> resourceMapper)
+        protected ProviderControllerBase(IProviderFactory<TProvider,
+            TProviderDefinition> providerFactory,
+            string resource,
+            ProviderResourceMapper<TProviderResource, TProviderDefinition> resourceMapper,
+            ProviderBulkResourceMapper<TBulkProviderResource, TProviderDefinition> bulkResourceMapper)
         {
             _providerFactory = providerFactory;
             _resourceMapper = resourceMapper;
+            _bulkResourceMapper = bulkResourceMapper;
 
             SharedValidator.RuleFor(c => c.Name).NotEmpty();
             SharedValidator.RuleFor(c => c.Name).Must((v, c) => !_providerFactory.All().Any(p => p.Name == c && p.Id != v.Id)).WithMessage("Should be unique");
@@ -91,6 +99,39 @@ namespace Whisparr.Api.V3
             return Accepted(providerResource.Id);
         }
 
+        [HttpPut("bulk")]
+        [Consumes("application/json")]
+        public ActionResult<TProviderResource> UpdateProvider([FromBody] TBulkProviderResource providerResource)
+        {
+            var definitionsToUpdate = _providerFactory.Get(providerResource.Ids).ToList();
+
+            foreach (var definition in definitionsToUpdate)
+            {
+                if (providerResource.Tags != null)
+                {
+                    var newTags = providerResource.Tags;
+                    var applyTags = providerResource.ApplyTags;
+
+                    switch (applyTags)
+                    {
+                        case ApplyTags.Add:
+                            newTags.ForEach(t => definition.Tags.Add(t));
+                            break;
+                        case ApplyTags.Remove:
+                            newTags.ForEach(t => definition.Tags.Remove(t));
+                            break;
+                        case ApplyTags.Replace:
+                            definition.Tags = new HashSet<int>(newTags);
+                            break;
+                    }
+                }
+            }
+
+            _bulkResourceMapper.UpdateModel(providerResource, definitionsToUpdate);
+
+            return Accepted(_providerFactory.Update(definitionsToUpdate).Select(x => _resourceMapper.ToResource(x)));
+        }
+
         private TProviderDefinition GetDefinition(TProviderResource providerResource, bool validate, bool includeWarnings, bool forceValidate)
         {
             var existingDefinition = providerResource.Id > 0 ? _providerFactory.Find(providerResource.Id) : null;
@@ -108,6 +149,15 @@ namespace Whisparr.Api.V3
         public object DeleteProvider(int id)
         {
             _providerFactory.Delete(id);
+
+            return new { };
+        }
+
+        [HttpDelete("bulk")]
+        [Consumes("application/json")]
+        public object DeleteProviders([FromBody] TBulkProviderResource resource)
+        {
+            _providerFactory.Delete(resource.Ids);
 
             return new { };
         }
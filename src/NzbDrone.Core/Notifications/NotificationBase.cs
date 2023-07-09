Sonarr/Sonarr

Fixed: Series added events being sent with series deleted title
Closes #5762

src/NzbDrone.Core/Notifications/NotificationBase.cs
@@ -23,7 +23,7 @@ namespace NzbDrone.Core.Notifications
         protected const string EPISODE_GRABBED_TITLE_BRANDED = "Sonarr - " + EPISODE_GRABBED_TITLE;
         protected const string EPISODE_DOWNLOADED_TITLE_BRANDED = "Sonarr - " + EPISODE_DOWNLOADED_TITLE;
         protected const string EPISODE_DELETED_TITLE_BRANDED = "Sonarr - " + EPISODE_DELETED_TITLE;
-        protected const string SERIES_ADDED_TITLE_BRANDED = "Sonarr - " + SERIES_DELETED_TITLE;
+        protected const string SERIES_ADDED_TITLE_BRANDED = "Sonarr - " + SERIES_ADDED_TITLE;
         protected const string SERIES_DELETED_TITLE_BRANDED = "Sonarr - " + SERIES_DELETED_TITLE;
         protected const string HEALTH_ISSUE_TITLE_BRANDED = "Sonarr - " + HEALTH_ISSUE_TITLE;
         protected const string HEALTH_RESTORED_TITLE_BRANDED = "Sonarr - " + HEALTH_RESTORED_TITLE;

Whisparr/Whisparr

(cherry picked from commit 5c48049702269276a43a54531b3377a6cff22d0c)

src/NzbDrone.Core/Notifications/NotificationBase.cs
@@ -23,7 +23,7 @@ namespace NzbDrone.Core.Notifications
         protected const string EPISODE_GRABBED_TITLE_BRANDED = "Whisparr - " + EPISODE_GRABBED_TITLE;
         protected const string EPISODE_DOWNLOADED_TITLE_BRANDED = "Whisparr - " + EPISODE_DOWNLOADED_TITLE;
         protected const string EPISODE_DELETED_TITLE_BRANDED = "Whisparr - " + EPISODE_DELETED_TITLE;
-        protected const string SERIES_ADDED_TITLE_BRANDED = "Whisparr - " + SERIES_DELETED_TITLE;
+        protected const string SERIES_ADDED_TITLE_BRANDED = "Whisparr - " + SERIES_ADDED_TITLE;
         protected const string SERIES_DELETED_TITLE_BRANDED = "Whisparr - " + SERIES_DELETED_TITLE;
         protected const string HEALTH_ISSUE_TITLE_BRANDED = "Whisparr - " + HEALTH_ISSUE_TITLE;
         protected const string HEALTH_RESTORED_TITLE_BRANDED = "Whisparr - " + HEALTH_RESTORED_TITLE;
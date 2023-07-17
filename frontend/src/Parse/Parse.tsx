nicksen/Sonarr

New: Added UI for parsing release names
Closes #5263

frontend/src/System/Parse/Parse.tsx
@@ -1,0 +1,111 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import TextInput from 'Components/Form/TextInput';
+import Icon from 'Components/Icon';
+import Button from 'Components/Link/Button';
+import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import PageContent from 'Components/Page/PageContent';
+import PageContentBody from 'Components/Page/PageContentBody';
+import { icons } from 'Helpers/Props';
+import { clear, fetch } from 'Store/Actions/parseActions';
+import getErrorMessage from 'Utilities/Object/getErrorMessage';
+import ParseResult from './ParseResult';
+import parseStateSelector from './parseStateSelector';
+import styles from './Parse.css';
+
+function Parse() {
+  const { isFetching, error, item } = useSelector(parseStateSelector());
+
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
+
+  const onInputChange = useCallback(
+    ({ value }: { value: string }) => {
+      const trimmedValue = value.trim();
+
+      setTitle(trimmedValue);
+
+      if (trimmedValue === '') {
+        dispatch(clear());
+      } else {
+        dispatch(fetch({ title: trimmedValue }));
+      }
+    },
+    [setTitle, dispatch]
+  );
+
+  const onClearPress = useCallback(() => {
+    setTitle('');
+    dispatch(fetch({ title: '' }));
+  }, [setTitle, dispatch]);
+
+  useEffect(
+    () => {
+      return () => {
+        dispatch(clear());
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  return (
+    <PageContent title="Parse">
+      <PageContentBody>
+        <div className={styles.inputContainer}>
+          <div className={styles.inputIconContainer}>
+            <Icon name={icons.PARSE} size={20} />
+          </div>
+
+          <TextInput
+            className={styles.input}
+            name="title"
+            value={title}
+            placeholder="eg. Series.Title.S01E05.720p.HDTV-RlsGroup"
+            autoFocus={true}
+            onChange={onInputChange}
+          />
+
+          <Button className={styles.clearButton} onPress={onClearPress}>
+            <Icon name={icons.REMOVE} size={20} />
+          </Button>
+        </div>
+
+        {isFetching ? <LoadingIndicator /> : null}
+
+        {!isFetching && !!error ? (
+          <div className={styles.message}>
+            <div className={styles.helpText}>
+              Error parsing, please try again.
+            </div>
+            <div>{getErrorMessage(error)}</div>
+          </div>
+        ) : null}
+
+        {!isFetching && title && !error && !item.parsedEpisodeInfo ? (
+          <div className={styles.message}>
+            Unable to parse the provided title, please try again.
+          </div>
+        ) : null}
+
+        {!isFetching && !error && item.parsedEpisodeInfo ? (
+          <ParseResult />
+        ) : null}
+
+        {title ? null : (
+          <div className={styles.message}>
+            <div className={styles.helpText}>
+              Enter a release title in the input above
+            </div>
+            <div>
+              Sonarr will attempt to parse the title and show you details about
+              it
+            </div>
+          </div>
+        )}
+      </PageContentBody>
+    </PageContent>
+  );
+}
+
+export default Parse;

src/Sonarr.Api.V3/Parse/ParseController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.Download.Aggregation;
 using NzbDrone.Core.Parser;
+using Sonarr.Api.V3.CustomFormats;
 using Sonarr.Api.V3.Episodes;
 using Sonarr.Api.V3.Series;
 using Sonarr.Http;
@@ -13,12 +15,15 @@ namespace Sonarr.Api.V3.Parse
     {
         private readonly IParsingService _parsingService;
         private readonly IRemoteEpisodeAggregationService _aggregationService;
+        private readonly ICustomFormatCalculationService _formatCalculator;
 
         public ParseController(IParsingService parsingService,
-                               IRemoteEpisodeAggregationService aggregationService)
+                               IRemoteEpisodeAggregationService aggregationService,
+                               ICustomFormatCalculationService formatCalculator)
         {
             _parsingService = parsingService;
             _aggregationService = aggregationService;
+            _formatCalculator = formatCalculator;
         }
 
         [HttpGet]
@@ -42,16 +47,22 @@ namespace Sonarr.Api.V3.Parse
 
             var remoteEpisode = _parsingService.Map(parsedEpisodeInfo, 0, 0);
 
-            _aggregationService.Augment(remoteEpisode);
-
             if (remoteEpisode != null)
             {
+                _aggregationService.Augment(remoteEpisode);
+
+                remoteEpisode.CustomFormats = _formatCalculator.ParseCustomFormat(remoteEpisode, 0);
+                remoteEpisode.CustomFormatScore = remoteEpisode?.Series?.QualityProfile?.Value.CalculateCustomFormatScore(remoteEpisode.CustomFormats) ?? 0;
+
                 return new ParseResource
                 {
                     Title = title,
                     ParsedEpisodeInfo = remoteEpisode.ParsedEpisodeInfo,
                     Series = remoteEpisode.Series.ToResource(),
-                    Episodes = remoteEpisode.Episodes.ToResource()
+                    Episodes = remoteEpisode.Episodes.ToResource(),
+                    Languages = remoteEpisode.Languages,
+                    CustomFormats = remoteEpisode.CustomFormats?.ToResource(false),
+                    CustomFormatScore = remoteEpisode.CustomFormatScore
                 };
             }
             else

src/Sonarr.Api.V3/Parse/ParseResource.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using NzbDrone.Core.Languages;
 using NzbDrone.Core.Parser.Model;
+using Sonarr.Api.V3.CustomFormats;
 using Sonarr.Api.V3.Episodes;
 using Sonarr.Api.V3.Series;
 using Sonarr.Http.REST;
@@ -12,5 +14,8 @@ namespace Sonarr.Api.V3.Parse
         public ParsedEpisodeInfo ParsedEpisodeInfo { get; set; }
         public SeriesResource Series { get; set; }
         public List<EpisodeResource> Episodes { get; set; }
+        public List<Language> Languages { get; set; }
+        public List<CustomFormatResource> CustomFormats { get; set; }
+        public int CustomFormatScore { get; set; }
     }
 }

Whisparr/Whisparr

(cherry picked from commit 85e285598106346099ceae676599c5cb4b789c92)

frontend/src/Parse/Parse.tsx
@@ -1,0 +1,111 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import TextInput from 'Components/Form/TextInput';
+import Icon from 'Components/Icon';
+import Button from 'Components/Link/Button';
+import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import PageContent from 'Components/Page/PageContent';
+import PageContentBody from 'Components/Page/PageContentBody';
+import { icons } from 'Helpers/Props';
+import { clear, fetch } from 'Store/Actions/parseActions';
+import getErrorMessage from 'Utilities/Object/getErrorMessage';
+import ParseResult from './ParseResult';
+import parseStateSelector from './parseStateSelector';
+import styles from './Parse.css';
+
+function Parse() {
+  const { isFetching, error, item } = useSelector(parseStateSelector());
+
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
+
+  const onInputChange = useCallback(
+    ({ value }: { value: string }) => {
+      const trimmedValue = value.trim();
+
+      setTitle(value);
+
+      if (trimmedValue === '') {
+        dispatch(clear());
+      } else {
+        dispatch(fetch({ title: trimmedValue }));
+      }
+    },
+    [setTitle, dispatch]
+  );
+
+  const onClearPress = useCallback(() => {
+    setTitle('');
+    dispatch(fetch({ title: '' }));
+  }, [setTitle, dispatch]);
+
+  useEffect(
+    () => {
+      return () => {
+        dispatch(clear());
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  return (
+    <PageContent title="Parse">
+      <PageContentBody>
+        <div className={styles.inputContainer}>
+          <div className={styles.inputIconContainer}>
+            <Icon name={icons.PARSE} size={20} />
+          </div>
+
+          <TextInput
+            className={styles.input}
+            name="title"
+            value={title}
+            placeholder="eg. Site.Title.23.02.05.Episode.Title.720p.HDTV-RlsGroup"
+            autoFocus={true}
+            onChange={onInputChange}
+          />
+
+          <Button className={styles.clearButton} onPress={onClearPress}>
+            <Icon name={icons.REMOVE} size={20} />
+          </Button>
+        </div>
+
+        {isFetching ? <LoadingIndicator /> : null}
+
+        {!isFetching && !!error ? (
+          <div className={styles.message}>
+            <div className={styles.helpText}>
+              Error parsing, please try again.
+            </div>
+            <div>{getErrorMessage(error)}</div>
+          </div>
+        ) : null}
+
+        {!isFetching && title && !error && !item.parsedEpisodeInfo ? (
+          <div className={styles.message}>
+            Unable to parse the provided title, please try again.
+          </div>
+        ) : null}
+
+        {!isFetching && !error && item.parsedEpisodeInfo ? (
+          <ParseResult item={item} />
+        ) : null}
+
+        {title ? null : (
+          <div className={styles.message}>
+            <div className={styles.helpText}>
+              Enter a release title in the input above
+            </div>
+            <div>
+              Whisparr will attempt to parse the title and show you details
+              about it
+            </div>
+          </div>
+        )}
+      </PageContentBody>
+    </PageContent>
+  );
+}
+
+export default Parse;

src/Whisparr.Api.V3/Parse/ParseController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.Download.Aggregation;
 using NzbDrone.Core.Parser;
+using Whisparr.Api.V3.CustomFormats;
 using Whisparr.Api.V3.Episodes;
 using Whisparr.Api.V3.Series;
 using Whisparr.Http;
@@ -13,12 +15,15 @@ namespace Whisparr.Api.V3.Parse
     {
         private readonly IParsingService _parsingService;
         private readonly IRemoteEpisodeAggregationService _aggregationService;
+        private readonly ICustomFormatCalculationService _formatCalculator;
 
         public ParseController(IParsingService parsingService,
-                               IRemoteEpisodeAggregationService aggregationService)
+                               IRemoteEpisodeAggregationService aggregationService,
+                               ICustomFormatCalculationService formatCalculator)
         {
             _parsingService = parsingService;
             _aggregationService = aggregationService;
+            _formatCalculator = formatCalculator;
         }
 
         [HttpGet]
@@ -42,16 +47,22 @@ namespace Whisparr.Api.V3.Parse
 
             var remoteEpisode = _parsingService.Map(parsedEpisodeInfo, 0);
 
-            _aggregationService.Augment(remoteEpisode);
-
             if (remoteEpisode != null)
             {
+                _aggregationService.Augment(remoteEpisode);
+
+                remoteEpisode.CustomFormats = _formatCalculator.ParseCustomFormat(remoteEpisode, 0);
+                remoteEpisode.CustomFormatScore = remoteEpisode?.Series?.QualityProfile?.Value.CalculateCustomFormatScore(remoteEpisode.CustomFormats) ?? 0;
+
                 return new ParseResource
                 {
                     Title = title,
                     ParsedEpisodeInfo = remoteEpisode.ParsedEpisodeInfo,
                     Series = remoteEpisode.Series.ToResource(),
-                    Episodes = remoteEpisode.Episodes.ToResource()
+                    Episodes = remoteEpisode.Episodes.ToResource(),
+                    Languages = remoteEpisode.Languages,
+                    CustomFormats = remoteEpisode.CustomFormats?.ToResource(false),
+                    CustomFormatScore = remoteEpisode.CustomFormatScore
                 };
             }
             else

src/Whisparr.Api.V3/Parse/ParseResource.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using NzbDrone.Core.Languages;
 using NzbDrone.Core.Parser.Model;
+using Whisparr.Api.V3.CustomFormats;
 using Whisparr.Api.V3.Episodes;
 using Whisparr.Api.V3.Series;
 using Whisparr.Http.REST;
@@ -12,5 +14,8 @@ namespace Whisparr.Api.V3.Parse
         public ParsedEpisodeInfo ParsedEpisodeInfo { get; set; }
         public SeriesResource Series { get; set; }
         public List<EpisodeResource> Episodes { get; set; }
+        public List<Language> Languages { get; set; }
+        public List<CustomFormatResource> CustomFormats { get; set; }
+        public int CustomFormatScore { get; set; }
     }
 }
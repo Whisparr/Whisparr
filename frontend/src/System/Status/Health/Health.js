Sonarr/Sonarr

Translate System pages

frontend/src/System/Status/Health/Health.js
@@ -11,6 +11,7 @@ import TableBody from 'Components/Table/TableBody';
 import TableRow from 'Components/Table/TableRow';
 import { icons, kinds } from 'Helpers/Props';
 import titleCase from 'Utilities/String/titleCase';
+import translate from 'Utilities/String/translate';
 import styles from './Health.css';
 
 function getInternalLink(source) {
@@ -23,7 +24,7 @@ function getInternalLink(source) {
       return (
         <IconButton
           name={icons.SETTINGS}
-          title="Settings"
+          title={translate('Settings')}
           to="/settings/indexers"
         />
       );
@@ -33,7 +34,7 @@ function getInternalLink(source) {
       return (
         <IconButton
           name={icons.SETTINGS}
-          title="Settings"
+          title={translate('Settings')}
           to="/settings/downloadclients"
         />
       );
@@ -41,7 +42,7 @@ function getInternalLink(source) {
       return (
         <IconButton
           name={icons.SERIES_CONTINUING}
-          title="Series Editor"
+          title={translate('SeriesEditor')}
           to="/serieseditor"
         />
       );
@@ -49,7 +50,7 @@ function getInternalLink(source) {
       return (
         <IconButton
           name={icons.UPDATE}
-          title="Updates"
+          title={translate('Updates')}
           to="/system/updates"
         />
       );
@@ -64,7 +65,7 @@ function getTestLink(source, props) {
       return (
         <SpinnerIconButton
           name={icons.TEST}
-          title="Test All"
+          title={translate('TestAll')}
           isSpinning={props.isTestingAllIndexers}
           onPress={props.dispatchTestAllIndexers}
         />
@@ -74,7 +75,7 @@ function getTestLink(source, props) {
       return (
         <SpinnerIconButton
           name={icons.TEST}
-          title="Test All"
+          title={translate('TestAll')}
           isSpinning={props.isTestingAllDownloadClients}
           onPress={props.dispatchTestAllDownloadClients}
         />
@@ -93,12 +94,16 @@ const columns = [
   },
   {
     name: 'message',
-    label: 'Message',
+    get label() {
+      return translate('Message');
+    },
     isVisible: true
   },
   {
     name: 'actions',
-    label: 'Actions',
+    get label() {
+      return translate('Actions');
+    },
     isVisible: true
   }
 ];
@@ -121,7 +126,7 @@ class Health extends Component {
       <FieldSet
         legend={
           <div className={styles.legend}>
-            Health
+            {translate('Health')}
 
             {
               isFetching && isPopulated &&
@@ -141,7 +146,7 @@ class Health extends Component {
         {
           !healthIssues &&
             <div className={styles.healthOk}>
-              No issues with your configuration
+              {translate('NoIssuesWithYourConfiguration')}
             </div>
         }
 
@@ -186,7 +191,7 @@ class Health extends Component {
                           <IconButton
                             name={icons.WIKI}
                             to={item.wikiUrl}
-                            title="Read the Wiki for more information"
+                            title={translate('ReadTheWikiForMoreInformation')}
                           />
 
                           {

frontend/src/System/Updates/Updates.js
@@ -12,6 +12,7 @@ import PageContentBody from 'Components/Page/PageContentBody';
 import { icons, kinds } from 'Helpers/Props';
 import formatDate from 'Utilities/Date/formatDate';
 import formatDateTime from 'Utilities/Date/formatDateTime';
+import translate from 'Utilities/String/translate';
 import UpdateChanges from './UpdateChanges';
 import styles from './Updates.css';
 
@@ -43,15 +44,15 @@ class Updates extends Component {
     const hasUpdateToInstall = hasUpdates && _.some(items, { installable: true, latest: true });
     const noUpdateToInstall = hasUpdates && !hasUpdateToInstall;
 
-    const externalUpdaterPrefix = 'Unable to update Sonarr directly,';
+    const externalUpdaterPrefix = translate('UnableToUpdateSonarrDirectly');
     const externalUpdaterMessages = {
-      external: 'Sonarr is configured to use an external update mechanism',
-      apt: 'use apt to install the update',
-      docker: 'update the docker container to receive the update'
+      external: translate('ExternalUpdater'),
+      apt: translate('AptUpdater'),
+      docker: translate('DockerUpdater')
     };
 
     return (
-      <PageContent title="Updates">
+      <PageContent title={translate('Updates')}>
         <PageContentBody>
           {
             !isPopulated && !hasError &&
@@ -61,7 +62,7 @@ class Updates extends Component {
           {
             noUpdates &&
               <Alert kind={kinds.INFO}>
-                No updates are available
+                {translate('NoUpdatesAreAvailable')}
               </Alert>
           }
 
@@ -76,7 +77,7 @@ class Updates extends Component {
                       isSpinning={isInstallingUpdate}
                       onPress={onInstallLatestPress}
                     >
-                      Install Latest
+                      {translate('InstallLatest')}
                     </SpinnerButton> :
 
                     <Fragment>
@@ -112,7 +113,7 @@ class Updates extends Component {
                 />
 
                 <div className={styles.message}>
-                  The latest version of Sonarr is already installed
+                  {translate('OnLatestVersion')}
                 </div>
 
                 {
@@ -164,7 +165,7 @@ class Updates extends Component {
                                 kind={kinds.SUCCESS}
                                 title={formatDateTime(update.installedOn, longDateFormat, timeFormat)}
                               >
-                                Currently Installed
+                                {translate('CurrentlyInstalled')}
                               </Label> :
                               null
                           }
@@ -176,7 +177,7 @@ class Updates extends Component {
                                 kind={kinds.INVERSE}
                                 title={formatDateTime(update.installedOn, longDateFormat, timeFormat)}
                               >
-                                Previously Installed
+                                {translate('PreviouslyInstalled')}
                               </Label> :
                               null
                           }
@@ -184,19 +185,21 @@ class Updates extends Component {
 
                         {
                           !hasChanges &&
-                            <div>Maintenance Release: See GitHub commit history for details.</div>
+                            <div>
+                              {translate('MaintenanceRelease')}
+                            </div>
                         }
 
                         {
                           hasChanges &&
                             <div className={styles.changes}>
                               <UpdateChanges
-                                title="New"
+                                title={translate('New')}
                                 changes={update.changes.new}
                               />
 
                               <UpdateChanges
-                                title="Fixed"
+                                title={translate('Fixed')}
                                 changes={update.changes.fixed}
                               />
                             </div>
@@ -211,14 +214,14 @@ class Updates extends Component {
           {
             !!updatesError &&
               <div>
-                Failed to fetch updates
+                {translate('FailedToFetchUpdates')}
               </div>
           }
 
           {
             !!generalSettingsError &&
               <div>
-                Failed to update settings
+                {translate('FailedToUpdateSettings')}
               </div>
           }
         </PageContentBody>

Whisparr/Whisparr

(cherry picked from commit 93e8ff0ac7610fa8739f2e577ece98c2c06c8881)

frontend/src/System/Status/Health/Health.js
@@ -11,6 +11,7 @@ import TableBody from 'Components/Table/TableBody';
 import TableRow from 'Components/Table/TableRow';
 import { icons, kinds } from 'Helpers/Props';
 import titleCase from 'Utilities/String/titleCase';
+import translate from 'Utilities/String/translate';
 import styles from './Health.css';
 
 function getInternalLink(source) {
@@ -23,7 +24,7 @@ function getInternalLink(source) {
       return (
         <IconButton
           name={icons.SETTINGS}
-          title="Settings"
+          title={translate('Settings')}
           to="/settings/indexers"
         />
       );
@@ -33,7 +34,7 @@ function getInternalLink(source) {
       return (
         <IconButton
           name={icons.SETTINGS}
-          title="Settings"
+          title={translate('Settings')}
           to="/settings/downloadclients"
         />
       );
@@ -41,7 +42,7 @@ function getInternalLink(source) {
       return (
         <IconButton
           name={icons.SERIES_CONTINUING}
-          title="Site Editor"
+          title={translate('SiteEditor')}
           to="/serieseditor"
         />
       );
@@ -49,7 +50,7 @@ function getInternalLink(source) {
       return (
         <IconButton
           name={icons.UPDATE}
-          title="Updates"
+          title={translate('Updates')}
           to="/system/updates"
         />
       );
@@ -64,7 +65,7 @@ function getTestLink(source, props) {
       return (
         <SpinnerIconButton
           name={icons.TEST}
-          title="Test All"
+          title={translate('TestAll')}
           isSpinning={props.isTestingAllIndexers}
           onPress={props.dispatchTestAllIndexers}
         />
@@ -74,7 +75,7 @@ function getTestLink(source, props) {
       return (
         <SpinnerIconButton
           name={icons.TEST}
-          title="Test All"
+          title={translate('TestAll')}
           isSpinning={props.isTestingAllDownloadClients}
           onPress={props.dispatchTestAllDownloadClients}
         />
@@ -93,12 +94,16 @@ const columns = [
   },
   {
     name: 'message',
-    label: 'Message',
+    get label() {
+      return translate('Message');
+    },
     isVisible: true
   },
   {
     name: 'actions',
-    label: 'Actions',
+    get label() {
+      return translate('Actions');
+    },
     isVisible: true
   }
 ];
@@ -121,7 +126,7 @@ class Health extends Component {
       <FieldSet
         legend={
           <div className={styles.legend}>
-            Health
+            {translate('Health')}
 
             {
               isFetching && isPopulated &&
@@ -141,7 +146,7 @@ class Health extends Component {
         {
           !healthIssues &&
             <div className={styles.healthOk}>
-              No issues with your configuration
+              {translate('NoIssuesWithYourConfiguration')}
             </div>
         }
 
@@ -186,7 +191,7 @@ class Health extends Component {
                           <IconButton
                             name={icons.WIKI}
                             to={item.wikiUrl}
-                            title="Read the Wiki for more information"
+                            title={translate('ReadTheWikiForMoreInformation')}
                           />
 
                           {

frontend/src/System/Updates/Updates.js
@@ -12,6 +12,7 @@ import PageContentBody from 'Components/Page/PageContentBody';
 import { icons, kinds } from 'Helpers/Props';
 import formatDate from 'Utilities/Date/formatDate';
 import formatDateTime from 'Utilities/Date/formatDateTime';
+import translate from 'Utilities/String/translate';
 import UpdateChanges from './UpdateChanges';
 import styles from './Updates.css';
 
@@ -43,15 +44,15 @@ class Updates extends Component {
     const hasUpdateToInstall = hasUpdates && _.some(items, { installable: true, latest: true });
     const noUpdateToInstall = hasUpdates && !hasUpdateToInstall;
 
-    const externalUpdaterPrefix = 'Unable to update Whisparr directly,';
+    const externalUpdaterPrefix = translate('UnableToUpdateWhisparrDirectly');
     const externalUpdaterMessages = {
-      external: 'Whisparr is configured to use an external update mechanism',
-      apt: 'use apt to install the update',
-      docker: 'update the docker container to receive the update'
+      external: translate('ExternalUpdater'),
+      apt: translate('AptUpdater'),
+      docker: translate('DockerUpdater')
     };
 
     return (
-      <PageContent title="Updates">
+      <PageContent title={translate('Updates')}>
         <PageContentBody>
           {
             !isPopulated && !hasError &&
@@ -61,7 +62,7 @@ class Updates extends Component {
           {
             noUpdates &&
               <Alert kind={kinds.INFO}>
-                No updates are available
+                {translate('NoUpdatesAreAvailable')}
               </Alert>
           }
 
@@ -76,7 +77,7 @@ class Updates extends Component {
                       isSpinning={isInstallingUpdate}
                       onPress={onInstallLatestPress}
                     >
-                      Install Latest
+                      {translate('InstallLatest')}
                     </SpinnerButton> :
 
                     <Fragment>
@@ -112,7 +113,7 @@ class Updates extends Component {
                 />
 
                 <div className={styles.message}>
-                  The latest version of Whisparr is already installed
+                  {translate('OnLatestVersion')}
                 </div>
 
                 {
@@ -164,7 +165,7 @@ class Updates extends Component {
                                 kind={kinds.SUCCESS}
                                 title={formatDateTime(update.installedOn, longDateFormat, timeFormat)}
                               >
-                                Currently Installed
+                                {translate('CurrentlyInstalled')}
                               </Label> :
                               null
                           }
@@ -176,7 +177,7 @@ class Updates extends Component {
                                 kind={kinds.INVERSE}
                                 title={formatDateTime(update.installedOn, longDateFormat, timeFormat)}
                               >
-                                Previously Installed
+                                {translate('PreviouslyInstalled')}
                               </Label> :
                               null
                           }
@@ -184,19 +185,21 @@ class Updates extends Component {
 
                         {
                           !hasChanges &&
-                            <div>Maintenance Release: See GitHub commit history for details.</div>
+                            <div>
+                              {translate('MaintenanceRelease')}
+                            </div>
                         }
 
                         {
                           hasChanges &&
                             <div className={styles.changes}>
                               <UpdateChanges
-                                title="New"
+                                title={translate('New')}
                                 changes={update.changes.new}
                               />
 
                               <UpdateChanges
-                                title="Fixed"
+                                title={translate('Fixed')}
                                 changes={update.changes.fixed}
                               />
                             </div>
@@ -211,14 +214,14 @@ class Updates extends Component {
           {
             !!updatesError &&
               <div>
-                Failed to fetch updates
+                {translate('FailedToFetchUpdates')}
               </div>
           }
 
           {
             !!generalSettingsError &&
               <div>
-                Failed to update settings
+                {translate('FailedToUpdateSettings')}
               </div>
           }
         </PageContentBody>
92097f5267c254762233d0467e46b8ce3121c2b6
Mark McDowall <mark@mcdowall.ca>
2023-09-01 17:10:07 -0700

    New: Server pushable health checks
    (cherry picked from commit faecdc855f449e459b7704ff5e6b891c85c24c5c)

---
+++ src/NzbDrone.Core/HealthCheck/ServerSideNotificationService.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using NLog;
+using NzbDrone.Common.Cache;
+using NzbDrone.Common.Cloud;
+using NzbDrone.Common.EnvironmentInfo;
+using NzbDrone.Common.Http;
+using NzbDrone.Common.Serializer;
+using NzbDrone.Core.Configuration;
+
+namespace NzbDrone.Core.HealthCheck
+{
+    public interface IServerSideNotificationService
+    {
+        public HealthCheck GetServerChecks();
+    }
+
+    public class ServerSideNotificationService : IServerSideNotificationService
+    {
+        private readonly IHttpClient _client;
+        private readonly IWhisparrCloudRequestBuilder _cloudRequestBuilder;
+        private readonly IConfigFileProvider _configFileProvider;
+        private readonly Logger _logger;
+
+        private readonly ICached<HealthCheck> _cache;
+
+        public ServerSideNotificationService(IHttpClient client,
+                                             IWhisparrCloudRequestBuilder cloudRequestBuilder,
+                                             IConfigFileProvider configFileProvider,
+                                             ICacheManager cacheManager,
+                                             Logger logger)
+        {
+            _client = client;
+            _cloudRequestBuilder = cloudRequestBuilder;
+            _configFileProvider = configFileProvider;
+            _logger = logger;
+
+            _cache = cacheManager.GetCache<HealthCheck>(GetType());
+        }
+
+        public HealthCheck GetServerChecks()
+        {
+            return _cache.Get("ServerChecks", RetrieveServerChecks, TimeSpan.FromHours(2));
+        }
+
+        private HealthCheck RetrieveServerChecks()
+        {
+            var request = _cloudRequestBuilder.Services.Create()
+                .Resource("/notification")
+                .AddQueryParam("version", BuildInfo.Version)
+                .AddQueryParam("os", OsInfo.Os.ToString().ToLowerInvariant())
+                .AddQueryParam("arch", RuntimeInformation.OSArchitecture)
+                .AddQueryParam("runtime", "netcore")
+                .AddQueryParam("branch", _configFileProvider.Branch)
+                .Build();
+
+            try
+            {
+                _logger.Trace("Getting notifications");
+
+                var response = _client.Execute(request);
+                var result = Json.Deserialize<List<ServerNotificationResponse>>(response.Content);
+
+                var checks = result.Select(x => new HealthCheck(GetType(), x.Type, x.Message, x.WikiUrl)).ToList();
+
+                // Only one health check is supported, services returns an ordered list, so use the first one
+                return checks.FirstOrDefault() ?? new HealthCheck(GetType());
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Failed to retrieve notifications");
+
+                return new HealthCheck(GetType());
+            }
+        }
+    }
+
+    public class ServerNotificationResponse
+    {
+        public HealthCheckResult Type { get; set; }
+        public string Message { get; set; }
+        public string WikiUrl { get; set; }
+    }
+}

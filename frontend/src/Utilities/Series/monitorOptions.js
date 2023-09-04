da66bc880e136c4bc013ab804104369815f9cf9b
Stevie Robinson <stevie.robinson@gmail.com>
2023-09-04 19:28:46 +0200

    Translate Frontend Utilities
    (cherry picked from commit 3f0e8ce8634b085d49638d11df96109797ac9cef)

--- frontend/src/Utilities/Series/monitorOptions.js
+++ frontend/src/Utilities/Series/monitorOptions.js
@@ -1,11 +1,48 @@
+import translate from 'Utilities/String/translate';
+
 const monitorOptions = [
-  { key: 'all', value: 'All Scenes' },
-  { key: 'future', value: 'Future Scenes' },
-  { key: 'missing', value: 'Missing Scenes' },
-  { key: 'existing', value: 'Existing Scenes' },
-  { key: 'firstSeason', value: 'Only First Year' },
-  { key: 'latestSeason', value: 'Only Latest Year' },
-  { key: 'none', value: 'None' }
+  {
+    key: 'all',
+    get value() {
+      return translate('MonitorAllEpisodes');
+    }
+  },
+  {
+    key: 'future',
+    get value() {
+      return translate('MonitorFutureEpisodes');
+    }
+  },
+  {
+    key: 'missing',
+    get value() {
+      return translate('MonitorMissingEpisodes');
+    }
+  },
+  {
+    key: 'existing',
+    get value() {
+      return translate('MonitorExistingEpisodes');
+    }
+  },
+  {
+    key: 'firstSeason',
+    get value() {
+      return translate('MonitorFirstSeason');
+    }
+  },
+  {
+    key: 'latestSeason',
+    get value() {
+      return translate('MonitorLatestSeason');
+    }
+  },
+  {
+    key: 'none',
+    get value() {
+      return translate('MonitorNone');
+    }
+  }
 ];
 
 export default monitorOptions;

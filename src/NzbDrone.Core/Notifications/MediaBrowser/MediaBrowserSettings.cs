Sonarr/Sonarr

New: Update matching series path in Jellyfin/Emby library
Closes #5826

src/NzbDrone.Core/Notifications/MediaBrowser/MediaBrowserSettings.cs
@@ -13,6 +13,8 @@ namespace NzbDrone.Core.Notifications.Emby
         {
             RuleFor(c => c.Host).ValidHost();
             RuleFor(c => c.ApiKey).NotEmpty();
+            RuleFor(c => c.MapFrom).NotEmpty().Unless(c => c.MapTo.IsNullOrWhiteSpace());
+            RuleFor(c => c.MapTo).NotEmpty().Unless(c => c.MapFrom.IsNullOrWhiteSpace());
         }
     }
 
@@ -31,7 +33,7 @@ namespace NzbDrone.Core.Notifications.Emby
         [FieldDefinition(1, Label = "Port")]
         public int Port { get; set; }
 
-        [FieldDefinition(2, Label = "Use SSL", Type = FieldType.Checkbox, HelpText = "Connect to Emby over HTTPS instead of HTTP")]
+        [FieldDefinition(2, Label = "Use SSL", Type = FieldType.Checkbox, HelpText = "Connect to Emby/Jellyfin over HTTPS instead of HTTP")]
         public bool UseSsl { get; set; }
 
         [FieldDefinition(3, Label = "API Key", Privacy = PrivacyLevel.ApiKey)]
@@ -42,6 +44,12 @@ namespace NzbDrone.Core.Notifications.Emby
 
         [FieldDefinition(5, Label = "Update Library", HelpText = "Update Library on Import, Rename, or Delete?", Type = FieldType.Checkbox)]
         public bool UpdateLibrary { get; set; }
+
+        [FieldDefinition(6, Label = "Map Paths From", HelpText = "Sonarr path, used to modify series paths when Emby/Jellyfin sees library path location differently from Sonarr(Requires 'UpdateLibrary')", Type = FieldType.Textbox, Advanced = true)]
+        public string MapFrom { get; set; }
+
+        [FieldDefinition(7, Label = "Map Paths To", HelpText = "Emby/Jellyfin path, used to modify series paths when Emby/Jellyfin sees library path location differently from Sonarr(Requires 'UpdateLibrary')", Type = FieldType.Textbox, Advanced = true)]
+        public string MapTo { get; set; }
 
         [JsonIgnore]
         public string Address => $"{Host.ToUrlHost()}:{Port}";

Whisparr/Whisparr

(cherry picked from commit ad0dc01cf7ed16ccfa8260717111ad8a44675221)

src/NzbDrone.Core/Notifications/MediaBrowser/MediaBrowserSettings.cs
@@ -13,6 +13,8 @@ namespace NzbDrone.Core.Notifications.Emby
         {
             RuleFor(c => c.Host).ValidHost();
             RuleFor(c => c.ApiKey).NotEmpty();
+            RuleFor(c => c.MapFrom).NotEmpty().Unless(c => c.MapTo.IsNullOrWhiteSpace());
+            RuleFor(c => c.MapTo).NotEmpty().Unless(c => c.MapFrom.IsNullOrWhiteSpace());
         }
     }
 
@@ -31,7 +33,7 @@ namespace NzbDrone.Core.Notifications.Emby
         [FieldDefinition(1, Label = "Port")]
         public int Port { get; set; }
 
-        [FieldDefinition(2, Label = "Use SSL", Type = FieldType.Checkbox, HelpText = "Connect to Emby over HTTPS instead of HTTP")]
+        [FieldDefinition(2, Label = "Use SSL", Type = FieldType.Checkbox, HelpText = "Connect to Emby/Jellyfin over HTTPS instead of HTTP")]
         public bool UseSsl { get; set; }
 
         [FieldDefinition(3, Label = "API Key", Privacy = PrivacyLevel.ApiKey)]
@@ -42,6 +44,12 @@ namespace NzbDrone.Core.Notifications.Emby
 
         [FieldDefinition(5, Label = "Update Library", HelpText = "Update Library on Import, Rename, or Delete?", Type = FieldType.Checkbox)]
         public bool UpdateLibrary { get; set; }
+
+        [FieldDefinition(6, Label = "Map Paths From", HelpText = "Whisparr path, used to modify series paths when Emby/Jellyfin sees library path location differently from Whisparr(Requires 'UpdateLibrary')", Type = FieldType.Textbox, Advanced = true)]
+        public string MapFrom { get; set; }
+
+        [FieldDefinition(7, Label = "Map Paths To", HelpText = "Emby/Jellyfin path, used to modify series paths when Emby/Jellyfin sees library path location differently from Whisparr(Requires 'UpdateLibrary')", Type = FieldType.Textbox, Advanced = true)]
+        public string MapTo { get; set; }
 
         [JsonIgnore]
         public string Address => $"{Host.ToUrlHost()}:{Port}";
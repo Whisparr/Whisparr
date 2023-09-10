Sonarr/Sonarr

Add Translations to Settings Pages

frontend/src/Settings/ImportLists/ImportListExclusions/EditImportListExclusionModalContent.js
@@ -13,6 +13,7 @@ import ModalFooter from 'Components/Modal/ModalFooter';
 import ModalHeader from 'Components/Modal/ModalHeader';
 import { inputTypes, kinds } from 'Helpers/Props';
 import { numberSettingShape, stringSettingShape } from 'Helpers/Props/Shapes/settingShape';
+import translate from 'Utilities/String/translate';
 import styles from './EditImportListExclusionModalContent.css';
 
 function EditImportListExclusionModalContent(props) {
@@ -38,7 +39,7 @@ function EditImportListExclusionModalContent(props) {
   return (
     <ModalContent onModalClose={onModalClose}>
       <ModalHeader>
-        {id ? 'Edit Import List Exclusion' : 'Add Import List Exclusion'}
+        {id ? translate('EditImportListExclusion') : translate('AddImportListExclusion')}
       </ModalHeader>
 
       <ModalBody className={styles.body}>
@@ -49,7 +50,9 @@ function EditImportListExclusionModalContent(props) {
 
         {
           !isFetching && !!error &&
-            <div>Unable to add a new import list exclusion, please try again.</div>
+            <div>
+              {translate('AddImportListExclusionError')}
+            </div>
         }
 
         {
@@ -58,24 +61,24 @@ function EditImportListExclusionModalContent(props) {
               {...otherProps}
             >
               <FormGroup>
-                <FormLabel>Title</FormLabel>
+                <FormLabel>{translate('Title')}</FormLabel>
 
                 <FormInputGroup
                   type={inputTypes.TEXT}
                   name="title"
-                  helpText="The name of the series to exclude"
+                  helpText={translate('SeriesTitleToExcludeHelpText')}
                   {...title}
                   onChange={onInputChange}
                 />
               </FormGroup>
 
               <FormGroup>
-                <FormLabel>TVDB ID</FormLabel>
+                <FormLabel>{translate('TvdbId')}</FormLabel>
 
                 <FormInputGroup
                   type={inputTypes.TEXT}
                   name="tvdbId"
-                  helpText="The TVDB ID of the series to exclude"
+                  helpText={translate('TvdbIdExcludeHelpText')}
                   {...tvdbId}
                   onChange={onInputChange}
                 />
@@ -92,14 +95,14 @@ function EditImportListExclusionModalContent(props) {
               kind={kinds.DANGER}
               onPress={onDeleteImportListExclusionPress}
             >
-              Delete
+              {translate('Delete')}
             </Button>
         }
 
         <Button
           onPress={onModalClose}
         >
-          Cancel
+          {translate('Cancel')}
         </Button>
 
         <SpinnerErrorButton
@@ -107,7 +110,7 @@ function EditImportListExclusionModalContent(props) {
           error={saveError}
           onPress={onSavePress}
         >
-          Save
+          {translate('Save')}
         </SpinnerErrorButton>
       </ModalFooter>
     </ModalContent>

Whisparr/Whisparr

(cherry picked from commit f2c31e92ceec7c6a8daffa78f30f15ab8684bef9)

frontend/src/Settings/ImportLists/ImportListExclusions/EditImportListExclusionModalContent.js
@@ -13,6 +13,7 @@ import ModalFooter from 'Components/Modal/ModalFooter';
 import ModalHeader from 'Components/Modal/ModalHeader';
 import { inputTypes, kinds } from 'Helpers/Props';
 import { numberSettingShape, stringSettingShape } from 'Helpers/Props/Shapes/settingShape';
+import translate from 'Utilities/String/translate';
 import styles from './EditImportListExclusionModalContent.css';
 
 function EditImportListExclusionModalContent(props) {
@@ -38,7 +39,7 @@ function EditImportListExclusionModalContent(props) {
   return (
     <ModalContent onModalClose={onModalClose}>
       <ModalHeader>
-        {id ? 'Edit Import List Exclusion' : 'Add Import List Exclusion'}
+        {id ? translate('EditImportListExclusion') : translate('AddImportListExclusion')}
       </ModalHeader>
 
       <ModalBody className={styles.body}>
@@ -49,7 +50,9 @@ function EditImportListExclusionModalContent(props) {
 
         {
           !isFetching && !!error &&
-            <div>Unable to add a new import list exclusion, please try again.</div>
+            <div>
+              {translate('AddImportListExclusionError')}
+            </div>
         }
 
         {
@@ -58,24 +61,24 @@ function EditImportListExclusionModalContent(props) {
               {...otherProps}
             >
               <FormGroup>
-                <FormLabel>Title</FormLabel>
+                <FormLabel>{translate('Title')}</FormLabel>
 
                 <FormInputGroup
                   type={inputTypes.TEXT}
                   name="title"
-                  helpText="The name of the site to exclude"
+                  helpText={translate('SiteTitleToExcludeHelpText')}
                   {...title}
                   onChange={onInputChange}
                 />
               </FormGroup>
 
               <FormGroup>
-                <FormLabel>TVDB ID</FormLabel>
+                <FormLabel>{translate('TvdbId')}</FormLabel>
 
                 <FormInputGroup
                   type={inputTypes.TEXT}
                   name="tvdbId"
-                  helpText="The TPDB ID of the site to exclude"
+                  helpText={translate('TpdbIdExcludeHelpText')}
                   {...tvdbId}
                   onChange={onInputChange}
                 />
@@ -92,14 +95,14 @@ function EditImportListExclusionModalContent(props) {
               kind={kinds.DANGER}
               onPress={onDeleteImportListExclusionPress}
             >
-              Delete
+              {translate('Delete')}
             </Button>
         }
 
         <Button
           onPress={onModalClose}
         >
-          Cancel
+          {translate('Cancel')}
         </Button>
 
         <SpinnerErrorButton
@@ -107,7 +110,7 @@ function EditImportListExclusionModalContent(props) {
           error={saveError}
           onPress={onSavePress}
         >
-          Save
+          {translate('Save')}
         </SpinnerErrorButton>
       </ModalFooter>
     </ModalContent>
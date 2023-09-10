Sonarr/Sonarr

Translate frontend/AddSeries

frontend/src/AddSeries/AddNewSeries/AddNewSeries.js
@@ -9,6 +9,7 @@ import PageContent from 'Components/Page/PageContent';
 import PageContentBody from 'Components/Page/PageContentBody';
 import { icons, kinds } from 'Helpers/Props';
 import getErrorMessage from 'Utilities/Object/getErrorMessage';
+import translate from 'Utilities/String/translate';
 import AddNewSeriesSearchResultConnector from './AddNewSeriesSearchResultConnector';
 import styles from './AddNewSeries.css';
 
@@ -87,7 +88,7 @@ class AddNewSeries extends Component {
     const isFetching = this.state.isFetching;
 
     return (
-      <PageContent title="Add New Series">
+      <PageContent title={translate('AddNewSeries')}>
         <PageContentBody>
           <div className={styles.searchContainer}>
             <div className={styles.searchIconContainer}>
@@ -126,7 +127,7 @@ class AddNewSeries extends Component {
             !isFetching && !!error ?
               <div className={styles.message}>
                 <div className={styles.helpText}>
-                  Failed to load search results, please try again.
+                  {translate('AddNewSeriesError')}
                 </div>
                 <div>{getErrorMessage(error)}</div>
               </div> : null
@@ -151,11 +152,11 @@ class AddNewSeries extends Component {
           {
             !isFetching && !error && !items.length && !!term &&
               <div className={styles.message}>
-                <div className={styles.noResults}>Couldn't find any results for '{term}'</div>
-                <div>You can also search using TVDB ID of a show. eg. tvdb:71663</div>
+                <div className={styles.noResults}>{translate('CouldNotFindResults', { term })}</div>
+                <div>{translate('SearchByTvdbId')}</div>
                 <div>
                   <Link to="https://wiki.servarr.com/sonarr/faq#why-cant-i-add-a-new-series-when-i-know-the-tvdb-id">
-                    Why can't I find my show?
+                    {translate('WhyCantIFindMyShow')}
                   </Link>
                 </div>
               </div>
@@ -166,9 +167,9 @@ class AddNewSeries extends Component {
               null :
               <div className={styles.message}>
                 <div className={styles.helpText}>
-                  It's easy to add a new series, just start typing the name the series you want to add.
+                  {translate('AddNewSeriesHelpText')}
                 </div>
-                <div>You can also search using TVDB ID of a show. eg. tvdb:71663</div>
+                <div>{translate('SearchByTvdbId')}</div>
               </div>
           }
 
@@ -176,14 +177,14 @@ class AddNewSeries extends Component {
             !term && !hasExistingSeries ?
               <div className={styles.message}>
                 <div className={styles.noSeriesText}>
-                  You haven't added any series yet, do you want to import some or all of your series first?
+                  {translate('NoSeriesHaveBeenAdded')}
                 </div>
                 <div>
                   <Button
                     to="/add/import"
                     kind={kinds.PRIMARY}
                   >
-                    Import Existing Series
+                    {translate('ImportExistingSeries')}
                   </Button>
                 </div>
               </div> :

frontend/src/AddSeries/ImportSeries/Import/ImportSeries.js
@@ -6,6 +6,7 @@ import LoadingIndicator from 'Components/Loading/LoadingIndicator';
 import PageContent from 'Components/Page/PageContent';
 import PageContentBody from 'Components/Page/PageContentBody';
 import { kinds } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
 import selectAll from 'Utilities/Table/selectAll';
 import toggleSelected from 'Utilities/Table/toggleSelected';
 import ImportSeriesFooterConnector from './ImportSeriesFooterConnector';
@@ -97,7 +98,7 @@ class ImportSeries extends Component {
     } = this.state;
 
     return (
-      <PageContent title="Import Series">
+      <PageContent title={translate('ImportSeries')}>
         <PageContentBody ref={this.scrollerRef} >
           {
             rootFoldersFetching ? <LoadingIndicator /> : null
@@ -106,7 +107,7 @@ class ImportSeries extends Component {
           {
             !rootFoldersFetching && !!rootFoldersError ?
               <Alert kind={kinds.DANGER}>
-                Unable to load root folders
+                {translate('RootFoldersLoadError')}
               </Alert> :
               null
           }
@@ -117,7 +118,7 @@ class ImportSeries extends Component {
             rootFoldersPopulated &&
             !unmappedFolders.length ?
               <Alert kind={kinds.INFO}>
-                All series in {path} have been imported
+                {translate('AllSeriesInRootFolderHaveBeenImported', { path })}
               </Alert> :
               null
           }

frontend/src/AddSeries/ImportSeries/Import/ImportSeriesHeader.js
@@ -8,6 +8,7 @@ import VirtualTableHeaderCell from 'Components/Table/VirtualTableHeaderCell';
 import VirtualTableSelectAllHeaderCell from 'Components/Table/VirtualTableSelectAllHeaderCell';
 import Popover from 'Components/Tooltip/Popover';
 import { icons, tooltipPositions } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
 import styles from './ImportSeriesHeader.css';
 
 function ImportSeriesHeader(props) {
@@ -29,14 +30,14 @@ function ImportSeriesHeader(props) {
         className={styles.folder}
         name="folder"
       >
-        Folder
+        {translate('Folder')}
       </VirtualTableHeaderCell>
 
       <VirtualTableHeaderCell
         className={styles.monitor}
         name="monitor"
       >
-        Monitor
+        {translate('Monitor')}
 
         <Popover
           anchor={
@@ -45,7 +46,7 @@ function ImportSeriesHeader(props) {
               name={icons.INFO}
             />
           }
-          title="Monitoring Options"
+          title={translate('MonitoringOptions')}
           body={<SeriesMonitoringOptionsPopoverContent />}
           position={tooltipPositions.RIGHT}
         />
@@ -55,14 +56,14 @@ function ImportSeriesHeader(props) {
         className={styles.qualityProfile}
         name="qualityProfileId"
       >
-        Quality Profile
+        {translate('QualityProfile')}
       </VirtualTableHeaderCell>
 
       <VirtualTableHeaderCell
         className={styles.seriesType}
         name="seriesType"
       >
-        Series Type
+        {translate('SeriesType')}
 
         <Popover
           anchor={
@@ -71,7 +72,7 @@ function ImportSeriesHeader(props) {
               name={icons.INFO}
             />
           }
-          title="Series Type"
+          title={translate('SeriesType')}
           body={<SeriesTypePopoverContent />}
           position={tooltipPositions.RIGHT}
         />
@@ -81,14 +82,14 @@ function ImportSeriesHeader(props) {
         className={styles.seasonFolder}
         name="seasonFolder"
       >
-        Season Folder
+        {translate('SeasonFolder')}
       </VirtualTableHeaderCell>
 
       <VirtualTableHeaderCell
         className={styles.series}
         name="series"
       >
-        Series
+        {translate('Series')}
       </VirtualTableHeaderCell>
     </VirtualTableHeader>
   );

frontend/src/AddSeries/ImportSeries/SelectFolder/ImportSeriesSelectFolder.js
@@ -6,10 +6,12 @@ import FileBrowserModal from 'Components/FileBrowser/FileBrowserModal';
 import Icon from 'Components/Icon';
 import Button from 'Components/Link/Button';
 import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
 import PageContent from 'Components/Page/PageContent';
 import PageContentBody from 'Components/Page/PageContentBody';
 import { icons, kinds, sizes } from 'Helpers/Props';
 import RootFolders from 'RootFolder/RootFolders';
+import translate from 'Utilities/String/translate';
 import styles from './ImportSeriesSelectFolder.css';
 
 class ImportSeriesSelectFolder extends Component {
@@ -55,9 +57,11 @@ class ImportSeriesSelectFolder extends Component {
     } = this.props;
 
     const hasRootFolders = items.length > 0;
+    const goodFolderExample = (isWindows) ? 'C:\\tv shows' : '/tv shows';
+    const badFolderExample = (isWindows) ? 'C:\\tv shows\\the simpsons' : '/tv shows/the simpsons';
 
     return (
-      <PageContent title="Import Series">
+      <PageContent title={translate('ImportSeries')}>
         <PageContentBody>
           {
             isFetching && !isPopulated ?
@@ -67,7 +71,7 @@ class ImportSeriesSelectFolder extends Component {
 
           {
             !isFetching && error ?
-              <Alert kind={kinds.DANGER}>Unable to load root folders</Alert> :
+              <Alert kind={kinds.DANGER}>{translate('RootFoldersLoadError')}</Alert> :
               null
           }
 
@@ -75,20 +79,20 @@ class ImportSeriesSelectFolder extends Component {
             !error && isPopulated &&
               <div>
                 <div className={styles.header}>
-                  Import series you already have
+                  {translate('LibraryImportHeader')}
                 </div>
 
                 <div className={styles.tips}>
-                  Some tips to ensure the import goes smoothly:
+                  {translate('LibraryImportTips')}
                   <ul>
                     <li className={styles.tip}>
-                      Make sure that your files include the quality in their filenames. eg. <span className={styles.code}>episode.s02e15.bluray.mkv</span>
+                      <InlineMarkdown data={translate('LibraryImportTipsQualityInFilename')} />
                     </li>
                     <li className={styles.tip}>
-                      Point Sonarr to the folder containing all of your tv shows, not a specific one. eg. <span className={styles.code}>"{isWindows ? 'C:\\tv shows' : '/tv shows'}"</span> and not <span className={styles.code}>"{isWindows ? 'C:\\tv shows\\the simpsons' : '/tv shows/the simpsons'}"</span> Additionally, each series must be in its own folder within the root/library folder.
+                      <InlineMarkdown data={translate('LibraryImportTipsUseRootFolder', { goodFolderExample, badFolderExample })} />
                     </li>
                     <li className={styles.tip}>
-                      Do not use for importing downloads from your download client, this is only for existing organized libraries, not unsorted files.
+                      {translate('LibraryImportTipsDontUseDownloadsFolder')}
                     </li>
                   </ul>
                 </div>
@@ -96,7 +100,7 @@ class ImportSeriesSelectFolder extends Component {
                 {
                   hasRootFolders ?
                     <div className={styles.recentFolders}>
-                      <FieldSet legend="Root Folders">
+                      <FieldSet legend={translate('RootFolders')}>
                         <RootFolders
                           isFetching={isFetching}
                           isPopulated={isPopulated}
@@ -114,7 +118,7 @@ class ImportSeriesSelectFolder extends Component {
                       className={styles.addErrorAlert}
                       kind={kinds.DANGER}
                     >
-                      Unable to add root folder
+                      {translate('RootFolderLoadError')}
 
                       <ul>
                         {
@@ -149,8 +153,8 @@ class ImportSeriesSelectFolder extends Component {
                     />
                     {
                       hasRootFolders ?
-                        'Choose another folder' :
-                        'Start Import'
+                        translate('ChooseAnotherFolder') :
+                        translate('StartImport')
                     }
                   </Button>
                 </div>

Whisparr/Whisparr

(cherry picked from commit 02b0710814e197953b27121fd6b207c61247d797)

frontend/src/AddSeries/AddNewSeries/AddNewSeries.js
@@ -9,6 +9,7 @@ import PageContent from 'Components/Page/PageContent';
 import PageContentBody from 'Components/Page/PageContentBody';
 import { icons, kinds } from 'Helpers/Props';
 import getErrorMessage from 'Utilities/Object/getErrorMessage';
+import translate from 'Utilities/String/translate';
 import AddNewSeriesSearchResultConnector from './AddNewSeriesSearchResultConnector';
 import styles from './AddNewSeries.css';
 
@@ -87,7 +88,7 @@ class AddNewSeries extends Component {
     const isFetching = this.state.isFetching;
 
     return (
-      <PageContent title="Add New Site">
+      <PageContent title={translate('AddNewSite')}>
         <PageContentBody>
           <div className={styles.searchContainer}>
             <div className={styles.searchIconContainer}>
@@ -126,7 +127,7 @@ class AddNewSeries extends Component {
             !isFetching && !!error ?
               <div className={styles.message}>
                 <div className={styles.helpText}>
-                  Failed to load search results, please try again.
+                  {translate('AddNewSeriesError')}
                 </div>
                 <div>{getErrorMessage(error)}</div>
               </div> : null
@@ -151,11 +152,11 @@ class AddNewSeries extends Component {
           {
             !isFetching && !error && !items.length && !!term &&
               <div className={styles.message}>
-                <div className={styles.noResults}>Couldn't find any results for '{term}'</div>
-                <div>You can also search using TPDB ID of a show. eg. tpdb:92</div>
+                <div className={styles.noResults}>{translate('CouldNotFindResults', { term })}</div>
+                <div>{translate('SearchByTpdbId')}</div>
                 <div>
-                  <Link to="https://wiki.servarr.com/whisparr/faq#why-cant-i-add-a-new-series-when-i-know-the-tvdb-id">
-                    Why can't I find my show?
+                  <Link to="https://wiki.servarr.com/whisparr/faq#why-cant-i-add-a-new-site-when-i-know-the-tpdb-id">
+                    {translate('WhyCantIFindMySite')}
                   </Link>
                 </div>
               </div>
@@ -166,9 +167,9 @@ class AddNewSeries extends Component {
               null :
               <div className={styles.message}>
                 <div className={styles.helpText}>
-                  It's easy to add a new site, just start typing the name the site you want to add.
+                  {translate('AddNewSeriesHelpText')}
                 </div>
-                <div>You can also search using TPDB ID of a show. eg. tpdb:92</div>
+                <div>{translate('SearchByTpdbId')}</div>
               </div>
           }
 
@@ -176,14 +177,14 @@ class AddNewSeries extends Component {
             !term && !hasExistingSeries ?
               <div className={styles.message}>
                 <div className={styles.noSeriesText}>
-                  You haven't added any sites yet, do you want to import some or all of your sites first?
+                  {translate('NoSitesHaveBeenAdded')}
                 </div>
                 <div>
                   <Button
                     to="/add/import"
                     kind={kinds.PRIMARY}
                   >
-                    Import Existing Sites
+                    {translate('ImportExistingSites')}
                   </Button>
                 </div>
               </div> :

frontend/src/AddSeries/ImportSeries/Import/ImportSeries.js
@@ -6,6 +6,7 @@ import LoadingIndicator from 'Components/Loading/LoadingIndicator';
 import PageContent from 'Components/Page/PageContent';
 import PageContentBody from 'Components/Page/PageContentBody';
 import { kinds } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
 import selectAll from 'Utilities/Table/selectAll';
 import toggleSelected from 'Utilities/Table/toggleSelected';
 import ImportSeriesFooterConnector from './ImportSeriesFooterConnector';
@@ -97,7 +98,7 @@ class ImportSeries extends Component {
     } = this.state;
 
     return (
-      <PageContent title="Import Site">
+      <PageContent title={translate('ImportSites')}>
         <PageContentBody ref={this.scrollerRef} >
           {
             rootFoldersFetching ? <LoadingIndicator /> : null
@@ -106,7 +107,7 @@ class ImportSeries extends Component {
           {
             !rootFoldersFetching && !!rootFoldersError ?
               <Alert kind={kinds.DANGER}>
-                Unable to load root folders
+                {translate('RootFoldersLoadError')}
               </Alert> :
               null
           }
@@ -117,7 +118,7 @@ class ImportSeries extends Component {
             rootFoldersPopulated &&
             !unmappedFolders.length ?
               <Alert kind={kinds.INFO}>
-                All sites in {path} have been imported
+                {translate('AllSitesInRootFolderHaveBeenImported', { path })}
               </Alert> :
               null
           }

frontend/src/AddSeries/ImportSeries/Import/ImportSeriesHeader.js
@@ -7,6 +7,7 @@ import VirtualTableHeaderCell from 'Components/Table/VirtualTableHeaderCell';
 import VirtualTableSelectAllHeaderCell from 'Components/Table/VirtualTableSelectAllHeaderCell';
 import Popover from 'Components/Tooltip/Popover';
 import { icons, tooltipPositions } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
 import styles from './ImportSeriesHeader.css';
 
 function ImportSeriesHeader(props) {
@@ -28,14 +29,14 @@ function ImportSeriesHeader(props) {
         className={styles.folder}
         name="folder"
       >
-        Folder
+        {translate('Folder')}
       </VirtualTableHeaderCell>
 
       <VirtualTableHeaderCell
         className={styles.monitor}
         name="monitor"
       >
-        Monitor
+        {translate('Monitor')}
 
         <Popover
           anchor={
@@ -44,7 +45,7 @@ function ImportSeriesHeader(props) {
               name={icons.INFO}
             />
           }
-          title="Monitoring Options"
+          title={translate('MonitoringOptions')}
           body={<SeriesMonitoringOptionsPopoverContent />}
           position={tooltipPositions.RIGHT}
         />
@@ -54,14 +55,14 @@ function ImportSeriesHeader(props) {
         className={styles.qualityProfile}
         name="qualityProfileId"
       >
-        Quality Profile
+        {translate('QualityProfile')}
       </VirtualTableHeaderCell>
 
       <VirtualTableHeaderCell
         className={styles.series}
         name="series"
       >
-        Sites
+        {translate('Sites')}
       </VirtualTableHeaderCell>
     </VirtualTableHeader>
   );

frontend/src/AddSeries/ImportSeries/SelectFolder/ImportSeriesSelectFolder.js
@@ -6,10 +6,12 @@ import FileBrowserModal from 'Components/FileBrowser/FileBrowserModal';
 import Icon from 'Components/Icon';
 import Button from 'Components/Link/Button';
 import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
 import PageContent from 'Components/Page/PageContent';
 import PageContentBody from 'Components/Page/PageContentBody';
 import { icons, kinds, sizes } from 'Helpers/Props';
 import RootFolders from 'RootFolder/RootFolders';
+import translate from 'Utilities/String/translate';
 import styles from './ImportSeriesSelectFolder.css';
 
 class ImportSeriesSelectFolder extends Component {
@@ -55,9 +57,11 @@ class ImportSeriesSelectFolder extends Component {
     } = this.props;
 
     const hasRootFolders = items.length > 0;
+    const goodFolderExample = (isWindows) ? 'C:\\tv shows' : '/tv shows';
+    const badFolderExample = (isWindows) ? 'C:\\tv shows\\the simpsons' : '/tv shows/the simpsons';
 
     return (
-      <PageContent title="Import Site">
+      <PageContent title={translate('ImportSites')}>
         <PageContentBody>
           {
             isFetching && !isPopulated ?
@@ -67,7 +71,7 @@ class ImportSeriesSelectFolder extends Component {
 
           {
             !isFetching && error ?
-              <Alert kind={kinds.DANGER}>Unable to load root folders</Alert> :
+              <Alert kind={kinds.DANGER}>{translate('RootFoldersLoadError')}</Alert> :
               null
           }
 
@@ -75,20 +79,20 @@ class ImportSeriesSelectFolder extends Component {
             !error && isPopulated &&
               <div>
                 <div className={styles.header}>
-                  Import sites you already have
+                  {translate('LibraryImportHeader')}
                 </div>
 
                 <div className={styles.tips}>
-                  Some tips to ensure the import goes smoothly:
+                  {translate('LibraryImportTips')}
                   <ul>
                     <li className={styles.tip}>
-                      Make sure that your files include the quality in their filenames. eg. <span className={styles.code}>site.23.01.15.episode.1080p.mkv</span>
+                      <InlineMarkdown data={translate('LibraryImportTipsQualityInFilename')} />
                     </li>
                     <li className={styles.tip}>
-                      Point Whisparr to the folder containing all of your sites, not a specific one. eg. <span className={styles.code}>"{isWindows ? 'C:\\sites' : '/sites'}"</span> and not <span className={styles.code}>"{isWindows ? 'C:\\sites\\brazzers' : '/sites/brazzers'}"</span> Additionally, each site must be in its own folder within the root/library folder.
+                      <InlineMarkdown data={translate('LibraryImportTipsUseRootFolder', { goodFolderExample, badFolderExample })} />
                     </li>
                     <li className={styles.tip}>
-                      Do not use for importing downloads from your download client, this is only for existing organized libraries, not unsorted files.
+                      {translate('LibraryImportTipsDontUseDownloadsFolder')}
                     </li>
                   </ul>
                 </div>
@@ -96,7 +100,7 @@ class ImportSeriesSelectFolder extends Component {
                 {
                   hasRootFolders ?
                     <div className={styles.recentFolders}>
-                      <FieldSet legend="Root Folders">
+                      <FieldSet legend={translate('RootFolders')}>
                         <RootFolders
                           isFetching={isFetching}
                           isPopulated={isPopulated}
@@ -114,7 +118,7 @@ class ImportSeriesSelectFolder extends Component {
                       className={styles.addErrorAlert}
                       kind={kinds.DANGER}
                     >
-                      Unable to add root folder
+                      {translate('RootFolderLoadError')}
 
                       <ul>
                         {
@@ -149,8 +153,8 @@ class ImportSeriesSelectFolder extends Component {
                     />
                     {
                       hasRootFolders ?
-                        'Choose another folder' :
-                        'Start Import'
+                        translate('ChooseAnotherFolder') :
+                        translate('StartImport')
                     }
                   </Button>
                 </div>
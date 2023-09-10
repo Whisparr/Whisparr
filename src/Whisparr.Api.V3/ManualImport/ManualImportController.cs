Sonarr/Sonarr

Fixed: Recalculate Custom Format Score in Manual Import

src/Sonarr.Api.V3/ManualImport/ManualImportController.cs
@@ -5,6 +5,7 @@ using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Languages;
 using NzbDrone.Core.MediaFiles.EpisodeImport.Manual;
 using NzbDrone.Core.Qualities;
+using Sonarr.Api.V3.CustomFormats;
 using Sonarr.Api.V3.Episodes;
 using Sonarr.Http;
 
@@ -43,6 +44,8 @@ namespace Sonarr.Api.V3.ManualImport
                 item.SeasonNumber = processedItem.SeasonNumber;
                 item.Episodes = processedItem.Episodes.ToResource();
                 item.Rejections = processedItem.Rejections;
+                item.CustomFormats = processedItem.CustomFormats.ToResource(false);
+                item.CustomFormatScore = processedItem.CustomFormatScore;
 
                 // Only set the language/quality if they're unknown and languages were returned.
                 // Languages won't be returned when reprocessing if the season/episode isn't filled in yet and we don't want to return no languages to the client.

src/Sonarr.Api.V3/ManualImport/ManualImportReprocessResource.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using NzbDrone.Core.DecisionEngine;
 using NzbDrone.Core.Languages;
 using NzbDrone.Core.Qualities;
+using Sonarr.Api.V3.CustomFormats;
 using Sonarr.Api.V3.Episodes;
 using Sonarr.Http.REST;
 
@@ -18,7 +19,8 @@ namespace Sonarr.Api.V3.ManualImport
         public List<Language> Languages { get; set; }
         public string ReleaseGroup { get; set; }
         public string DownloadId { get; set; }
-
+        public List<CustomFormatResource> CustomFormats { get; set; }
+        public int CustomFormatScore { get; set; }
         public IEnumerable<Rejection> Rejections { get; set; }
     }
 }

Whisparr/Whisparr

(cherry picked from commit cda9cf726ac644dde1ad7328f7af652c80dddc10)

src/Whisparr.Api.V3/ManualImport/ManualImportController.cs
@@ -5,6 +5,7 @@ using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Languages;
 using NzbDrone.Core.MediaFiles.EpisodeImport.Manual;
 using NzbDrone.Core.Qualities;
+using Whisparr.Api.V3.CustomFormats;
 using Whisparr.Api.V3.Episodes;
 using Whisparr.Http;
 
@@ -43,6 +44,8 @@ namespace Whisparr.Api.V3.ManualImport
                 item.SeasonNumber = processedItem.SeasonNumber;
                 item.Episodes = processedItem.Episodes.ToResource();
                 item.Rejections = processedItem.Rejections;
+                item.CustomFormats = processedItem.CustomFormats.ToResource(false);
+                item.CustomFormatScore = processedItem.CustomFormatScore;
 
                 // Only set the language/quality if they're unknown and languages were returned.
                 // Languages won't be returned when reprocessing if the season/episode isn't filled in yet and we don't want to return no languages to the client.

src/Whisparr.Api.V3/ManualImport/ManualImportReprocessResource.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using NzbDrone.Core.DecisionEngine;
 using NzbDrone.Core.Languages;
 using NzbDrone.Core.Qualities;
+using Whisparr.Api.V3.CustomFormats;
 using Whisparr.Api.V3.Episodes;
 using Whisparr.Http.REST;
 
@@ -18,7 +19,8 @@ namespace Whisparr.Api.V3.ManualImport
         public List<Language> Languages { get; set; }
         public string ReleaseGroup { get; set; }
         public string DownloadId { get; set; }
-
+        public List<CustomFormatResource> CustomFormats { get; set; }
+        public int CustomFormatScore { get; set; }
         public IEnumerable<Rejection> Rejections { get; set; }
     }
 }
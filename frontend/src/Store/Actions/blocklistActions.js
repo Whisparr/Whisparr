Sonarr/Sonarr

New: More translations for columns

frontend/src/Store/Actions/blocklistActions.js
@@ -4,6 +4,7 @@ import { sortDirections } from 'Helpers/Props';
 import { createThunk, handleThunks } from 'Store/thunks';
 import createAjaxRequest from 'Utilities/createAjaxRequest';
 import serverSideCollectionHandlers from 'Utilities/serverSideCollectionHandlers';
+import translate from 'Utilities/String/translate';
 import { set, updateItem } from './baseActions';
 import createHandleActions from './Creators/createHandleActions';
 import createRemoveItemHandler from './Creators/createRemoveItemHandler';
@@ -32,47 +33,47 @@ export const defaultState = {
   columns: [
     {
       name: 'series.sortTitle',
-      label: 'Series Title',
+      label: () => translate('SeriesTitle'),
       isSortable: true,
       isVisible: true
     },
     {
       name: 'sourceTitle',
-      label: 'Source Title',
+      label: () => translate('SourceTitle'),
       isSortable: true,
       isVisible: true
     },
     {
       name: 'languages',
-      label: 'Languages',
+      label: () => translate('Languages'),
       isVisible: false
     },
     {
       name: 'quality',
-      label: 'Quality',
+      label: () => translate('Quality'),
       isVisible: true
     },
     {
       name: 'customFormats',
-      label: 'Formats',
+      label: () => translate('Formats'),
       isSortable: false,
       isVisible: true
     },
     {
       name: 'date',
-      label: 'Date',
+      label: () => translate('Date'),
       isSortable: true,
       isVisible: true
     },
     {
       name: 'indexer',
-      label: 'Indexer',
+      label: () => translate('Indexer'),
       isSortable: true,
       isVisible: false
     },
     {
       name: 'actions',
-      columnLabel: 'Actions',
+      columnLabel: () => translate('Actions'),
       isVisible: true,
       isModifiable: false
     }

frontend/src/Store/Actions/episodeActions.js
@@ -33,7 +33,7 @@ export const defaultState = {
   columns: [
     {
       name: 'monitored',
-      columnLabel: 'Monitored',
+      columnLabel: () => translate('Monitored'),
       isVisible: true,
       isModifiable: false
     },
@@ -44,94 +44,91 @@ export const defaultState = {
     },
     {
       name: 'title',
-      label: 'Title',
+      label: () => translate('Title'),
       isVisible: true
     },
     {
       name: 'path',
-      label: 'Path',
+      label: () => translate('Path'),
       isVisible: false
     },
     {
       name: 'relativePath',
-      label: 'Relative Path',
+      label: () => translate('RelativePath'),
       isVisible: false
     },
     {
       name: 'airDateUtc',
-      label: 'Air Date',
+      label: () => translate('AirDate'),
       isVisible: true
     },
     {
       name: 'runtime',
-      label: 'Runtime',
+      label: () => translate('Runtime'),
       isVisible: false
     },
     {
       name: 'languages',
-      label: 'Languages',
+      label: () => translate('Languages'),
       isVisible: false
     },
     {
       name: 'audioInfo',
-      label: 'Audio Info',
+      label: () => translate('AudioInfo'),
       isVisible: false
     },
     {
       name: 'videoCodec',
-      label: 'Video Codec',
+      label: () => translate('VideoCodec'),
       isVisible: false
     },
     {
       name: 'videoDynamicRangeType',
-      label: 'Video Dynamic Range',
+      label: () => translate('VideoDynamicRange'),
       isVisible: false
     },
     {
       name: 'audioLanguages',
-      label: 'Audio Languages',
+      label: () => translate('AudioLanguages'),
       isVisible: false
     },
     {
       name: 'subtitleLanguages',
-      label: 'Subtitle Languages',
+      label: () => translate('SubtitleLanguages'),
       isVisible: false
     },
     {
       name: 'size',
-      label: 'Size',
+      label: () => translate('Size'),
       isVisible: false
     },
     {
       name: 'releaseGroup',
-      label: 'Release Group',
+      label: () => translate('ReleaseGroup'),
       isVisible: false
     },
     {
       name: 'customFormats',
-      label: 'Formats',
+      label: () => translate('Formats'),
       isVisible: false
     },
     {
       name: 'customFormatScore',
-      get columnLabel() {
-        return translate('CustomFormatScore');
-      },
+      columnLabel: () => translate('CustomFormatScore'),
       label: React.createElement(Icon, {
         name: icons.SCORE,
         title: () => translate('CustomFormatScore')
-
       }),
       isVisible: false
     },
     {
       name: 'status',
-      label: 'Status',
+      label: () => translate('Status'),
       isVisible: true
     },
     {
       name: 'actions',
-      columnLabel: 'Actions',
+      columnLabel: () => translate('Actions'),
       isVisible: true,
       isModifiable: false
     }

frontend/src/Store/Actions/queueActions.js
@@ -7,6 +7,7 @@ import { icons, sortDirections } from 'Helpers/Props';
 import { createThunk, handleThunks } from 'Store/thunks';
 import createAjaxRequest from 'Utilities/createAjaxRequest';
 import serverSideCollectionHandlers from 'Utilities/serverSideCollectionHandlers';
+import translate from 'Utilities/String/translate';
 import { set, updateItem } from './baseActions';
 import createFetchHandler from './Creators/createFetchHandler';
 import createHandleActions from './Creators/createHandleActions';
@@ -59,113 +60,113 @@ export const defaultState = {
     columns: [
       {
         name: 'status',
-        columnLabel: 'Status',
+        columnLabel: () => translate('Status'),
         isSortable: true,
         isVisible: true,
         isModifiable: false
       },
       {
         name: 'series.sortTitle',
-        label: 'Series',
+        label: () => translate('Series'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'episode',
-        label: 'Episode',
+        label: () => translate('Episode'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'episodes.title',
-        label: 'Episode Title',
+        label: () => translate('EpisodeTitle'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'episodes.airDateUtc',
-        label: 'Episode Air Date',
+        label: () => translate('EpisodeAirDate'),
         isSortable: true,
         isVisible: false
       },
       {
         name: 'languages',
-        label: 'Languages',
+        label: () => translate('Languages'),
         isSortable: true,
         isVisible: false
       },
       {
         name: 'quality',
-        label: 'Quality',
+        label: () => translate('Quality'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'customFormats',
-        label: 'Formats',
+        label: () => translate('Formats'),
         isSortable: false,
         isVisible: true
       },
       {
         name: 'customFormatScore',
-        columnLabel: 'Custom Format Score',
+        columnLabel: () => translate('CustomFormatScore'),
         label: React.createElement(Icon, {
           name: icons.SCORE,
-          title: 'Custom format score'
+          title: () => translate('CustomFormatScore')
         }),
         isVisible: false
       },
       {
         name: 'protocol',
-        label: 'Protocol',
+        label: () => translate('Protocol'),
         isSortable: true,
         isVisible: false
       },
       {
         name: 'indexer',
-        label: 'Indexer',
+        label: () => translate('Indexer'),
         isSortable: true,
         isVisible: false
       },
       {
         name: 'downloadClient',
-        label: 'Download Client',
+        label: () => translate('DownloadClient'),
         isSortable: true,
         isVisible: false
       },
       {
         name: 'title',
-        label: 'Release Title',
+        label: () => translate('ReleaseTitle'),
         isSortable: true,
         isVisible: false
       },
       {
         name: 'size',
-        label: 'Size',
+        label: () => translate('Size'),
         isSortable: true,
         isVisibile: false
       },
       {
         name: 'outputPath',
-        label: 'Output Path',
+        label: () => translate('OutputPath'),
         isSortable: false,
         isVisible: false
       },
       {
         name: 'estimatedCompletionTime',
-        label: 'Time Left',
+        label: () => translate('TimeLeft'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'progress',
-        label: 'Progress',
+        label: () => translate('Progress'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'actions',
-        columnLabel: 'Actions',
+        columnLabel: () => translate('Actions'),
         isVisible: true,
         isModifiable: false
       }

frontend/src/Store/Actions/wantedActions.js
@@ -2,6 +2,7 @@ import { createAction } from 'redux-actions';
 import { filterTypes, sortDirections } from 'Helpers/Props';
 import { createThunk, handleThunks } from 'Store/thunks';
 import serverSideCollectionHandlers from 'Utilities/serverSideCollectionHandlers';
+import translate from 'Utilities/String/translate';
 import createBatchToggleEpisodeMonitoredHandler from './Creators/createBatchToggleEpisodeMonitoredHandler';
 import createHandleActions from './Creators/createHandleActions';
 import createServerSideCollectionHandlers from './Creators/createServerSideCollectionHandlers';
@@ -29,34 +30,34 @@ export const defaultState = {
     columns: [
       {
         name: 'series.sortTitle',
-        label: 'Series Title',
+        label: () => translate('SeriesTitle'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'episode',
-        label: 'Episode',
+        label: () => translate('Episode'),
         isVisible: true
       },
       {
         name: 'episodes.title',
-        label: 'Episode Title',
+        label: () => translate('EpisodeTitle'),
         isVisible: true
       },
       {
         name: 'episodes.airDateUtc',
-        label: 'Air Date',
+        label: () => translate('AirDate'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'status',
-        label: 'Status',
+        label: () => translate('Status'),
         isVisible: true
       },
       {
         name: 'actions',
-        columnLabel: 'Actions',
+        columnLabel: () => translate('Actions'),
         isVisible: true,
         isModifiable: false
       }
@@ -67,7 +68,7 @@ export const defaultState = {
     filters: [
       {
         key: 'monitored',
-        label: 'Monitored',
+        label: () => translate('Monitored'),
         filters: [
           {
             key: 'monitored',
@@ -78,7 +79,7 @@ export const defaultState = {
       },
       {
         key: 'unmonitored',
-        label: 'Unmonitored',
+        label: () => translate('Unmonitored'),
         filters: [
           {
             key: 'monitored',
@@ -101,39 +102,39 @@ export const defaultState = {
     columns: [
       {
         name: 'series.sortTitle',
-        label: 'Series Title',
+        label: () => translate('SeriesTitle'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'episode',
-        label: 'Episode',
+        label: () => translate('Episode'),
         isVisible: true
       },
       {
         name: 'episodes.title',
-        label: 'Episode Title',
+        label: () => translate('EpisodeTitle'),
         isVisible: true
       },
       {
         name: 'episodes.airDateUtc',
-        label: 'Air Date',
+        label: () => translate('AirDate'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'languages',
-        label: 'Languages',
+        label: () => translate('Languages'),
         isVisible: false
       },
       {
         name: 'status',
-        label: 'Status',
+        label: () => translate('Status'),
         isVisible: true
       },
       {
         name: 'actions',
-        columnLabel: 'Actions',
+        columnLabel: () => translate('Actions'),
         isVisible: true,
         isModifiable: false
       }
@@ -144,7 +145,7 @@ export const defaultState = {
     filters: [
       {
         key: 'monitored',
-        label: 'Monitored',
+        label: () => translate('Monitored'),
         filters: [
           {
             key: 'monitored',
@@ -155,7 +156,7 @@ export const defaultState = {
       },
       {
         key: 'unmonitored',
-        label: 'Unmonitored',
+        label: () => translate('Unmonitored'),
         filters: [
           {
             key: 'monitored',

Whisparr/Whisparr

(cherry picked from commit aee8579d1823b7dfb94c0055fe33b5fb5a7fbf17)

frontend/src/Store/Actions/blocklistActions.js
@@ -4,6 +4,7 @@ import { sortDirections } from 'Helpers/Props';
 import { createThunk, handleThunks } from 'Store/thunks';
 import createAjaxRequest from 'Utilities/createAjaxRequest';
 import serverSideCollectionHandlers from 'Utilities/serverSideCollectionHandlers';
+import translate from 'Utilities/String/translate';
 import { set, updateItem } from './baseActions';
 import createHandleActions from './Creators/createHandleActions';
 import createRemoveItemHandler from './Creators/createRemoveItemHandler';
@@ -32,47 +33,47 @@ export const defaultState = {
   columns: [
     {
       name: 'series.sortTitle',
-      label: 'Site',
+      label: () => translate('Site'),
       isSortable: true,
       isVisible: true
     },
     {
       name: 'sourceTitle',
-      label: 'Source Title',
+      label: () => translate('SourceTitle'),
       isSortable: true,
       isVisible: true
     },
     {
       name: 'languages',
-      label: 'Languages',
+      label: () => translate('Languages'),
       isVisible: false
     },
     {
       name: 'quality',
-      label: 'Quality',
+      label: () => translate('Quality'),
       isVisible: true
     },
     {
       name: 'customFormats',
-      label: 'Formats',
+      label: () => translate('Formats'),
       isSortable: false,
       isVisible: true
     },
     {
       name: 'date',
-      label: 'Date',
+      label: () => translate('Date'),
       isSortable: true,
       isVisible: true
     },
     {
       name: 'indexer',
-      label: 'Indexer',
+      label: () => translate('Indexer'),
       isSortable: true,
       isVisible: false
     },
     {
       name: 'actions',
-      columnLabel: 'Actions',
+      columnLabel: () => translate('Actions'),
       isVisible: true,
       isModifiable: false
     }

frontend/src/Store/Actions/episodeActions.js
@@ -33,105 +33,102 @@ export const defaultState = {
   columns: [
     {
       name: 'monitored',
-      columnLabel: 'Monitored',
+      columnLabel: () => translate('Monitored'),
       isVisible: true,
       isModifiable: false
     },
     {
       name: 'title',
-      label: 'Title',
+      label: () => translate('Title'),
       isVisible: true
     },
     {
       name: 'actors',
-      label: 'Performers',
+      label: () => translate('Performers'),
       isVisible: true
     },
     {
       name: 'path',
-      label: 'Path',
+      label: () => translate('Path'),
       isVisible: false
     },
     {
       name: 'relativePath',
-      label: 'Relative Path',
+      label: () => translate('RelativePath'),
       isVisible: false
     },
     {
       name: 'releaseDate',
-      label: 'Release Date',
+      label: () => translate('ReleaseDate'),
       isVisible: true
     },
     {
       name: 'runtime',
-      label: 'Runtime',
+      label: () => translate('Runtime'),
       isVisible: false
     },
     {
       name: 'languages',
-      label: 'Languages',
+      label: () => translate('Languages'),
       isVisible: false
     },
     {
       name: 'audioInfo',
-      label: 'Audio Info',
+      label: () => translate('AudioInfo'),
       isVisible: false
     },
     {
       name: 'videoCodec',
-      label: 'Video Codec',
+      label: () => translate('VideoCodec'),
       isVisible: false
     },
     {
       name: 'videoDynamicRangeType',
-      label: 'Video Dynamic Range',
+      label: () => translate('VideoDynamicRange'),
       isVisible: false
     },
     {
       name: 'audioLanguages',
-      label: 'Audio Languages',
+      label: () => translate('AudioLanguages'),
       isVisible: false
     },
     {
       name: 'subtitleLanguages',
-      label: 'Subtitle Languages',
+      label: () => translate('SubtitleLanguages'),
       isVisible: false
     },
     {
       name: 'size',
-      label: 'Size',
+      label: () => translate('Size'),
       isVisible: false
     },
     {
       name: 'releaseGroup',
-      label: 'Release Group',
+      label: () => translate('ReleaseGroup'),
       isVisible: false
     },
     {
       name: 'customFormats',
-      label: 'Formats',
+      label: () => translate('Formats'),
       isVisible: false
     },
     {
       name: 'customFormatScore',
-      get columnLabel() {
-        return translate('CustomFormatScore');
-      },
+      columnLabel: () => translate('CustomFormatScore'),
       label: React.createElement(Icon, {
         name: icons.SCORE,
         title: () => translate('CustomFormatScore')
-
       }),
       isVisible: false
     },
     {
       name: 'status',
-      label: 'Status',
+      label: () => translate('Status'),
       isVisible: true
     },
     {
       name: 'actions',
-      columnLabel: 'Actions',
+      columnLabel: () => translate('Actions'),
       isVisible: true,
       isModifiable: false
     }

frontend/src/Store/Actions/queueActions.js
@@ -7,6 +7,7 @@ import { icons, sortDirections } from 'Helpers/Props';
 import { createThunk, handleThunks } from 'Store/thunks';
 import createAjaxRequest from 'Utilities/createAjaxRequest';
 import serverSideCollectionHandlers from 'Utilities/serverSideCollectionHandlers';
+import translate from 'Utilities/String/translate';
 import { set, updateItem } from './baseActions';
 import createFetchHandler from './Creators/createFetchHandler';
 import createHandleActions from './Creators/createHandleActions';
@@ -59,107 +60,107 @@ export const defaultState = {
     columns: [
       {
         name: 'status',
-        columnLabel: 'Status',
+        columnLabel: () => translate('Status'),
         isSortable: true,
         isVisible: true,
         isModifiable: false
       },
       {
         name: 'series.sortTitle',
-        label: 'Site',
+        label: () => translate('Site'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'episodes.releaseDate',
-        label: 'Release Date',
+        label: () => translate('ReleaseDate'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'episodes.title',
-        label: 'Scene Title',
+        label: () => translate('SceneTitle'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'languages',
-        label: 'Languages',
+        label: () => translate('Languages'),
         isSortable: true,
         isVisible: false
       },
       {
         name: 'quality',
-        label: 'Quality',
+        label: () => translate('Quality'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'customFormats',
-        label: 'Formats',
+        label: () => translate('Formats'),
         isSortable: false,
         isVisible: true
       },
       {
         name: 'customFormatScore',
-        columnLabel: 'Custom Format Score',
+        columnLabel: () => translate('CustomFormatScore'),
         label: React.createElement(Icon, {
           name: icons.SCORE,
-          title: 'Custom format score'
+          title: () => translate('CustomFormatScore')
         }),
         isVisible: false
       },
       {
         name: 'protocol',
-        label: 'Protocol',
+        label: () => translate('Protocol'),
         isSortable: true,
         isVisible: false
       },
       {
         name: 'indexer',
-        label: 'Indexer',
+        label: () => translate('Indexer'),
         isSortable: true,
         isVisible: false
       },
       {
         name: 'downloadClient',
-        label: 'Download Client',
+        label: () => translate('DownloadClient'),
         isSortable: true,
         isVisible: false
       },
       {
         name: 'title',
-        label: 'Release Title',
+        label: () => translate('ReleaseTitle'),
         isSortable: true,
         isVisible: false
       },
       {
         name: 'size',
-        label: 'Size',
+        label: () => translate('Size'),
         isSortable: true,
         isVisibile: false
       },
       {
         name: 'outputPath',
-        label: 'Output Path',
+        label: () => translate('OutputPath'),
         isSortable: false,
         isVisible: false
       },
       {
         name: 'estimatedCompletionTime',
-        label: 'Time Left',
+        label: () => translate('TimeLeft'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'progress',
-        label: 'Progress',
+        label: () => translate('Progress'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'actions',
-        columnLabel: 'Actions',
+        columnLabel: () => translate('Actions'),
         isVisible: true,
         isModifiable: false
       }

frontend/src/Store/Actions/wantedActions.js
@@ -2,6 +2,7 @@ import { createAction } from 'redux-actions';
 import { filterTypes, sortDirections } from 'Helpers/Props';
 import { createThunk, handleThunks } from 'Store/thunks';
 import serverSideCollectionHandlers from 'Utilities/serverSideCollectionHandlers';
+import translate from 'Utilities/String/translate';
 import createBatchToggleEpisodeMonitoredHandler from './Creators/createBatchToggleEpisodeMonitoredHandler';
 import createHandleActions from './Creators/createHandleActions';
 import createServerSideCollectionHandlers from './Creators/createServerSideCollectionHandlers';
@@ -29,13 +30,13 @@ export const defaultState = {
     columns: [
       {
         name: 'series.sortTitle',
-        label: 'Site Title',
+        label: () => translate('SiteTitle'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'episodes.title',
-        label: 'Episode Title',
+        label: () => translate('EpisodeTitle'),
         isVisible: true
       },
       {
@@ -45,18 +46,18 @@ export const defaultState = {
       },
       {
         name: 'episodes.airDateUtc',
-        label: 'Release Date',
+        label: () => translate('ReleaseDate'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'status',
-        label: 'Status',
+        label: () => translate('Status'),
         isVisible: true
       },
       {
         name: 'actions',
-        columnLabel: 'Actions',
+        columnLabel: () => translate('Actions'),
         isVisible: true,
         isModifiable: false
       }
@@ -67,7 +68,7 @@ export const defaultState = {
     filters: [
       {
         key: 'monitored',
-        label: 'Monitored',
+        label: () => translate('Monitored'),
         filters: [
           {
             key: 'monitored',
@@ -78,7 +79,7 @@ export const defaultState = {
       },
       {
         key: 'unmonitored',
-        label: 'Unmonitored',
+        label: () => translate('Unmonitored'),
         filters: [
           {
             key: 'monitored',
@@ -101,13 +102,13 @@ export const defaultState = {
     columns: [
       {
         name: 'series.sortTitle',
-        label: 'Site Title',
+        label: () => translate('SiteTitle'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'episodes.title',
-        label: 'Episode Title',
+        label: () => translate('EpisodeTitle'),
         isVisible: true
       },
       {
@@ -117,23 +118,23 @@ export const defaultState = {
       },
       {
         name: 'episodes.airDateUtc',
-        label: 'Release Date',
+        label: () => translate('ReleaseDate'),
         isSortable: true,
         isVisible: true
       },
       {
         name: 'languages',
-        label: 'Languages',
+        label: () => translate('Languages'),
         isVisible: false
       },
       {
         name: 'status',
-        label: 'Status',
+        label: () => translate('Status'),
         isVisible: true
       },
       {
         name: 'actions',
-        columnLabel: 'Actions',
+        columnLabel: () => translate('Actions'),
         isVisible: true,
         isModifiable: false
       }
@@ -144,7 +145,7 @@ export const defaultState = {
     filters: [
       {
         key: 'monitored',
-        label: 'Monitored',
+        label: () => translate('Monitored'),
         filters: [
           {
             key: 'monitored',
@@ -155,7 +156,7 @@ export const defaultState = {
       },
       {
         key: 'unmonitored',
-        label: 'Unmonitored',
+        label: () => translate('Unmonitored'),
         filters: [
           {
             key: 'monitored',
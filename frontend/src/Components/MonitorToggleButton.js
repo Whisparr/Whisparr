Sonarr/Sonarr

Translate Frontend Components, Episode and Helpers

frontend/src/Components/MonitorToggleButton.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import SpinnerIconButton from 'Components/Link/SpinnerIconButton';
 import { icons } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
 import styles from './MonitorToggleButton.css';
 
 function getTooltip(monitored, isDisabled) {
   if (isDisabled) {
-    return 'Cannot toggle monitored state when series is unmonitored';
+    return translate('ToggleMonitoredSeriesUnmonitored ');
   }
 
   if (monitored) {
-    return 'Monitored, click to unmonitor';
+    return translate('ToggleMonitoredToUnmonitored');
   }
 
-  return 'Unmonitored, click to monitor';
+  return translate('ToggleUnmonitoredToMonitored');
 }
 
 class MonitorToggleButton extends Component {

frontend/src/Components/Page/ErrorPage.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import getErrorMessage from 'Utilities/Object/getErrorMessage';
+import translate from 'Utilities/String/translate';
 import styles from './ErrorPage.css';
 
 function ErrorPage(props) {
@@ -16,24 +17,24 @@ function ErrorPage(props) {
     systemStatusError
   } = props;
 
-  let errorMessage = 'Failed to load Sonarr';
+  let errorMessage = translate('FailedToLoadSonarr');
 
   if (!isLocalStorageSupported) {
-    errorMessage = 'Local Storage is not supported or disabled. A plugin or private browsing may have disabled it.';
+    errorMessage = translate('LocalStorageIsNotSupported');
   } else if (translationsError) {
-    errorMessage = getErrorMessage(translationsError, 'Failed to load translations from API');
+    errorMessage = getErrorMessage(translationsError, translate('FailedToLoadTranslationsFromApi'));
   } else if (seriesError) {
-    errorMessage = getErrorMessage(seriesError, 'Failed to load series from API');
+    errorMessage = getErrorMessage(seriesError, translate('FailedToLoadSeriesFromApi'));
   } else if (customFiltersError) {
-    errorMessage = getErrorMessage(customFiltersError, 'Failed to load custom filters from API');
+    errorMessage = getErrorMessage(customFiltersError, translate('FailedToLoadCustomFiltersFromApi'));
   } else if (tagsError) {
-    errorMessage = getErrorMessage(tagsError, 'Failed to load tags from API');
+    errorMessage = getErrorMessage(tagsError, translate('FailedToLoadTagsFromApi'));
   } else if (qualityProfilesError) {
-    errorMessage = getErrorMessage(qualityProfilesError, 'Failed to load quality profiles from API');
+    errorMessage = getErrorMessage(qualityProfilesError, translate('FailedToLoadQualityProfilesFromApi'));
   } else if (uiSettingsError) {
-    errorMessage = getErrorMessage(uiSettingsError, 'Failed to load UI settings from API');
+    errorMessage = getErrorMessage(uiSettingsError, translate('FailedToLoadUiSettingsFromApi'));
   } else if (systemStatusError) {
-    errorMessage = getErrorMessage(uiSettingsError, 'Failed to load system status from API');
+    errorMessage = getErrorMessage(uiSettingsError, translate('FailedToLoadSystemStatusFromApi'));
   }
 
   return (
@@ -43,7 +44,7 @@ function ErrorPage(props) {
       </div>
 
       <div className={styles.version}>
-        Version {version}
+        {translate('VersionNumber', { version })}
       </div>
     </div>
   );

frontend/src/Episode/EpisodeDetailsModalContent.js
@@ -8,6 +8,7 @@ import ModalFooter from 'Components/Modal/ModalFooter';
 import ModalHeader from 'Components/Modal/ModalHeader';
 import MonitorToggleButton from 'Components/MonitorToggleButton';
 import episodeEntities from 'Episode/episodeEntities';
+import translate from 'Utilities/String/translate';
 import EpisodeHistoryConnector from './History/EpisodeHistoryConnector';
 import EpisodeSearchConnector from './Search/EpisodeSearchConnector';
 import SeasonEpisodeNumber from './SeasonEpisodeNumber';
@@ -117,21 +118,21 @@ class EpisodeDetailsModalContent extends Component {
                 className={styles.tab}
                 selectedClassName={styles.selectedTab}
               >
-                Details
+                {translate('Details')}
               </Tab>
 
               <Tab
                 className={styles.tab}
                 selectedClassName={styles.selectedTab}
               >
-                History
+                {translate('History')}
               </Tab>
 
               <Tab
                 className={styles.tab}
                 selectedClassName={styles.selectedTab}
               >
-                Search
+                {translate('Search')}
               </Tab>
             </TabList>
 
@@ -173,14 +174,14 @@ class EpisodeDetailsModalContent extends Component {
                 to={seriesLink}
                 onPress={onModalClose}
               >
-                Open Series
+                {translate('OpenSeries')}
               </Button>
           }
 
           <Button
             onPress={onModalClose}
           >
-            Close
+            {translate('Close')}
           </Button>
         </ModalFooter>
       </ModalContent>

Whisparr/Whisparr

(cherry picked from commit e777b7018481b18ef18f1116f75983a037bf0849)

frontend/src/Components/MonitorToggleButton.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import SpinnerIconButton from 'Components/Link/SpinnerIconButton';
 import { icons } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
 import styles from './MonitorToggleButton.css';
 
 function getTooltip(monitored, isDisabled) {
   if (isDisabled) {
-    return 'Cannot toggle monitored state when series is unmonitored';
+    return translate('ToggleMonitoredSiteUnmonitored ');
   }
 
   if (monitored) {
-    return 'Monitored, click to unmonitor';
+    return translate('ToggleMonitoredToUnmonitored');
   }
 
-  return 'Unmonitored, click to monitor';
+  return translate('ToggleUnmonitoredToMonitored');
 }
 
 class MonitorToggleButton extends Component {

frontend/src/Components/Page/ErrorPage.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import getErrorMessage from 'Utilities/Object/getErrorMessage';
+import translate from 'Utilities/String/translate';
 import styles from './ErrorPage.css';
 
 function ErrorPage(props) {
@@ -16,24 +17,24 @@ function ErrorPage(props) {
     systemStatusError
   } = props;
 
-  let errorMessage = 'Failed to load Whisparr';
+  let errorMessage = translate('FailedToLoadWhisparr');
 
   if (!isLocalStorageSupported) {
-    errorMessage = 'Local Storage is not supported or disabled. A plugin or private browsing may have disabled it.';
+    errorMessage = translate('LocalStorageIsNotSupported');
   } else if (translationsError) {
-    errorMessage = getErrorMessage(translationsError, 'Failed to load translations from API');
+    errorMessage = getErrorMessage(translationsError, translate('FailedToLoadTranslationsFromApi'));
   } else if (seriesError) {
-    errorMessage = getErrorMessage(seriesError, 'Failed to load sites from API');
+    errorMessage = getErrorMessage(seriesError, translate('FailedToLoadSitesFromApi'));
   } else if (customFiltersError) {
-    errorMessage = getErrorMessage(customFiltersError, 'Failed to load custom filters from API');
+    errorMessage = getErrorMessage(customFiltersError, translate('FailedToLoadCustomFiltersFromApi'));
   } else if (tagsError) {
-    errorMessage = getErrorMessage(tagsError, 'Failed to load tags from API');
+    errorMessage = getErrorMessage(tagsError, translate('FailedToLoadTagsFromApi'));
   } else if (qualityProfilesError) {
-    errorMessage = getErrorMessage(qualityProfilesError, 'Failed to load quality profiles from API');
+    errorMessage = getErrorMessage(qualityProfilesError, translate('FailedToLoadQualityProfilesFromApi'));
   } else if (uiSettingsError) {
-    errorMessage = getErrorMessage(uiSettingsError, 'Failed to load UI settings from API');
+    errorMessage = getErrorMessage(uiSettingsError, translate('FailedToLoadUiSettingsFromApi'));
   } else if (systemStatusError) {
-    errorMessage = getErrorMessage(uiSettingsError, 'Failed to load system status from API');
+    errorMessage = getErrorMessage(uiSettingsError, translate('FailedToLoadSystemStatusFromApi'));
   }
 
   return (
@@ -43,7 +44,7 @@ function ErrorPage(props) {
       </div>
 
       <div className={styles.version}>
-        Version {version}
+        {translate('VersionNumber', { version })}
       </div>
     </div>
   );

frontend/src/Episode/EpisodeDetailsModalContent.js
@@ -8,6 +8,7 @@ import ModalFooter from 'Components/Modal/ModalFooter';
 import ModalHeader from 'Components/Modal/ModalHeader';
 import MonitorToggleButton from 'Components/MonitorToggleButton';
 import episodeEntities from 'Episode/episodeEntities';
+import translate from 'Utilities/String/translate';
 import EpisodeNumber from './EpisodeNumber';
 import EpisodeHistoryConnector from './History/EpisodeHistoryConnector';
 import EpisodeSearchConnector from './Search/EpisodeSearchConnector';
@@ -116,21 +117,21 @@ class EpisodeDetailsModalContent extends Component {
                 className={styles.tab}
                 selectedClassName={styles.selectedTab}
               >
-                Details
+                {translate('Details')}
               </Tab>
 
               <Tab
                 className={styles.tab}
                 selectedClassName={styles.selectedTab}
               >
-                History
+                {translate('History')}
               </Tab>
 
               <Tab
                 className={styles.tab}
                 selectedClassName={styles.selectedTab}
               >
-                Search
+                {translate('Search')}
               </Tab>
             </TabList>
 
@@ -172,14 +173,14 @@ class EpisodeDetailsModalContent extends Component {
                 to={seriesLink}
                 onPress={onModalClose}
               >
-                Open Site
+                {translate('OpenSite')}
               </Button>
           }
 
           <Button
             onPress={onModalClose}
           >
-            Close
+            {translate('Close')}
           </Button>
         </ModalFooter>
       </ModalContent>
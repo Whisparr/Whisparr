Sonarr/Sonarr

Add info box to Remote Path Mappings Settings

frontend/src/Settings/DownloadClients/RemotePathMappings/RemotePathMappings.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import Alert from 'Components/Alert';
 import FieldSet from 'Components/FieldSet';
 import Icon from 'Components/Icon';
 import Link from 'Components/Link/Link';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
 import PageSectionContent from 'Components/Page/PageSectionContent';
-import { icons } from 'Helpers/Props';
+import { icons, kinds } from 'Helpers/Props';
 import translate from 'Utilities/String/translate';
 import EditRemotePathMappingModalConnector from './EditRemotePathMappingModalConnector';
 import RemotePathMapping from './RemotePathMapping';
@@ -50,6 +52,11 @@ class RemotePathMappings extends Component {
           errorMessage={translate('RemotePathMappingsLoadError')}
           {...otherProps}
         >
+
+          <Alert kind={kinds.INFO}>
+            <InlineMarkdown data={translate('RemotePathMappingsInfo', { app: 'Sonarr', wikiLink: 'https://wiki.servarr.com/sonarr/settings#remote-path-mappings' })} />
+          </Alert>
+
           <div className={styles.remotePathMappingsHeader}>
             <div className={styles.host}>
               {translate('Host')}

Whisparr/Whisparr

(cherry picked from commit d8f3d7d3eafeafd8d4372db0076a63f935a29218)

frontend/src/Settings/DownloadClients/RemotePathMappings/RemotePathMappings.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import Alert from 'Components/Alert';
 import FieldSet from 'Components/FieldSet';
 import Icon from 'Components/Icon';
 import Link from 'Components/Link/Link';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
 import PageSectionContent from 'Components/Page/PageSectionContent';
-import { icons } from 'Helpers/Props';
+import { icons, kinds } from 'Helpers/Props';
 import translate from 'Utilities/String/translate';
 import EditRemotePathMappingModalConnector from './EditRemotePathMappingModalConnector';
 import RemotePathMapping from './RemotePathMapping';
@@ -50,6 +52,11 @@ class RemotePathMappings extends Component {
           errorMessage={translate('RemotePathMappingsLoadError')}
           {...otherProps}
         >
+
+          <Alert kind={kinds.INFO}>
+            <InlineMarkdown data={translate('RemotePathMappingsInfo', { app: 'Whisparr', wikiLink: 'https://wiki.servarr.com/whisparr/settings#remote-path-mappings' })} />
+          </Alert>
+
           <div className={styles.remotePathMappingsHeader}>
             <div className={styles.host}>
               {translate('Host')}
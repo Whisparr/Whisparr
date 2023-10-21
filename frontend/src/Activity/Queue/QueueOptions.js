Sonarr/Sonarr

Fix missing translations and correct some keys

frontend/src/Activity/Queue/QueueOptions.js
@@ -61,7 +61,7 @@ class QueueOptions extends Component {
             type={inputTypes.CHECK}
             name="includeUnknownSeriesItems"
             value={includeUnknownSeriesItems}
-            helpText={translate('ShownUnknownSeriesItemsHelpText')}
+            helpText={translate('ShowUnknownSeriesItemsHelpText')}
             onChange={this.onOptionChange}
           />
         </FormGroup>

Whisparr/Whisparr

(cherry picked from commit ce4ac7594192fa8f63ba239607be61d229c24ce1)

frontend/src/Activity/Queue/QueueOptions.js
@@ -61,7 +61,7 @@ class QueueOptions extends Component {
             type={inputTypes.CHECK}
             name="includeUnknownSeriesItems"
             value={includeUnknownSeriesItems}
-            helpText={translate('ShownUnknownSiteItemsHelpText')}
+            helpText={translate('ShowUnknownSiteItemsHelpText')}
             onChange={this.onOptionChange}
           />
         </FormGroup>
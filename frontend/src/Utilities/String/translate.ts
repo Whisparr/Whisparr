Sonarr/Sonarr

Use variable for App name in translations

frontend/src/Utilities/String/translate.ts
@@ -25,7 +25,7 @@ export async function fetchTranslations(): Promise<boolean> {
 
 export default function translate(
   key: string,
-  tokens?: Record<string, string | number | boolean>
+  tokens: Record<string, string | number | boolean> = { appName: 'Sonarr' }
 ) {
   const translation = translations[key] || key;
 

Whisparr/Whisparr

(cherry picked from commit ddabe66262f8f166cb88025c98f79c0a6058735e)

frontend/src/Utilities/String/translate.ts
@@ -25,7 +25,7 @@ export async function fetchTranslations(): Promise<boolean> {
 
 export default function translate(
   key: string,
-  tokens?: Record<string, string | number | boolean>
+  tokens: Record<string, string | number | boolean> = { appName: 'Whisparr' }
 ) {
   const translation = translations[key] || key;
 
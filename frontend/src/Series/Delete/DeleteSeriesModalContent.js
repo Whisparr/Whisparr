Sonarr/Sonarr

Translate frontend series pages

frontend/src/Series/Delete/DeleteSeriesModalContent.js
@@ -5,12 +5,14 @@ import FormInputGroup from 'Components/Form/FormInputGroup';
 import FormLabel from 'Components/Form/FormLabel';
 import Icon from 'Components/Icon';
 import Button from 'Components/Link/Button';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
 import ModalBody from 'Components/Modal/ModalBody';
 import ModalContent from 'Components/Modal/ModalContent';
 import ModalFooter from 'Components/Modal/ModalFooter';
 import ModalHeader from 'Components/Modal/ModalHeader';
 import { icons, inputTypes, kinds } from 'Helpers/Props';
 import formatBytes from 'Utilities/Number/formatBytes';
+import translate from 'Utilities/String/translate';
 import styles from './DeleteSeriesModalContent.css';
 
 class DeleteSeriesModalContent extends Component {
@@ -61,20 +63,13 @@ class DeleteSeriesModalContent extends Component {
 
     const deleteFiles = this.state.deleteFiles;
     const addImportListExclusion = deleteOptions.addImportListExclusion;
-    let deleteFilesLabel = `Delete ${episodeFileCount} Episode Files`;
-    let deleteFilesHelpText = 'Delete the episode files and series folder';
-
-    if (episodeFileCount === 0) {
-      deleteFilesLabel = 'Delete Series Folder';
-      deleteFilesHelpText = 'Delete the series folder and its contents';
-    }
 
     return (
       <ModalContent
         onModalClose={onModalClose}
       >
         <ModalHeader>
-          Delete - {title}
+          {translate('DeleteSeriesModalHeader', { title })}
         </ModalHeader>
 
         <ModalBody>
@@ -88,25 +83,25 @@ class DeleteSeriesModalContent extends Component {
           </div>
 
           <FormGroup>
-            <FormLabel>Add List Exclusion</FormLabel>
+            <FormLabel>{translate('AddListExclusion')}</FormLabel>
 
             <FormInputGroup
               type={inputTypes.CHECK}
               name="addImportListExclusion"
               value={addImportListExclusion}
-              helpText="Prevent series from being added to Sonarr by lists"
+              helpText={translate('AddListExclusionHelpText')}
               onChange={onDeleteOptionChange}
             />
           </FormGroup>
 
           <FormGroup>
-            <FormLabel>{deleteFilesLabel}</FormLabel>
+            <FormLabel>{episodeFileCount === 0 ? translate('DeleteSeriesFolder') : translate('DeleteEpisodesFiles', { episodeFileCount })}</FormLabel>
 
             <FormInputGroup
               type={inputTypes.CHECK}
               name="deleteFiles"
               value={deleteFiles}
-              helpText={deleteFilesHelpText}
+              helpText={episodeFileCount === 0 ? translate('DeleteSeriesFolderHelpText') : translate('DeleteEpisodesFilesHelpText')}
               kind={kinds.DANGER}
               onChange={this.onDeleteFilesChange}
             />
@@ -115,11 +110,10 @@ class DeleteSeriesModalContent extends Component {
           {
             deleteFiles &&
               <div className={styles.deleteFilesMessage}>
-                <div>The series folder <strong>{path}</strong> and all of its content will be deleted.</div>
-
+                <div><InlineMarkdown data={translate('DeleteSeriesFolderConfirmation', { path })} blockClassName={styles.folderPath} /></div>
                 {
                   !!episodeFileCount &&
-                    <div>{episodeFileCount} episode files totaling {formatBytes(sizeOnDisk)}</div>
+                    <div>{translate('DeleteSeriesFolderEpisodeCount', { episodeFileCount, size: formatBytes(sizeOnDisk) })}</div>
                 }
               </div>
           }
@@ -128,14 +122,14 @@ class DeleteSeriesModalContent extends Component {
 
         <ModalFooter>
           <Button onPress={onModalClose}>
-            Close
+            {translate('Close')}
           </Button>
 
           <Button
             kind={kinds.DANGER}
             onPress={this.onDeleteSeriesConfirmed}
           >
-            Delete
+            {translate('Delete')}
           </Button>
         </ModalFooter>
       </ModalContent>

frontend/src/Series/Details/SeriesDetailsPageConnector.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 import NotFound from 'Components/NotFound';
 import createAllSeriesSelector from 'Store/Selectors/createAllSeriesSelector';
+import translate from 'Utilities/String/translate';
 import SeriesDetailsConnector from './SeriesDetailsConnector';
 
 function createMapStateToProps() {
@@ -54,7 +55,7 @@ class SeriesDetailsPageConnector extends Component {
     if (!titleSlug) {
       return (
         <NotFound
-          message="Sorry, that series cannot be found."
+          message={translate('SeriesCannotBeFound')}
         />
       );
     }

frontend/src/Series/Index/SeriesIndexFooter.tsx
@@ -9,6 +9,7 @@ import DescriptionListItem from 'Components/DescriptionList/DescriptionListItem'
 import createClientSideCollectionSelector from 'Store/Selectors/createClientSideCollectionSelector';
 import createDeepEqualSelector from 'Store/Selectors/createDeepEqualSelector';
 import formatBytes from 'Utilities/Number/formatBytes';
+import translate from 'Utilities/String/translate';
 import styles from './SeriesIndexFooter.css';
 
 function createUnoptimizedSelector() {
@@ -85,7 +86,7 @@ export default function SeriesIndexFooter() {
                     enableColorImpairedMode && 'colorImpaired'
                   )}
                 />
-                <div>Continuing (All episodes downloaded)</div>
+                <div>{translate('SeriesIndexFooterContinuing')}</div>
               </div>
 
               <div className={styles.legendItem}>
@@ -95,7 +96,7 @@ export default function SeriesIndexFooter() {
                     enableColorImpairedMode && 'colorImpaired'
                   )}
                 />
-                <div>Ended (All episodes downloaded)</div>
+                <div>{translate('SeriesIndexFooterEnded')}</div>
               </div>
 
               <div className={styles.legendItem}>
@@ -105,7 +106,7 @@ export default function SeriesIndexFooter() {
                     enableColorImpairedMode && 'colorImpaired'
                   )}
                 />
-                <div>Missing Episodes (Series monitored)</div>
+                <div>{translate('SeriesIndexFooterMissingMonitored')}</div>
               </div>
 
               <div className={styles.legendItem}>
@@ -115,7 +116,7 @@ export default function SeriesIndexFooter() {
                     enableColorImpairedMode && 'colorImpaired'
                   )}
                 />
-                <div>Missing Episodes (Series not monitored)</div>
+                <div>{translate('SeriesIndexFooterMissingUnmonitored')}</div>
               </div>
 
               <div className={styles.legendItem}>
@@ -125,37 +126,49 @@ export default function SeriesIndexFooter() {
                     enableColorImpairedMode && 'colorImpaired'
                   )}
                 />
-                <div>Downloading (One or more episodes)</div>
+                <div>{translate('SeriesIndexFooterDownloading')}</div>
               </div>
             </div>
 
             <div className={styles.statistics}>
               <DescriptionList>
-                <DescriptionListItem title="Series" data={count} />
+                <DescriptionListItem title={translate('Series')} data={count} />
 
-                <DescriptionListItem title="Ended" data={ended} />
+                <DescriptionListItem title={translate('Ended')} data={ended} />
 
-                <DescriptionListItem title="Continuing" data={continuing} />
+                <DescriptionListItem
+                  title={translate('Continuing')}
+                  data={continuing}
+                />
               </DescriptionList>
 
               <DescriptionList>
-                <DescriptionListItem title="Monitored" data={monitored} />
+                <DescriptionListItem
+                  title={translate('Monitored')}
+                  data={monitored}
+                />
 
                 <DescriptionListItem
-                  title="Unmonitored"
+                  title={translate('Unmonitored')}
                   data={count - monitored}
                 />
               </DescriptionList>
 
               <DescriptionList>
-                <DescriptionListItem title="Episodes" data={episodes} />
+                <DescriptionListItem
+                  title={translate('Episodes')}
+                  data={episodes}
+                />
 
-                <DescriptionListItem title="Files" data={episodeFiles} />
+                <DescriptionListItem
+                  title={translate('Files')}
+                  data={episodeFiles}
+                />
               </DescriptionList>
 
               <DescriptionList>
                 <DescriptionListItem
-                  title="Total File Size"
+                  title={translate('TotalFileSize')}
                   data={formatBytes(totalFileSize)}
                 />
               </DescriptionList>

frontend/src/Series/Index/Table/SeriesIndexRow.tsx
@@ -24,6 +24,7 @@ import { executeCommand } from 'Store/Actions/commandActions';
 import { SelectStateInputProps } from 'typings/props';
 import formatBytes from 'Utilities/Number/formatBytes';
 import titleCase from 'Utilities/String/titleCase';
+import translate from 'Utilities/String/translate';
 import SeriesIndexProgressBar from '../ProgressBar/SeriesIndexProgressBar';
 import hasGrowableColumns from './hasGrowableColumns';
 import SeasonsCell from './SeasonsCell';
@@ -454,7 +455,7 @@ function SeriesIndexRow(props: SeriesIndexRowProps) {
             <VirtualTableRowCell key={name} className={styles[name]}>
               <SpinnerIconButton
                 name={icons.REFRESH}
-                title="Refresh series"
+                title={translate('RefreshSeries')}
                 isSpinning={isRefreshingSeries}
                 onPress={onRefreshPress}
               />
@@ -462,7 +463,7 @@ function SeriesIndexRow(props: SeriesIndexRowProps) {
               {showSearchAction ? (
                 <SpinnerIconButton
                   name={icons.SEARCH}
-                  title="Search for monitored episodes"
+                  title={translate('SearchForMonitoredEpisodes')}
                   isSpinning={isSearchingSeries}
                   onPress={onSearchPress}
                 />
@@ -470,7 +471,7 @@ function SeriesIndexRow(props: SeriesIndexRowProps) {
 
               <IconButton
                 name={icons.EDIT}
-                title="Edit Series"
+                title={translate('EditSeries')}
                 onPress={onEditSeriesPress}
               />
             </VirtualTableRowCell>

frontend/src/Series/MoveSeries/MoveSeriesModal.js
@@ -7,6 +7,7 @@ import ModalContent from 'Components/Modal/ModalContent';
 import ModalFooter from 'Components/Modal/ModalFooter';
 import ModalHeader from 'Components/Modal/ModalHeader';
 import { kinds, sizes } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
 import styles from './MoveSeriesModal.css';
 
 function MoveSeriesModal(props) {
@@ -26,7 +27,7 @@ function MoveSeriesModal(props) {
     !destinationPath &&
     !destinationRootFolder
   ) {
-    console.error('orginalPath and destinationPath OR destinationRootFolder must be provided');
+    console.error('originalPath and destinationPath OR destinationRootFolder must be provided');
   }
 
   return (
@@ -41,14 +42,14 @@ function MoveSeriesModal(props) {
         onModalClose={onModalClose}
       >
         <ModalHeader>
-          Move Files
+          {translate('MoveFiles')}
         </ModalHeader>
 
         <ModalBody>
           {
             destinationRootFolder ?
-              `Would you like to move the series folders to '${destinationRootFolder}'?` :
-              `Would you like to move the series files from '${originalPath}' to '${destinationPath}'?`
+              translate('MoveSeriesFoldersToRootFolder', { destinationRootFolder }) :
+              translate('MoveSeriesFoldersToNewPath', { originalPath, destinationPath })
           }
         </ModalBody>
 
@@ -57,14 +58,14 @@ function MoveSeriesModal(props) {
             className={styles.doNotMoveButton}
             onPress={onSavePress}
           >
-            No, I'll Move the Files Myself
+            {translate('MoveSeriesFoldersDontMoveFiles')}
           </Button>
 
           <Button
             kind={kinds.DANGER}
             onPress={onMoveSeriesPress}
           >
-            Yes, Move the Files
+            {translate('MoveSeriesFoldersMoveFiles')}
           </Button>
         </ModalFooter>
       </ModalContent>

frontend/src/Series/SeriesStatus.js
@@ -1,31 +1,32 @@
 
 import { icons } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
 
 export function getSeriesStatusDetails(status) {
 
   let statusDetails = {
     icon: icons.SERIES_CONTINUING,
-    title: 'Continuing',
-    message: 'More episodes/another season is expected'
+    title: translate('Continuing'),
+    message: translate('ContinuingSeriesDescription')
   };
 
   if (status === 'deleted') {
     statusDetails = {
       icon: icons.SERIES_DELETED,
-      title: 'Deleted',
-      message: 'Series was deleted from TheTVDB'
+      title: translate('Deleted'),
+      message: translate('DeletedSeriesDescription')
     };
   } else if (status === 'ended') {
     statusDetails = {
       icon: icons.SERIES_ENDED,
-      title: 'Ended',
-      message: 'No additional episodes or seasons are expected'
+      title: translate('Ended'),
+      message: translate('EndedSeriesDescription')
     };
   } else if (status === 'upcoming') {
     statusDetails = {
       icon: icons.SERIES_CONTINUING,
-      title: 'Upcoming',
-      message: 'Series has been announced but no exact air date yet'
+      title: translate('Upcoming'),
+      message: translate('UpcomingSeriesDescription')
     };
   }
 

Whisparr/Whisparr

(cherry picked from commit c123596c686ef49485a4a23c0b83817ab3f41824)

frontend/src/Series/Delete/DeleteSeriesModalContent.js
@@ -5,12 +5,14 @@ import FormInputGroup from 'Components/Form/FormInputGroup';
 import FormLabel from 'Components/Form/FormLabel';
 import Icon from 'Components/Icon';
 import Button from 'Components/Link/Button';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
 import ModalBody from 'Components/Modal/ModalBody';
 import ModalContent from 'Components/Modal/ModalContent';
 import ModalFooter from 'Components/Modal/ModalFooter';
 import ModalHeader from 'Components/Modal/ModalHeader';
 import { icons, inputTypes, kinds } from 'Helpers/Props';
 import formatBytes from 'Utilities/Number/formatBytes';
+import translate from 'Utilities/String/translate';
 import styles from './DeleteSeriesModalContent.css';
 
 class DeleteSeriesModalContent extends Component {
@@ -61,20 +63,13 @@ class DeleteSeriesModalContent extends Component {
 
     const deleteFiles = this.state.deleteFiles;
     const addImportListExclusion = deleteOptions.addImportListExclusion;
-    let deleteFilesLabel = `Delete ${episodeFileCount} Episode Files`;
-    let deleteFilesHelpText = 'Delete the episode files and site folder';
-
-    if (episodeFileCount === 0) {
-      deleteFilesLabel = 'Delete Site Folder';
-      deleteFilesHelpText = 'Delete the site folder and its contents';
-    }
 
     return (
       <ModalContent
         onModalClose={onModalClose}
       >
         <ModalHeader>
-          Delete - {title}
+          {translate('DeleteSiteModalHeader', { title })}
         </ModalHeader>
 
         <ModalBody>
@@ -88,25 +83,25 @@ class DeleteSeriesModalContent extends Component {
           </div>
 
           <FormGroup>
-            <FormLabel>Add List Exclusion</FormLabel>
+            <FormLabel>{translate('AddListExclusion')}</FormLabel>
 
             <FormInputGroup
               type={inputTypes.CHECK}
               name="addImportListExclusion"
               value={addImportListExclusion}
-              helpText="Prevent site from being added to Whisparr by lists"
+              helpText={translate('AddListExclusionHelpText')}
               onChange={onDeleteOptionChange}
             />
           </FormGroup>
 
           <FormGroup>
-            <FormLabel>{deleteFilesLabel}</FormLabel>
+            <FormLabel>{episodeFileCount === 0 ? translate('DeleteSiteFolder') : translate('DeleteEpisodesFiles', { episodeFileCount })}</FormLabel>
 
             <FormInputGroup
               type={inputTypes.CHECK}
               name="deleteFiles"
               value={deleteFiles}
-              helpText={deleteFilesHelpText}
+              helpText={episodeFileCount === 0 ? translate('DeleteSiteFolderHelpText') : translate('DeleteEpisodesFilesHelpText')}
               kind={kinds.DANGER}
               onChange={this.onDeleteFilesChange}
             />
@@ -115,11 +110,10 @@ class DeleteSeriesModalContent extends Component {
           {
             deleteFiles &&
               <div className={styles.deleteFilesMessage}>
-                <div>The site folder <strong>{path}</strong> and all of its content will be deleted.</div>
-
+                <div><InlineMarkdown data={translate('DeleteSiteFolderConfirmation', { path })} blockClassName={styles.folderPath} /></div>
                 {
                   !!episodeFileCount &&
-                    <div>{episodeFileCount} episode files totaling {formatBytes(sizeOnDisk)}</div>
+                    <div>{translate('DeleteSiteFolderEpisodeCount', { episodeFileCount, size: formatBytes(sizeOnDisk) })}</div>
                 }
               </div>
           }
@@ -128,14 +122,14 @@ class DeleteSeriesModalContent extends Component {
 
         <ModalFooter>
           <Button onPress={onModalClose}>
-            Close
+            {translate('Close')}
           </Button>
 
           <Button
             kind={kinds.DANGER}
             onPress={this.onDeleteSeriesConfirmed}
           >
-            Delete
+            {translate('Delete')}
           </Button>
         </ModalFooter>
       </ModalContent>

frontend/src/Series/Details/SeriesDetailsPageConnector.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 import NotFound from 'Components/NotFound';
 import createAllSeriesSelector from 'Store/Selectors/createAllSeriesSelector';
+import translate from 'Utilities/String/translate';
 import SeriesDetailsConnector from './SeriesDetailsConnector';
 
 function createMapStateToProps() {
@@ -54,7 +55,7 @@ class SeriesDetailsPageConnector extends Component {
     if (!titleSlug) {
       return (
         <NotFound
-          message="Sorry, that site cannot be found."
+          message={translate('SiteCannotBeFound')}
         />
       );
     }

frontend/src/Series/Index/SeriesIndexFooter.tsx
@@ -9,6 +9,7 @@ import DescriptionListItem from 'Components/DescriptionList/DescriptionListItem'
 import createClientSideCollectionSelector from 'Store/Selectors/createClientSideCollectionSelector';
 import createDeepEqualSelector from 'Store/Selectors/createDeepEqualSelector';
 import formatBytes from 'Utilities/Number/formatBytes';
+import translate from 'Utilities/String/translate';
 import styles from './SeriesIndexFooter.css';
 
 function createUnoptimizedSelector() {
@@ -85,7 +86,7 @@ export default function SeriesIndexFooter() {
                     enableColorImpairedMode && 'colorImpaired'
                   )}
                 />
-                <div>Site Active (All scenes downloaded)</div>
+                <div>{translate('SiteIndexFooterContinuing')}</div>
               </div>
 
               <div className={styles.legendItem}>
@@ -95,7 +96,7 @@ export default function SeriesIndexFooter() {
                     enableColorImpairedMode && 'colorImpaired'
                   )}
                 />
-                <div>Site Inactive (All scenes downloaded)</div>
+                <div>{translate('SiteIndexFooterEnded')}</div>
               </div>
 
               <div className={styles.legendItem}>
@@ -105,7 +106,7 @@ export default function SeriesIndexFooter() {
                     enableColorImpairedMode && 'colorImpaired'
                   )}
                 />
-                <div>Missing Scenes (Site monitored)</div>
+                <div>{translate('SiteIndexFooterMissingMonitored')}</div>
               </div>
 
               <div className={styles.legendItem}>
@@ -115,7 +116,7 @@ export default function SeriesIndexFooter() {
                     enableColorImpairedMode && 'colorImpaired'
                   )}
                 />
-                <div>Missing Scenes (Site not monitored)</div>
+                <div>{translate('SiteIndexFooterMissingUnmonitored')}</div>
               </div>
 
               <div className={styles.legendItem}>
@@ -125,37 +126,49 @@ export default function SeriesIndexFooter() {
                     enableColorImpairedMode && 'colorImpaired'
                   )}
                 />
-                <div>Downloading (One or more episodes)</div>
+                <div>{translate('SiteIndexFooterDownloading')}</div>
               </div>
             </div>
 
             <div className={styles.statistics}>
               <DescriptionList>
-                <DescriptionListItem title="Sites" data={count} />
+                <DescriptionListItem title={translate('Sites')} data={count} />
 
-                <DescriptionListItem title="Ended" data={ended} />
+                <DescriptionListItem title={translate('Ended')} data={ended} />
 
-                <DescriptionListItem title="Continuing" data={continuing} />
+                <DescriptionListItem
+                  title={translate('Continuing')}
+                  data={continuing}
+                />
               </DescriptionList>
 
               <DescriptionList>
-                <DescriptionListItem title="Monitored" data={monitored} />
+                <DescriptionListItem
+                  title={translate('Monitored')}
+                  data={monitored}
+                />
 
                 <DescriptionListItem
-                  title="Unmonitored"
+                  title={translate('Unmonitored')}
                   data={count - monitored}
                 />
               </DescriptionList>
 
               <DescriptionList>
-                <DescriptionListItem title="Scenes" data={episodes} />
+                <DescriptionListItem
+                  title={translate('Episodes')}
+                  data={episodes}
+                />
 
-                <DescriptionListItem title="Files" data={episodeFiles} />
+                <DescriptionListItem
+                  title={translate('Files')}
+                  data={episodeFiles}
+                />
               </DescriptionList>
 
               <DescriptionList>
                 <DescriptionListItem
-                  title="Total File Size"
+                  title={translate('TotalFileSize')}
                   data={formatBytes(totalFileSize)}
                 />
               </DescriptionList>

frontend/src/Series/Index/Table/SeriesIndexRow.tsx
@@ -22,6 +22,7 @@ import SeriesTitleLink from 'Series/SeriesTitleLink';
 import { executeCommand } from 'Store/Actions/commandActions';
 import { SelectStateInputProps } from 'typings/props';
 import formatBytes from 'Utilities/Number/formatBytes';
+import translate from 'Utilities/String/translate';
 import SeriesIndexProgressBar from '../ProgressBar/SeriesIndexProgressBar';
 import hasGrowableColumns from './hasGrowableColumns';
 import SeasonsCell from './SeasonsCell';
@@ -434,7 +435,7 @@ function SeriesIndexRow(props: SeriesIndexRowProps) {
             <VirtualTableRowCell key={name} className={styles[name]}>
               <SpinnerIconButton
                 name={icons.REFRESH}
-                title="Refresh site"
+                title={translate('RefreshSite')}
                 isSpinning={isRefreshingSeries}
                 onPress={onRefreshPress}
               />
@@ -442,7 +443,7 @@ function SeriesIndexRow(props: SeriesIndexRowProps) {
               {showSearchAction ? (
                 <SpinnerIconButton
                   name={icons.SEARCH}
-                  title="Search for monitored scenes"
+                  title={translate('SearchForMonitoredEpisodes')}
                   isSpinning={isSearchingSeries}
                   onPress={onSearchPress}
                 />
@@ -450,7 +451,7 @@ function SeriesIndexRow(props: SeriesIndexRowProps) {
 
               <IconButton
                 name={icons.EDIT}
-                title="Edit Site"
+                title={translate('EditSite')}
                 onPress={onEditSeriesPress}
               />
             </VirtualTableRowCell>

frontend/src/Series/MoveSeries/MoveSeriesModal.js
@@ -7,6 +7,7 @@ import ModalContent from 'Components/Modal/ModalContent';
 import ModalFooter from 'Components/Modal/ModalFooter';
 import ModalHeader from 'Components/Modal/ModalHeader';
 import { kinds, sizes } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
 import styles from './MoveSeriesModal.css';
 
 function MoveSeriesModal(props) {
@@ -26,7 +27,7 @@ function MoveSeriesModal(props) {
     !destinationPath &&
     !destinationRootFolder
   ) {
-    console.error('orginalPath and destinationPath OR destinationRootFolder must be provided');
+    console.error('originalPath and destinationPath OR destinationRootFolder must be provided');
   }
 
   return (
@@ -41,14 +42,14 @@ function MoveSeriesModal(props) {
         onModalClose={onModalClose}
       >
         <ModalHeader>
-          Move Files
+          {translate('MoveFiles')}
         </ModalHeader>
 
         <ModalBody>
           {
             destinationRootFolder ?
-              `Would you like to move the sites folders to '${destinationRootFolder}'?` :
-              `Would you like to move the sites files from '${originalPath}' to '${destinationPath}'?`
+              translate('MoveSiteFoldersToRootFolder', { destinationRootFolder }) :
+              translate('MoveSiteFoldersToNewPath', { originalPath, destinationPath })
           }
         </ModalBody>
 
@@ -57,14 +58,14 @@ function MoveSeriesModal(props) {
             className={styles.doNotMoveButton}
             onPress={onSavePress}
           >
-            No, I'll Move the Files Myself
+            {translate('MoveSiteFoldersDontMoveFiles')}
           </Button>
 
           <Button
             kind={kinds.DANGER}
             onPress={onMoveSeriesPress}
           >
-            Yes, Move the Files
+            {translate('MoveSiteFoldersMoveFiles')}
           </Button>
         </ModalFooter>
       </ModalContent>

frontend/src/Series/SeriesStatus.js
@@ -1,31 +1,32 @@
 
 import { icons } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
 
 export function getSeriesStatusDetails(status) {
 
   let statusDetails = {
     icon: icons.SERIES_CONTINUING,
-    title: 'Continuing',
-    message: 'More episodes are expected'
+    title: translate('Continuing'),
+    message: translate('ContinuingSiteDescription')
   };
 
   if (status === 'deleted') {
     statusDetails = {
       icon: icons.SERIES_DELETED,
-      title: 'Deleted',
-      message: 'Site was deleted from TheTPDB'
+      title: translate('Deleted'),
+      message: translate('DeletedSiteDescription')
     };
   } else if (status === 'ended') {
     statusDetails = {
       icon: icons.SERIES_ENDED,
-      title: 'Ended',
-      message: 'No additional episodes are expected'
+      title: translate('Ended'),
+      message: translate('EndedSiteDescription')
     };
   } else if (status === 'upcoming') {
     statusDetails = {
       icon: icons.SERIES_CONTINUING,
-      title: 'Upcoming',
-      message: 'Site has been announced but no exact air date yet'
+      title: translate('Upcoming'),
+      message: translate('UpcomingSiteDescription')
     };
   }
 
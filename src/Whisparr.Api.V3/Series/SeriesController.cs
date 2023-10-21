Sonarr/Sonarr

Fixed: 'includeSeasonImages' parameter to get single series from API
Closes #5971

src/Sonarr.Api.V3/Series/SeriesController.cs
@@ -4,6 +4,7 @@ using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.DataAugmentation.Scene;
+using NzbDrone.Core.Datastore;
 using NzbDrone.Core.Datastore.Events;
 using NzbDrone.Core.MediaCover;
 using NzbDrone.Core.MediaFiles;
@@ -19,6 +20,7 @@ using NzbDrone.Core.Validation;
 using NzbDrone.Core.Validation.Paths;
 using NzbDrone.SignalR;
 using Sonarr.Http;
+using Sonarr.Http.Extensions;
 using Sonarr.Http.REST;
 using Sonarr.Http.REST.Attributes;
 
@@ -118,12 +120,40 @@ namespace Sonarr.Api.V3.Series
             return seriesResources;
         }
 
+        [NonAction]
+        public override ActionResult<SeriesResource> GetResourceByIdWithErrorHandler(int id)
+        {
+            return base.GetResourceByIdWithErrorHandler(id);
+        }
+
+        [RestGetById]
+        [Produces("application/json")]
+        public ActionResult<SeriesResource> GetResourceByIdWithErrorHandler(int id, [FromQuery] bool includeSeasonImages = false)
+        {
+            try
+            {
+                return GetSeriesResourceById(id, includeSeasonImages);
+            }
+            catch (ModelNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
         protected override SeriesResource GetResourceById(int id)
+        {
+            var includeSeasonImages = Request?.GetBooleanQueryParameter("includeSeasonImages", false) ?? false;
+
+            // Parse IncludeImages and use it
+            return GetSeriesResourceById(id, includeSeasonImages);
+        }
+
+        private SeriesResource GetSeriesResourceById(int id, bool includeSeasonImages = false)
         {
             var series = _seriesService.GetSeries(id);
 
             // Parse IncludeImages and use it
-            return GetSeriesResource(series, false);
+            return GetSeriesResource(series, includeSeasonImages);
         }
 
         [RestPostById]

Whisparr/Whisparr

(cherry picked from commit 87b3a305e90f753d5b2053cdf5c2a7f7b14a55ed)

src/Whisparr.Api.V3/Series/SeriesController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Datastore;
 using NzbDrone.Core.Datastore.Events;
 using NzbDrone.Core.MediaCover;
 using NzbDrone.Core.MediaFiles;
@@ -18,6 +19,7 @@ using NzbDrone.Core.Validation;
 using NzbDrone.Core.Validation.Paths;
 using NzbDrone.SignalR;
 using Whisparr.Http;
+using Whisparr.Http.Extensions;
 using Whisparr.Http.REST;
 using Whisparr.Http.REST.Attributes;
 
@@ -113,12 +115,40 @@ namespace Whisparr.Api.V3.Series
             return seriesResources;
         }
 
+        [NonAction]
+        public override ActionResult<SeriesResource> GetResourceByIdWithErrorHandler(int id)
+        {
+            return base.GetResourceByIdWithErrorHandler(id);
+        }
+
+        [RestGetById]
+        [Produces("application/json")]
+        public ActionResult<SeriesResource> GetResourceByIdWithErrorHandler(int id, [FromQuery] bool includeSeasonImages = false)
+        {
+            try
+            {
+                return GetSeriesResourceById(id, includeSeasonImages);
+            }
+            catch (ModelNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
         protected override SeriesResource GetResourceById(int id)
+        {
+            var includeSeasonImages = Request?.GetBooleanQueryParameter("includeSeasonImages", false) ?? false;
+
+            // Parse IncludeImages and use it
+            return GetSeriesResourceById(id, includeSeasonImages);
+        }
+
+        private SeriesResource GetSeriesResourceById(int id, bool includeSeasonImages = false)
         {
             var series = _seriesService.GetSeries(id);
 
             // Parse IncludeImages and use it
-            return GetSeriesResource(series, false);
+            return GetSeriesResource(series, includeSeasonImages);
         }
 
         [RestPostById]
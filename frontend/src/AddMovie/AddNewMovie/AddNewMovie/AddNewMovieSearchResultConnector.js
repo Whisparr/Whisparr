ecccb7ad143fa73e016355bb2837d4b94b0f9490
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-12-12 21:55:41 +0200

    Fixed: Movie status label in add movie search results
    (cherry picked from commit 79bbf9c50ba4b0c2efa3abd37ba833ad9f2d29fb)

--- frontend/src/AddMovie/AddNewMovie/AddNewMovie/AddNewMovieSearchResultConnector.js
+++ frontend/src/AddMovie/AddNewMovie/AddNewMovie/AddNewMovieSearchResultConnector.js
@@ -10,15 +10,19 @@ function createMapStateToProps() {
     createExistingMovieSelector(),
     createExclusionMovieSelector(),
     createDimensionsSelector(),
+    (state) => state.queue.details.items,
     (state, { internalId }) => internalId,
     (state) => state.settings.ui.item.movieRuntimeFormat,
     (state) => state.settings.safeForWorkMode,
-    (isExistingMovie, isExclusionMovie, dimensions, internalId, movieRuntimeFormat, safeForWorkMode) => {
+    (isExistingMovie, isExclusionMovie, dimensions, queueItems, internalId, movieRuntimeFormat, safeForWorkMode) => {
+      const queueItem = queueItems.find((item) => internalId > 0 && item.movieId === internalId);
+
       return {
         existingMovieId: internalId,
         isExistingMovie,
         isExclusionMovie,
         isSmallScreen: dimensions.isSmallScreen,
+        queueItem,
         movieRuntimeFormat,
         safeForWorkMode
       };

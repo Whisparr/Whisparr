Sonarr/Sonarr

Add translations to history and queue custom filters

frontend/src/Store/Actions/historyActions.js
@@ -190,25 +190,25 @@ export const defaultState = {
   filterBuilderProps: [
     {
       name: 'eventType',
-      label: 'Event Type',
+      label: () => translate('EventType'),
       type: filterBuilderTypes.EQUAL,
       valueType: filterBuilderValueTypes.HISTORY_EVENT_TYPE
     },
     {
       name: 'seriesIds',
-      label: 'Series',
+      label: () => translate('Series'),
       type: filterBuilderTypes.EQUAL,
       valueType: filterBuilderValueTypes.SERIES
     },
     {
       name: 'quality',
-      label: 'Quality',
+      label: () => translate('Quality'),
       type: filterBuilderTypes.EQUAL,
       valueType: filterBuilderValueTypes.QUALITY
     },
     {
       name: 'languages',
-      label: 'Languages',
+      label: () => translate('Languages'),
       type: filterBuilderTypes.CONTAINS,
       valueType: filterBuilderValueTypes.LANGUAGE
     }

frontend/src/Store/Actions/queueActions.js
@@ -185,25 +185,25 @@ export const defaultState = {
     filterBuilderProps: [
       {
         name: 'seriesIds',
-        label: 'Series',
+        label: () => translate('Series'),
         type: filterBuilderTypes.EQUAL,
         valueType: filterBuilderValueTypes.SERIES
       },
       {
         name: 'quality',
-        label: 'Quality',
+        label: () => translate('Quality'),
         type: filterBuilderTypes.EQUAL,
         valueType: filterBuilderValueTypes.QUALITY
       },
       {
         name: 'languages',
-        label: 'Languages',
+        label: () => translate('Languages'),
         type: filterBuilderTypes.CONTAINS,
         valueType: filterBuilderValueTypes.LANGUAGE
       },
       {
         name: 'protocol',
-        label: 'Protocol',
+        label: () => translate('Protocol'),
         type: filterBuilderTypes.EQUAL,
         valueType: filterBuilderValueTypes.PROTOCOL
       }

Whisparr/Whisparr

(cherry picked from commit 910c403d841ab46b0ec7b68c4bbb76445d340484)

frontend/src/Store/Actions/historyActions.js
@@ -195,25 +195,25 @@ export const defaultState = {
   filterBuilderProps: [
     {
       name: 'eventType',
-      label: 'Event Type',
+      label: () => translate('EventType'),
       type: filterBuilderTypes.EQUAL,
       valueType: filterBuilderValueTypes.HISTORY_EVENT_TYPE
     },
     {
       name: 'seriesIds',
-      label: 'Sites',
+      label: () => translate('Sites'),
       type: filterBuilderTypes.EQUAL,
       valueType: filterBuilderValueTypes.SERIES
     },
     {
       name: 'quality',
-      label: 'Quality',
+      label: () => translate('Quality'),
       type: filterBuilderTypes.EQUAL,
       valueType: filterBuilderValueTypes.QUALITY
     },
     {
       name: 'languages',
-      label: 'Languages',
+      label: () => translate('Languages'),
       type: filterBuilderTypes.CONTAINS,
       valueType: filterBuilderValueTypes.LANGUAGE
     }

frontend/src/Store/Actions/queueActions.js
@@ -179,25 +179,25 @@ export const defaultState = {
     filterBuilderProps: [
       {
         name: 'seriesIds',
-        label: 'Sites',
+        label: () => translate('Sites'),
         type: filterBuilderTypes.EQUAL,
         valueType: filterBuilderValueTypes.SERIES
       },
       {
         name: 'quality',
-        label: 'Quality',
+        label: () => translate('Quality'),
         type: filterBuilderTypes.EQUAL,
         valueType: filterBuilderValueTypes.QUALITY
       },
       {
         name: 'languages',
-        label: 'Languages',
+        label: () => translate('Languages'),
         type: filterBuilderTypes.CONTAINS,
         valueType: filterBuilderValueTypes.LANGUAGE
       },
       {
         name: 'protocol',
-        label: 'Protocol',
+        label: () => translate('Protocol'),
         type: filterBuilderTypes.EQUAL,
         valueType: filterBuilderValueTypes.PROTOCOL
       }
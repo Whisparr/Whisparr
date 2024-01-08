Sonarr/Sonarr

Fix various typos
Found via `codespell -q 3`

src/NzbDrone.Core/ImportLists/ImportListSyncService.cs
@@ -106,7 +106,7 @@ namespace NzbDrone.Core.ImportLists
 
                 if (excludedSeries != null)
                 {
-                    _logger.Debug("{0} [{1}] Rejected due to list exlcusion", report.TvdbId, report.Title);
+                    _logger.Debug("{0} [{1}] Rejected due to list exclusion", report.TvdbId, report.Title);
                     continue;
                 }
 

Whisparr/Whisparr

(cherry picked from commit 209a250079fdf7ad2bc9168f81bfb45b9531d6b3)
(cherry picked from commit 260fb88f859c39644642669f4622161f7310d7e0)

src/NzbDrone.Core/ImportLists/ImportListSyncService.cs
@@ -93,7 +93,7 @@ namespace NzbDrone.Core.ImportLists
 
             if (excludedMovie != null)
             {
-                _logger.Debug("{0} [{1}] Rejected due to list exlcusion", report.ForeignId, report.Title);
+                _logger.Debug("{0} [{1}] Rejected due to list exclusion", report.ForeignId, report.Title);
                 return;
             }
 
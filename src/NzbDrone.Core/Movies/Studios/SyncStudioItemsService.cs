4937d2a41891e0aa6dfcd84f23bb08788429e623
Tom-Estes <testes@rtfm.co>
2024-01-14 12:41:56 -0800

    Fixup: Studio sync items and rescan

--- src/NzbDrone.Core/Movies/Studios/SyncStudioItemsService.cs
+++ src/NzbDrone.Core/Movies/Studios/SyncStudioItemsService.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Linq;
 using NLog;
+using NzbDrone.Core.Configuration;
 using NzbDrone.Core.Exceptions;
 using NzbDrone.Core.ImportLists.ImportExclusions;
+using NzbDrone.Core.MediaFiles;
+using NzbDrone.Core.MediaFiles.Events;
 using NzbDrone.Core.Messaging.Commands;
+using NzbDrone.Core.Messaging.Events;
 using NzbDrone.Core.MetadataSource;
 using NzbDrone.Core.Movies.Commands;
 
@@ -15,6 +19,9 @@ namespace NzbDrone.Core.Movies.Studios
         private readonly IStudioService _studioService;
         private readonly IMovieService _movieService;
         private readonly IAddMovieService _addMovieService;
+        private readonly IConfigService _configService;
+        private readonly IDiskScanService _diskScanService;
+        private readonly IEventAggregator _eventAggregator;
         private readonly IImportExclusionsService _importExclusionService;
 
         private readonly Logger _logger;
@@ -23,6 +30,9 @@ namespace NzbDrone.Core.Movies.Studios
                                         IStudioService studioService,
                                         IMovieService movieService,
                                         IAddMovieService addMovieService,
+                                        IConfigService configService,
+                                        IDiskScanService diskScanService,
+                                        IEventAggregator eventAggregator,
                                         IImportExclusionsService importExclusionsService,
                                         Logger logger)
         {
@@ -30,6 +40,9 @@ namespace NzbDrone.Core.Movies.Studios
             _studioService = studioService;
             _movieService = movieService;
             _addMovieService = addMovieService;
+            _configService = configService;
+            _diskScanService = diskScanService;
+            _eventAggregator = eventAggregator;
             _importExclusionService = importExclusionsService;
             _logger = logger;
         }
@@ -62,6 +75,39 @@ namespace NzbDrone.Core.Movies.Studios
             }
         }
 
+        private void RescanMovie(Movie movie, bool isNew, CommandTrigger trigger)
+        {
+            var rescanAfterRefresh = _configService.RescanAfterRefresh;
+
+            if (isNew)
+            {
+                _logger.Trace("Forcing rescan of {0}. Reason: New movie", movie);
+            }
+            else if (rescanAfterRefresh == RescanAfterRefreshType.Never)
+            {
+                _logger.Trace("Skipping rescan of {0}. Reason: Never rescan after refresh", movie);
+                _eventAggregator.PublishEvent(new MovieScanSkippedEvent(movie, MovieScanSkippedReason.NeverRescanAfterRefresh));
+
+                return;
+            }
+            else if (rescanAfterRefresh == RescanAfterRefreshType.AfterManual && trigger != CommandTrigger.Manual)
+            {
+                _logger.Trace("Skipping rescan of {0}. Reason: Not after automatic scans", movie);
+                _eventAggregator.PublishEvent(new MovieScanSkippedEvent(movie, MovieScanSkippedReason.RescanAfterManualRefreshOnly));
+
+                return;
+            }
+
+            try
+            {
+                _diskScanService.Scan(movie);
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, "Couldn't rescan movie {0}", movie);
+            }
+        }
+
         public void Execute(SyncStudioItemsCommand message)
         {
             if (message.StudioIds.Any())
@@ -69,6 +115,17 @@ namespace NzbDrone.Core.Movies.Studios
                 foreach (var studioId in message.StudioIds)
                 {
                     var studio = _studioService.GetById(studioId);
+                    var items = _movieService.GetByStudioForeignId(studio.ForeignId);
+                    var trigger = message.Trigger;
+                    var isNew = false;
+
+                    // Rescan before sync since syncing will add a new movie and scan automatically
+                    foreach (var movieItem in items)
+                    {
+                        var movie = _movieService.GetMovie(movieItem.Id);
+                        RescanMovie(movie, isNew, trigger);
+                    }
+
                     SyncStudioItems(studio);
                 }
             }

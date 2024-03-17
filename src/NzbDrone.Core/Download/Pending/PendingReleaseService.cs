Sonarr/Sonarr

Fixed: Persist release source for pending releases

src/NzbDrone.Core/Download/Pending/PendingReleaseService.cs
@@ -388,7 +388,8 @@ namespace NzbDrone.Core.Download.Pending
                 Reason = reason,
                 AdditionalInfo = new PendingReleaseAdditionalInfo
                 {
-                    SeriesMatchType = decision.RemoteEpisode.SeriesMatchType
+                    SeriesMatchType = decision.RemoteEpisode.SeriesMatchType,
+                    ReleaseSource = decision.RemoteEpisode.ReleaseSource
                 }
             });
 

Whisparr/Whisparr

(cherry picked from commit 1d8cf6a7f50a095cd832a1ade9b20143ea95b110)

src/NzbDrone.Core/Download/Pending/PendingReleaseService.cs
@@ -326,7 +326,8 @@ namespace NzbDrone.Core.Download.Pending
                 Reason = reason,
                 AdditionalInfo = new PendingReleaseAdditionalInfo
                 {
-                    MovieMatchType = decision.RemoteMovie.MovieMatchType
+                    MovieMatchType = decision.RemoteMovie.MovieMatchType,
+                    ReleaseSource = decision.RemoteMovie.ReleaseSource
                 }
             };
 
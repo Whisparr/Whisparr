68249a79fec7d775c12442f4dd9c42cd3fce6116
Yurii <chivokram@gmail.com>
2024-03-17 10:33:06 +0200

    Use branded message title for Telegram nitifications
    (cherry picked from commit 1ee30290efd1dcb79afb0ca45b1f7d7f6010bd51)

--- src/NzbDrone.Core/Notifications/NotificationBase.cs
+++ src/NzbDrone.Core/Notifications/NotificationBase.cs
@@ -24,6 +24,7 @@ namespace NzbDrone.Core.Notifications
         protected const string MOVIE_GRABBED_TITLE_BRANDED = "Whisparr - " + MOVIE_GRABBED_TITLE;
         protected const string MOVIE_ADDED_TITLE_BRANDED = "Whisparr - " + MOVIE_ADDED_TITLE;
         protected const string MOVIE_DOWNLOADED_TITLE_BRANDED = "Whisparr - " + MOVIE_DOWNLOADED_TITLE;
+        protected const string MOVIE_UPGRADED_TITLE_BRANDED = "Whisparr - " + MOVIE_UPGRADED_TITLE;
         protected const string MOVIE_DELETED_TITLE_BRANDED = "Whisparr - " + MOVIE_DELETED_TITLE;
         protected const string MOVIE_FILE_DELETED_TITLE_BRANDED = "Whisparr - " + MOVIE_FILE_DELETED_TITLE;
         protected const string HEALTH_ISSUE_TITLE_BRANDED = "Whisparr - " + HEALTH_ISSUE_TITLE;

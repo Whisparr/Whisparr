4e98f2139638410b73dc2cf1ddb76cfc5ff909b0
Mark McDowall <mark@mcdowall.ca>
2024-04-28 09:25:42 -0700

    Forward X-Forwarded-Host header
    (cherry picked from commit 3fbe4361386e9fb8dafdf82ad9f00f02bec746cc)
    (cherry picked from commit 71e1003358ddbbe5ecdedc4ca3d6d0781a5188c9)

--- src/NzbDrone.Host/Startup.cs
+++ src/NzbDrone.Host/Startup.cs
@@ -60,7 +60,7 @@ namespace NzbDrone.Host
 
             services.Configure<ForwardedHeadersOptions>(options =>
             {
-                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost;
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost | ForwardedHeaders.XForwardedHost;
                 options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("10.0.0.0"), 8));
                 options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("172.16.0.0"), 12));
                 options.KnownNetworks.Add(new IPNetwork(IPAddress.Parse("192.168.0.0"), 16));

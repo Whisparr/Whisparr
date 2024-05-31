46397b0c715d6f578132d05d37609d00f88b9704
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-05-31 13:50:37 +0300

    Fixed: Manual Interaction Required with possible null movie
    Prevent a NullRef when the notification is sent due to an invalid movie title
    
    Fixes #10053
    
    (cherry picked from commit 0c654377f493fcb0f31f852efc8561330b1c52cb)

--- src/NzbDrone.Core/Notifications/NotificationService.cs
+++ src/NzbDrone.Core/Notifications/NotificationService.cs
@@ -261,7 +261,7 @@ namespace NzbDrone.Core.Notifications
 
         public void Handle(ManualInteractionRequiredEvent message)
         {
-            var movie = message.RemoteMovie.Movie;
+            var movie = message.RemoteMovie?.Movie;
             var mess = "";
 
             if (movie != null)
@@ -283,7 +283,7 @@ namespace NzbDrone.Core.Notifications
             {
                 Message = mess,
                 Movie = movie,
-                Quality = message.RemoteMovie.ParsedMovieInfo.Quality,
+                Quality = message.RemoteMovie?.ParsedMovieInfo.Quality,
                 RemoteMovie = message.RemoteMovie,
                 TrackedDownload = message.TrackedDownload,
                 DownloadClientInfo = message.TrackedDownload.DownloadItem?.DownloadClientInfo,

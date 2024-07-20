9bbfc32cc3440ec5fcd0bdc6e5f4877ad0643ff7
Mark McDowall <mark@mcdowall.ca>
2024-07-19 20:42:59 -0700

    Convert RelativeDateCell to TypeScript
    (cherry picked from commit 824ed0a36931ce7aae9aa544a7baf0738dae568c)
    (cherry picked from commit fa80608394cd37ca6930af443e413520282e74f2)

--- frontend/src/Movie/Index/Table/MovieIndexRow.tsx
+++ frontend/src/Movie/Index/Table/MovieIndexRow.tsx
@@ -210,6 +210,7 @@ function MovieIndexRow(props: MovieIndexRowProps) {
           );
         }
 
+
         if (name === 'releaseDate') {
           return (
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment

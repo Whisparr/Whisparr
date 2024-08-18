Radarr/Radarr

New: Add recycle bin path for deleted movies to webhook/custom script
(cherry picked from commit a71d40edba1388d67e4deefd8bfc354a7a83c6b1)

Closes #9674

src/NzbDrone.Core/MediaFiles/ScriptImportDecider.cs
@@ -170,9 +170,9 @@ namespace NzbDrone.Core.MediaFiles
 
             if (oldFiles.Any())
             {
-                environmentVariables.Add("Radarr_DeletedRelativePaths", string.Join("|", oldFiles.Select(e => e.RelativePath)));
-                environmentVariables.Add("Radarr_DeletedPaths", string.Join("|", oldFiles.Select(e => Path.Combine(movie.Path, e.RelativePath))));
-                environmentVariables.Add("Radarr_DeletedDateAdded", string.Join("|", oldFiles.Select(e => e.DateAdded)));
+                environmentVariables.Add("Radarr_DeletedRelativePaths", string.Join("|", oldFiles.Select(e => e.MovieFile.RelativePath)));
+                environmentVariables.Add("Radarr_DeletedPaths", string.Join("|", oldFiles.Select(e => Path.Combine(movie.Path, e.MovieFile.RelativePath))));
+                environmentVariables.Add("Radarr_DeletedDateAdded", string.Join("|", oldFiles.Select(e => e.MovieFile.DateAdded)));
             }
 
             _logger.Debug("Executing external script: {0}", _configService.ScriptImportPath);

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -143,9 +143,10 @@ namespace NzbDrone.Core.Notifications.CustomScript
 
             if (message.OldMovieFiles.Any())
             {
-                environmentVariables.Add("Radarr_DeletedRelativePaths", string.Join("|", message.OldMovieFiles.Select(e => e.RelativePath)));
-                environmentVariables.Add("Radarr_DeletedPaths", string.Join("|", message.OldMovieFiles.Select(e => Path.Combine(movie.Path, e.RelativePath))));
-                environmentVariables.Add("Radarr_DeletedDateAdded", string.Join("|", message.OldMovieFiles.Select(e => e.DateAdded)));
+                environmentVariables.Add("Radarr_DeletedRelativePaths", string.Join("|", message.OldMovieFiles.Select(e => e.MovieFile.RelativePath)));
+                environmentVariables.Add("Radarr_DeletedPaths", string.Join("|", message.OldMovieFiles.Select(e => Path.Combine(movie.Path, e.MovieFile.RelativePath))));
+                environmentVariables.Add("Radarr_DeletedDateAdded", string.Join("|", message.OldMovieFiles.Select(e => e.MovieFile.DateAdded)));
+                environmentVariables.Add("Radarr_DeletedRecycleBinPaths", string.Join("|", message.OldMovieFiles.Select(e => e.RecycleBinPath ?? string.Empty)));
             }
 
             ExecuteScript(environmentVariables);

Whisparr/Whisparr

(cherry picked from commit 99ab65f7902a133865400fbf4d03390ce11c0834)

src/NzbDrone.Core/MediaFiles/ScriptImportDecider.cs
@@ -169,9 +169,9 @@ namespace NzbDrone.Core.MediaFiles
 
             if (oldFiles.Any())
             {
-                environmentVariables.Add("Whisparr_DeletedRelativePaths", string.Join("|", oldFiles.Select(e => e.RelativePath)));
-                environmentVariables.Add("Whisparr_DeletedPaths", string.Join("|", oldFiles.Select(e => Path.Combine(movie.Path, e.RelativePath))));
-                environmentVariables.Add("Whisparr_DeletedDateAdded", string.Join("|", oldFiles.Select(e => e.DateAdded)));
+                environmentVariables.Add("Whisparr_DeletedRelativePaths", string.Join("|", oldFiles.Select(e => e.MovieFile.RelativePath)));
+                environmentVariables.Add("Whisparr_DeletedPaths", string.Join("|", oldFiles.Select(e => Path.Combine(movie.Path, e.MovieFile.RelativePath))));
+                environmentVariables.Add("Whisparr_DeletedDateAdded", string.Join("|", oldFiles.Select(e => e.MovieFile.DateAdded)));
             }
 
             _logger.Debug("Executing external script: {0}", _configService.ScriptImportPath);

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -141,9 +141,10 @@ namespace NzbDrone.Core.Notifications.CustomScript
 
             if (message.OldMovieFiles.Any())
             {
-                environmentVariables.Add("Whisparr_DeletedRelativePaths", string.Join("|", message.OldMovieFiles.Select(e => e.RelativePath)));
-                environmentVariables.Add("Whisparr_DeletedPaths", string.Join("|", message.OldMovieFiles.Select(e => Path.Combine(movie.Path, e.RelativePath))));
-                environmentVariables.Add("Whisparr_DeletedDateAdded", string.Join("|", message.OldMovieFiles.Select(e => e.DateAdded)));
+                environmentVariables.Add("Whisparr_DeletedRelativePaths", string.Join("|", message.OldMovieFiles.Select(e => e.MovieFile.RelativePath)));
+                environmentVariables.Add("Whisparr_DeletedPaths", string.Join("|", message.OldMovieFiles.Select(e => Path.Combine(movie.Path, e.MovieFile.RelativePath))));
+                environmentVariables.Add("Whisparr_DeletedDateAdded", string.Join("|", message.OldMovieFiles.Select(e => e.MovieFile.DateAdded)));
+                environmentVariables.Add("Whisparr_DeletedRecycleBinPaths", string.Join("|", message.OldMovieFiles.Select(e => e.RecycleBinPath ?? string.Empty)));
             }
 
             ExecuteScript(environmentVariables);
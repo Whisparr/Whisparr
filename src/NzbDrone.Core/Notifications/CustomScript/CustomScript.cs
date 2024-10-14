Radarr/Radarr

Fixed: Align DownloadClientInfo in ManualInteractionRequiredMessage with DownloadMessage
Also handle null ref exceptions when DownloadClientInfo is null in notification service

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -323,8 +323,8 @@ namespace NzbDrone.Core.Notifications.CustomScript
             environmentVariables.Add("Radarr_Movie_ImdbId", movie.MovieMetadata.Value.ImdbId ?? string.Empty);
             environmentVariables.Add("Radarr_Movie_TmdbId", movie.MovieMetadata.Value.TmdbId.ToString());
             environmentVariables.Add("Radarr_Movie_Overview", movie.MovieMetadata.Value.Overview);
-            environmentVariables.Add("Radarr_Download_Client", message.DownloadClientName ?? string.Empty);
-            environmentVariables.Add("Radarr_Download_Client_Type", message.DownloadClientType ?? string.Empty);
+            environmentVariables.Add("Radarr_Download_Client", message.DownloadClientInfo?.Name ?? string.Empty);
+            environmentVariables.Add("Radarr_Download_Client_Type", message.DownloadClientInfo?.Type ?? string.Empty);
             environmentVariables.Add("Radarr_Download_Id", message.DownloadId ?? string.Empty);
             environmentVariables.Add("Radarr_Download_Size", message.TrackedDownload.DownloadItem.TotalSize.ToString());
             environmentVariables.Add("Radarr_Download_Title", message.TrackedDownload.DownloadItem.Title);

Whisparr/Whisparr

(cherry picked from commit 173b1d6a4c0f2125c4413c0c09b269d87a1f1ee8)

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -320,8 +320,8 @@ namespace NzbDrone.Core.Notifications.CustomScript
             environmentVariables.Add("Whisparr_Movie_ImdbId", movie.MovieMetadata.Value.ImdbId ?? string.Empty);
             environmentVariables.Add("Whisparr_Movie_TmdbId", movie.MovieMetadata.Value.ForeignId.ToString());
             environmentVariables.Add("Whisparr_Movie_Overview", movie.MovieMetadata.Value.Overview);
-            environmentVariables.Add("Whisparr_Download_Client", message.DownloadClientName ?? string.Empty);
-            environmentVariables.Add("Whisparr_Download_Client_Type", message.DownloadClientType ?? string.Empty);
+            environmentVariables.Add("Whisparr_Download_Client", message.DownloadClientInfo?.Name ?? string.Empty);
+            environmentVariables.Add("Whisparr_Download_Client_Type", message.DownloadClientInfo?.Type ?? string.Empty);
             environmentVariables.Add("Whisparr_Download_Id", message.DownloadId ?? string.Empty);
             environmentVariables.Add("Whisparr_Download_Size", message.TrackedDownload.DownloadItem.TotalSize.ToString());
             environmentVariables.Add("Whisparr_Download_Title", message.TrackedDownload.DownloadItem.Title);
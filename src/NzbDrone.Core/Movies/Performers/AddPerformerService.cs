c3e7c7da72cb1df7c5bfdd5038d3c89eee8ca28e
sampulsar <80544409+sampulsar@users.noreply.github.com>
2024-11-11 05:10:59 +1030

    Merge Difference

--- src/NzbDrone.Core/Movies/Performers/AddPerformerService.cs
+++ src/NzbDrone.Core/Movies/Performers/AddPerformerService.cs
@@ -105,7 +105,7 @@ namespace NzbDrone.Core.Movies.Performers
                         throw;
                     }
 
-                    _logger.Error("StashId {0} was not added due to Exception. {1}", m.ForeignId, ex.Message);
+                    _logger.Error("{0} was not added due to validation failures. {1}", m.ForeignId, ex.Message);
                 }
             }
 

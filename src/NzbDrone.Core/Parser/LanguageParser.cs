d684bb673d4b0ffe0e9c0f8c4777de51438f1344
bpoxy <bejhan.jetha@gmail.com>
2024-11-27 16:00:33 -0700

    New: Add Albanian language (#10663)
    (cherry picked from commit 219477273675b23a426554896a145d553fc9a319)

--- src/NzbDrone.Core/Parser/LanguageParser.cs
+++ src/NzbDrone.Core/Parser/LanguageParser.cs
@@ -224,6 +224,11 @@ namespace NzbDrone.Core.Parser
                 languages.Add(Language.Kannada);
             }
 
+            if (lowerTitle.Contains("albanian"))
+            {
+                languages.Add(Language.Albanian);
+            }
+
             // Case sensitive
             var caseSensitiveMatches = CaseSensitiveLanguageRegex.Matches(title);
 

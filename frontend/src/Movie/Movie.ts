c4120d2febf96cc2f91b6a01dde47debf91d338e
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-03-07 13:53:50 +0200

    Convert Movie Formats/Status/CollectionLabel to TypeScript
    (cherry picked from commit 8ec60eb0a63e5c63fab012a7662f44b5e7b2f614)

--- frontend/src/Movie/Movie.ts
+++ frontend/src/Movie/Movie.ts
@@ -67,6 +67,7 @@ interface Movie extends ModelBase {
   images: Image[];
   movieFile: MovieFile;
   hasFile: boolean;
+  grabbed?: boolean;
   lastSearchTime?: string;
   isAvailable: boolean;
   isSaving?: boolean;

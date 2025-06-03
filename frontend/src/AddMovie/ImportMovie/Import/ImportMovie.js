ccd1de48792d5abbaa58d050629705e6527e69ea
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-06-03 15:22:20 +0300

    Fix translation token for root folders load error
    (cherry picked from commit radarr/radarr@22ef334de6)

--- frontend/src/AddMovie/ImportMovie/Import/ImportMovie.js
+++ frontend/src/AddMovie/ImportMovie/Import/ImportMovie.js
@@ -111,7 +111,7 @@ class ImportMovie extends Component {
           {
             !rootFoldersFetching && !!rootFoldersError ?
               <Alert kind={kinds.DANGER}>
-                {translate('UnableToLoadRootFolders')}
+                {translate('RootFoldersLoadError')}
               </Alert> :
               null
           }

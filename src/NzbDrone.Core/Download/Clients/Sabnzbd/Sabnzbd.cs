Sonarr/Sonarr

New: Support SABnzb's new format for sorters
Closes #6125

src/NzbDrone.Core/Download/Clients/Sabnzbd/Sabnzbd.cs
@@ -482,6 +482,16 @@ namespace NzbDrone.Core.Download.Clients.Sabnzbd
                 }
             }
 
+            // New in SABnzbd 4.1, but on older versions this will be empty and not apply
+            if (config.Sorters.Any(s => s.is_active && ContainsCategory(s.sort_cats, Settings.TvCategory)))
+            {
+                return new NzbDroneValidationFailure("TvCategory", "Disable TV Sorting")
+                {
+                    InfoLink = _proxy.GetBaseUrl(Settings, "config/sorting/"),
+                    DetailedDescription = "You must disable sorting for the category Sonarr uses to prevent import issues. Go to Sabnzbd to fix it."
+                };
+            }
+
             if (config.Misc.enable_tv_sorting && ContainsCategory(config.Misc.tv_categories, Settings.TvCategory))
             {
                 return new NzbDroneValidationFailure("TvCategory", "Disable TV Sorting")

Whisparr/Whisparr

(cherry picked from commit d484553b310af4257c841c37a503ef54650c1426)
Closes #9351
(cherry picked from commit 4c4073ce1c48874b3b6c3d22211d254a3f71522a)

src/NzbDrone.Core/Download/Clients/Sabnzbd/Sabnzbd.cs
@@ -503,6 +503,25 @@ namespace NzbDrone.Core.Download.Clients.Sabnzbd
                 };
             }
 
+            // New in SABnzbd 4.1, but on older versions this will be empty and not apply
+            if (config.Sorters.Any(s => s.is_active && ContainsCategory(s.sort_cats, Settings.MovieCategory)))
+            {
+                return new NzbDroneValidationFailure("MovieCategory", "Disable TV Sorting")
+                {
+                    InfoLink = _proxy.GetBaseUrl(Settings, "config/sorting/"),
+                    DetailedDescription = "You must disable sorting for the category Radarr uses to prevent import issues. Go to Sabnzbd to fix it."
+                };
+            }
+
+            if (config.Misc.enable_tv_sorting && ContainsCategory(config.Misc.tv_categories, Settings.MovieCategory))
+            {
+                return new NzbDroneValidationFailure("MovieCategory", _localizationService.GetLocalizedString("DownloadClientSabnzbdValidationEnableDisableTvSorting"))
+                {
+                    InfoLink = _proxy.GetBaseUrl(Settings, "config/sorting/"),
+                    DetailedDescription = _localizationService.GetLocalizedString("DownloadClientSabnzbdValidationEnableDisableTvSortingDetail")
+                };
+            }
+
             if (config.Misc.enable_tv_sorting && ContainsCategory(config.Misc.tv_categories, Settings.MovieCategory))
             {
                 return new NzbDroneValidationFailure("MovieCategory", _localizationService.GetLocalizedString("DownloadClientSabnzbdValidationEnableDisableTvSorting"))
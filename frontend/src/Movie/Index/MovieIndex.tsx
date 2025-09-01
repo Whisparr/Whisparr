d15c7b48c4ed1c2979339c382eddf6638e44300e
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2025-09-01 11:02:08 -0500

    Fixed: Add missing translation keys
    (cherry picked from commit radarr/radarr@81bbaf8946)

--- frontend/src/Movie/Index/MovieIndex.tsx
+++ frontend/src/Movie/Index/MovieIndex.tsx
@@ -275,7 +275,7 @@ const MovieIndex = withScrollPosition((props: MovieIndexProps) => {
             />
 
             <MovieIndexSelectAllButton
-              label="SelectAll"
+              label={translate('SelectAll')}
               isSelectMode={isSelectMode}
               overflowComponent={MovieIndexSelectAllMenuItem}
             />

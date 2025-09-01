05de0a08e82607fbc1b5eee4e54510937de22981
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2025-09-01 10:54:35 -0500

    New: UI Note that Filters are for movie properties only
    (cherry picked from commit radarr/radarr@8b4288fa18)

--- frontend/src/Components/Filter/CustomFilters/CustomFiltersModalContent.js
+++ frontend/src/Components/Filter/CustomFilters/CustomFiltersModalContent.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Alert from 'Components/Alert';
 import Button from 'Components/Link/Button';
 import ModalBody from 'Components/Modal/ModalBody';
 import ModalContent from 'Components/Modal/ModalContent';
@@ -56,6 +57,9 @@ function CustomFiltersModalContent(props) {
             {translate('AddCustomFilter')}
           </Button>
         </div>
+        <Alert type="info">
+          {translate('FilterMoviePropertiesOnlyNotFileWarning')}
+        </Alert>
       </ModalBody>
 
       <ModalFooter>

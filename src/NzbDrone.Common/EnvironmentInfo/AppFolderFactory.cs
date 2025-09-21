1ec201c914f02ba4c90fa013f3a522d6f9d4f829
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-09-21 18:43:40 +0300

    Improve error tracing in migrate app data folder
    (cherry picked from commit radarr/radarr@554a54b009)

--- src/NzbDrone.Common/EnvironmentInfo/AppFolderFactory.cs
+++ src/NzbDrone.Common/EnvironmentInfo/AppFolderFactory.cs
@@ -122,7 +122,7 @@ namespace NzbDrone.Common.EnvironmentInfo
             catch (Exception ex)
             {
                 _logger.Debug(ex, ex.Message);
-                throw new WhisparrStartupException("Unable to migrate DB from nzbdrone.db to {0}. Migrate manually", _appFolderInfo.GetDatabase());
+                throw new WhisparrStartupException(ex, "Unable to migrate DB from nzbdrone.db to {0}. Migrate manually", _appFolderInfo.GetDatabase());
             }
         }
 

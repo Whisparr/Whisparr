82c85c8b290a601af0dfaa86d07c3b0f9537aed9
plz12345 <132735020+plz12345@users.noreply.github.com>
2025-11-09 12:15:50 -0500

    Update name for errors with metadata API
    (cherry picked from commit 94287d942780faf4303b703f5e364c8723797387)

--- src/NzbDrone.Core/MetadataSource/SkyHook/SkyHookProxy.cs
+++ src/NzbDrone.Core/MetadataSource/SkyHook/SkyHookProxy.cs
@@ -720,17 +720,17 @@ namespace NzbDrone.Core.MetadataSource.SkyHook
             catch (HttpException ex)
             {
                 _logger.Warn(ex);
-                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with TMDb.", ex, title);
+                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with WhisparrAPI.", ex, title);
             }
             catch (WebException ex)
             {
                 _logger.Warn(ex);
-                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with TMDb.", ex, title, ex.Message);
+                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with WhisparrAPI.", ex, title, ex.Message);
             }
             catch (Exception ex)
             {
                 _logger.Warn(ex);
-                throw new SkyHookException("Search for '{0}' failed. Invalid response received from TMDb.", ex, title);
+                throw new SkyHookException("Search for '{0}' failed. Invalid response received from WhisparrAPI.", ex, title);
             }
         }
 

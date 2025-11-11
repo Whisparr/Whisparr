Radarr/Radarr

Fixed: Initialize databases after app folder migrations

src/NzbDrone.Host/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using DryIoc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
@@ -27,6 +28,7 @@ using NzbDrone.SignalR;
 using Radarr.Api.V3.System;
 using Radarr.Http;
 using Radarr.Http.Authentication;
+using Radarr.Http.ClientSchema;
 using Radarr.Http.ErrorManagement;
 using Radarr.Http.Frontend;
 using Radarr.Http.Middleware;
@@ -209,6 +211,7 @@ namespace NzbDrone.Host
         }
 
         public void Configure(IApplicationBuilder app,
+                              IContainer container,
                               IStartupContext startupContext,
                               Lazy<IMainDatabase> mainDatabaseFactory,
                               Lazy<ILogDatabase> logDatabaseFactory,
@@ -239,6 +242,7 @@ namespace NzbDrone.Host
             _ = logDatabaseFactory.Value;
 
             dbTarget.Register();
+            SchemaBuilder.Initialize(container);
 
             if (OsInfo.IsNotWindows)
             {

Whisparr/Whisparr

(cherry picked from commit 711637c448f8f42d69785ea69dd7b5e29a38e952)

src/NzbDrone.Host/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using DryIoc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
@@ -28,6 +29,7 @@ using NzbDrone.SignalR;
 using Whisparr.Api.V3.System;
 using Whisparr.Http;
 using Whisparr.Http.Authentication;
+using Whisparr.Http.ClientSchema;
 using Whisparr.Http.ErrorManagement;
 using Whisparr.Http.Frontend;
 using Whisparr.Http.Middleware;
@@ -213,6 +215,7 @@ namespace NzbDrone.Host
         }
 
         public void Configure(IApplicationBuilder app,
+                              IContainer container,
                               IStartupContext startupContext,
                               Lazy<IMainDatabase> mainDatabaseFactory,
                               Lazy<ILogDatabase> logDatabaseFactory,
@@ -243,6 +246,7 @@ namespace NzbDrone.Host
             _ = logDatabaseFactory.Value;
 
             dbTarget.Register();
+            SchemaBuilder.Initialize(container);
 
             if (OsInfo.IsNotWindows)
             {
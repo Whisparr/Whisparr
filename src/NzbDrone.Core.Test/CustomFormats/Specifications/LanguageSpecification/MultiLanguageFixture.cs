Sonarr/Sonarr

New: Improve multi-language negate Custom Format
Closes #6408

src/NzbDrone.Core.Test/CustomFormats/Specifications/LanguageSpecification/MultiLanguageFixture.cs
@@ -1,0 +1,71 @@
+using System.Collections.Generic;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.CustomFormats;
+using NzbDrone.Core.Languages;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Core.Tv;
+
+namespace NzbDrone.Core.Test.CustomFormats.Specifications.LanguageSpecification
+{
+    [TestFixture]
+    public class MultiLanguageFixture : CoreTest<Core.CustomFormats.LanguageSpecification>
+    {
+        private CustomFormatInput _input;
+
+        [SetUp]
+        public void Setup()
+        {
+            _input = new CustomFormatInput
+            {
+                EpisodeInfo = Builder<ParsedEpisodeInfo>.CreateNew().Build(),
+                Series = Builder<Series>.CreateNew().With(s => s.OriginalLanguage = Language.English).Build(),
+                Size = 100.Megabytes(),
+                Languages = new List<Language>
+                {
+                    Language.English,
+                    Language.French
+                },
+                Filename = "Series.Title.S01E01"
+            };
+        }
+
+        [Test]
+        public void should_match_one_language()
+        {
+            Subject.Value = Language.French.Id;
+            Subject.Negate = false;
+
+            Subject.IsSatisfiedBy(_input).Should().BeTrue();
+        }
+
+        [Test]
+        public void should_not_match_different_language()
+        {
+            Subject.Value = Language.Spanish.Id;
+            Subject.Negate = false;
+
+            Subject.IsSatisfiedBy(_input).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_match_negated_when_one_language_matches()
+        {
+            Subject.Value = Language.French.Id;
+            Subject.Negate = true;
+
+            Subject.IsSatisfiedBy(_input).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_match_negated_when_all_languages_do_not_match()
+        {
+            Subject.Value = Language.Spanish.Id;
+            Subject.Negate = true;
+
+            Subject.IsSatisfiedBy(_input).Should().BeTrue();
+        }
+    }
+}

src/NzbDrone.Core.Test/CustomFormats/Specifications/LanguageSpecification/OriginalLanguageFixture.cs
@@ -1,0 +1,80 @@
+using System.Collections.Generic;
+using System.Linq;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.CustomFormats;
+using NzbDrone.Core.Languages;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Core.Tv;
+
+namespace NzbDrone.Core.Test.CustomFormats.Specifications.LanguageSpecification
+{
+    [TestFixture]
+    public class OriginalLanguageFixture : CoreTest<Core.CustomFormats.LanguageSpecification>
+    {
+        private CustomFormatInput _input;
+
+        [SetUp]
+        public void Setup()
+        {
+            _input = new CustomFormatInput
+            {
+                EpisodeInfo = Builder<ParsedEpisodeInfo>.CreateNew().Build(),
+                Series = Builder<Series>.CreateNew().With(s => s.OriginalLanguage = Language.English).Build(),
+                Size = 100.Megabytes(),
+                Languages = new List<Language>
+                {
+                    Language.French
+                },
+                Filename = "Series.Title.S01E01"
+            };
+        }
+
+        public void GivenLanguages(params Language[] languages)
+        {
+            _input.Languages = languages.ToList();
+        }
+
+        [Test]
+        public void should_match_same_single_language()
+        {
+            GivenLanguages(Language.English);
+
+            Subject.Value = Language.Original.Id;
+            Subject.Negate = false;
+
+            Subject.IsSatisfiedBy(_input).Should().BeTrue();
+        }
+
+        [Test]
+        public void should_not_match_different_single_language()
+        {
+            Subject.Value = Language.Original.Id;
+            Subject.Negate = false;
+
+            Subject.IsSatisfiedBy(_input).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_match_negated_same_single_language()
+        {
+            GivenLanguages(Language.English);
+
+            Subject.Value = Language.Original.Id;
+            Subject.Negate = true;
+
+            Subject.IsSatisfiedBy(_input).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_match_negated_different_single_language()
+        {
+            Subject.Value = Language.Original.Id;
+            Subject.Negate = true;
+
+            Subject.IsSatisfiedBy(_input).Should().BeTrue();
+        }
+    }
+}

src/NzbDrone.Core.Test/CustomFormats/Specifications/LanguageSpecification/SingleLanguageFixture.cs
@@ -1,0 +1,70 @@
+using System.Collections.Generic;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.CustomFormats;
+using NzbDrone.Core.Languages;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Core.Tv;
+
+namespace NzbDrone.Core.Test.CustomFormats.Specifications.LanguageSpecification
+{
+    [TestFixture]
+    public class SingleLanguageFixture : CoreTest<Core.CustomFormats.LanguageSpecification>
+    {
+        private CustomFormatInput _input;
+
+        [SetUp]
+        public void Setup()
+        {
+            _input = new CustomFormatInput
+            {
+                EpisodeInfo = Builder<ParsedEpisodeInfo>.CreateNew().Build(),
+                Series = Builder<Series>.CreateNew().With(s => s.OriginalLanguage = Language.English).Build(),
+                Size = 100.Megabytes(),
+                Languages = new List<Language>
+                {
+                    Language.French
+                },
+                Filename = "Series.Title.S01E01"
+            };
+        }
+
+        [Test]
+        public void should_match_same_language()
+        {
+            Subject.Value = Language.French.Id;
+            Subject.Negate = false;
+
+            Subject.IsSatisfiedBy(_input).Should().BeTrue();
+        }
+
+        [Test]
+        public void should_not_match_different_language()
+        {
+            Subject.Value = Language.Spanish.Id;
+            Subject.Negate = false;
+
+            Subject.IsSatisfiedBy(_input).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_match_negated_same_language()
+        {
+            Subject.Value = Language.French.Id;
+            Subject.Negate = true;
+
+            Subject.IsSatisfiedBy(_input).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_match_negated_different_language()
+        {
+            Subject.Value = Language.Spanish.Id;
+            Subject.Negate = true;
+
+            Subject.IsSatisfiedBy(_input).Should().BeTrue();
+        }
+    }
+}

Whisparr/Whisparr

(cherry picked from commit 42b11528b4699b8343887185c93a02b139192d83)
Closes #9720
(cherry picked from commit 3191a883dc1fce380b54d28a6ecbe84c022465fa)

src/NzbDrone.Core.Test/CustomFormats/Specifications/LanguageSpecification/MultiLanguageFixture.cs
@@ -1,0 +1,71 @@
+using System.Collections.Generic;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.CustomFormats;
+using NzbDrone.Core.Languages;
+using NzbDrone.Core.Movies;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.CustomFormats.Specifications.LanguageSpecification
+{
+    [TestFixture]
+    public class MultiLanguageFixture : CoreTest<Core.CustomFormats.LanguageSpecification>
+    {
+        private CustomFormatInput _input;
+
+        [SetUp]
+        public void Setup()
+        {
+            _input = new CustomFormatInput
+            {
+                MovieInfo = Builder<ParsedMovieInfo>.CreateNew().Build(),
+                Movie = Builder<Movie>.CreateNew().With(m => m.MovieMetadata.Value.OriginalLanguage = Language.English).Build(),
+                Size = 100.Megabytes(),
+                Languages = new List<Language>
+                {
+                    Language.English,
+                    Language.French
+                },
+                Filename = "Movie.Title.2024"
+            };
+        }
+
+        [Test]
+        public void should_match_one_language()
+        {
+            Subject.Value = Language.French.Id;
+            Subject.Negate = false;
+
+            Subject.IsSatisfiedBy(_input).Should().BeTrue();
+        }
+
+        [Test]
+        public void should_not_match_different_language()
+        {
+            Subject.Value = Language.Spanish.Id;
+            Subject.Negate = false;
+
+            Subject.IsSatisfiedBy(_input).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_match_negated_when_one_language_matches()
+        {
+            Subject.Value = Language.French.Id;
+            Subject.Negate = true;
+
+            Subject.IsSatisfiedBy(_input).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_match_negated_when_all_languages_do_not_match()
+        {
+            Subject.Value = Language.Spanish.Id;
+            Subject.Negate = true;
+
+            Subject.IsSatisfiedBy(_input).Should().BeTrue();
+        }
+    }
+}

src/NzbDrone.Core.Test/CustomFormats/Specifications/LanguageSpecification/OriginalLanguageFixture.cs
@@ -1,0 +1,80 @@
+using System.Collections.Generic;
+using System.Linq;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.CustomFormats;
+using NzbDrone.Core.Languages;
+using NzbDrone.Core.Movies;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.CustomFormats.Specifications.LanguageSpecification
+{
+    [TestFixture]
+    public class OriginalLanguageFixture : CoreTest<Core.CustomFormats.LanguageSpecification>
+    {
+        private CustomFormatInput _input;
+
+        [SetUp]
+        public void Setup()
+        {
+            _input = new CustomFormatInput
+            {
+                MovieInfo = Builder<ParsedMovieInfo>.CreateNew().Build(),
+                Movie = Builder<Movie>.CreateNew().With(m => m.MovieMetadata.Value.OriginalLanguage = Language.English).Build(),
+                Size = 100.Megabytes(),
+                Languages = new List<Language>
+                {
+                    Language.French
+                },
+                Filename = "Movie.Title.2024"
+            };
+        }
+
+        public void GivenLanguages(params Language[] languages)
+        {
+            _input.Languages = languages.ToList();
+        }
+
+        [Test]
+        public void should_match_same_single_language()
+        {
+            GivenLanguages(Language.English);
+
+            Subject.Value = Language.Original.Id;
+            Subject.Negate = false;
+
+            Subject.IsSatisfiedBy(_input).Should().BeTrue();
+        }
+
+        [Test]
+        public void should_not_match_different_single_language()
+        {
+            Subject.Value = Language.Original.Id;
+            Subject.Negate = false;
+
+            Subject.IsSatisfiedBy(_input).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_match_negated_same_single_language()
+        {
+            GivenLanguages(Language.English);
+
+            Subject.Value = Language.Original.Id;
+            Subject.Negate = true;
+
+            Subject.IsSatisfiedBy(_input).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_match_negated_different_single_language()
+        {
+            Subject.Value = Language.Original.Id;
+            Subject.Negate = true;
+
+            Subject.IsSatisfiedBy(_input).Should().BeTrue();
+        }
+    }
+}

src/NzbDrone.Core.Test/CustomFormats/Specifications/LanguageSpecification/SingleLanguageFixture.cs
@@ -1,0 +1,70 @@
+using System.Collections.Generic;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.CustomFormats;
+using NzbDrone.Core.Languages;
+using NzbDrone.Core.Movies;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.CustomFormats.Specifications.LanguageSpecification
+{
+    [TestFixture]
+    public class SingleLanguageFixture : CoreTest<Core.CustomFormats.LanguageSpecification>
+    {
+        private CustomFormatInput _input;
+
+        [SetUp]
+        public void Setup()
+        {
+            _input = new CustomFormatInput
+            {
+                MovieInfo = Builder<ParsedMovieInfo>.CreateNew().Build(),
+                Movie = Builder<Movie>.CreateNew().With(m => m.MovieMetadata.Value.OriginalLanguage = Language.English).Build(),
+                Size = 100.Megabytes(),
+                Languages = new List<Language>
+                {
+                    Language.French
+                },
+                Filename = "Movie.Title.2024"
+            };
+        }
+
+        [Test]
+        public void should_match_same_language()
+        {
+            Subject.Value = Language.French.Id;
+            Subject.Negate = false;
+
+            Subject.IsSatisfiedBy(_input).Should().BeTrue();
+        }
+
+        [Test]
+        public void should_not_match_different_language()
+        {
+            Subject.Value = Language.Spanish.Id;
+            Subject.Negate = false;
+
+            Subject.IsSatisfiedBy(_input).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_not_match_negated_same_language()
+        {
+            Subject.Value = Language.French.Id;
+            Subject.Negate = true;
+
+            Subject.IsSatisfiedBy(_input).Should().BeFalse();
+        }
+
+        [Test]
+        public void should_match_negated_different_language()
+        {
+            Subject.Value = Language.Spanish.Id;
+            Subject.Negate = true;
+
+            Subject.IsSatisfiedBy(_input).Should().BeTrue();
+        }
+    }
+}
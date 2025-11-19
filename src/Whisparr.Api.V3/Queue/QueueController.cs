Sonarr/Sonarr

Fixed: Removing pending release without blocklisting

src/Sonarr.Api.V3/Queue/QueueController.cs
@@ -77,7 +77,7 @@ namespace Sonarr.Api.V3.Queue
 
             if (pendingRelease != null)
             {
-                Remove(pendingRelease);
+                Remove(pendingRelease, blocklist);
 
                 return;
             }
@@ -120,7 +120,7 @@ namespace Sonarr.Api.V3.Queue
 
             foreach (var pendingRelease in pendingToRemove.DistinctBy(p => p.Id))
             {
-                Remove(pendingRelease);
+                Remove(pendingRelease, blocklist);
             }
 
             foreach (var trackedDownload in trackedToRemove.DistinctBy(t => t.DownloadItem.DownloadId))
@@ -286,9 +286,13 @@ namespace Sonarr.Api.V3.Queue
             }
         }
 
-        private void Remove(NzbDrone.Core.Queue.Queue pendingRelease)
+        private void Remove(NzbDrone.Core.Queue.Queue pendingRelease, bool blocklist)
         {
-            _blocklistService.Block(pendingRelease.RemoteEpisode, "Pending release manually blocklisted");
+            if (blocklist)
+            {
+                _blocklistService.Block(pendingRelease.RemoteEpisode, "Pending release manually blocklisted");
+            }
+
             _pendingReleaseService.RemovePendingQueueItems(pendingRelease.Id);
         }
 

Whisparr/Whisparr

(cherry picked from commit 85b310c81cb0fb9a5cad50068aa71e19dc88f349)

src/Whisparr.Api.V3/Queue/QueueController.cs
@@ -72,7 +72,7 @@ namespace Whisparr.Api.V3.Queue
 
             if (pendingRelease != null)
             {
-                Remove(pendingRelease);
+                Remove(pendingRelease, blocklist);
 
                 return;
             }
@@ -115,7 +115,7 @@ namespace Whisparr.Api.V3.Queue
 
             foreach (var pendingRelease in pendingToRemove.DistinctBy(p => p.Id))
             {
-                Remove(pendingRelease);
+                Remove(pendingRelease, blocklist);
             }
 
             foreach (var trackedDownload in trackedToRemove.DistinctBy(t => t.DownloadItem.DownloadId))
@@ -274,9 +274,13 @@ namespace Whisparr.Api.V3.Queue
             }
         }
 
-        private void Remove(NzbDrone.Core.Queue.Queue pendingRelease)
+        private void Remove(NzbDrone.Core.Queue.Queue pendingRelease, bool blocklist)
         {
-            _blocklistService.Block(pendingRelease.RemoteMovie, "Pending release manually blocklisted");
+            if (blocklist)
+            {
+                _blocklistService.Block(pendingRelease.RemoteMovie, "Pending release manually blocklisted");
+            }
+
             _pendingReleaseService.RemovePendingQueueItems(pendingRelease.Id);
         }
 
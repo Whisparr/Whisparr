Radarr/Radarr

Rename ImportExclusion to ImportListExclusion

src/NzbDrone.Core.Test/ImportListTests/ImportListSyncServiceFixture.cs
@@ -76,9 +76,9 @@ namespace NzbDrone.Core.Test.ImportList
                   .Setup(v => v.Enabled(It.IsAny<bool>()))
                   .Returns(_importLists);
 
-            Mocker.GetMock<IImportExclusionsService>()
-                  .Setup(v => v.GetAllExclusions())
-                  .Returns(new List<ImportExclusion>());
+            Mocker.GetMock<IImportListExclusionService>()
+                  .Setup(v => v.All())
+                  .Returns(new List<ImportListExclusion>());
 
             Mocker.GetMock<IMovieService>()
                   .Setup(v => v.MovieExists(It.IsAny<Movie>()))
@@ -167,8 +167,8 @@ namespace NzbDrone.Core.Test.ImportList
             Mocker.GetMock<IMovieService>()
                   .Verify(v => v.UpdateMovie(new List<Movie>(), true), Times.Never());
 
-            Mocker.GetMock<IImportExclusionsService>()
-                  .Verify(v => v.GetAllExclusions(), Times.Never);
+            Mocker.GetMock<IImportListExclusionService>()
+                  .Verify(v => v.All(), Times.Never);
         }
 
         [Test]
@@ -427,9 +427,9 @@ namespace NzbDrone.Core.Test.ImportList
 
             GivenCleanLevel("disabled");
 
-            Mocker.GetMock<IImportExclusionsService>()
-                  .Setup(v => v.GetAllExclusions())
-                  .Returns(new List<ImportExclusion> { new ImportExclusion { TmdbId = _existingMovies[0].TmdbId } });
+            Mocker.GetMock<IImportListExclusionService>()
+                  .Setup(v => v.All())
+                  .Returns(new List<ImportListExclusion> { new ImportListExclusion { TmdbId = _existingMovies[0].TmdbId } });
 
             Subject.Execute(_commandAll);
 

src/Radarr.Api.V3/ImportLists/ImportListMoviesController.cs
@@ -25,7 +25,7 @@ namespace Radarr.Api.V3.ImportLists
         private readonly IBuildFileNames _fileNameBuilder;
         private readonly IImportListMovieService _listMovieService;
         private readonly IImportListFactory _importListFactory;
-        private readonly IImportExclusionsService _importExclusionService;
+        private readonly IImportListExclusionService _importListExclusionService;
         private readonly INamingConfigService _namingService;
         private readonly IMovieTranslationService _movieTranslationService;
         private readonly IConfigService _configService;
@@ -36,7 +36,7 @@ namespace Radarr.Api.V3.ImportLists
                                     IBuildFileNames fileNameBuilder,
                                     IImportListMovieService listMovieService,
                                     IImportListFactory importListFactory,
-                                    IImportExclusionsService importExclusionsService,
+                                    IImportListExclusionService importListExclusionService,
                                     INamingConfigService namingService,
                                     IMovieTranslationService movieTranslationService,
                                     IConfigService configService)
@@ -47,7 +47,7 @@ namespace Radarr.Api.V3.ImportLists
             _fileNameBuilder = fileNameBuilder;
             _listMovieService = listMovieService;
             _importListFactory = importListFactory;
-            _importExclusionService = importExclusionsService;
+            _importListExclusionService = importListExclusionService;
             _namingService = namingService;
             _movieTranslationService = movieTranslationService;
             _configService = configService;
@@ -59,7 +59,7 @@ namespace Radarr.Api.V3.ImportLists
             var movieLanguage = (Language)_configService.MovieInfoLanguage;
 
             var realResults = new List<ImportListMoviesResource>();
-            var listExclusions = _importExclusionService.GetAllExclusions();
+            var listExclusions = _importListExclusionService.All();
             var existingTmdbIds = _movieService.AllMovieTmdbIds();
 
             if (includeRecommendations)

Whisparr/Whisparr

(cherry picked from commit b1a7652753220bb7877a81d83b53ebe04c927397)

src/NzbDrone.Core.Test/ImportListTests/ImportListSyncServiceFixture.cs
@@ -76,9 +76,9 @@ namespace NzbDrone.Core.Test.ImportList
                         .Setup(v => v.Enabled(It.IsAny<bool>()))
                         .Returns(_importLists);
 
-                  Mocker.GetMock<IImportExclusionsService>()
+                  Mocker.GetMock<IImportListExclusionService>()
                         .Setup(v => v.GetAllExclusions())
-                        .Returns(new List<ImportExclusion>());
+                        .Returns(new List<ImportListExclusion>());
 
                   Mocker.GetMock<IMovieService>()
                         .Setup(v => v.MovieExists(It.IsAny<Movie>()))
@@ -167,7 +167,7 @@ namespace NzbDrone.Core.Test.ImportList
                   Mocker.GetMock<IMovieService>()
                         .Verify(v => v.UpdateMovie(new List<Movie>(), true), Times.Never());
 
-                  Mocker.GetMock<IImportExclusionsService>()
+                  Mocker.GetMock<IImportListExclusionService>()
                         .Verify(v => v.GetAllExclusions(), Times.Never);
             }
 
@@ -403,9 +403,9 @@ namespace NzbDrone.Core.Test.ImportList
 
                   GivenCleanLevel("disabled");
 
-                  Mocker.GetMock<IImportExclusionsService>()
+                  Mocker.GetMock<IImportListExclusionService>()
                         .Setup(v => v.GetAllExclusions())
-                        .Returns(new List<ImportExclusion> { new ImportExclusion { ForeignId = _existingMovies[0].ForeignId } });
+                        .Returns(new List<ImportListExclusion> { new ImportListExclusion { ForeignId = _existingMovies[0].ForeignId } });
 
                   Subject.Execute(_commandAll);
 

src/Whisparr.Api.V3/ImportLists/ImportListMoviesController.cs
@@ -23,7 +23,7 @@ namespace Whisparr.Api.V3.ImportLists
         private readonly IBuildFileNames _fileNameBuilder;
         private readonly IImportListMovieService _listMovieService;
         private readonly IImportListFactory _importListFactory;
-        private readonly IImportExclusionsService _importExclusionService;
+        private readonly IImportListExclusionService _importListExclusionService;
         private readonly INamingConfigService _namingService;
         private readonly IConfigService _configService;
 
@@ -33,7 +33,7 @@ namespace Whisparr.Api.V3.ImportLists
                                     IBuildFileNames fileNameBuilder,
                                     IImportListMovieService listMovieService,
                                     IImportListFactory importListFactory,
-                                    IImportExclusionsService importExclusionsService,
+                                    IImportListExclusionService importListExclusionService,
                                     INamingConfigService namingService,
                                     IConfigService configService)
         {
@@ -43,7 +43,7 @@ namespace Whisparr.Api.V3.ImportLists
             _fileNameBuilder = fileNameBuilder;
             _listMovieService = listMovieService;
             _importListFactory = importListFactory;
-            _importExclusionService = importExclusionsService;
+            _importListExclusionService = importListExclusionService;
             _namingService = namingService;
             _configService = configService;
         }
@@ -52,7 +52,7 @@ namespace Whisparr.Api.V3.ImportLists
         public object GetDiscoverMovies()
         {
             var realResults = new List<ImportListMoviesResource>();
-            var listExclusions = _importExclusionService.GetAllExclusions();
+            var listExclusions = _importListExclusionService.GetAllExclusions();
             var existingTmdbIds = _movieService.AllMovieTmdbIds();
 
             var listMovies = MapToResource(_listMovieService.GetAllForLists(_importListFactory.Enabled().Select(x => x.Definition.Id).ToList())).ToList();
@@ -100,7 +100,7 @@ namespace Whisparr.Api.V3.ImportLists
                     resource.RemotePoster = poster.RemoteUrl;
                 }
 
-                resource.Title =  resource.Title;
+                resource.Title = resource.Title;
                 resource.Overview = resource.Overview;
                 resource.Folder = _fileNameBuilder.GetMovieFolder(currentMovie, namingConfig);
 
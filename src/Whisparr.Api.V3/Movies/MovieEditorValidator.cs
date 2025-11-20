Radarr/Radarr

Validation for bulk movies editor

src/Radarr.Api.V3/Movies/MovieEditorValidator.cs
@@ -1,0 +1,23 @@
+using FluentValidation;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Movies;
+using NzbDrone.Core.Validation;
+using NzbDrone.Core.Validation.Paths;
+
+namespace Radarr.Api.V3.Movies
+{
+    public class MovieEditorValidator : AbstractValidator<Movie>
+    {
+        public MovieEditorValidator(RootFolderExistsValidator rootFolderExistsValidator, QualityProfileExistsValidator qualityProfileExistsValidator)
+        {
+            RuleFor(s => s.RootFolderPath).Cascade(CascadeMode.Stop)
+                .IsValidPath()
+                .SetValidator(rootFolderExistsValidator)
+                .When(s => s.RootFolderPath.IsNotNullOrWhiteSpace());
+
+            RuleFor(c => c.QualityProfileId).Cascade(CascadeMode.Stop)
+                .ValidId()
+                .SetValidator(qualityProfileExistsValidator);
+        }
+    }
+}

Whisparr/Whisparr

(cherry picked from commit 68c20713e50635c9f292a68c09ef85168d05aa53)

src/Whisparr.Api.V3/Movies/MovieEditorValidator.cs
@@ -1,0 +1,23 @@
+using FluentValidation;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Movies;
+using NzbDrone.Core.Validation;
+using NzbDrone.Core.Validation.Paths;
+
+namespace Whisparr.Api.V3.Movies
+{
+    public class MovieEditorValidator : AbstractValidator<Movie>
+    {
+        public MovieEditorValidator(RootFolderExistsValidator rootFolderExistsValidator, QualityProfileExistsValidator qualityProfileExistsValidator)
+        {
+            RuleFor(s => s.RootFolderPath).Cascade(CascadeMode.Stop)
+                .IsValidPath()
+                .SetValidator(rootFolderExistsValidator)
+                .When(s => s.RootFolderPath.IsNotNullOrWhiteSpace());
+
+            RuleFor(c => c.QualityProfileId).Cascade(CascadeMode.Stop)
+                .ValidId()
+                .SetValidator(qualityProfileExistsValidator);
+        }
+    }
+}
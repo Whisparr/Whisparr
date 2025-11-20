Sonarr/Sonarr

New: Use natural sorting for lists of items in the UI
Closes #6955

frontend/src/System/Tasks/Queued/QueuedTaskRowNameCell.tsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { CommandBody } from 'Commands/Command';
 import TableRowCell from 'Components/Table/Cells/TableRowCell';
 import createMultiSeriesSelector from 'Store/Selectors/createMultiSeriesSelector';
+import sortByProp from 'Utilities/Array/sortByProp';
 import translate from 'Utilities/String/translate';
 import styles from './QueuedTaskRowNameCell.css';
 
@@ -39,9 +40,7 @@ export default function QueuedTaskRowNameCell(
   }
 
   const series = useSelector(createMultiSeriesSelector(seriesIds));
-  const sortedSeries = series.sort((a, b) =>
-    a.sortTitle.localeCompare(b.sortTitle)
-  );
+  const sortedSeries = series.sort(sortByProp('sortTitle'));
 
   return (
     <TableRowCell>

Whisparr/Whisparr

(cherry picked from commit 1a1c8e6c08a6db5fcd2b5d17e65fa1f943d2e746)
Closes #10177
(cherry picked from commit f2af7a1b727c5be24e3fda1b58e6ba2c0153b658)

frontend/src/System/Tasks/Queued/QueuedTaskRowNameCell.tsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { CommandBody } from 'Commands/Command';
 import TableRowCell from 'Components/Table/Cells/TableRowCell';
 import createMultiMoviesSelector from 'Store/Selectors/createMultiMoviesSelector';
+import sortByProp from 'Utilities/Array/sortByProp';
 import translate from 'Utilities/String/translate';
 import styles from './QueuedTaskRowNameCell.css';
 
@@ -23,9 +24,7 @@ export default function QueuedTaskRowNameCell(
   }
 
   const movies = useSelector(createMultiMoviesSelector(movieIds));
-  const sortedMovies = movies.sort((a, b) =>
-    a.sortTitle.localeCompare(b.sortTitle)
-  );
+  const sortedMovies = movies.sort(sortByProp('sortTitle'));
 
   return (
     <TableRowCell>
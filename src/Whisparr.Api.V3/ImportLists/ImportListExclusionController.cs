Radarr/Radarr

Fixed: Validate uniqueness for import list exclusions

src/Radarr.Api.V3/ImportLists/ImportListExclusionController.cs
@@ -16,11 +16,15 @@ namespace Radarr.Api.V3.ImportLists
     {
         private readonly IImportListExclusionService _importListExclusionService;
 
-        public ImportListExclusionController(IImportListExclusionService importListExclusionService)
+        public ImportListExclusionController(IImportListExclusionService importListExclusionService,
+                                             ImportListExclusionExistsValidator importListExclusionExistsValidator)
         {
             _importListExclusionService = importListExclusionService;
 
-            SharedValidator.RuleFor(c => c.TmdbId).GreaterThan(0);
+            SharedValidator.RuleFor(c => c.TmdbId).Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .SetValidator(importListExclusionExistsValidator);
+
             SharedValidator.RuleFor(c => c.MovieTitle).NotEmpty();
             SharedValidator.RuleFor(c => c.MovieYear).GreaterThan(0);
         }

Whisparr/Whisparr

(cherry picked from commit 3be2c6b0bea9e6e9e890a4699f4594ab7697797b)

src/Whisparr.Api.V3/ImportLists/ImportListExclusionController.cs
@@ -15,11 +15,15 @@ namespace Whisparr.Api.V3.ImportLists
     {
         private readonly IImportListExclusionService _importListExclusionService;
 
-        public ImportListExclusionController(IImportListExclusionService importListExclusionService)
+        public ImportListExclusionController(IImportListExclusionService importListExclusionService,
+                                             ImportListExclusionExistsValidator importListExclusionExistsValidator)
         {
             _importListExclusionService = importListExclusionService;
 
-            SharedValidator.RuleFor(c => c.ForeignId).NotEmpty();
+            SharedValidator.RuleFor(c => c.ForeignId).Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .SetValidator(importListExclusionExistsValidator);
+
             SharedValidator.RuleFor(c => c.MovieTitle).NotEmpty();
             SharedValidator.RuleFor(c => c.MovieYear).GreaterThan(0);
         }
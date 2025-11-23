Radarr/Radarr

Convert MovieTitleLink to TypeScript
Closes #10322

frontend/src/Movie/MovieTitleLink.tsx
@@ -1,0 +1,26 @@
+import React from 'react';
+import Link, { LinkProps } from 'Components/Link/Link';
+
+interface MovieTitleLinkProps extends LinkProps {
+  titleSlug: string;
+  title: string;
+  year?: number;
+}
+
+function MovieTitleLink({
+  titleSlug,
+  title,
+  year = 0,
+  ...otherProps
+}: MovieTitleLinkProps) {
+  const link = `/movie/${titleSlug}`;
+
+  return (
+    <Link to={link} title={title} {...otherProps}>
+      {title}
+      {year > 0 ? ` (${year})` : ''}
+    </Link>
+  );
+}
+
+export default MovieTitleLink;

Whisparr/Whisparr

(cherry picked from commit 644876123d0745a0aee471b0726a6bf6827e276c)

frontend/src/Movie/MovieTitleLink.tsx
@@ -1,0 +1,26 @@
+import React from 'react';
+import Link, { LinkProps } from 'Components/Link/Link';
+
+interface MovieTitleLinkProps extends LinkProps {
+  foreignId: string;
+  title: string;
+  year?: number;
+}
+
+function MovieTitleLink({
+  foreignId,
+  title,
+  year = 0,
+  ...otherProps
+}: MovieTitleLinkProps) {
+  const link = `/movie/${foreignId}`;
+
+  return (
+    <Link to={link} title={title} {...otherProps}>
+      {title}
+      {year > 0 ? ` (${year})` : ''}
+    </Link>
+  );
+}
+
+export default MovieTitleLink;
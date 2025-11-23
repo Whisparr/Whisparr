Radarr/Radarr

New: Bulk manage custom formats

frontend/src/Settings/CustomFormats/CustomFormats/Manage/ManageCustomFormatsToolbarButton.tsx
@@ -1,0 +1,28 @@
+import React from 'react';
+import PageToolbarButton from 'Components/Page/Toolbar/PageToolbarButton';
+import useModalOpenState from 'Helpers/Hooks/useModalOpenState';
+import { icons } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
+import ManageCustomFormatsModal from './ManageCustomFormatsModal';
+
+function ManageCustomFormatsToolbarButton() {
+  const [isManageModalOpen, openManageModal, closeManageModal] =
+    useModalOpenState(false);
+
+  return (
+    <>
+      <PageToolbarButton
+        label={translate('ManageCustomFormats')}
+        iconName={icons.MANAGE}
+        onPress={openManageModal}
+      />
+
+      <ManageCustomFormatsModal
+        isOpen={isManageModalOpen}
+        onModalClose={closeManageModal}
+      />
+    </>
+  );
+}
+
+export default ManageCustomFormatsToolbarButton;

src/Radarr.Api.V3/CustomFormats/CustomFormatBulkResource.cs
@@ -1,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Radarr.Api.V3.CustomFormats
+{
+    public class CustomFormatBulkResource
+    {
+        public HashSet<int> Ids { get; set; } = new ();
+        public bool? IncludeCustomFormatWhenRenaming { get; set; }
+    }
+}

Whisparr/Whisparr

(cherry picked from commit da5323a08f2dbe42024f2d3e82d2d901abcbeb16)

frontend/src/Settings/CustomFormats/CustomFormats/Manage/ManageCustomFormatsToolbarButton.tsx
@@ -1,0 +1,28 @@
+import React from 'react';
+import PageToolbarButton from 'Components/Page/Toolbar/PageToolbarButton';
+import useModalOpenState from 'Helpers/Hooks/useModalOpenState';
+import { icons } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
+import ManageCustomFormatsModal from './ManageCustomFormatsModal';
+
+function ManageCustomFormatsToolbarButton() {
+  const [isManageModalOpen, openManageModal, closeManageModal] =
+    useModalOpenState(false);
+
+  return (
+    <>
+      <PageToolbarButton
+        label={translate('Manage')}
+        iconName={icons.MANAGE}
+        onPress={openManageModal}
+      />
+
+      <ManageCustomFormatsModal
+        isOpen={isManageModalOpen}
+        onModalClose={closeManageModal}
+      />
+    </>
+  );
+}
+
+export default ManageCustomFormatsToolbarButton;

src/Whisparr.Api.V3/CustomFormats/CustomFormatBulkResource.cs
@@ -1,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Whisparr.Api.V3.CustomFormats
+{
+    public class CustomFormatBulkResource
+    {
+        public HashSet<int> Ids { get; set; } = new ();
+        public bool? IncludeCustomFormatWhenRenaming { get; set; }
+    }
+}
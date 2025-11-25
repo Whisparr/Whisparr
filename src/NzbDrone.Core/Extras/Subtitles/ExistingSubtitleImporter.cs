Sonarr/Sonarr

Do not remove all extras when script importing

src/NzbDrone.Core/Extras/Subtitles/ExistingSubtitleImporter.cs
@@ -29,12 +29,12 @@ namespace NzbDrone.Core.Extras.Subtitles
 
         public override int Order => 1;
 
-        public override IEnumerable<ExtraFile> ProcessFiles(Series series, List<string> filesOnDisk, List<string> importedFiles)
+        public override IEnumerable<ExtraFile> ProcessFiles(Series series, List<string> filesOnDisk, List<string> importedFiles, bool keepExistingEntries)
         {
             _logger.Debug("Looking for existing subtitle files in {0}", series.Path);
 
             var subtitleFiles = new List<SubtitleFile>();
-            var filterResult = FilterAndClean(series, filesOnDisk, importedFiles);
+            var filterResult = FilterAndClean(series, filesOnDisk, importedFiles, keepExistingEntries);
 
             foreach (var possibleSubtitleFile in filterResult.FilesOnDisk)
             {

Whisparr/Whisparr

(cherry picked from commit 0a7f3a12c2be783c6374864e1e8f49ff5e969166)
(cherry picked from commit 0a9ae45ed11b25e57decbc345f2267f3434007b0)

src/NzbDrone.Core/Extras/Subtitles/ExistingSubtitleImporter.cs
@@ -27,12 +27,12 @@ namespace NzbDrone.Core.Extras.Subtitles
 
         public override int Order => 1;
 
-        public override IEnumerable<ExtraFile> ProcessFiles(Movie movie, List<string> filesOnDisk, List<string> importedFiles)
+        public override IEnumerable<ExtraFile> ProcessFiles(Movie movie, List<string> filesOnDisk, List<string> importedFiles, bool keepExistingEntries)
         {
             _logger.Debug("Looking for existing subtitle files in {0}", movie.Path);
 
             var subtitleFiles = new List<SubtitleFile>();
-            var filterResult = FilterAndClean(movie, filesOnDisk, importedFiles);
+            var filterResult = FilterAndClean(movie, filesOnDisk, importedFiles, keepExistingEntries);
 
             foreach (var possibleSubtitleFile in filterResult.FilesOnDisk)
             {
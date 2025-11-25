Radarr/Radarr

Refactor include time for movie history
(cherry picked from commit 05edd44ed6dfe73c25da021ef2600e609852f7e0)
(cherry picked from commit ca372bee258523339aff2b868f8f9a619d44dbca)
(cherry picked from commit 2a662afaef0bed9f8cd153d60eee5ecbe85508ba)

Closes #10039
Closes #10045
Closes #10046

frontend/src/Movie/Details/MovieReleaseDates.tsx
@@ -38,7 +38,10 @@ function MovieReleaseDates(props: MovieReleaseDatesProps) {
           <div className={styles.dateIcon}>
             <Icon name={icons.IN_CINEMAS} />
           </div>
-          {getRelativeDate(inCinemas, shortDateFormat, showRelativeDates, {
+          {getRelativeDate({
+            date: inCinemas,
+            shortDateFormat,
+            showRelativeDates,
             timeFormat,
             timeForToday: false,
           })}
@@ -50,7 +53,10 @@ function MovieReleaseDates(props: MovieReleaseDatesProps) {
           <div className={styles.dateIcon}>
             <Icon name={icons.MOVIE_FILE} />
           </div>
-          {getRelativeDate(digitalRelease, shortDateFormat, showRelativeDates, {
+          {getRelativeDate({
+            date: digitalRelease,
+            shortDateFormat,
+            showRelativeDates,
             timeFormat,
             timeForToday: false,
           })}
@@ -62,12 +68,13 @@ function MovieReleaseDates(props: MovieReleaseDatesProps) {
           <div className={styles.dateIcon}>
             <Icon name={icons.DISC} />
           </div>
-          {getRelativeDate(
-            physicalRelease,
+          {getRelativeDate({
+            date: physicalRelease,
             shortDateFormat,
             showRelativeDates,
-            { timeFormat, timeForToday: false }
-          )}
+            timeFormat,
+            timeForToday: false,
+          })}
         </div>
       ) : null}
     </>

Whisparr/Whisparr

(cherry picked from commit 6e81d5917ea44830c26782bd4bd665c32c89c026)

frontend/src/Movie/Details/MovieReleaseDates.tsx
@@ -36,9 +36,11 @@ function MovieReleaseDates(props: MovieReleaseDatesProps) {
           <div className={styles.dateIcon}>
             <Icon name={icons.CALENDAR} />
           </div>
-          {getRelativeDate(releaseDate, shortDateFormat, showRelativeDates, {
+          {getRelativeDate({
+            date: releaseDate,
+            shortDateFormat,
+            showRelativeDates,
             timeFormat,
-            includeSeconds: false,
             timeForToday: false,
           })}
         </div>
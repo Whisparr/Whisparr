Radarr/Radarr

New: Last Searched column on Wanted screens (#10392)
* Adding lastSearchTime to API and "Last Searched" to Frontend (cutoff unmet & missing)
Picking lastSearchTime from movie instead of movieMetaData

---------

Co-authored-by: Bogdan <mynameisbogdan@users.noreply.github.com>

src/Radarr.Api.V3/Movies/MovieResource.cs
@@ -83,6 +83,7 @@ namespace Radarr.Api.V3.Movies
         public MovieFileResource MovieFile { get; set; }
         public MovieCollectionResource Collection { get; set; }
         public float Popularity { get; set; }
+        public DateTime? LastSearchTime { get; set; }
         public MovieStatisticsResource Statistics { get; set; }
 
         // Hiding this so people don't think its usable (only used to set the initial state)
@@ -161,7 +162,8 @@ namespace Radarr.Api.V3.Movies
                 YouTubeTrailerId = model.MovieMetadata.Value.YouTubeTrailerId,
                 Studio = model.MovieMetadata.Value.Studio,
                 Collection = collection,
-                Popularity = model.MovieMetadata.Value.Popularity
+                Popularity = model.MovieMetadata.Value.Popularity,
+                LastSearchTime = model.LastSearchTime,
             };
         }
 

Whisparr/Whisparr

(cherry picked from commit 0361299a73ac1e24813e274ac69161a937b452b8)

src/Whisparr.Api.V3/Movies/MovieResource.cs
@@ -78,6 +78,7 @@ namespace Whisparr.Api.V3.Movies
         public MovieFileResource MovieFile { get; set; }
         public List<Credit> Credits { get; set; }
         public ItemType ItemType { get; set; }
+        public DateTime? LastSearchTime { get; set; }
         public MovieStatisticsResource Statistics { get; set; }
 
         // Hiding this so people don't think its usable (only used to set the initial state)
@@ -149,7 +150,8 @@ namespace Whisparr.Api.V3.Movies
                 StudioTitle = model.MovieMetadata.Value.StudioTitle,
                 StudioForeignId = model.MovieMetadata.Value.StudioForeignId,
                 Credits = model.MovieMetadata.Value.Credits,
-                ItemType = model.MovieMetadata.Value.ItemType
+                ItemType = model.MovieMetadata.Value.ItemType,
+                LastSearchTime = model.LastSearchTime,
             };
         }
 
Sonarr/Sonarr

Do not remove all extras when script importing

src/NzbDrone.Core/Extras/ImportExistingExtraFilesBase.cs
@@ -19,10 +19,15 @@ namespace NzbDrone.Core.Extras
         }
 
         public abstract int Order { get; }
-        public abstract IEnumerable<ExtraFile> ProcessFiles(Series series, List<string> filesOnDisk, List<string> importedFiles);
+        public abstract IEnumerable<ExtraFile> ProcessFiles(Series series, List<string> filesOnDisk, List<string> importedFiles, bool keepExistingEntries);
 
-        public virtual ImportExistingExtraFileFilterResult<TExtraFile> FilterAndClean(Series series, List<string> filesOnDisk, List<string> importedFiles)
+        public virtual ImportExistingExtraFileFilterResult<TExtraFile> FilterAndClean(Series series, List<string> filesOnDisk, List<string> importedFiles, bool keepExistingEntries)
         {
+            if (keepExistingEntries)
+            {
+                return Filter(series, filesOnDisk, importedFiles, new List<TExtraFile>());
+            }
+
             var seriesFiles = _extraFileService.GetFilesBySeries(series.Id);
 
             Clean(series, filesOnDisk, importedFiles, seriesFiles);

Whisparr/Whisparr

(cherry picked from commit 0a7f3a12c2be783c6374864e1e8f49ff5e969166)
(cherry picked from commit 0a9ae45ed11b25e57decbc345f2267f3434007b0)

src/NzbDrone.Core/Extras/ImportExistingExtraFilesBase.cs
@@ -23,6 +23,11 @@ namespace NzbDrone.Core.Extras
 
         public virtual ImportExistingExtraFileFilterResult<TExtraFile> FilterAndClean(Movie movie, List<string> filesOnDisk, List<string> importedFiles, bool keepExistingEntries)
         {
+            if (keepExistingEntries)
+            {
+                return Filter(movie, filesOnDisk, importedFiles, new List<TExtraFile>());
+            }
+
             var movieFiles = _extraFileService.GetFilesByMovie(movie.Id);
 
             if (keepExistingEntries)
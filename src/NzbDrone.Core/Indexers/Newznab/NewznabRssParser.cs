Sonarr/Sonarr

New: Scene and Nuked IndexerFlags for Newznab indexers
Closes #6932

src/NzbDrone.Core/Indexers/Newznab/NewznabRssParser.cs
@@ -91,6 +91,7 @@ namespace NzbDrone.Core.Indexers.Newznab
             releaseInfo.TvdbId = GetTvdbId(item);
             releaseInfo.TvRageId = GetTvRageId(item);
             releaseInfo.ImdbId = GetImdbId(item);
+            releaseInfo.IndexerFlags = GetFlags(item);
 
             return releaseInfo;
         }
@@ -193,6 +194,23 @@ namespace NzbDrone.Core.Indexers.Newznab
             }
 
             return null;
+        }
+
+        protected IndexerFlags GetFlags(XElement item)
+        {
+            IndexerFlags flags = 0;
+
+            if (TryGetNewznabAttribute(item, "prematch") == "1" || TryGetNewznabAttribute(item, "haspretime") == "1")
+            {
+                flags |= IndexerFlags.Scene;
+            }
+
+            if (TryGetNewznabAttribute(item, "nuked") == "1")
+            {
+                flags |= IndexerFlags.Nuked;
+            }
+
+            return flags;
         }
 
         protected string TryGetNewznabAttribute(XElement item, string key, string defaultValue = "")

Whisparr/Whisparr

(cherry picked from commit 278c7891a3add639b4ff5bc1f4f5e8912dabc897)
(cherry picked from commit 6095819005e4ad8b3a14d0af143cdad225ad832e)

src/NzbDrone.Core/Indexers/Newznab/NewznabRssParser.cs
@@ -92,6 +92,7 @@ namespace NzbDrone.Core.Indexers.Newznab
         {
             releaseInfo = base.ProcessItem(item, releaseInfo);
             releaseInfo.ImdbId = GetImdbId(item);
+            releaseInfo.IndexerFlags = GetFlags(item);
 
             return releaseInfo;
         }
@@ -207,6 +208,23 @@ namespace NzbDrone.Core.Indexers.Newznab
             }
 
             return 1900;
+        }
+
+        protected IndexerFlags GetFlags(XElement item)
+        {
+            IndexerFlags flags = 0;
+
+            if (TryGetNewznabAttribute(item, "prematch") == "1" || TryGetNewznabAttribute(item, "haspretime") == "1")
+            {
+                flags |= IndexerFlags.G_Scene;
+            }
+
+            if (TryGetNewznabAttribute(item, "nuked") == "1")
+            {
+                flags |= IndexerFlags.Nuked;
+            }
+
+            return flags;
         }
 
         protected string TryGetNewznabAttribute(XElement item, string key, string defaultValue = "")
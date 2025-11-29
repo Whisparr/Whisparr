Radarr/Radarr

Include exception message in SkyHook failure message

src/NzbDrone.Core/MetadataSource/SkyHook/SkyHookProxy.cs
@@ -534,17 +534,17 @@ namespace NzbDrone.Core.MetadataSource.SkyHook
             catch (HttpException ex)
             {
                 _logger.Warn(ex);
-                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with RadarrAPI.", ex, title);
+                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with RadarrAPI. {1}", ex, title, ex.Message);
             }
             catch (WebException ex)
             {
                 _logger.Warn(ex);
-                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with RadarrAPI.", ex, title, ex.Message);
+                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with RadarrAPI. {1}", ex, title, ex.Message);
             }
             catch (Exception ex)
             {
                 _logger.Warn(ex);
-                throw new SkyHookException("Search for '{0}' failed. Invalid response received from RadarrAPI.", ex, title);
+                throw new SkyHookException("Search for '{0}' failed. Invalid response received from RadarrAPI. {1}", ex, title, ex.Message);
             }
         }
 

Whisparr/Whisparr

(cherry picked from commit 0049922ab6809be48e96726a4f4006d711051b7d)

src/NzbDrone.Core/MetadataSource/SkyHook/SkyHookProxy.cs
@@ -730,17 +730,17 @@ namespace NzbDrone.Core.MetadataSource.SkyHook
             catch (HttpException ex)
             {
                 _logger.Warn(ex);
-                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with WhisparrAPI.", ex, title);
+                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with WhisparrAPI. {1}", ex, title, ex.Message);
             }
             catch (WebException ex)
             {
                 _logger.Warn(ex);
-                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with WhisparrAPI.", ex, title, ex.Message);
+                throw new SkyHookException("Search for '{0}' failed. Unable to communicate with WhisparrAPI. {1}", ex, title, ex.Message);
             }
             catch (Exception ex)
             {
                 _logger.Warn(ex);
-                throw new SkyHookException("Search for '{0}' failed. Invalid response received from WhisparrAPI.", ex, title);
+                throw new SkyHookException("Search for '{0}' failed. Invalid response received from WhisparrAPI. {1}", ex, title, ex.Message);
             }
         }
 
d0d6ec392d784b4dea159e400d4ae9ce6ba2cb69
plz12345 <132735020+plz12345@users.noreply.github.com>
2025-11-29 08:08:46 -0500

    Fix for failing SkyHook tests, added scene search tests

--- src/NzbDrone.Core.Test/MetadataSource/SkyHook/SkyHookProxySearchFixture.cs
+++ src/NzbDrone.Core.Test/MetadataSource/SkyHook/SkyHookProxySearchFixture.cs
@@ -17,17 +17,26 @@ namespace NzbDrone.Core.Test.MetadataSource.SkyHook
             UseRealHttp();
         }
 
-        [TestCase("Prometheus", "Prometheus")]
-
-        // TODO: TMDB Doesn't like when we clean periods from this
-        // [TestCase("The Man from U.N.C.L.E.", "The Man from U.N.C.L.E.")]
-        // [TestCase("imdb:tt0081593", "Taboo")]
-        // [TestCase("imdb:tt0084755", "Taboo II")]
-        [TestCase("https://stashdb.org/scenes/019ac9b6-b854-7f30-99a5-ba094cd8dd42", "019ac9b6-b854-7f30-99a5-ba094cd8dd42")]
-        [TestCase("https://www.themoviedb.org/movie/775-le-voyage-dans-la-lune", "A Trip to the Moon")]
-        public void successful_search(string title, string expected)
+        // TODO: Add more movie search test cases
+        [TestCase("Consumed by Desire", "Consumed by Desire")]
+        [TestCase("https://www.themoviedb.org/movie/699665-consumed-by-desire", "Consumed by Desire")]
+        public void successful_movie_search(string title, string expected)
         {
             var result = Subject.SearchForNewMovie(title);
+
+            result.Should().NotBeEmpty();
+
+            result[0].Title.Should().Be(expected);
+
+            ExceptionVerification.IgnoreWarns();
+        }
+
+        // TODO: add more scene search test cases
+        [TestCase("slick and naughty tiffany tatum", "Slick and Naughty")]
+        [TestCase("https://stashdb.org/scenes/019ac9a7-cdd2-7fc5-abca-4c64f97f6e17", "The Dad")]
+        public void successful_scene_search(string title, string expected)
+        {
+            var result = Subject.SearchForNewScene(title);
 
             result.Should().NotBeEmpty();
 
@@ -45,9 +54,25 @@ namespace NzbDrone.Core.Test.MetadataSource.SkyHook
         [TestCase("imdb: tt9805708")]
         [TestCase("https://www.UNKNOWN-DOMAIN.com/title/tt0033467/")]
         [TestCase("https://www.themoviedb.org/MALFORMED/775-le-voyage-dans-la-lune")]
-        public void no_search_result(string term)
+        public void no_movie_search_result(string term)
         {
             var result = Subject.SearchForNewMovie(term);
+            result.Should().BeEmpty();
+
+            ExceptionVerification.IgnoreWarns();
+        }
+
+        [TestCase("stash:")]
+        [TestCase("stash: 99999999999999999999")]
+        [TestCase("stash: 0")]
+        [TestCase("stash: -12")]
+        [TestCase("stash:1")]
+        [TestCase("adjalkwdjkalwdjklawjdlKAJD;EF")]
+        [TestCase("https://www.UNKNOWN-DOMAIN.com/scenes/tt0033467/")]
+        [TestCase("https://stashdb.org/scenes/dhhiuooidhoiuhdojhdoduh")]
+        public void no_scene_search_result(string term)
+        {
+            var result = Subject.SearchForNewScene(term);
             result.Should().BeEmpty();
 
             ExceptionVerification.IgnoreWarns();

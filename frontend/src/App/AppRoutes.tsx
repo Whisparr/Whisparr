Sonarr/Sonarr

Convert App to TypeScript

frontend/src/App/AppRoutes.tsx
@@ -35,60 +35,37 @@ import getPathWithUrlBase from 'Utilities/getPathWithUrlBase';
 import CutoffUnmetConnector from 'Wanted/CutoffUnmet/CutoffUnmetConnector';
 import MissingConnector from 'Wanted/Missing/MissingConnector';
 
-function AppRoutes(props) {
-  const {
-    app
-  } = props;
-
+function AppRoutes() {
   return (
     <Switch>
       {/*
         Series
       */}
 
-      <Route
-        exact={true}
-        path="/"
-        component={SeriesIndex}
-      />
+      <Route exact={true} path="/" component={SeriesIndex} />
 
-      {
-        window.Sonarr.urlBase &&
-          <Route
-            exact={true}
-            path="/"
-            addUrlBase={false}
-            render={() => {
-              return (
-                <Redirect
-                  to={getPathWithUrlBase('/')}
-                  component={app}
-                />
-              );
-            }}
-          />
-      }
+      {window.Sonarr.urlBase && (
+        <Route
+          exact={true}
+          path="/"
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          addUrlBase={false}
+          render={() => {
+            return <Redirect to={getPathWithUrlBase('/')} />;
+          }}
+        />
+      )}
 
-      <Route
-        path="/add/new"
-        component={AddNewSeriesConnector}
-      />
+      <Route path="/add/new" component={AddNewSeriesConnector} />
 
-      <Route
-        path="/add/import"
-        component={ImportSeries}
-      />
+      <Route path="/add/import" component={ImportSeries} />
 
       <Route
         path="/serieseditor"
         exact={true}
         render={() => {
-          return (
-            <Redirect
-              to={getPathWithUrlBase('/')}
-              component={app}
-            />
-          );
+          return <Redirect to={getPathWithUrlBase('/')} />;
         }}
       />
 
@@ -96,96 +73,57 @@ function AppRoutes(props) {
         path="/seasonpass"
         exact={true}
         render={() => {
-          return (
-            <Redirect
-              to={getPathWithUrlBase('/')}
-              component={app}
-            />
-          );
+          return <Redirect to={getPathWithUrlBase('/')} />;
         }}
       />
 
-      <Route
-        path="/series/:titleSlug"
-        component={SeriesDetailsPageConnector}
-      />
+      <Route path="/series/:titleSlug" component={SeriesDetailsPageConnector} />
 
       {/*
         Calendar
       */}
 
-      <Route
-        path="/calendar"
-        component={CalendarPageConnector}
-      />
+      <Route path="/calendar" component={CalendarPageConnector} />
 
       {/*
         Activity
       */}
 
-      <Route
-        path="/activity/history"
-        component={History}
-      />
+      <Route path="/activity/history" component={History} />
 
-      <Route
-        path="/activity/queue"
-        component={Queue}
-      />
+      <Route path="/activity/queue" component={Queue} />
 
-      <Route
-        path="/activity/blocklist"
-        component={Blocklist}
-      />
+      <Route path="/activity/blocklist" component={Blocklist} />
 
       {/*
         Wanted
       */}
 
-      <Route
-        path="/wanted/missing"
-        component={MissingConnector}
-      />
+      <Route path="/wanted/missing" component={MissingConnector} />
 
-      <Route
-        path="/wanted/cutoffunmet"
-        component={CutoffUnmetConnector}
-      />
+      <Route path="/wanted/cutoffunmet" component={CutoffUnmetConnector} />
 
       {/*
         Settings
       */}
 
-      <Route
-        exact={true}
-        path="/settings"
-        component={Settings}
-      />
+      <Route exact={true} path="/settings" component={Settings} />
 
       <Route
         path="/settings/mediamanagement"
         component={MediaManagementConnector}
       />
 
-      <Route
-        path="/settings/profiles"
-        component={Profiles}
-      />
+      <Route path="/settings/profiles" component={Profiles} />
 
-      <Route
-        path="/settings/quality"
-        component={QualityConnector}
-      />
+      <Route path="/settings/quality" component={QualityConnector} />
 
       <Route
         path="/settings/customformats"
         component={CustomFormatSettingsPage}
       />
 
-      <Route
-        path="/settings/indexers"
-        component={IndexerSettingsConnector}
-      />
+      <Route path="/settings/indexers" component={IndexerSettingsConnector} />
 
       <Route
         path="/settings/downloadclients"
@@ -197,84 +135,48 @@ function AppRoutes(props) {
         component={ImportListSettingsConnector}
       />
 
-      <Route
-        path="/settings/connect"
-        component={NotificationSettings}
-      />
+      <Route path="/settings/connect" component={NotificationSettings} />
 
-      <Route
-        path="/settings/metadata"
-        component={MetadataSettings}
-      />
+      <Route path="/settings/metadata" component={MetadataSettings} />
 
       <Route
         path="/settings/metadatasource"
         component={MetadataSourceSettings}
       />
 
-      <Route
-        path="/settings/tags"
-        component={TagSettings}
-      />
+      <Route path="/settings/tags" component={TagSettings} />
 
-      <Route
-        path="/settings/general"
-        component={GeneralSettingsConnector}
-      />
+      <Route path="/settings/general" component={GeneralSettingsConnector} />
 
-      <Route
-        path="/settings/ui"
-        component={UISettingsConnector}
-      />
+      <Route path="/settings/ui" component={UISettingsConnector} />
 
       {/*
         System
       */}
 
-      <Route
-        path="/system/status"
-        component={Status}
-      />
+      <Route path="/system/status" component={Status} />
 
-      <Route
-        path="/system/tasks"
-        component={Tasks}
-      />
+      <Route path="/system/tasks" component={Tasks} />
 
-      <Route
-        path="/system/backup"
-        component={BackupsConnector}
-      />
+      <Route path="/system/backup" component={BackupsConnector} />
 
-      <Route
-        path="/system/updates"
-        component={Updates}
-      />
+      <Route path="/system/updates" component={Updates} />
 
-      <Route
-        path="/system/events"
-        component={LogsTableConnector}
-      />
+      <Route path="/system/events" component={LogsTableConnector} />
 
-      <Route
-        path="/system/logs/files"
-        component={Logs}
-      />
+      <Route path="/system/logs/files" component={Logs} />
 
       {/*
         Not Found
       */}
 
-      <Route
-        path="*"
-        component={NotFound}
-      />
+      <Route path="*" component={NotFound} />
     </Switch>
   );
 }
 
 AppRoutes.propTypes = {
-  app: PropTypes.func.isRequired
+  app: PropTypes.func.isRequired,
 };
 
 export default AppRoutes;

frontend/src/App/AppUpdatedModalContent.tsx
@@ -1,0 +1,145 @@
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Button from 'Components/Link/Button';
+import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
+import ModalBody from 'Components/Modal/ModalBody';
+import ModalContent from 'Components/Modal/ModalContent';
+import ModalFooter from 'Components/Modal/ModalFooter';
+import ModalHeader from 'Components/Modal/ModalHeader';
+import usePrevious from 'Helpers/Hooks/usePrevious';
+import { kinds } from 'Helpers/Props';
+import { fetchUpdates } from 'Store/Actions/systemActions';
+import UpdateChanges from 'System/Updates/UpdateChanges';
+import Update from 'typings/Update';
+import translate from 'Utilities/String/translate';
+import AppState from './State/AppState';
+import styles from './AppUpdatedModalContent.css';
+
+function mergeUpdates(items: Update[], version: string, prevVersion?: string) {
+  let installedIndex = items.findIndex((u) => u.version === version);
+  let installedPreviouslyIndex = items.findIndex(
+    (u) => u.version === prevVersion
+  );
+
+  if (installedIndex === -1) {
+    installedIndex = 0;
+  }
+
+  if (installedPreviouslyIndex === -1) {
+    installedPreviouslyIndex = items.length;
+  } else if (installedPreviouslyIndex === installedIndex && items.length) {
+    installedPreviouslyIndex += 1;
+  }
+
+  const appliedUpdates = items.slice(installedIndex, installedPreviouslyIndex);
+
+  if (!appliedUpdates.length) {
+    return null;
+  }
+
+  const appliedChanges: Update['changes'] = { new: [], fixed: [] };
+
+  appliedUpdates.forEach((u: Update) => {
+    if (u.changes) {
+      appliedChanges.new.push(...u.changes.new);
+      appliedChanges.fixed.push(...u.changes.fixed);
+    }
+  });
+
+  const mergedUpdate: Update = Object.assign({}, appliedUpdates[0], {
+    changes: appliedChanges,
+  });
+
+  if (!appliedChanges.new.length && !appliedChanges.fixed.length) {
+    mergedUpdate.changes = null;
+  }
+
+  return mergedUpdate;
+}
+
+interface AppUpdatedModalContentProps {
+  onModalClose: () => void;
+}
+
+function AppUpdatedModalContent(props: AppUpdatedModalContentProps) {
+  const dispatch = useDispatch();
+  const { version, prevVersion } = useSelector((state: AppState) => state.app);
+  const { isPopulated, error, items } = useSelector(
+    (state: AppState) => state.system.updates
+  );
+  const previousVersion = usePrevious(version);
+
+  const { onModalClose } = props;
+
+  const update = mergeUpdates(items, version, prevVersion);
+
+  const handleSeeChangesPress = useCallback(() => {
+    window.location.href = `${window.Sonarr.urlBase}/system/updates`;
+  }, []);
+
+  useEffect(() => {
+    dispatch(fetchUpdates());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (version !== previousVersion) {
+      dispatch(fetchUpdates());
+    }
+  }, [version, previousVersion, dispatch]);
+
+  return (
+    <ModalContent onModalClose={onModalClose}>
+      <ModalHeader>{translate('AppUpdated')}</ModalHeader>
+
+      <ModalBody>
+        <div>
+          <InlineMarkdown
+            data={translate('AppUpdatedVersion', { version })}
+            blockClassName={styles.version}
+          />
+        </div>
+
+        {isPopulated && !error && !!update ? (
+          <div>
+            {update.changes ? (
+              <div className={styles.maintenance}>
+                {translate('MaintenanceRelease')}
+              </div>
+            ) : null}
+
+            {update.changes ? (
+              <div>
+                <div className={styles.changes}>{translate('WhatsNew')}</div>
+
+                <UpdateChanges
+                  title={translate('New')}
+                  changes={update.changes.new}
+                />
+
+                <UpdateChanges
+                  title={translate('Fixed')}
+                  changes={update.changes.fixed}
+                />
+              </div>
+            ) : null}
+          </div>
+        ) : null}
+
+        {!isPopulated && !error ? <LoadingIndicator /> : null}
+      </ModalBody>
+
+      <ModalFooter>
+        <Button onPress={handleSeeChangesPress}>
+          {translate('RecentChanges')}
+        </Button>
+
+        <Button kind={kinds.PRIMARY} onPress={onModalClose}>
+          {translate('Reload')}
+        </Button>
+      </ModalFooter>
+    </ModalContent>
+  );
+}
+
+export default AppUpdatedModalContent;

Whisparr/Whisparr

(cherry picked from commit d6d90a64a39d3b9d3a95fb6b265517693a70fdd7)
Closes #10233
(cherry picked from commit dc29526961c3f1a5bc37d0127b68b82e85a4d9d8)

frontend/src/App/AppRoutes.tsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import Blocklist from 'Activity/Blocklist/Blocklist';
@@ -41,11 +40,11 @@ import getPathWithUrlBase from 'Utilities/getPathWithUrlBase';
 import CutoffUnmetConnector from 'Wanted/CutoffUnmet/CutoffUnmetConnector';
 import MissingConnector from 'Wanted/Missing/MissingConnector';
 
-function AppRoutes(props) {
-  const {
-    app
-  } = props;
+function RedirectWithUrlBase() {
+  return <Redirect to={getPathWithUrlBase('/')} />;
+}
 
+function AppRoutes() {
   return (
     <Switch>
       {/*
@@ -58,22 +57,16 @@ function AppRoutes(props) {
         component={SceneIndex}
       />
 
-      {
-        window.Whisparr.urlBase &&
-          <Route
-            exact={true}
-            path="/"
-            addUrlBase={false}
-            render={() => {
-              return (
-                <Redirect
-                  to={getPathWithUrlBase('/')}
-                  component={app}
-                />
-              );
-            }}
-          />
-      }
+      {window.Whisparr.urlBase && (
+        <Route
+          exact={true}
+          path="/"
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          addUrlBase={false}
+          render={RedirectWithUrlBase}
+        />
+      )}
 
       <Route
         path="/movies"
@@ -134,78 +127,47 @@ function AppRoutes(props) {
         Calendar
       */}
 
-      <Route
-        path="/calendar"
-        component={CalendarPageConnector}
-      />
+      <Route path="/calendar" component={CalendarPageConnector} />
 
       {/*
         Activity
       */}
 
-      <Route
-        path="/activity/history"
-        component={HistoryConnector}
-      />
+      <Route path="/activity/history" component={HistoryConnector} />
 
-      <Route
-        path="/activity/queue"
-        component={Queue}
-      />
+      <Route path="/activity/queue" component={Queue} />
 
-      <Route
-        path="/activity/blocklist"
-        component={Blocklist}
-      />
+      <Route path="/activity/blocklist" component={Blocklist} />
 
       {/*
         Wanted
       */}
 
-      <Route
-        path="/wanted/missing"
-        component={MissingConnector}
-      />
+      <Route path="/wanted/missing" component={MissingConnector} />
 
-      <Route
-        path="/wanted/cutoffunmet"
-        component={CutoffUnmetConnector}
-      />
+      <Route path="/wanted/cutoffunmet" component={CutoffUnmetConnector} />
 
       {/*
         Settings
       */}
 
-      <Route
-        exact={true}
-        path="/settings"
-        component={Settings}
-      />
+      <Route exact={true} path="/settings" component={Settings} />
 
       <Route
         path="/settings/mediamanagement"
         component={MediaManagementConnector}
       />
 
-      <Route
-        path="/settings/profiles"
-        component={Profiles}
-      />
+      <Route path="/settings/profiles" component={Profiles} />
 
-      <Route
-        path="/settings/quality"
-        component={QualityConnector}
-      />
+      <Route path="/settings/quality" component={QualityConnector} />
 
       <Route
         path="/settings/customformats"
         component={CustomFormatSettingsPage}
       />
 
-      <Route
-        path="/settings/indexers"
-        component={IndexerSettingsConnector}
-      />
+      <Route path="/settings/indexers" component={IndexerSettingsConnector} />
 
       <Route
         path="/settings/downloadclients"
@@ -217,79 +179,39 @@ function AppRoutes(props) {
         component={ImportListSettingsConnector}
       />
 
-      <Route
-        path="/settings/connect"
-        component={NotificationSettings}
-      />
+      <Route path="/settings/connect" component={NotificationSettings} />
 
-      <Route
-        path="/settings/metadata"
-        component={MetadataSettings}
-      />
+      <Route path="/settings/metadata" component={MetadataSettings} />
 
-      <Route
-        path="/settings/tags"
-        component={TagSettings}
-      />
+      <Route path="/settings/tags" component={TagSettings} />
 
-      <Route
-        path="/settings/general"
-        component={GeneralSettingsConnector}
-      />
+      <Route path="/settings/general" component={GeneralSettingsConnector} />
 
-      <Route
-        path="/settings/ui"
-        component={UISettingsConnector}
-      />
+      <Route path="/settings/ui" component={UISettingsConnector} />
 
       {/*
         System
       */}
 
-      <Route
-        path="/system/status"
-        component={Status}
-      />
+      <Route path="/system/status" component={Status} />
 
-      <Route
-        path="/system/tasks"
-        component={Tasks}
-      />
+      <Route path="/system/tasks" component={Tasks} />
 
-      <Route
-        path="/system/backup"
-        component={BackupsConnector}
-      />
+      <Route path="/system/backup" component={BackupsConnector} />
 
-      <Route
-        path="/system/updates"
-        component={Updates}
-      />
+      <Route path="/system/updates" component={Updates} />
 
-      <Route
-        path="/system/events"
-        component={LogsTableConnector}
-      />
+      <Route path="/system/events" component={LogsTableConnector} />
 
-      <Route
-        path="/system/logs/files"
-        component={Logs}
-      />
+      <Route path="/system/logs/files" component={Logs} />
 
       {/*
         Not Found
       */}
 
-      <Route
-        path="*"
-        component={NotFound}
-      />
+      <Route path="*" component={NotFound} />
     </Switch>
   );
 }
-
-AppRoutes.propTypes = {
-  app: PropTypes.func.isRequired
-};
 
 export default AppRoutes;

frontend/src/App/AppUpdatedModalContent.tsx
@@ -1,0 +1,145 @@
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Button from 'Components/Link/Button';
+import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
+import ModalBody from 'Components/Modal/ModalBody';
+import ModalContent from 'Components/Modal/ModalContent';
+import ModalFooter from 'Components/Modal/ModalFooter';
+import ModalHeader from 'Components/Modal/ModalHeader';
+import usePrevious from 'Helpers/Hooks/usePrevious';
+import { kinds } from 'Helpers/Props';
+import { fetchUpdates } from 'Store/Actions/systemActions';
+import UpdateChanges from 'System/Updates/UpdateChanges';
+import Update from 'typings/Update';
+import translate from 'Utilities/String/translate';
+import AppState from './State/AppState';
+import styles from './AppUpdatedModalContent.css';
+
+function mergeUpdates(items: Update[], version: string, prevVersion?: string) {
+  let installedIndex = items.findIndex((u) => u.version === version);
+  let installedPreviouslyIndex = items.findIndex(
+    (u) => u.version === prevVersion
+  );
+
+  if (installedIndex === -1) {
+    installedIndex = 0;
+  }
+
+  if (installedPreviouslyIndex === -1) {
+    installedPreviouslyIndex = items.length;
+  } else if (installedPreviouslyIndex === installedIndex && items.length) {
+    installedPreviouslyIndex += 1;
+  }
+
+  const appliedUpdates = items.slice(installedIndex, installedPreviouslyIndex);
+
+  if (!appliedUpdates.length) {
+    return null;
+  }
+
+  const appliedChanges: Update['changes'] = { new: [], fixed: [] };
+
+  appliedUpdates.forEach((u: Update) => {
+    if (u.changes) {
+      appliedChanges.new.push(...u.changes.new);
+      appliedChanges.fixed.push(...u.changes.fixed);
+    }
+  });
+
+  const mergedUpdate: Update = Object.assign({}, appliedUpdates[0], {
+    changes: appliedChanges,
+  });
+
+  if (!appliedChanges.new.length && !appliedChanges.fixed.length) {
+    mergedUpdate.changes = null;
+  }
+
+  return mergedUpdate;
+}
+
+interface AppUpdatedModalContentProps {
+  onModalClose: () => void;
+}
+
+function AppUpdatedModalContent(props: AppUpdatedModalContentProps) {
+  const dispatch = useDispatch();
+  const { version, prevVersion } = useSelector((state: AppState) => state.app);
+  const { isPopulated, error, items } = useSelector(
+    (state: AppState) => state.system.updates
+  );
+  const previousVersion = usePrevious(version);
+
+  const { onModalClose } = props;
+
+  const update = mergeUpdates(items, version, prevVersion);
+
+  const handleSeeChangesPress = useCallback(() => {
+    window.location.href = `${window.Whisparr.urlBase}/system/updates`;
+  }, []);
+
+  useEffect(() => {
+    dispatch(fetchUpdates());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (version !== previousVersion) {
+      dispatch(fetchUpdates());
+    }
+  }, [version, previousVersion, dispatch]);
+
+  return (
+    <ModalContent onModalClose={onModalClose}>
+      <ModalHeader>{translate('AppUpdated')}</ModalHeader>
+
+      <ModalBody>
+        <div>
+          <InlineMarkdown
+            data={translate('AppUpdatedVersion', { version })}
+            blockClassName={styles.version}
+          />
+        </div>
+
+        {isPopulated && !error && !!update ? (
+          <div>
+            {update.changes ? (
+              <div className={styles.maintenance}>
+                {translate('MaintenanceRelease')}
+              </div>
+            ) : null}
+
+            {update.changes ? (
+              <div>
+                <div className={styles.changes}>{translate('WhatsNew')}</div>
+
+                <UpdateChanges
+                  title={translate('New')}
+                  changes={update.changes.new}
+                />
+
+                <UpdateChanges
+                  title={translate('Fixed')}
+                  changes={update.changes.fixed}
+                />
+              </div>
+            ) : null}
+          </div>
+        ) : null}
+
+        {!isPopulated && !error ? <LoadingIndicator /> : null}
+      </ModalBody>
+
+      <ModalFooter>
+        <Button onPress={handleSeeChangesPress}>
+          {translate('RecentChanges')}
+        </Button>
+
+        <Button kind={kinds.PRIMARY} onPress={onModalClose}>
+          {translate('Reload')}
+        </Button>
+      </ModalFooter>
+    </ModalContent>
+  );
+}
+
+export default AppUpdatedModalContent;
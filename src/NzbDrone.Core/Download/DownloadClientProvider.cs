Sonarr/Sonarr

Improve message for grab errors due to no matching tags
Co-authored-by: zakary <zak@ary.dev>

src/NzbDrone.Core/Download/DownloadClientProvider.cs
@@ -41,18 +41,23 @@ namespace NzbDrone.Core.Download
             var blockedProviders = new HashSet<int>(_downloadClientStatusService.GetBlockedProviders().Select(v => v.ProviderId));
             var availableProviders = _downloadClientFactory.GetAvailableProviders().Where(v => v.Protocol == downloadProtocol).ToList();
 
-            if (tags != null)
+            if (!availableProviders.Any())
+            {
+                return null;
+            }
+
+            if (tags is { Count: > 0 })
             {
                 var matchingTagsClients = availableProviders.Where(i => i.Definition.Tags.Intersect(tags).Any()).ToList();
 
                 availableProviders = matchingTagsClients.Count > 0 ?
                     matchingTagsClients :
                     availableProviders.Where(i => i.Definition.Tags.Empty()).ToList();
-            }
 
-            if (!availableProviders.Any())
-            {
-                return null;
+                if (!availableProviders.Any())
+                {
+                    throw new DownloadClientUnavailableException("No download client was found without tags or a matching series tag. Please check your settings.");
+                }
             }
 
             if (indexerId > 0)

Whisparr/Whisparr

(cherry picked from commit df672487cf1d5f067849367a2bfb0068defc315d)
Closes #10593
(cherry picked from commit ab8a2d190e3d173ebd99117190e8281b048c9cf5)

src/NzbDrone.Core/Download/DownloadClientProvider.cs
@@ -41,18 +41,23 @@ namespace NzbDrone.Core.Download
             var blockedProviders = new HashSet<int>(_downloadClientStatusService.GetBlockedProviders().Select(v => v.ProviderId));
             var availableProviders = _downloadClientFactory.GetAvailableProviders().Where(v => v.Protocol == downloadProtocol).ToList();
 
-            if (tags != null)
+            if (!availableProviders.Any())
+            {
+                return null;
+            }
+
+            if (tags is { Count: > 0 })
             {
                 var matchingTagsClients = availableProviders.Where(i => i.Definition.Tags.Intersect(tags).Any()).ToList();
 
                 availableProviders = matchingTagsClients.Count > 0 ?
                     matchingTagsClients :
                     availableProviders.Where(i => i.Definition.Tags.Empty()).ToList();
-            }
 
-            if (!availableProviders.Any())
-            {
-                return null;
+                if (!availableProviders.Any())
+                {
+                    throw new DownloadClientUnavailableException("No download client was found without tags or a matching movie tag. Please check your settings.");
+                }
             }
 
             if (indexerId > 0)
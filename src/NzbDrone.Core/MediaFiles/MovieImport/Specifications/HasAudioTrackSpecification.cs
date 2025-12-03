Sonarr/Sonarr

New: Reject files during import that have no audio tracks
Closes #7298

src/NzbDrone.Core/MediaFiles/EpisodeImport/Specifications/HasAudioTrackSpecification.cs
@@ -1,0 +1,35 @@
+using NLog;
+using NzbDrone.Core.DecisionEngine;
+using NzbDrone.Core.Download;
+using NzbDrone.Core.Parser.Model;
+
+namespace NzbDrone.Core.MediaFiles.EpisodeImport.Specifications
+{
+    public class HasAudioTrackSpecification : IImportDecisionEngineSpecification
+    {
+        private readonly Logger _logger;
+
+        public HasAudioTrackSpecification(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public Decision IsSatisfiedBy(LocalEpisode localEpisode, DownloadClientItem downloadClientItem)
+        {
+            if (localEpisode.MediaInfo == null)
+            {
+                _logger.Debug("Failed to get media info from the file, make sure ffprobe is available, skipping check");
+                return Decision.Accept();
+            }
+
+            if (localEpisode.MediaInfo.AudioStreamCount == 0)
+            {
+                _logger.Debug("No audio tracks found in file");
+
+                return Decision.Reject("No audio tracks detected");
+            }
+
+            return Decision.Accept();
+        }
+    }
+}

Whisparr/Whisparr

(cherry picked from commit 978349e24135572889095c743d0e7fac734ba7e0)
Closes #10643
(cherry picked from commit 5976d66511ce1ac5250d7f613517324be5233a35)

src/NzbDrone.Core/MediaFiles/MovieImport/Specifications/HasAudioTrackSpecification.cs
@@ -1,0 +1,35 @@
+using NLog;
+using NzbDrone.Core.DecisionEngine;
+using NzbDrone.Core.Download;
+using NzbDrone.Core.Parser.Model;
+
+namespace NzbDrone.Core.MediaFiles.MovieImport.Specifications
+{
+    public class HasAudioTrackSpecification : IImportDecisionEngineSpecification
+    {
+        private readonly Logger _logger;
+
+        public HasAudioTrackSpecification(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public Decision IsSatisfiedBy(LocalMovie localMovie, DownloadClientItem downloadClientItem)
+        {
+            if (localMovie.MediaInfo == null)
+            {
+                _logger.Debug("Failed to get media info from the file, make sure ffprobe is available, skipping check");
+                return Decision.Accept();
+            }
+
+            if (localMovie.MediaInfo.AudioStreamCount == 0)
+            {
+                _logger.Debug("No audio tracks found in file");
+
+                return Decision.Reject("No audio tracks detected");
+            }
+
+            return Decision.Accept();
+        }
+    }
+}
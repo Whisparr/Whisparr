Sonarr/Sonarr

Console warnings for missing translations on development builds

frontend/src/Utilities/String/translate.ts
@@ -27,6 +27,12 @@ export default function translate(
   key: string,
   tokens: Record<string, string | number | boolean> = {}
 ) {
+  const { isProduction = true } = window.Sonarr;
+
+  if (!isProduction && !(key in translations)) {
+    console.warn(`Missing translation for key: ${key}`);
+  }
+
   const translation = translations[key] || key;
 
   tokens.appName = 'Sonarr';

Whisparr/Whisparr

(cherry picked from commit 67a1ecb0fea4e6c7dfdb68fbe3ef30d4c22398d8)
Closes #10669
(cherry picked from commit 617b9c5d35e73561f3ca8f28cff0541528531e91)

frontend/src/Utilities/String/translate.ts
@@ -27,6 +27,12 @@ export default function translate(
   key: string,
   tokens: Record<string, string | number | boolean> = {}
 ) {
+  const { isProduction = true } = window.Radarr;
+
+  if (!isProduction && !(key in translations)) {
+    console.warn(`Missing translation for key: ${key}`);
+  }
+
   const translation = translations[key] || key;
 
   tokens.appName = 'Whisparr';
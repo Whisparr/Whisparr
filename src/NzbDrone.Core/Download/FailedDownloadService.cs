Radarr/Radarr

Enhance failed download warning for items not grabbed by Radarr
(cherry picked from commit c9027359271690f7d374d1330fa39776e2b8ec40)

src/NzbDrone.Core/Download/FailedDownloadService.cs
@@ -70,7 +70,7 @@ namespace NzbDrone.Core.Download
 
                 if (grabbedItems.Empty())
                 {
-                    trackedDownload.Warn("Download wasn't grabbed by Radarr, skipping");
+                    trackedDownload.Warn(trackedDownload.DownloadItem.IsEncrypted ? "Download is encrypted and wasn't grabbed by Radarr, skipping automatic download handling" : "Download has failed wasn't grabbed by Radarr, skipping automatic download handling");
                     return;
                 }
 

Whisparr/Whisparr

(cherry picked from commit 64e865f29675dd89b4372b0942c1555e6b837b3a)

src/NzbDrone.Core/Download/FailedDownloadService.cs
@@ -70,7 +70,7 @@ namespace NzbDrone.Core.Download
 
                 if (grabbedItems.Empty())
                 {
-                    trackedDownload.Warn("Download wasn't grabbed by Whisparr, skipping");
+                    trackedDownload.Warn(trackedDownload.DownloadItem.IsEncrypted ? "Download is encrypted and wasn't grabbed by Whisparr, skipping automatic download handling" : "Download has failed wasn't grabbed by Whisparr, skipping automatic download handling");
                     return;
                 }
 
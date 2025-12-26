Radarr/Radarr

Add React Query
(cherry picked from commit 4491df3ae7530f2167beebc3548dd01fd2cc1a12)

frontend/src/Helpers/Hooks/useApiQuery.ts
@@ -1,0 +1,56 @@
+import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
+
+interface QueryOptions {
+  url: string;
+  headers?: HeadersInit;
+}
+
+const absUrlRegex = /^(https?:)?\/\//i;
+const apiRoot = window.Radarr.apiRoot;
+
+function isAbsolute(url: string) {
+  return absUrlRegex.test(url);
+}
+
+function getUrl(url: string) {
+  return apiRoot + url;
+}
+
+function useApiQuery<T>(options: QueryOptions) {
+  const { url, headers } = options;
+
+  const final = useMemo(() => {
+    if (isAbsolute(url)) {
+      return {
+        url,
+        headers,
+      };
+    }
+
+    return {
+      url: getUrl(url),
+      headers: {
+        ...headers,
+        'X-Api-Key': window.Radarr.apiKey,
+      },
+    };
+  }, [url, headers]);
+
+  return useQuery({
+    queryKey: [final.url],
+    queryFn: async () => {
+      const result = await fetch(final.url, {
+        headers: final.headers,
+      });
+
+      if (!result.ok) {
+        throw new Error('Failed to fetch');
+      }
+
+      return result.json() as T;
+    },
+  });
+}
+
+export default useApiQuery;

Whisparr/Whisparr

(cherry picked from commit b91517afd55b91a62dffc7e4a79e1c7e76e4504c)

frontend/src/Helpers/Hooks/useApiQuery.ts
@@ -1,0 +1,58 @@
+import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
+
+interface QueryOptions {
+  url: string;
+  headers?: HeadersInit;
+}
+
+const absUrlRegex = /^(https?:)?\/\//i;
+const apiRoot = window.Whisparr.apiRoot;
+
+function isAbsolute(url: string) {
+  return absUrlRegex.test(url);
+}
+
+function getUrl(url: string) {
+  return apiRoot + url;
+}
+
+function useApiQuery<T>(options: QueryOptions) {
+  const { url, headers } = options;
+
+  const final = useMemo(() => {
+    if (isAbsolute(url)) {
+      return {
+        url,
+        headers,
+      };
+    }
+
+    return {
+      url: getUrl(url),
+      headers: {
+        ...headers,
+        'X-Api-Key': window.Whisparr.apiKey,
+      },
+    };
+  }, [url, headers]);
+
+  return useQuery({
+    queryKey: [final.url],
+    queryFn: async () => {
+      const result = await fetch(final.url, {
+        headers: final.headers,
+      });
+
+      if (!result.ok) {
+        throw new Error(
+          `Failed to fetch ${final.url}: ${result.status} ${result.statusText}`
+        );
+      }
+
+      return result.json() as T;
+    },
+  });
+}
+
+export default useApiQuery;
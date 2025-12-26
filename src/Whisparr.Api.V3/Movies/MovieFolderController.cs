Radarr/Radarr

New: Ability to change root folder when editing movie
(cherry picked from commit 417af2b91542e709e4b99aa5ca55b0501ba426ad)

src/Radarr.Api.V3/Movies/MovieFolderController.cs
@@ -1,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Core.Movies;
+using NzbDrone.Core.Organizer;
+using Radarr.Http;
+
+namespace Radarr.Api.V3.Movies;
+
+[V3ApiController("movie")]
+public class MovieFolderController : Controller
+{
+    private readonly IMovieService _movieService;
+    private readonly IBuildFileNames _fileNameBuilder;
+
+    public MovieFolderController(IMovieService movieService, IBuildFileNames fileNameBuilder)
+    {
+        _movieService = movieService;
+        _fileNameBuilder = fileNameBuilder;
+    }
+
+    [HttpGet("{id}/folder")]
+    [Produces("application/json")]
+    public object GetFolder([FromRoute] int id)
+    {
+        var series = _movieService.GetMovie(id);
+        var folder = _fileNameBuilder.GetMovieFolder(series);
+
+        return new
+        {
+            folder
+        };
+    }
+}

Whisparr/Whisparr

(cherry picked from commit c52f9c5ec46bf8f520c984bc09fa6d8bba316873)

src/Whisparr.Api.V3/Movies/MovieFolderController.cs
@@ -1,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Core.Movies;
+using NzbDrone.Core.Organizer;
+using Whisparr.Http;
+
+namespace Whisparr.Api.V3.Movies;
+
+[V3ApiController("movie")]
+public class MovieFolderController : Controller
+{
+    private readonly IMovieService _movieService;
+    private readonly IBuildFileNames _fileNameBuilder;
+
+    public MovieFolderController(IMovieService movieService, IBuildFileNames fileNameBuilder)
+    {
+        _movieService = movieService;
+        _fileNameBuilder = fileNameBuilder;
+    }
+
+    [HttpGet("{id}/folder")]
+    [Produces("application/json")]
+    public object GetFolder([FromRoute] int id)
+    {
+        var series = _movieService.GetMovie(id);
+        var folder = _fileNameBuilder.GetMovieFolder(series);
+
+        return new
+        {
+            folder
+        };
+    }
+}
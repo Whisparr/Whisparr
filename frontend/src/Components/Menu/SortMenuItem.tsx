Sonarr/Sonarr

Convert Menu components to TypeScript

frontend/src/Components/Menu/SortMenuItem.tsx
@@ -1,0 +1,36 @@
+import React from 'react';
+import { icons } from 'Helpers/Props';
+import { SortDirection } from 'Helpers/Props/sortDirections';
+import SelectedMenuItem from './SelectedMenuItem';
+
+interface SortMenuItemProps {
+  name?: string;
+  sortKey?: string;
+  sortDirection?: SortDirection;
+  children: string | React.ReactNode;
+  onPress: (sortKey: string) => void;
+}
+
+function SortMenuItem({
+  name,
+  sortKey,
+  sortDirection,
+  ...otherProps
+}: SortMenuItemProps) {
+  const isSelected = name === sortKey;
+
+  return (
+    <SelectedMenuItem
+      name={name}
+      selectedIconName={
+        sortDirection === 'ascending'
+          ? icons.SORT_ASCENDING
+          : icons.SORT_DESCENDING
+      }
+      isSelected={isSelected}
+      {...otherProps}
+    />
+  );
+}
+
+export default SortMenuItem;

Whisparr/Whisparr

(cherry picked from commit 12a1ef038753cdab89ae7137d06e1ba3810166b1)
(cherry picked from commit 78f3b1f4030a8399cb4c57593176825a9537dd16)

frontend/src/Components/Menu/SortMenuItem.tsx
@@ -1,0 +1,35 @@
+import React from 'react';
+import { icons, sortDirections } from 'Helpers/Props';
+import SelectedMenuItem from './SelectedMenuItem';
+
+interface SortMenuItemProps {
+  name?: string;
+  sortKey?: string;
+  sortDirection?: sortDirections;
+  children: string | React.ReactNode;
+  onPress: (sortKey: string) => void;
+}
+
+function SortMenuItem({
+  name,
+  sortKey,
+  sortDirection,
+  ...otherProps
+}: SortMenuItemProps) {
+  const isSelected = name === sortKey;
+
+  return (
+    <SelectedMenuItem
+      name={name}
+      selectedIconName={
+        sortDirection === 'ascending'
+          ? icons.SORT_ASCENDING
+          : icons.SORT_DESCENDING
+      }
+      isSelected={isSelected}
+      {...otherProps}
+    />
+  );
+}
+
+export default SortMenuItem;
5b1227182db266754f8163b7a00f5299e0d25fcc
plz12345 <132735020+plz12345@users.noreply.github.com>
2025-12-29 13:41:39 -0500

    Logging refined

--- src/NzbDrone.Core/Movies/MovieService.cs
+++ src/NzbDrone.Core/Movies/MovieService.cs
@@ -817,7 +817,7 @@ namespace NzbDrone.Core.Movies
                 // This helps when performer aliases/order differ but the release is for the same movie.
                 if (cleanTitle.IsNotNullOrWhiteSpace() && parsedMovieTitle.Contains(cleanTitle, StringComparison.InvariantCultureIgnoreCase))
                 {
-                    _logger.Debug("Match {0} against {1} [FallbackContainsTitle]", parsedMovieTitle, cleanTitle);
+                    _logger.Debug("Matched [{0}] against [{1}] [ParsedTitleContainsCleanTitle]", parsedMovieTitle, cleanTitle);
                     matches.Add(movie, MovieParseMatchType.ParsedTitleContainsCleanTitle);
                     continue;
                 }

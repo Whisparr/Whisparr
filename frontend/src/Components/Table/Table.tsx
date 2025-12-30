Sonarr/Sonarr

Convert Table to TypeScript

frontend/src/Components/Table/Table.tsx
@@ -1,0 +1,124 @@
+import classNames from 'classnames';
+import React from 'react';
+import IconButton from 'Components/Link/IconButton';
+import Scroller from 'Components/Scroller/Scroller';
+import TableOptionsModalWrapper from 'Components/Table/TableOptions/TableOptionsModalWrapper';
+import { icons, scrollDirections } from 'Helpers/Props';
+import { SortDirection } from 'Helpers/Props/sortDirections';
+import { CheckInputChanged } from 'typings/inputs';
+import { TableOptionsChangePayload } from 'typings/Table';
+import Column from './Column';
+import TableHeader from './TableHeader';
+import TableHeaderCell from './TableHeaderCell';
+import TableSelectAllHeaderCell from './TableSelectAllHeaderCell';
+import styles from './Table.css';
+
+interface TableProps {
+  className?: string;
+  horizontalScroll?: boolean;
+  selectAll?: boolean;
+  allSelected?: boolean;
+  allUnselected?: boolean;
+  columns: Column[];
+  optionsComponent?: React.ElementType;
+  pageSize?: number;
+  canModifyColumns?: boolean;
+  sortKey?: string;
+  sortDirection?: SortDirection;
+  children?: React.ReactNode;
+  onSortPress?: (name: string, sortDirection?: SortDirection) => void;
+  onTableOptionChange?: (payload: TableOptionsChangePayload) => void;
+  onSelectAllChange?: (change: CheckInputChanged) => void;
+}
+
+function Table({
+  className = styles.table,
+  horizontalScroll = true,
+  selectAll = false,
+  allSelected = false,
+  allUnselected = false,
+  columns,
+  optionsComponent,
+  pageSize,
+  canModifyColumns,
+  sortKey,
+  sortDirection,
+  children,
+  onSortPress,
+  onTableOptionChange,
+  onSelectAllChange,
+}: TableProps) {
+  return (
+    <Scroller
+      className={classNames(
+        styles.tableContainer,
+        horizontalScroll && styles.horizontalScroll
+      )}
+      scrollDirection={
+        horizontalScroll ? scrollDirections.HORIZONTAL : scrollDirections.NONE
+      }
+      autoFocus={false}
+    >
+      <table className={className}>
+        <TableHeader>
+          {selectAll && onSelectAllChange ? (
+            <TableSelectAllHeaderCell
+              allSelected={allSelected}
+              allUnselected={allUnselected}
+              onSelectAllChange={onSelectAllChange}
+            />
+          ) : null}
+
+          {columns.map((column) => {
+            const { name, isVisible, isSortable, ...otherColumnProps } = column;
+
+            if (!isVisible) {
+              return null;
+            }
+
+            if (
+              (name === 'actions' || name === 'details') &&
+              onTableOptionChange
+            ) {
+              return (
+                <TableHeaderCell
+                  key={name}
+                  name={name}
+                  isSortable={false}
+                  {...otherColumnProps}
+                >
+                  <TableOptionsModalWrapper
+                    columns={columns}
+                    optionsComponent={optionsComponent}
+                    pageSize={pageSize}
+                    canModifyColumns={canModifyColumns}
+                    onTableOptionChange={onTableOptionChange}
+                  >
+                    <IconButton name={icons.ADVANCED_SETTINGS} />
+                  </TableOptionsModalWrapper>
+                </TableHeaderCell>
+              );
+            }
+
+            return (
+              <TableHeaderCell
+                key={column.name}
+                {...column}
+                sortKey={sortKey}
+                sortDirection={sortDirection}
+                onSortPress={onSortPress}
+              >
+                {typeof column.label === 'function'
+                  ? column.label()
+                  : column.label}
+              </TableHeaderCell>
+            );
+          })}
+        </TableHeader>
+        {children}
+      </table>
+    </Scroller>
+  );
+}
+
+export default Table;

frontend/src/Components/Table/TableHeaderCell.tsx
@@ -1,0 +1,70 @@
+import React, { useCallback } from 'react';
+import Icon from 'Components/Icon';
+import Link from 'Components/Link/Link';
+import { icons, sortDirections } from 'Helpers/Props';
+import { SortDirection } from 'Helpers/Props/sortDirections';
+import styles from './TableHeaderCell.css';
+
+interface TableHeaderCellProps {
+  className?: string;
+  name: string;
+  label?: string | (() => string) | React.ReactNode;
+  columnLabel?: string | (() => string);
+  isSortable?: boolean;
+  isVisible?: boolean;
+  isModifiable?: boolean;
+  sortKey?: string;
+  fixedSortDirection?: SortDirection;
+  sortDirection?: string;
+  children?: React.ReactNode;
+  onSortPress?: (name: string, sortDirection?: SortDirection) => void;
+}
+
+function TableHeaderCell({
+  className = styles.headerCell,
+  name,
+  label,
+  columnLabel,
+  isSortable = false,
+  isVisible,
+  isModifiable,
+  sortKey,
+  sortDirection,
+  fixedSortDirection,
+  children,
+  onSortPress,
+  ...otherProps
+}: TableHeaderCellProps) {
+  const isSorting = isSortable && sortKey === name;
+  const sortIcon =
+    sortDirection === sortDirections.ASCENDING
+      ? icons.SORT_ASCENDING
+      : icons.SORT_DESCENDING;
+
+  const handlePress = useCallback(() => {
+    if (fixedSortDirection) {
+      onSortPress?.(name, fixedSortDirection);
+    } else {
+      onSortPress?.(name);
+    }
+  }, [name, fixedSortDirection, onSortPress]);
+
+  return isSortable ? (
+    <Link
+      {...otherProps}
+      component="th"
+      className={className}
+      // label={typeof label === 'function' ? label() : label}
+      title={typeof columnLabel === 'function' ? columnLabel() : columnLabel}
+      onPress={handlePress}
+    >
+      {children}
+
+      {isSorting && <Icon name={sortIcon} className={styles.sortIcon} />}
+    </Link>
+  ) : (
+    <th className={className}>{children}</th>
+  );
+}
+
+export default TableHeaderCell;

frontend/src/Components/Table/VirtualTable.tsx
@@ -1,0 +1,167 @@
+import React, { ReactNode, useEffect, useRef } from 'react';
+import { Grid, GridCellProps, WindowScroller } from 'react-virtualized';
+import ModelBase from 'App/ModelBase';
+import Scroller from 'Components/Scroller/Scroller';
+import useMeasure from 'Helpers/Hooks/useMeasure';
+import usePrevious from 'Helpers/Hooks/usePrevious';
+import { scrollDirections } from 'Helpers/Props';
+import hasDifferentItemsOrOrder from 'Utilities/Object/hasDifferentItemsOrOrder';
+import styles from './VirtualTable.css';
+
+const ROW_HEIGHT = 38;
+
+function overscanIndicesGetter(options: {
+  cellCount: number;
+  overscanCellsCount: number;
+  startIndex: number;
+  stopIndex: number;
+}) {
+  const { cellCount, overscanCellsCount, startIndex, stopIndex } = options;
+
+  // The default getter takes the scroll direction into account,
+  // but that can cause issues. Ignore the scroll direction and
+  // always over return more items.
+
+  const overscanStartIndex = startIndex - overscanCellsCount;
+  const overscanStopIndex = stopIndex + overscanCellsCount;
+
+  return {
+    overscanStartIndex: Math.max(0, overscanStartIndex),
+    overscanStopIndex: Math.min(cellCount - 1, overscanStopIndex),
+  };
+}
+
+interface VirtualTableProps<T extends ModelBase> {
+  isSmallScreen: boolean;
+  className?: string;
+  items: T[];
+  scrollIndex?: number;
+  scrollTop?: number;
+  scroller: Element;
+  header: React.ReactNode;
+  headerHeight?: number;
+  rowRenderer: (rowProps: GridCellProps) => ReactNode;
+  rowHeight?: number;
+}
+
+function VirtualTable<T extends ModelBase>({
+  isSmallScreen,
+  className = styles.tableContainer,
+  items,
+  scroller,
+  scrollIndex,
+  scrollTop,
+  header,
+  headerHeight = 38,
+  rowHeight = ROW_HEIGHT,
+  rowRenderer,
+  ...otherProps
+}: VirtualTableProps<T>) {
+  const [measureRef, bounds] = useMeasure();
+  const gridRef = useRef<Grid>(null);
+  const scrollRestored = useRef(false);
+  const previousScrollIndex = usePrevious(scrollIndex);
+  const previousItems = usePrevious(items);
+
+  const width = bounds.width;
+
+  const gridStyle = {
+    boxSizing: undefined,
+    direction: undefined,
+    height: undefined,
+    position: undefined,
+    willChange: undefined,
+    overflow: undefined,
+    width: undefined,
+  };
+
+  const containerStyle = {
+    position: undefined,
+  };
+
+  useEffect(() => {
+    if (gridRef.current && width > 0) {
+      gridRef.current.recomputeGridSize();
+    }
+  }, [width]);
+
+  useEffect(() => {
+    if (
+      gridRef.current &&
+      previousItems &&
+      hasDifferentItemsOrOrder(previousItems, items)
+    ) {
+      gridRef.current.recomputeGridSize();
+    }
+  }, [items, previousItems]);
+
+  useEffect(() => {
+    if (gridRef.current && scrollTop && !scrollRestored.current) {
+      gridRef.current.scrollToPosition({ scrollLeft: 0, scrollTop });
+      scrollRestored.current = true;
+    }
+  }, [scrollTop]);
+
+  useEffect(() => {
+    if (
+      gridRef.current &&
+      scrollIndex != null &&
+      scrollIndex !== previousScrollIndex
+    ) {
+      gridRef.current.scrollToCell({
+        rowIndex: scrollIndex,
+        columnIndex: 0,
+      });
+    }
+  }, [scrollIndex, previousScrollIndex]);
+
+  return (
+    <WindowScroller scrollElement={isSmallScreen ? undefined : scroller}>
+      {({ height, registerChild, onChildScroll, scrollTop }) => {
+        if (!height) {
+          return null;
+        }
+        return (
+          <div ref={measureRef}>
+            <Scroller
+              className={className}
+              scrollDirection={scrollDirections.HORIZONTAL}
+            >
+              {header}
+
+              {/* @ts-expect-error - ref type is incompatible */}
+              <div ref={registerChild}>
+                <Grid
+                  {...otherProps}
+                  ref={gridRef}
+                  autoContainerWidth={true}
+                  autoHeight={true}
+                  autoWidth={true}
+                  width={width}
+                  height={height}
+                  headerHeight={height - headerHeight}
+                  rowHeight={rowHeight}
+                  rowCount={items.length}
+                  columnCount={1}
+                  columnWidth={width}
+                  scrollTop={scrollTop}
+                  overscanRowCount={2}
+                  cellRenderer={rowRenderer}
+                  overscanIndicesGetter={overscanIndicesGetter}
+                  scrollToAlignment="start"
+                  isScrollingOptout={true}
+                  className={styles.tableBodyContainer}
+                  style={gridStyle}
+                  containerStyle={containerStyle}
+                  onScroll={onChildScroll}
+                />
+              </div>
+            </Scroller>
+          </div>
+        );
+      }}
+    </WindowScroller>
+  );
+}
+
+export default VirtualTable;

frontend/src/Components/Table/VirtualTableHeaderCell.tsx
@@ -1,0 +1,60 @@
+import React, { useCallback } from 'react';
+import Icon from 'Components/Icon';
+import Link from 'Components/Link/Link';
+import { icons, sortDirections } from 'Helpers/Props';
+import { SortDirection } from 'Helpers/Props/sortDirections';
+import styles from './VirtualTableHeaderCell.css';
+
+interface VirtualTableHeaderCellProps {
+  className?: string;
+  name: string;
+  isSortable?: boolean;
+  sortKey?: string;
+  fixedSortDirection?: SortDirection;
+  sortDirection?: string;
+  children?: React.ReactNode;
+  onSortPress?: (name: string, sortDirection?: SortDirection) => void;
+}
+
+function VirtualTableHeaderCell({
+  className = styles.headerCell,
+  name,
+  isSortable = false,
+  sortKey,
+  sortDirection,
+  fixedSortDirection,
+  children,
+  onSortPress,
+  ...otherProps
+}: VirtualTableHeaderCellProps) {
+  const isSorting = isSortable && sortKey === name;
+  const sortIcon =
+    sortDirection === sortDirections.ASCENDING
+      ? icons.SORT_ASCENDING
+      : icons.SORT_DESCENDING;
+
+  const handlePress = useCallback(() => {
+    if (fixedSortDirection) {
+      onSortPress?.(name, fixedSortDirection);
+    } else {
+      onSortPress?.(name);
+    }
+  }, [name, fixedSortDirection, onSortPress]);
+
+  return isSortable ? (
+    <Link
+      component="div"
+      className={className}
+      onPress={handlePress}
+      {...otherProps}
+    >
+      {children}
+
+      {isSorting ? <Icon name={sortIcon} className={styles.sortIcon} /> : null}
+    </Link>
+  ) : (
+    <div className={className}>{children}</div>
+  );
+}
+
+export default VirtualTableHeaderCell;

frontend/src/InteractiveImport/Interactive/InteractiveImportModalContent.tsx
@@ -58,7 +58,7 @@ import {
 } from 'Store/Actions/interactiveImportActions';
 import createClientSideCollectionSelector from 'Store/Selectors/createClientSideCollectionSelector';
 import { SortCallback } from 'typings/callbacks';
-import { SelectStateInputProps } from 'typings/props';
+import { CheckInputChanged } from 'typings/inputs';
 import getErrorMessage from 'Utilities/Object/getErrorMessage';
 import hasDifferentItems from 'Utilities/Object/hasDifferentItems';
 import translate from 'Utilities/String/translate';
@@ -431,7 +431,7 @@ function InteractiveImportModalContent(
   }, [previousIsDeleting, isDeleting, deleteError, onModalClose]);
 
   const onSelectAllChange = useCallback(
-    ({ value }: SelectStateInputProps) => {
+    ({ value }: CheckInputChanged) => {
       setSelectState({ type: value ? 'selectAll' : 'unselectAll', items });
     },
     [items, setSelectState]
@@ -449,8 +449,8 @@ function InteractiveImportModalContent(
 
       setWithoutEpisodeFileIdRowsSelected(
         hasEpisodeFileId || !value
-          ? without(withoutEpisodeFileIdRowsSelected, id)
-          : [...withoutEpisodeFileIdRowsSelected, id]
+          ? without(withoutEpisodeFileIdRowsSelected, id as number)
+          : [...withoutEpisodeFileIdRowsSelected, id as number]
       );
     },
     [

frontend/src/Settings/ImportLists/ImportListExclusions/ImportListExclusions.tsx
@@ -19,6 +19,7 @@ import useModalOpenState from 'Helpers/Hooks/useModalOpenState';
 import usePrevious from 'Helpers/Hooks/usePrevious';
 import useSelectState from 'Helpers/Hooks/useSelectState';
 import { icons, kinds } from 'Helpers/Props';
+import { SortDirection } from 'Helpers/Props/sortDirections';
 import {
   bulkDeleteImportListExclusions,
   clearImportListExclusions,
@@ -150,8 +151,8 @@ function ImportListExclusions() {
   });
 
   const handleSortPress = useCallback(
-    (sortKey: { sortKey: string }) => {
-      dispatch(setImportListExclusionSort({ sortKey }));
+    (sortKey: string, sortDirection?: SortDirection) => {
+      dispatch(setImportListExclusionSort({ sortKey, sortDirection }));
     },
     [dispatch]
   );

Whisparr/Whisparr

(cherry picked from commit radarr/radarr@8caa839d99)

frontend/src/Components/Table/Table.tsx
@@ -1,0 +1,124 @@
+import classNames from 'classnames';
+import React from 'react';
+import IconButton from 'Components/Link/IconButton';
+import Scroller from 'Components/Scroller/Scroller';
+import TableOptionsModalWrapper from 'Components/Table/TableOptions/TableOptionsModalWrapper';
+import { icons, scrollDirections } from 'Helpers/Props';
+import { SortDirection } from 'Helpers/Props/SortDirection';
+import { CheckInputChanged } from 'typings/inputs';
+import { TableOptionsChangePayload } from 'typings/Table';
+import Column from './Column';
+import TableHeader from './TableHeader';
+import TableHeaderCell from './TableHeaderCell';
+import TableSelectAllHeaderCell from './TableSelectAllHeaderCell';
+import styles from './Table.css';
+
+interface TableProps {
+  className?: string;
+  horizontalScroll?: boolean;
+  selectAll?: boolean;
+  allSelected?: boolean;
+  allUnselected?: boolean;
+  columns: Column[];
+  optionsComponent?: React.ElementType;
+  pageSize?: number;
+  canModifyColumns?: boolean;
+  sortKey?: string;
+  sortDirection?: SortDirection;
+  children?: React.ReactNode;
+  onSortPress?: (name: string, sortDirection?: SortDirection) => void;
+  onTableOptionChange?: (payload: TableOptionsChangePayload) => void;
+  onSelectAllChange?: (change: CheckInputChanged) => void;
+}
+
+function Table({
+  className = styles.table,
+  horizontalScroll = true,
+  selectAll = false,
+  allSelected = false,
+  allUnselected = false,
+  columns,
+  optionsComponent,
+  pageSize,
+  canModifyColumns,
+  sortKey,
+  sortDirection,
+  children,
+  onSortPress,
+  onTableOptionChange,
+  onSelectAllChange,
+}: TableProps) {
+  return (
+    <Scroller
+      className={classNames(
+        styles.tableContainer,
+        horizontalScroll && styles.horizontalScroll
+      )}
+      scrollDirection={
+        horizontalScroll ? scrollDirections.HORIZONTAL : scrollDirections.NONE
+      }
+      autoFocus={false}
+    >
+      <table className={className}>
+        <TableHeader>
+          {selectAll && onSelectAllChange ? (
+            <TableSelectAllHeaderCell
+              allSelected={allSelected}
+              allUnselected={allUnselected}
+              onSelectAllChange={onSelectAllChange}
+            />
+          ) : null}
+
+          {columns.map((column) => {
+            const { name, isVisible, isSortable, ...otherColumnProps } = column;
+
+            if (!isVisible) {
+              return null;
+            }
+
+            if (
+              (name === 'actions' || name === 'details') &&
+              onTableOptionChange
+            ) {
+              return (
+                <TableHeaderCell
+                  key={name}
+                  name={name}
+                  isSortable={false}
+                  {...otherColumnProps}
+                >
+                  <TableOptionsModalWrapper
+                    columns={columns}
+                    optionsComponent={optionsComponent}
+                    pageSize={pageSize}
+                    canModifyColumns={canModifyColumns}
+                    onTableOptionChange={onTableOptionChange}
+                  >
+                    <IconButton name={icons.ADVANCED_SETTINGS} />
+                  </TableOptionsModalWrapper>
+                </TableHeaderCell>
+              );
+            }
+
+            return (
+              <TableHeaderCell
+                key={column.name}
+                {...column}
+                sortKey={sortKey}
+                sortDirection={sortDirection}
+                onSortPress={onSortPress}
+              >
+                {typeof column.label === 'function'
+                  ? column.label()
+                  : column.label}
+              </TableHeaderCell>
+            );
+          })}
+        </TableHeader>
+        {children}
+      </table>
+    </Scroller>
+  );
+}
+
+export default Table;

frontend/src/Components/Table/TableHeaderCell.tsx
@@ -1,0 +1,70 @@
+import React, { useCallback } from 'react';
+import Icon from 'Components/Icon';
+import Link from 'Components/Link/Link';
+import { icons, sortDirections } from 'Helpers/Props';
+import { SortDirection } from 'Helpers/Props/SortDirection';
+import styles from './TableHeaderCell.css';
+
+interface TableHeaderCellProps {
+  className?: string;
+  name: string;
+  label?: string | (() => string) | React.ReactNode;
+  columnLabel?: string | (() => string);
+  isSortable?: boolean;
+  isVisible?: boolean;
+  isModifiable?: boolean;
+  sortKey?: string;
+  fixedSortDirection?: SortDirection;
+  sortDirection?: string;
+  children?: React.ReactNode;
+  onSortPress?: (name: string, sortDirection?: SortDirection) => void;
+}
+
+function TableHeaderCell({
+  className = styles.headerCell,
+  name,
+  label,
+  columnLabel,
+  isSortable = false,
+  isVisible,
+  isModifiable,
+  sortKey,
+  sortDirection,
+  fixedSortDirection,
+  children,
+  onSortPress,
+  ...otherProps
+}: TableHeaderCellProps) {
+  const isSorting = isSortable && sortKey === name;
+  const sortIcon =
+    sortDirection === sortDirections.ASCENDING
+      ? icons.SORT_ASCENDING
+      : icons.SORT_DESCENDING;
+
+  const handlePress = useCallback(() => {
+    if (fixedSortDirection) {
+      onSortPress?.(name, fixedSortDirection);
+    } else {
+      onSortPress?.(name);
+    }
+  }, [name, fixedSortDirection, onSortPress]);
+
+  return isSortable ? (
+    <Link
+      {...otherProps}
+      component="th"
+      className={className}
+      // label={typeof label === 'function' ? label() : label}
+      title={typeof columnLabel === 'function' ? columnLabel() : columnLabel}
+      onPress={handlePress}
+    >
+      {children}
+
+      {isSorting && <Icon name={sortIcon} className={styles.sortIcon} />}
+    </Link>
+  ) : (
+    <th className={className}>{children}</th>
+  );
+}
+
+export default TableHeaderCell;

frontend/src/Components/Table/VirtualTable.tsx
@@ -1,0 +1,177 @@
+import React, { ReactNode, useEffect, useRef } from 'react';
+import {
+  Grid,
+  GridCellProps,
+  WindowScroller,
+  WindowScrollerChildProps,
+} from 'react-virtualized';
+import ModelBase from 'App/ModelBase';
+import Scroller from 'Components/Scroller/Scroller';
+import useMeasure from 'Helpers/Hooks/useMeasure';
+import usePrevious from 'Helpers/Hooks/usePrevious';
+import { scrollDirections } from 'Helpers/Props';
+import hasDifferentItemsOrOrder from 'Utilities/Object/hasDifferentItemsOrOrder';
+import styles from './VirtualTable.css';
+
+const ROW_HEIGHT = 38;
+
+function overscanIndicesGetter(options: {
+  cellCount: number;
+  overscanCellsCount: number;
+  startIndex: number;
+  stopIndex: number;
+}) {
+  const { cellCount, overscanCellsCount, startIndex, stopIndex } = options;
+
+  // The default getter takes the scroll direction into account,
+  // but that can cause issues. Ignore the scroll direction and
+  // always over return more items.
+
+  const overscanStartIndex = startIndex - overscanCellsCount;
+  const overscanStopIndex = stopIndex + overscanCellsCount;
+
+  return {
+    overscanStartIndex: Math.max(0, overscanStartIndex),
+    overscanStopIndex: Math.min(cellCount - 1, overscanStopIndex),
+  };
+}
+
+interface VirtualTableProps<T extends ModelBase> {
+  isSmallScreen: boolean;
+  className?: string;
+  items: T[];
+  scrollIndex?: number;
+  scrollTop?: number;
+  scroller: Element;
+  header: React.ReactNode;
+  headerHeight?: number;
+  rowRenderer: (rowProps: GridCellProps) => ReactNode;
+  rowHeight?: number;
+}
+
+function VirtualTable<T extends ModelBase>({
+  isSmallScreen,
+  className = styles.tableContainer,
+  items,
+  scroller,
+  scrollIndex,
+  scrollTop,
+  header,
+  headerHeight = 38,
+  rowHeight = ROW_HEIGHT,
+  rowRenderer,
+  ...otherProps
+}: VirtualTableProps<T>) {
+  const [measureRef, bounds] = useMeasure();
+  const gridRef = useRef<Grid>(null);
+  const scrollRestored = useRef(false);
+  const previousScrollIndex = usePrevious(scrollIndex);
+  const previousItems = usePrevious(items);
+
+  const width = bounds.width;
+
+  const gridStyle = {
+    boxSizing: undefined,
+    direction: undefined,
+    height: undefined,
+    position: undefined,
+    willChange: undefined,
+    overflow: undefined,
+    width: undefined,
+  };
+
+  const containerStyle = {
+    position: undefined,
+  };
+
+  useEffect(() => {
+    if (gridRef.current && width > 0) {
+      gridRef.current.recomputeGridSize();
+    }
+  }, [width]);
+
+  useEffect(() => {
+    if (
+      gridRef.current &&
+      previousItems &&
+      hasDifferentItemsOrOrder(previousItems, items)
+    ) {
+      gridRef.current.recomputeGridSize();
+    }
+  }, [items, previousItems]);
+
+  useEffect(() => {
+    if (gridRef.current && scrollTop && !scrollRestored.current) {
+      gridRef.current.scrollToPosition({ scrollLeft: 0, scrollTop });
+      scrollRestored.current = true;
+    }
+  }, [scrollTop]);
+
+  useEffect(() => {
+    if (
+      gridRef.current &&
+      scrollIndex != null &&
+      scrollIndex !== previousScrollIndex
+    ) {
+      gridRef.current.scrollToCell({
+        rowIndex: scrollIndex,
+        columnIndex: 0,
+      });
+    }
+  }, [scrollIndex, previousScrollIndex]);
+
+  return (
+    <WindowScroller scrollElement={isSmallScreen ? undefined : scroller}>
+      {(props: WindowScrollerChildProps) => {
+        const { height, registerChild, onChildScroll, scrollTop } = props;
+        const registerRef = (el: HTMLDivElement | null) => {
+          // `registerChild` expects a ReactNode-like value in the lib typings;
+          // wrap/cast here so we can pass the DOM element as the ref.
+          (registerChild as unknown as (el?: HTMLElement | null) => void)(el);
+        };
+        if (!height) {
+          return null;
+        }
+        return (
+          <div ref={measureRef}>
+            <Scroller
+              className={className}
+              scrollDirection={scrollDirections.HORIZONTAL}
+            >
+              {header}
+
+              <div ref={registerRef}>
+                <Grid
+                  {...otherProps}
+                  ref={gridRef}
+                  autoContainerWidth={true}
+                  autoHeight={true}
+                  autoWidth={true}
+                  width={width}
+                  height={height}
+                  headerHeight={height - headerHeight}
+                  rowHeight={rowHeight}
+                  rowCount={items.length}
+                  columnCount={1}
+                  columnWidth={width}
+                  scrollTop={scrollTop}
+                  overscanRowCount={2}
+                  cellRenderer={rowRenderer}
+                  overscanIndicesGetter={overscanIndicesGetter}
+                  scrollToAlignment="start"
+                  isScrollingOptout={true}
+                  className={styles.tableBodyContainer}
+                  style={gridStyle}
+                  containerStyle={containerStyle}
+                  onScroll={onChildScroll}
+                />
+              </div>
+            </Scroller>
+          </div>
+        );
+      }}
+    </WindowScroller>
+  );
+}
+
+export default VirtualTable;

frontend/src/Components/Table/VirtualTableHeaderCell.tsx
@@ -1,0 +1,60 @@
+import React, { useCallback } from 'react';
+import Icon from 'Components/Icon';
+import Link from 'Components/Link/Link';
+import { icons, sortDirections } from 'Helpers/Props';
+import { SortDirection } from 'Helpers/Props/SortDirection';
+import styles from './VirtualTableHeaderCell.css';
+
+interface VirtualTableHeaderCellProps {
+  className?: string;
+  name: string;
+  isSortable?: boolean;
+  sortKey?: string;
+  fixedSortDirection?: SortDirection;
+  sortDirection?: string;
+  children?: React.ReactNode;
+  onSortPress?: (name: string, sortDirection?: SortDirection) => void;
+}
+
+function VirtualTableHeaderCell({
+  className = styles.headerCell,
+  name,
+  isSortable = false,
+  sortKey,
+  sortDirection,
+  fixedSortDirection,
+  children,
+  onSortPress,
+  ...otherProps
+}: VirtualTableHeaderCellProps) {
+  const isSorting = isSortable && sortKey === name;
+  const sortIcon =
+    sortDirection === sortDirections.ASCENDING
+      ? icons.SORT_ASCENDING
+      : icons.SORT_DESCENDING;
+
+  const handlePress = useCallback(() => {
+    if (fixedSortDirection) {
+      onSortPress?.(name, fixedSortDirection);
+    } else {
+      onSortPress?.(name);
+    }
+  }, [name, fixedSortDirection, onSortPress]);
+
+  return isSortable ? (
+    <Link
+      component="div"
+      className={className}
+      onPress={handlePress}
+      {...otherProps}
+    >
+      {children}
+
+      {isSorting ? <Icon name={sortIcon} className={styles.sortIcon} /> : null}
+    </Link>
+  ) : (
+    <div className={className}>{children}</div>
+  );
+}
+
+export default VirtualTableHeaderCell;

frontend/src/InteractiveImport/Interactive/InteractiveImportModalContent.tsx
@@ -53,7 +53,7 @@ import {
 } from 'Store/Actions/movieFileActions';
 import createClientSideCollectionSelector from 'Store/Selectors/createClientSideCollectionSelector';
 import { SortCallback } from 'typings/callbacks';
-import { SelectStateInputProps } from 'typings/props';
+import { CheckInputChanged } from 'typings/inputs';
 import getErrorMessage from 'Utilities/Object/getErrorMessage';
 import translate from 'Utilities/String/translate';
 import getSelectedIds from 'Utilities/Table/getSelectedIds';
@@ -363,7 +363,7 @@ function InteractiveImportModalContent(
   }, [previousIsDeleting, isDeleting, deleteError, onModalClose]);
 
   const onSelectAllChange = useCallback(
-    ({ value }: SelectStateInputProps) => {
+    ({ value }: CheckInputChanged) => {
       setSelectState({ type: value ? 'selectAll' : 'unselectAll', items });
     },
     [items, setSelectState]
@@ -381,8 +381,8 @@ function InteractiveImportModalContent(
 
       setWithoutMovieFileIdRowsSelected(
         hasMovieFileId || !value
-          ? without(withoutMovieFileIdRowsSelected, id)
-          : [...withoutMovieFileIdRowsSelected, id]
+          ? without(withoutMovieFileIdRowsSelected, id as number)
+          : [...withoutMovieFileIdRowsSelected, id as number]
       );
     },
     [

frontend/src/Settings/ImportLists/ImportListExclusions/ImportListExclusions.tsx
@@ -19,6 +19,7 @@ import useModalOpenState from 'Helpers/Hooks/useModalOpenState';
 import usePrevious from 'Helpers/Hooks/usePrevious';
 import useSelectState from 'Helpers/Hooks/useSelectState';
 import { icons, kinds } from 'Helpers/Props';
+import { SortDirection } from 'Helpers/Props/SortDirection';
 import {
   bulkDeleteImportListExclusions,
   clearImportListExclusions,
@@ -162,8 +163,8 @@ function ImportListExclusions() {
   });
 
   const handleSortPress = useCallback(
-    (sortKey: { sortKey: string }) => {
-      dispatch(setImportListExclusionSort({ sortKey }));
+    (sortKey: string, sortDirection?: SortDirection) => {
+      dispatch(setImportListExclusionSort({ sortKey, sortDirection }));
     },
     [dispatch]
   );
Radarr/Radarr

Fixed: Update AutoTags on movie add (#11079)
Co-authored-by: aden <aden@hughorse.net>

src/NzbDrone.Integration.Test/ApiTests/MovieFixture.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
+using Radarr.Api.V3.AutoTagging;
+using Radarr.Http.ClientSchema;
 
 namespace NzbDrone.Integration.Test.ApiTests
 {
@@ -27,6 +29,53 @@ namespace NzbDrone.Integration.Test.ApiTests
 
             result.Should().NotBeNull();
             result.Tags.Should().Equal(tag.Id);
+        }
+
+        [Test]
+        [Order(0)]
+        public void add_movie_should_trigger_autotag()
+        {
+            var tag = EnsureTag("autotag-test");
+            var movie = Movies.Lookup("imdb:tt0110912").Single();
+            movie.Genres = new List<string> { "Thriller" };
+
+            var item = AutoTagging.Post(new AutoTaggingResource
+            {
+                Name = "Test",
+                RemoveTagsAutomatically = false,
+                Tags = new HashSet<int> { tag.Id },
+                Specifications = new List<AutoTaggingSpecificationSchema>
+                {
+                    new AutoTaggingSpecificationSchema
+                    {
+                        Name = "Test",
+                        Implementation = "GenreSpecification",
+                        ImplementationName = "Genre",
+                        Negate = false,
+                        Required = false,
+                        Fields = new List<Field>
+                        {
+                            new Field
+                            {
+                                Name = "value",
+                                Label = "Genre(s)",
+                                Type = "tag",
+                                Value = new List<string> { "Thriller" }
+                            }
+                        }
+                    }
+                }
+            });
+
+            EnsureNoMovie(680, "Pulp Fiction");
+
+            movie.QualityProfileId = 1;
+            movie.Path = Path.Combine(MovieRootFolder, movie.Title);
+
+            var result = Movies.Post(movie);
+
+            result.Should().NotBeNull();
+            result.Tags.Should().Contain(tag.Id);
         }
 
         [Test]

src/NzbDrone.Integration.Test/IntegrationTestBase.cs
@@ -17,6 +17,7 @@ using NzbDrone.Core.Qualities;
 using NzbDrone.Integration.Test.Client;
 using NzbDrone.SignalR;
 using NzbDrone.Test.Common.Categories;
+using Radarr.Api.V3.AutoTagging;
 using Radarr.Api.V3.Blocklist;
 using Radarr.Api.V3.Config;
 using Radarr.Api.V3.DownloadClient;
@@ -36,6 +37,7 @@ namespace NzbDrone.Integration.Test
     {
         protected RestClient RestClient { get; private set; }
 
+        public ClientBase<AutoTaggingResource> AutoTagging;
         public ClientBase<BlocklistResource> Blocklist;
         public CommandClient Commands;
         public ClientBase<TaskResource> Tasks;
@@ -99,6 +101,7 @@ namespace NzbDrone.Integration.Test
             RestClient.AddDefaultHeader("Authentication", ApiKey);
             RestClient.AddDefaultHeader("X-Api-Key", ApiKey);
 
+            AutoTagging = new ClientBase<AutoTaggingResource>(RestClient, ApiKey);
             Blocklist = new ClientBase<BlocklistResource>(RestClient, ApiKey);
             Commands = new CommandClient(RestClient, ApiKey);
             Tasks = new ClientBase<TaskResource>(RestClient, ApiKey, "system/task");

Whisparr/Whisparr

(cherry picked from commit radarr/radarr@f5faf52469)

src/NzbDrone.Integration.Test/ApiTests/MovieFixture.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
+using Whisparr.Api.V3.AutoTagging;
+using Whisparr.Http.ClientSchema;
 
 namespace NzbDrone.Integration.Test.ApiTests
 {
@@ -27,6 +29,53 @@ namespace NzbDrone.Integration.Test.ApiTests
 
             result.Should().NotBeNull();
             result.Tags.Should().Equal(tag.Id);
+        }
+
+        [Test]
+        [Order(0)]
+        public void add_movie_should_trigger_autotag()
+        {
+            var tag = EnsureTag("autotag-test");
+            var movie = Movies.Lookup("imdb:tt0110912").Single();
+            movie.Genres = new List<string> { "Thriller" };
+
+            var item = AutoTagging.Post(new AutoTaggingResource
+            {
+                Name = "Test",
+                RemoveTagsAutomatically = false,
+                Tags = new HashSet<int> { tag.Id },
+                Specifications = new List<AutoTaggingSpecificationSchema>
+                {
+                    new AutoTaggingSpecificationSchema
+                    {
+                        Name = "Test",
+                        Implementation = "GenreSpecification",
+                        ImplementationName = "Genre",
+                        Negate = false,
+                        Required = false,
+                        Fields = new List<Field>
+                        {
+                            new Field
+                            {
+                                Name = "value",
+                                Label = "Genre(s)",
+                                Type = "tag",
+                                Value = new List<string> { "Thriller" }
+                            }
+                        }
+                    }
+                }
+            });
+
+            EnsureNoMovie(680, "Pulp Fiction");
+
+            movie.QualityProfileId = 1;
+            movie.Path = Path.Combine(MovieRootFolder, movie.Title);
+
+            var result = Movies.Post(movie);
+
+            result.Should().NotBeNull();
+            result.Tags.Should().Contain(tag.Id);
         }
 
         [Test]

src/NzbDrone.Integration.Test/IntegrationTestBase.cs
@@ -18,6 +18,7 @@ using NzbDrone.Integration.Test.Client;
 using NzbDrone.SignalR;
 using NzbDrone.Test.Common.Categories;
 using RestSharp;
+using Whisparr.Api.V3.AutoTagging;
 using Whisparr.Api.V3.Blocklist;
 using Whisparr.Api.V3.Config;
 using Whisparr.Api.V3.DownloadClient;
@@ -36,6 +37,7 @@ namespace NzbDrone.Integration.Test
     {
         protected RestClient RestClient { get; private set; }
 
+        public ClientBase<AutoTaggingResource> AutoTagging;
         public ClientBase<BlocklistResource> Blocklist;
         public CommandClient Commands;
         public ClientBase<TaskResource> Tasks;
@@ -99,6 +101,7 @@ namespace NzbDrone.Integration.Test
             RestClient.AddDefaultHeader("Authentication", ApiKey);
             RestClient.AddDefaultHeader("X-Api-Key", ApiKey);
 
+            AutoTagging = new ClientBase<AutoTaggingResource>(RestClient, ApiKey);
             Blocklist = new ClientBase<BlocklistResource>(RestClient, ApiKey);
             Commands = new CommandClient(RestClient, ApiKey);
             Tasks = new ClientBase<TaskResource>(RestClient, ApiKey, "system/task");
Radarr/Radarr

Convert Calendar to TypeScript
(cherry picked from commit 811eb36c7b1a5124270ff93d18d16944e654de81)

Co-authored-by: Mark McDowall <mark@mcdowall.ca>

Closes #10764
Closes #10776
Closes #10781

frontend/src/Calendar/CalendarPage.tsx
@@ -1,0 +1,226 @@
+import moment from 'moment';
+import React, { useCallback, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+import AppState from 'App/State/AppState';
+import * as commandNames from 'Commands/commandNames';
+import Measure from 'Components/Measure';
+import FilterMenu from 'Components/Menu/FilterMenu';
+import PageContent from 'Components/Page/PageContent';
+import PageContentBody from 'Components/Page/PageContentBody';
+import PageToolbar from 'Components/Page/Toolbar/PageToolbar';
+import PageToolbarButton from 'Components/Page/Toolbar/PageToolbarButton';
+import PageToolbarSection from 'Components/Page/Toolbar/PageToolbarSection';
+import PageToolbarSeparator from 'Components/Page/Toolbar/PageToolbarSeparator';
+import { align, icons } from 'Helpers/Props';
+import NoMovie from 'Movie/NoMovie';
+import {
+  searchMissing,
+  setCalendarDaysCount,
+  setCalendarFilter,
+} from 'Store/Actions/calendarActions';
+import { executeCommand } from 'Store/Actions/commandActions';
+import { createCustomFiltersSelector } from 'Store/Selectors/createClientSideCollectionSelector';
+import createCommandExecutingSelector from 'Store/Selectors/createCommandExecutingSelector';
+import createCommandsSelector from 'Store/Selectors/createCommandsSelector';
+import createMovieCountSelector from 'Store/Selectors/createMovieCountSelector';
+import { isCommandExecuting } from 'Utilities/Command';
+import isBefore from 'Utilities/Date/isBefore';
+import translate from 'Utilities/String/translate';
+import Calendar from './Calendar';
+import CalendarFilterModal from './CalendarFilterModal';
+import CalendarLinkModal from './iCal/CalendarLinkModal';
+import Legend from './Legend/Legend';
+import CalendarOptionsModal from './Options/CalendarOptionsModal';
+import styles from './CalendarPage.css';
+
+const MINIMUM_DAY_WIDTH = 120;
+
+function createMissingMovieIdsSelector() {
+  return createSelector(
+    (state: AppState) => state.calendar.start,
+    (state: AppState) => state.calendar.end,
+    (state: AppState) => state.calendar.items,
+    (state: AppState) => state.queue.details.items,
+    (start, end, movies, queueDetails) => {
+      return movies.reduce<number[]>((acc, movie) => {
+        const { inCinemas } = movie;
+
+        if (
+          !movie.movieFileId &&
+          moment(inCinemas).isAfter(start) &&
+          moment(inCinemas).isBefore(end) &&
+          isBefore(movie.inCinemas) &&
+          !queueDetails.some(
+            (details) => !!details.movie && details.movie.id === movie.id
+          )
+        ) {
+          acc.push(movie.id);
+        }
+
+        return acc;
+      }, []);
+    }
+  );
+}
+
+function createIsSearchingSelector() {
+  return createSelector(
+    (state: AppState) => state.calendar.searchMissingCommandId,
+    createCommandsSelector(),
+    (searchMissingCommandId, commands) => {
+      if (searchMissingCommandId == null) {
+        return false;
+      }
+
+      return isCommandExecuting(
+        commands.find((command) => {
+          return command.id === searchMissingCommandId;
+        })
+      );
+    }
+  );
+}
+
+function CalendarPage() {
+  const dispatch = useDispatch();
+
+  const { selectedFilterKey, filters } = useSelector(
+    (state: AppState) => state.calendar
+  );
+  const missingMovieIds = useSelector(createMissingMovieIdsSelector());
+  const isSearchingForMissing = useSelector(createIsSearchingSelector());
+  const isRssSyncExecuting = useSelector(
+    createCommandExecutingSelector(commandNames.RSS_SYNC)
+  );
+  const customFilters = useSelector(createCustomFiltersSelector('calendar'));
+  const hasMovies = !!useSelector(createMovieCountSelector());
+
+  const [isCalendarLinkModalOpen, setIsCalendarLinkModalOpen] = useState(false);
+  const [isOptionsModalOpen, setIsOptionsModalOpen] = useState(false);
+  const [width, setWidth] = useState(0);
+
+  const isMeasured = width > 0;
+  const PageComponent = hasMovies ? Calendar : NoMovie;
+
+  const handleMeasure = useCallback(
+    ({ width: newWidth }: { width: number }) => {
+      setWidth(newWidth);
+
+      const dayCount = Math.max(
+        3,
+        Math.min(7, Math.floor(newWidth / MINIMUM_DAY_WIDTH))
+      );
+
+      dispatch(setCalendarDaysCount({ dayCount }));
+    },
+    [dispatch]
+  );
+
+  const handleGetCalendarLinkPress = useCallback(() => {
+    setIsCalendarLinkModalOpen(true);
+  }, []);
+
+  const handleGetCalendarLinkModalClose = useCallback(() => {
+    setIsCalendarLinkModalOpen(false);
+  }, []);
+
+  const handleOptionsPress = useCallback(() => {
+    setIsOptionsModalOpen(true);
+  }, []);
+
+  const handleOptionsModalClose = useCallback(() => {
+    setIsOptionsModalOpen(false);
+  }, []);
+
+  const handleRssSyncPress = useCallback(() => {
+    dispatch(
+      executeCommand({
+        name: commandNames.RSS_SYNC,
+      })
+    );
+  }, [dispatch]);
+
+  const handleSearchMissingPress = useCallback(() => {
+    dispatch(searchMissing({ movieIds: missingMovieIds }));
+  }, [missingMovieIds, dispatch]);
+
+  const handleFilterSelect = useCallback(
+    (key: string | number) => {
+      dispatch(setCalendarFilter({ selectedFilterKey: key }));
+    },
+    [dispatch]
+  );
+
+  return (
+    <PageContent title={translate('Calendar')}>
+      <PageToolbar>
+        <PageToolbarSection>
+          <PageToolbarButton
+            label={translate('ICalLink')}
+            iconName={icons.CALENDAR}
+            onPress={handleGetCalendarLinkPress}
+          />
+
+          <PageToolbarSeparator />
+
+          <PageToolbarButton
+            label={translate('RssSync')}
+            iconName={icons.RSS}
+            isSpinning={isRssSyncExecuting}
+            onPress={handleRssSyncPress}
+          />
+
+          <PageToolbarButton
+            label={translate('SearchForMissing')}
+            iconName={icons.SEARCH}
+            isDisabled={!missingMovieIds.length}
+            isSpinning={isSearchingForMissing}
+            onPress={handleSearchMissingPress}
+          />
+        </PageToolbarSection>
+
+        <PageToolbarSection alignContent={align.RIGHT}>
+          <PageToolbarButton
+            label={translate('Options')}
+            iconName={icons.POSTER}
+            onPress={handleOptionsPress}
+          />
+
+          <FilterMenu
+            alignMenu={align.RIGHT}
+            isDisabled={!hasMovies}
+            selectedFilterKey={selectedFilterKey}
+            filters={filters}
+            customFilters={customFilters}
+            filterModalConnectorComponent={CalendarFilterModal}
+            onFilterSelect={handleFilterSelect}
+          />
+        </PageToolbarSection>
+      </PageToolbar>
+
+      <PageContentBody
+        className={styles.calendarPageBody}
+        innerClassName={styles.calendarInnerPageBody}
+      >
+        <Measure whitelist={['width']} onMeasure={handleMeasure}>
+          {isMeasured ? <PageComponent totalItems={0} /> : <div />}
+        </Measure>
+
+        {hasMovies && <Legend />}
+      </PageContentBody>
+
+      <CalendarLinkModal
+        isOpen={isCalendarLinkModalOpen}
+        onModalClose={handleGetCalendarLinkModalClose}
+      />
+
+      <CalendarOptionsModal
+        isOpen={isOptionsModalOpen}
+        onModalClose={handleOptionsModalClose}
+      />
+    </PageContent>
+  );
+}
+
+export default CalendarPage;

frontend/src/Calendar/iCal/CalendarLinkModalContent.tsx
@@ -1,0 +1,149 @@
+import React, { FocusEvent, useCallback, useMemo, useState } from 'react';
+import Form from 'Components/Form/Form';
+import FormGroup from 'Components/Form/FormGroup';
+import FormInputButton from 'Components/Form/FormInputButton';
+import FormInputGroup from 'Components/Form/FormInputGroup';
+import FormLabel from 'Components/Form/FormLabel';
+import Icon from 'Components/Icon';
+import Button from 'Components/Link/Button';
+import ClipboardButton from 'Components/Link/ClipboardButton';
+import ModalBody from 'Components/Modal/ModalBody';
+import ModalContent from 'Components/Modal/ModalContent';
+import ModalFooter from 'Components/Modal/ModalFooter';
+import ModalHeader from 'Components/Modal/ModalHeader';
+import { icons, inputTypes, kinds, sizes } from 'Helpers/Props';
+import { InputChanged } from 'typings/inputs';
+import translate from 'Utilities/String/translate';
+
+interface CalendarLinkModalContentProps {
+  onModalClose: () => void;
+}
+
+function CalendarLinkModalContent({
+  onModalClose,
+}: CalendarLinkModalContentProps) {
+  const [state, setState] = useState({
+    unmonitored: false,
+    asAllDay: false,
+    tags: [],
+  });
+
+  const { unmonitored, asAllDay, tags } = state;
+
+  const handleInputChange = useCallback(({ name, value }: InputChanged) => {
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  }, []);
+
+  const handleLinkFocus = useCallback(
+    (event: FocusEvent<HTMLInputElement, Element>) => {
+      event.target.select();
+    },
+    []
+  );
+
+  const { iCalHttpUrl, iCalWebCalUrl } = useMemo(() => {
+    let icalUrl = `${window.location.host}${window.Radarr.urlBase}/feed/v3/calendar/Radarr.ics?`;
+
+    if (unmonitored) {
+      icalUrl += 'unmonitored=true&';
+    }
+
+    if (asAllDay) {
+      icalUrl += 'asAllDay=true&';
+    }
+
+    if (tags.length) {
+      icalUrl += `tags=${tags.toString()}&`;
+    }
+
+    icalUrl += `apikey=${encodeURIComponent(window.Radarr.apiKey)}`;
+
+    return {
+      iCalHttpUrl: `${window.location.protocol}//${icalUrl}`,
+      iCalWebCalUrl: `webcal://${icalUrl}`,
+    };
+  }, [unmonitored, asAllDay, tags]);
+
+  return (
+    <ModalContent onModalClose={onModalClose}>
+      <ModalHeader>{translate('CalendarFeed')}</ModalHeader>
+
+      <ModalBody>
+        <Form>
+          <FormGroup>
+            <FormLabel>{translate('IncludeUnmonitored')}</FormLabel>
+
+            <FormInputGroup
+              type={inputTypes.CHECK}
+              name="unmonitored"
+              value={unmonitored}
+              helpText={translate('ICalIncludeUnmonitoredMoviesHelpText')}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <FormLabel>{translate('ICalShowAsAllDayEvents')}</FormLabel>
+
+            <FormInputGroup
+              type={inputTypes.CHECK}
+              name="asAllDay"
+              value={asAllDay}
+              helpText={translate('ICalShowAsAllDayEventsHelpText')}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <FormLabel>{translate('Tags')}</FormLabel>
+
+            <FormInputGroup
+              type={inputTypes.MOVIE_TAG}
+              name="tags"
+              value={tags}
+              helpText={translate('ICalTagsMoviesHelpText')}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+
+          <FormGroup size={sizes.LARGE}>
+            <FormLabel>{translate('ICalFeed')}</FormLabel>
+
+            <FormInputGroup
+              type={inputTypes.TEXT}
+              name="iCalHttpUrl"
+              value={iCalHttpUrl}
+              readOnly={true}
+              helpText={translate('ICalFeedHelpText')}
+              buttons={[
+                <ClipboardButton
+                  key="copy"
+                  value={iCalHttpUrl}
+                  kind={kinds.DEFAULT}
+                />,
+
+                <FormInputButton
+                  key="webcal"
+                  kind={kinds.DEFAULT}
+                  to={iCalWebCalUrl}
+                  target="_blank"
+                  noRouter={true}
+                >
+                  <Icon name={icons.CALENDAR_O} />
+                </FormInputButton>,
+              ]}
+              onChange={handleInputChange}
+              onFocus={handleLinkFocus}
+            />
+          </FormGroup>
+        </Form>
+      </ModalBody>
+
+      <ModalFooter>
+        <Button onPress={onModalClose}>{translate('Close')}</Button>
+      </ModalFooter>
+    </ModalContent>
+  );
+}
+
+export default CalendarLinkModalContent;

Whisparr/Whisparr

3f35b7c782 Convert Calendar to TypeScript
(cherry picked from commit radarr/radarr@)

frontend/src/Calendar/CalendarPage.tsx
@@ -1,0 +1,226 @@
+import moment from 'moment';
+import React, { useCallback, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+import AppState from 'App/State/AppState';
+import * as commandNames from 'Commands/commandNames';
+import Measure from 'Components/Measure';
+import FilterMenu from 'Components/Menu/FilterMenu';
+import PageContent from 'Components/Page/PageContent';
+import PageContentBody from 'Components/Page/PageContentBody';
+import PageToolbar from 'Components/Page/Toolbar/PageToolbar';
+import PageToolbarButton from 'Components/Page/Toolbar/PageToolbarButton';
+import PageToolbarSection from 'Components/Page/Toolbar/PageToolbarSection';
+import PageToolbarSeparator from 'Components/Page/Toolbar/PageToolbarSeparator';
+import { align, icons } from 'Helpers/Props';
+import NoMovie from 'Movie/NoMovie';
+import {
+  searchMissing,
+  setCalendarDaysCount,
+  setCalendarFilter,
+} from 'Store/Actions/calendarActions';
+import { executeCommand } from 'Store/Actions/commandActions';
+import { createCustomFiltersSelector } from 'Store/Selectors/createClientSideCollectionSelector';
+import createCommandExecutingSelector from 'Store/Selectors/createCommandExecutingSelector';
+import createCommandsSelector from 'Store/Selectors/createCommandsSelector';
+import createMovieCountSelector from 'Store/Selectors/createMovieCountSelector';
+import { isCommandExecuting } from 'Utilities/Command';
+import isBefore from 'Utilities/Date/isBefore';
+import translate from 'Utilities/String/translate';
+import Calendar from './Calendar';
+import CalendarFilterModal from './CalendarFilterModal';
+import CalendarLinkModal from './iCal/CalendarLinkModal';
+import Legend from './Legend/Legend';
+import CalendarOptionsModal from './Options/CalendarOptionsModal';
+import styles from './CalendarPage.css';
+
+const MINIMUM_DAY_WIDTH = 120;
+
+function createMissingMovieIdsSelector() {
+  return createSelector(
+    (state: AppState) => state.calendar.start,
+    (state: AppState) => state.calendar.end,
+    (state: AppState) => state.calendar.items,
+    (state: AppState) => state.queue.details.items,
+    (start, end, movies, queueDetails) => {
+      return movies.reduce<number[]>((acc, movie) => {
+        const { releaseDate } = movie;
+
+        if (
+          !movie.movieFileId &&
+          moment(releaseDate).isAfter(start) &&
+          moment(releaseDate).isBefore(end) &&
+          isBefore(movie.releaseDate) &&
+          !queueDetails.some(
+            (details) => !!details.movie && details.movie.id === movie.id
+          )
+        ) {
+          acc.push(movie.id);
+        }
+
+        return acc;
+      }, []);
+    }
+  );
+}
+
+function createIsSearchingSelector() {
+  return createSelector(
+    (state: AppState) => state.calendar.searchMissingCommandId,
+    createCommandsSelector(),
+    (searchMissingCommandId, commands) => {
+      if (searchMissingCommandId == null) {
+        return false;
+      }
+
+      return isCommandExecuting(
+        commands.find((command) => {
+          return command.id === searchMissingCommandId;
+        })
+      );
+    }
+  );
+}
+
+function CalendarPage() {
+  const dispatch = useDispatch();
+
+  const { selectedFilterKey, filters } = useSelector(
+    (state: AppState) => state.calendar
+  );
+  const missingMovieIds = useSelector(createMissingMovieIdsSelector());
+  const isSearchingForMissing = useSelector(createIsSearchingSelector());
+  const isRssSyncExecuting = useSelector(
+    createCommandExecutingSelector(commandNames.RSS_SYNC)
+  );
+  const customFilters = useSelector(createCustomFiltersSelector('calendar'));
+  const hasMovies = !!useSelector(createMovieCountSelector());
+
+  const [isCalendarLinkModalOpen, setIsCalendarLinkModalOpen] = useState(false);
+  const [isOptionsModalOpen, setIsOptionsModalOpen] = useState(false);
+  const [width, setWidth] = useState(0);
+
+  const isMeasured = width > 0;
+  const PageComponent = hasMovies ? Calendar : NoMovie;
+
+  const handleMeasure = useCallback(
+    ({ width: newWidth }: { width: number }) => {
+      setWidth(newWidth);
+
+      const dayCount = Math.max(
+        3,
+        Math.min(7, Math.floor(newWidth / MINIMUM_DAY_WIDTH))
+      );
+
+      dispatch(setCalendarDaysCount({ dayCount }));
+    },
+    [dispatch]
+  );
+
+  const handleGetCalendarLinkPress = useCallback(() => {
+    setIsCalendarLinkModalOpen(true);
+  }, []);
+
+  const handleGetCalendarLinkModalClose = useCallback(() => {
+    setIsCalendarLinkModalOpen(false);
+  }, []);
+
+  const handleOptionsPress = useCallback(() => {
+    setIsOptionsModalOpen(true);
+  }, []);
+
+  const handleOptionsModalClose = useCallback(() => {
+    setIsOptionsModalOpen(false);
+  }, []);
+
+  const handleRssSyncPress = useCallback(() => {
+    dispatch(
+      executeCommand({
+        name: commandNames.RSS_SYNC,
+      })
+    );
+  }, [dispatch]);
+
+  const handleSearchMissingPress = useCallback(() => {
+    dispatch(searchMissing({ movieIds: missingMovieIds }));
+  }, [missingMovieIds, dispatch]);
+
+  const handleFilterSelect = useCallback(
+    (key: string | number) => {
+      dispatch(setCalendarFilter({ selectedFilterKey: key }));
+    },
+    [dispatch]
+  );
+
+  return (
+    <PageContent title={translate('Calendar')}>
+      <PageToolbar>
+        <PageToolbarSection>
+          <PageToolbarButton
+            label={translate('ICalLink')}
+            iconName={icons.CALENDAR}
+            onPress={handleGetCalendarLinkPress}
+          />
+
+          <PageToolbarSeparator />
+
+          <PageToolbarButton
+            label={translate('RssSync')}
+            iconName={icons.RSS}
+            isSpinning={isRssSyncExecuting}
+            onPress={handleRssSyncPress}
+          />
+
+          <PageToolbarButton
+            label={translate('SearchForMissing')}
+            iconName={icons.SEARCH}
+            isDisabled={!missingMovieIds.length}
+            isSpinning={isSearchingForMissing}
+            onPress={handleSearchMissingPress}
+          />
+        </PageToolbarSection>
+
+        <PageToolbarSection alignContent={align.RIGHT}>
+          <PageToolbarButton
+            label={translate('Options')}
+            iconName={icons.POSTER}
+            onPress={handleOptionsPress}
+          />
+
+          <FilterMenu
+            alignMenu={align.RIGHT}
+            isDisabled={!hasMovies}
+            selectedFilterKey={selectedFilterKey}
+            filters={filters}
+            customFilters={customFilters}
+            filterModalConnectorComponent={CalendarFilterModal}
+            onFilterSelect={handleFilterSelect}
+          />
+        </PageToolbarSection>
+      </PageToolbar>
+
+      <PageContentBody
+        className={styles.calendarPageBody}
+        innerClassName={styles.calendarInnerPageBody}
+      >
+        <Measure whitelist={['width']} onMeasure={handleMeasure}>
+          {isMeasured ? <PageComponent totalItems={0} /> : <div />}
+        </Measure>
+
+        {hasMovies && <Legend />}
+      </PageContentBody>
+
+      <CalendarLinkModal
+        isOpen={isCalendarLinkModalOpen}
+        onModalClose={handleGetCalendarLinkModalClose}
+      />
+
+      <CalendarOptionsModal
+        isOpen={isOptionsModalOpen}
+        onModalClose={handleOptionsModalClose}
+      />
+    </PageContent>
+  );
+}
+
+export default CalendarPage;

frontend/src/Calendar/iCal/CalendarLinkModalContent.tsx
@@ -1,0 +1,149 @@
+import React, { FocusEvent, useCallback, useMemo, useState } from 'react';
+import Form from 'Components/Form/Form';
+import FormGroup from 'Components/Form/FormGroup';
+import FormInputButton from 'Components/Form/FormInputButton';
+import FormInputGroup from 'Components/Form/FormInputGroup';
+import FormLabel from 'Components/Form/FormLabel';
+import Icon from 'Components/Icon';
+import Button from 'Components/Link/Button';
+import ClipboardButton from 'Components/Link/ClipboardButton';
+import ModalBody from 'Components/Modal/ModalBody';
+import ModalContent from 'Components/Modal/ModalContent';
+import ModalFooter from 'Components/Modal/ModalFooter';
+import ModalHeader from 'Components/Modal/ModalHeader';
+import { icons, inputTypes, kinds, sizes } from 'Helpers/Props';
+import { InputChanged } from 'typings/inputs';
+import translate from 'Utilities/String/translate';
+
+interface CalendarLinkModalContentProps {
+  onModalClose: () => void;
+}
+
+function CalendarLinkModalContent({
+  onModalClose,
+}: CalendarLinkModalContentProps) {
+  const [state, setState] = useState({
+    unmonitored: false,
+    asAllDay: false,
+    tags: [],
+  });
+
+  const { unmonitored, asAllDay, tags } = state;
+
+  const handleInputChange = useCallback(({ name, value }: InputChanged) => {
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  }, []);
+
+  const handleLinkFocus = useCallback(
+    (event: FocusEvent<HTMLInputElement, Element>) => {
+      event.target.select();
+    },
+    []
+  );
+
+  const { iCalHttpUrl, iCalWebCalUrl } = useMemo(() => {
+    let icalUrl = `${window.location.host}${window.Whisparr.urlBase}/feed/v3/calendar/Whisparr.ics?`;
+
+    if (unmonitored) {
+      icalUrl += 'unmonitored=true&';
+    }
+
+    if (asAllDay) {
+      icalUrl += 'asAllDay=true&';
+    }
+
+    if (tags.length) {
+      icalUrl += `tags=${tags.toString()}&`;
+    }
+
+    icalUrl += `apikey=${encodeURIComponent(window.Whisparr.apiKey)}`;
+
+    return {
+      iCalHttpUrl: `${window.location.protocol}//${icalUrl}`,
+      iCalWebCalUrl: `webcal://${icalUrl}`,
+    };
+  }, [unmonitored, asAllDay, tags]);
+
+  return (
+    <ModalContent onModalClose={onModalClose}>
+      <ModalHeader>{translate('CalendarFeed')}</ModalHeader>
+
+      <ModalBody>
+        <Form>
+          <FormGroup>
+            <FormLabel>{translate('IncludeUnmonitored')}</FormLabel>
+
+            <FormInputGroup
+              type={inputTypes.CHECK}
+              name="unmonitored"
+              value={unmonitored}
+              helpText={translate('ICalIncludeUnmonitoredMoviesHelpText')}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <FormLabel>{translate('ICalShowAsAllDayEvents')}</FormLabel>
+
+            <FormInputGroup
+              type={inputTypes.CHECK}
+              name="asAllDay"
+              value={asAllDay}
+              helpText={translate('ICalShowAsAllDayEventsHelpText')}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <FormLabel>{translate('Tags')}</FormLabel>
+
+            <FormInputGroup
+              type={inputTypes.MOVIE_TAG}
+              name="tags"
+              value={tags}
+              helpText={translate('ICalTagsMoviesHelpText')}
+              onChange={handleInputChange}
+            />
+          </FormGroup>
+
+          <FormGroup size={sizes.LARGE}>
+            <FormLabel>{translate('ICalFeed')}</FormLabel>
+
+            <FormInputGroup
+              type={inputTypes.TEXT}
+              name="iCalHttpUrl"
+              value={iCalHttpUrl}
+              readOnly={true}
+              helpText={translate('ICalFeedHelpText')}
+              buttons={[
+                <ClipboardButton
+                  key="copy"
+                  value={iCalHttpUrl}
+                  kind={kinds.DEFAULT}
+                />,
+
+                <FormInputButton
+                  key="webcal"
+                  kind={kinds.DEFAULT}
+                  to={iCalWebCalUrl}
+                  target="_blank"
+                  noRouter={true}
+                >
+                  <Icon name={icons.CALENDAR_O} />
+                </FormInputButton>,
+              ]}
+              onChange={handleInputChange}
+              onFocus={handleLinkFocus}
+            />
+          </FormGroup>
+        </Form>
+      </ModalBody>
+
+      <ModalFooter>
+        <Button onPress={onModalClose}>{translate('Close')}</Button>
+      </ModalFooter>
+    </ModalContent>
+  );
+}
+
+export default CalendarLinkModalContent;
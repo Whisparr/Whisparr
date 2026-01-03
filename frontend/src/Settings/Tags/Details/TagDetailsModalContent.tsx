Sonarr/Sonarr

Convert Tags to TypeScript

frontend/src/Settings/Tags/Details/TagDetailsModalContent.tsx
@@ -1,0 +1,269 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+import ModelBase from 'App/ModelBase';
+import AppState from 'App/State/AppState';
+import FieldSet from 'Components/FieldSet';
+import Label from 'Components/Label';
+import Button from 'Components/Link/Button';
+import ModalBody from 'Components/Modal/ModalBody';
+import ModalContent from 'Components/Modal/ModalContent';
+import ModalFooter from 'Components/Modal/ModalFooter';
+import ModalHeader from 'Components/Modal/ModalHeader';
+import { kinds } from 'Helpers/Props';
+import createAllSeriesSelector from 'Store/Selectors/createAllSeriesSelector';
+import translate from 'Utilities/String/translate';
+import TagDetailsDelayProfile from './TagDetailsDelayProfile';
+import styles from './TagDetailsModalContent.css';
+
+function findMatchingItems<T extends ModelBase>(ids: number[], items: T[]) {
+  return items.filter((s) => {
+    return ids.includes(s.id);
+  });
+}
+
+function createUnorderedMatchingSeriesSelector(seriesIds: number[]) {
+  return createSelector(createAllSeriesSelector(), (series) =>
+    findMatchingItems(seriesIds, series)
+  );
+}
+
+function createMatchingSeriesSelector(seriesIds: number[]) {
+  return createSelector(
+    createUnorderedMatchingSeriesSelector(seriesIds),
+    (series) => {
+      return series.sort((seriesA, seriesB) => {
+        const sortTitleA = seriesA.sortTitle;
+        const sortTitleB = seriesB.sortTitle;
+
+        if (sortTitleA > sortTitleB) {
+          return 1;
+        } else if (sortTitleA < sortTitleB) {
+          return -1;
+        }
+
+        return 0;
+      });
+    }
+  );
+}
+
+function createMatchingItemSelector<T extends ModelBase>(
+  ids: number[],
+  selector: (state: AppState) => T[]
+) {
+  return createSelector(selector, (items) => findMatchingItems<T>(ids, items));
+}
+
+export interface TagDetailsModalContentProps {
+  label: string;
+  isTagUsed: boolean;
+  delayProfileIds: number[];
+  importListIds: number[];
+  notificationIds: number[];
+  restrictionIds: number[];
+  indexerIds: number[];
+  downloadClientIds: number[];
+  autoTagIds: number[];
+  seriesIds: number[];
+  onModalClose: () => void;
+  onDeleteTagPress: () => void;
+}
+
+function TagDetailsModalContent({
+  label,
+  isTagUsed,
+  delayProfileIds = [],
+  importListIds = [],
+  notificationIds = [],
+  restrictionIds = [],
+  indexerIds = [],
+  downloadClientIds = [],
+  autoTagIds = [],
+  seriesIds = [],
+  onModalClose,
+  onDeleteTagPress,
+}: TagDetailsModalContentProps) {
+  const series = useSelector(createMatchingSeriesSelector(seriesIds));
+
+  const delayProfiles = useSelector(
+    createMatchingItemSelector(
+      delayProfileIds,
+      (state: AppState) => state.settings.delayProfiles.items
+    )
+  );
+
+  const importLists = useSelector(
+    createMatchingItemSelector(
+      importListIds,
+      (state: AppState) => state.settings.importLists.items
+    )
+  );
+
+  const notifications = useSelector(
+    createMatchingItemSelector(
+      notificationIds,
+      (state: AppState) => state.settings.notifications.items
+    )
+  );
+
+  const releaseProfiles = useSelector(
+    createMatchingItemSelector(
+      restrictionIds,
+      (state: AppState) => state.settings.releaseProfiles.items
+    )
+  );
+
+  const indexers = useSelector(
+    createMatchingItemSelector(
+      indexerIds,
+      (state: AppState) => state.settings.indexers.items
+    )
+  );
+
+  const downloadClients = useSelector(
+    createMatchingItemSelector(
+      downloadClientIds,
+      (state: AppState) => state.settings.downloadClients.items
+    )
+  );
+
+  const autoTags = useSelector(
+    createMatchingItemSelector(
+      autoTagIds,
+      (state: AppState) => state.settings.autoTaggings.items
+    )
+  );
+
+  return (
+    <ModalContent onModalClose={onModalClose}>
+      <ModalHeader>{translate('TagDetails', { label })}</ModalHeader>
+
+      <ModalBody>
+        {!isTagUsed && <div>{translate('TagIsNotUsedAndCanBeDeleted')}</div>}
+
+        {series.length ? (
+          <FieldSet legend={translate('Series')}>
+            {series.map((item) => {
+              return <div key={item.id}>{item.title}</div>;
+            })}
+          </FieldSet>
+        ) : null}
+
+        {delayProfiles.length ? (
+          <FieldSet legend={translate('DelayProfile')}>
+            {delayProfiles.map((item) => {
+              const {
+                id,
+                preferredProtocol,
+                enableUsenet,
+                enableTorrent,
+                usenetDelay,
+                torrentDelay,
+              } = item;
+
+              return (
+                <TagDetailsDelayProfile
+                  key={id}
+                  preferredProtocol={preferredProtocol}
+                  enableUsenet={enableUsenet}
+                  enableTorrent={enableTorrent}
+                  usenetDelay={usenetDelay}
+                  torrentDelay={torrentDelay}
+                />
+              );
+            })}
+          </FieldSet>
+        ) : null}
+
+        {notifications.length ? (
+          <FieldSet legend={translate('Connections')}>
+            {notifications.map((item) => {
+              return <div key={item.id}>{item.name}</div>;
+            })}
+          </FieldSet>
+        ) : null}
+
+        {importLists.length ? (
+          <FieldSet legend={translate('ImportLists')}>
+            {importLists.map((item) => {
+              return <div key={item.id}>{item.name}</div>;
+            })}
+          </FieldSet>
+        ) : null}
+
+        {releaseProfiles.length ? (
+          <FieldSet legend={translate('ReleaseProfiles')}>
+            {releaseProfiles.map((item) => {
+              return (
+                <div key={item.id} className={styles.restriction}>
+                  <div>
+                    {item.required.map((r) => {
+                      return (
+                        <Label key={r} kind={kinds.SUCCESS}>
+                          {r}
+                        </Label>
+                      );
+                    })}
+                  </div>
+
+                  <div>
+                    {item.ignored.map((i) => {
+                      return (
+                        <Label key={i} kind={kinds.DANGER}>
+                          {i}
+                        </Label>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </FieldSet>
+        ) : null}
+
+        {indexers.length ? (
+          <FieldSet legend={translate('Indexers')}>
+            {indexers.map((item) => {
+              return <div key={item.id}>{item.name}</div>;
+            })}
+          </FieldSet>
+        ) : null}
+
+        {downloadClients.length ? (
+          <FieldSet legend={translate('DownloadClients')}>
+            {downloadClients.map((item) => {
+              return <div key={item.id}>{item.name}</div>;
+            })}
+          </FieldSet>
+        ) : null}
+
+        {autoTags.length ? (
+          <FieldSet legend={translate('AutoTagging')}>
+            {autoTags.map((item) => {
+              return <div key={item.id}>{item.name}</div>;
+            })}
+          </FieldSet>
+        ) : null}
+      </ModalBody>
+
+      <ModalFooter>
+        <Button
+          className={styles.deleteButton}
+          kind={kinds.DANGER}
+          title={
+            isTagUsed ? translate('TagCannotBeDeletedWhileInUse') : undefined
+          }
+          isDisabled={isTagUsed}
+          onPress={onDeleteTagPress}
+        >
+          {translate('Delete')}
+        </Button>
+
+        <Button onPress={onModalClose}>{translate('Close')}</Button>
+      </ModalFooter>
+    </ModalContent>
+  );
+}
+
+export default TagDetailsModalContent;

frontend/src/Settings/Tags/Tag.tsx
@@ -1,0 +1,151 @@
+import React, { useCallback, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Card from 'Components/Card';
+import ConfirmModal from 'Components/Modal/ConfirmModal';
+import { kinds } from 'Helpers/Props';
+import { deleteTag } from 'Store/Actions/tagActions';
+import createTagDetailsSelector from 'Store/Selectors/createTagDetailsSelector';
+import translate from 'Utilities/String/translate';
+import TagDetailsModal from './Details/TagDetailsModal';
+import TagInUse from './TagInUse';
+import styles from './Tag.css';
+
+interface TagProps {
+  id: number;
+  label: string;
+}
+
+function Tag({ id, label }: TagProps) {
+  const dispatch = useDispatch();
+  const {
+    delayProfileIds = [],
+    importListIds = [],
+    notificationIds = [],
+    restrictionIds = [],
+    indexerIds = [],
+    downloadClientIds = [],
+    autoTagIds = [],
+    seriesIds = [],
+  } = useSelector(createTagDetailsSelector(id)) ?? {};
+  const [isDetailsModalOpen, setIsDetailsModalOpen] = useState(false);
+  const [isDeleteTagModalOpen, setIsDeleteTagModalOpen] = useState(false);
+
+  const isTagUsed = !!(
+    delayProfileIds.length ||
+    importListIds.length ||
+    notificationIds.length ||
+    restrictionIds.length ||
+    indexerIds.length ||
+    downloadClientIds.length ||
+    autoTagIds.length ||
+    seriesIds.length
+  );
+
+  const handleShowDetailsPress = useCallback(() => {
+    setIsDetailsModalOpen(true);
+  }, []);
+
+  const handeDetailsModalClose = useCallback(() => {
+    setIsDetailsModalOpen(false);
+  }, []);
+
+  const handleDeleteTagPress = useCallback(() => {
+    setIsDetailsModalOpen(false);
+    setIsDeleteTagModalOpen(true);
+  }, []);
+
+  const handleConfirmDeleteTag = useCallback(() => {
+    setIsDeleteTagModalOpen(false);
+  }, []);
+
+  const handleDeleteTagModalClose = useCallback(() => {
+    dispatch(deleteTag({ id }));
+  }, [id, dispatch]);
+
+  return (
+    <Card
+      className={styles.tag}
+      overlayContent={true}
+      onPress={handleShowDetailsPress}
+    >
+      <div className={styles.label}>{label}</div>
+
+      {isTagUsed ? (
+        <div>
+          <TagInUse label={translate('Series')} count={seriesIds.length} />
+
+          <TagInUse
+            label={translate('DelayProfile')}
+            labelPlural={translate('DelayProfiles')}
+            count={delayProfileIds.length}
+          />
+
+          <TagInUse
+            label={translate('ImportList')}
+            labelPlural={translate('ImportLists')}
+            count={importListIds.length}
+          />
+
+          <TagInUse
+            label={translate('Connection')}
+            labelPlural={translate('Connections')}
+            count={notificationIds.length}
+          />
+
+          <TagInUse
+            label={translate('ReleaseProfile')}
+            labelPlural={translate('ReleaseProfiles')}
+            count={restrictionIds.length}
+          />
+
+          <TagInUse
+            label={translate('Indexer')}
+            labelPlural={translate('Indexers')}
+            count={indexerIds.length}
+          />
+
+          <TagInUse
+            label={translate('DownloadClient')}
+            labelPlural={translate('DownloadClients')}
+            count={downloadClientIds.length}
+          />
+
+          <TagInUse
+            label={translate('AutoTagging')}
+            count={autoTagIds.length}
+          />
+        </div>
+      ) : null}
+
+      {!isTagUsed && <div>{translate('NoLinks')}</div>}
+
+      <TagDetailsModal
+        label={label}
+        isTagUsed={isTagUsed}
+        seriesIds={seriesIds}
+        delayProfileIds={delayProfileIds}
+        importListIds={importListIds}
+        notificationIds={notificationIds}
+        restrictionIds={restrictionIds}
+        indexerIds={indexerIds}
+        downloadClientIds={downloadClientIds}
+        autoTagIds={autoTagIds}
+        isOpen={isDetailsModalOpen}
+        onModalClose={handeDetailsModalClose}
+        onDeleteTagPress={handleDeleteTagPress}
+      />
+
+      <ConfirmModal
+        isOpen={isDeleteTagModalOpen}
+        kind={kinds.DANGER}
+        title={translate('DeleteTag')}
+        message={translate('DeleteTagMessageText', { label })}
+        confirmLabel={translate('Delete')}
+        onConfirm={handleConfirmDeleteTag}
+        onCancel={handleDeleteTagModalClose}
+      />
+    </Card>
+  );
+}
+
+export default Tag;

Whisparr/Whisparr

(cherry picked from commit radarr/radarr@ac33b15048)

frontend/src/Settings/Tags/Details/TagDetailsModalContent.tsx
@@ -1,0 +1,269 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+import ModelBase from 'App/ModelBase';
+import AppState from 'App/State/AppState';
+import FieldSet from 'Components/FieldSet';
+import Label from 'Components/Label';
+import Button from 'Components/Link/Button';
+import ModalBody from 'Components/Modal/ModalBody';
+import ModalContent from 'Components/Modal/ModalContent';
+import ModalFooter from 'Components/Modal/ModalFooter';
+import ModalHeader from 'Components/Modal/ModalHeader';
+import { kinds } from 'Helpers/Props';
+import createAllMoviesSelector from 'Store/Selectors/createAllMoviesSelector';
+import translate from 'Utilities/String/translate';
+import TagDetailsDelayProfile from './TagDetailsDelayProfile';
+import styles from './TagDetailsModalContent.css';
+
+function findMatchingItems<T extends ModelBase>(ids: number[], items: T[]) {
+  return items.filter((s) => {
+    return ids.includes(s.id);
+  });
+}
+
+function createUnorderedMatchingMoviesSelector(movieIds: number[]) {
+  return createSelector(createAllMoviesSelector(), (movies) =>
+    findMatchingItems(movieIds, movies)
+  );
+}
+
+function createMatchingMoviesSelector(movieIds: number[]) {
+  return createSelector(
+    createUnorderedMatchingMoviesSelector(movieIds),
+    (movies) => {
+      return movies.sort((movieA, movieB) => {
+        const sortTitleA = movieA.sortTitle;
+        const sortTitleB = movieB.sortTitle;
+
+        if (sortTitleA > sortTitleB) {
+          return 1;
+        } else if (sortTitleA < sortTitleB) {
+          return -1;
+        }
+
+        return 0;
+      });
+    }
+  );
+}
+
+function createMatchingItemSelector<T extends ModelBase>(
+  ids: number[],
+  selector: (state: AppState) => T[]
+) {
+  return createSelector(selector, (items) => findMatchingItems<T>(ids, items));
+}
+
+export interface TagDetailsModalContentProps {
+  label: string;
+  isTagUsed: boolean;
+  delayProfileIds: number[];
+  importListIds: number[];
+  notificationIds: number[];
+  releaseProfileIds: number[];
+  indexerIds: number[];
+  downloadClientIds: number[];
+  autoTagIds: number[];
+  movieIds: number[];
+  onModalClose: () => void;
+  onDeleteTagPress: () => void;
+}
+
+function TagDetailsModalContent({
+  label,
+  isTagUsed,
+  delayProfileIds = [],
+  importListIds = [],
+  notificationIds = [],
+  releaseProfileIds = [],
+  indexerIds = [],
+  downloadClientIds = [],
+  autoTagIds = [],
+  movieIds = [],
+  onModalClose,
+  onDeleteTagPress,
+}: TagDetailsModalContentProps) {
+  const movies = useSelector(createMatchingMoviesSelector(movieIds));
+
+  const delayProfiles = useSelector(
+    createMatchingItemSelector(
+      delayProfileIds,
+      (state: AppState) => state.settings.delayProfiles.items
+    )
+  );
+
+  const importLists = useSelector(
+    createMatchingItemSelector(
+      importListIds,
+      (state: AppState) => state.settings.importLists.items
+    )
+  );
+
+  const notifications = useSelector(
+    createMatchingItemSelector(
+      notificationIds,
+      (state: AppState) => state.settings.notifications.items
+    )
+  );
+
+  const releaseProfiles = useSelector(
+    createMatchingItemSelector(
+      releaseProfileIds,
+      (state: AppState) => state.settings.releaseProfiles.items
+    )
+  );
+
+  const indexers = useSelector(
+    createMatchingItemSelector(
+      indexerIds,
+      (state: AppState) => state.settings.indexers.items
+    )
+  );
+
+  const downloadClients = useSelector(
+    createMatchingItemSelector(
+      downloadClientIds,
+      (state: AppState) => state.settings.downloadClients.items
+    )
+  );
+
+  const autoTags = useSelector(
+    createMatchingItemSelector(
+      autoTagIds,
+      (state: AppState) => state.settings.autoTaggings.items
+    )
+  );
+
+  return (
+    <ModalContent onModalClose={onModalClose}>
+      <ModalHeader>{translate('TagDetails', { label })}</ModalHeader>
+
+      <ModalBody>
+        {!isTagUsed && <div>{translate('TagIsNotUsedAndCanBeDeleted')}</div>}
+
+        {movies.length ? (
+          <FieldSet legend={translate('Movies')}>
+            {movies.map((item) => {
+              return <div key={item.id}>{item.title}</div>;
+            })}
+          </FieldSet>
+        ) : null}
+
+        {delayProfiles.length ? (
+          <FieldSet legend={translate('DelayProfile')}>
+            {delayProfiles.map((item) => {
+              const {
+                id,
+                preferredProtocol,
+                enableUsenet,
+                enableTorrent,
+                usenetDelay,
+                torrentDelay,
+              } = item;
+
+              return (
+                <TagDetailsDelayProfile
+                  key={id}
+                  preferredProtocol={preferredProtocol}
+                  enableUsenet={enableUsenet}
+                  enableTorrent={enableTorrent}
+                  usenetDelay={usenetDelay}
+                  torrentDelay={torrentDelay}
+                />
+              );
+            })}
+          </FieldSet>
+        ) : null}
+
+        {notifications.length ? (
+          <FieldSet legend={translate('Connections')}>
+            {notifications.map((item) => {
+              return <div key={item.id}>{item.name}</div>;
+            })}
+          </FieldSet>
+        ) : null}
+
+        {importLists.length ? (
+          <FieldSet legend={translate('ImportLists')}>
+            {importLists.map((item) => {
+              return <div key={item.id}>{item.name}</div>;
+            })}
+          </FieldSet>
+        ) : null}
+
+        {releaseProfiles.length ? (
+          <FieldSet legend={translate('ReleaseProfiles')}>
+            {releaseProfiles.map((item) => {
+              return (
+                <div key={item.id} className={styles.restriction}>
+                  <div>
+                    {item.required.map((r) => {
+                      return (
+                        <Label key={r} kind={kinds.SUCCESS}>
+                          {r}
+                        </Label>
+                      );
+                    })}
+                  </div>
+
+                  <div>
+                    {item.ignored.map((i) => {
+                      return (
+                        <Label key={i} kind={kinds.DANGER}>
+                          {i}
+                        </Label>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </FieldSet>
+        ) : null}
+
+        {indexers.length ? (
+          <FieldSet legend={translate('Indexers')}>
+            {indexers.map((item) => {
+              return <div key={item.id}>{item.name}</div>;
+            })}
+          </FieldSet>
+        ) : null}
+
+        {downloadClients.length ? (
+          <FieldSet legend={translate('DownloadClients')}>
+            {downloadClients.map((item) => {
+              return <div key={item.id}>{item.name}</div>;
+            })}
+          </FieldSet>
+        ) : null}
+
+        {autoTags.length ? (
+          <FieldSet legend={translate('AutoTagging')}>
+            {autoTags.map((item) => {
+              return <div key={item.id}>{item.name}</div>;
+            })}
+          </FieldSet>
+        ) : null}
+      </ModalBody>
+
+      <ModalFooter>
+        <Button
+          className={styles.deleteButton}
+          kind={kinds.DANGER}
+          title={
+            isTagUsed ? translate('TagCannotBeDeletedWhileInUse') : undefined
+          }
+          isDisabled={isTagUsed}
+          onPress={onDeleteTagPress}
+        >
+          {translate('Delete')}
+        </Button>
+
+        <Button onPress={onModalClose}>{translate('Close')}</Button>
+      </ModalFooter>
+    </ModalContent>
+  );
+}
+
+export default TagDetailsModalContent;

frontend/src/Settings/Tags/Tag.tsx
@@ -1,0 +1,155 @@
+import React, { useCallback, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Card from 'Components/Card';
+import ConfirmModal from 'Components/Modal/ConfirmModal';
+import { kinds } from 'Helpers/Props';
+import { deleteTag } from 'Store/Actions/tagActions';
+import createTagDetailsSelector from 'Store/Selectors/createTagDetailsSelector';
+import translate from 'Utilities/String/translate';
+import TagDetailsModal from './Details/TagDetailsModal';
+import TagInUse from './TagInUse';
+import styles from './Tag.css';
+
+interface TagProps {
+  id: number;
+  label: string;
+}
+
+function Tag({ id, label }: TagProps) {
+  const dispatch = useDispatch();
+  const {
+    delayProfileIds = [],
+    importListIds = [],
+    notificationIds = [],
+    releaseProfileIds = [],
+    indexerIds = [],
+    downloadClientIds = [],
+    autoTagIds = [],
+    movieIds = [],
+  } = useSelector(createTagDetailsSelector(id)) ?? {};
+  const [isDetailsModalOpen, setIsDetailsModalOpen] = useState(false);
+  const [isDeleteTagModalOpen, setIsDeleteTagModalOpen] = useState(false);
+
+  const isTagUsed = !!(
+    delayProfileIds.length ||
+    importListIds.length ||
+    notificationIds.length ||
+    releaseProfileIds.length ||
+    indexerIds.length ||
+    downloadClientIds.length ||
+    autoTagIds.length ||
+    movieIds.length
+  );
+
+  const handleShowDetailsPress = useCallback(() => {
+    setIsDetailsModalOpen(true);
+  }, []);
+
+  const handeDetailsModalClose = useCallback(() => {
+    setIsDetailsModalOpen(false);
+  }, []);
+
+  const handleDeleteTagPress = useCallback(() => {
+    setIsDetailsModalOpen(false);
+    setIsDeleteTagModalOpen(true);
+  }, []);
+
+  const handleConfirmDeleteTag = useCallback(() => {
+    setIsDeleteTagModalOpen(false);
+  }, []);
+
+  const handleDeleteTagModalClose = useCallback(() => {
+    dispatch(deleteTag({ id }));
+  }, [id, dispatch]);
+
+  return (
+    <Card
+      className={styles.tag}
+      overlayContent={true}
+      onPress={handleShowDetailsPress}
+    >
+      <div className={styles.label}>{label}</div>
+
+      {isTagUsed ? (
+        <div>
+          <TagInUse
+            label={translate('Movie')}
+            labelPlural={translate('Movies')}
+            count={movieIds.length}
+          />
+
+          <TagInUse
+            label={translate('DelayProfile')}
+            labelPlural={translate('DelayProfiles')}
+            count={delayProfileIds.length}
+          />
+
+          <TagInUse
+            label={translate('ImportList')}
+            labelPlural={translate('ImportLists')}
+            count={importListIds.length}
+          />
+
+          <TagInUse
+            label={translate('Connection')}
+            labelPlural={translate('Connections')}
+            count={notificationIds.length}
+          />
+
+          <TagInUse
+            label={translate('ReleaseProfile')}
+            labelPlural={translate('ReleaseProfiles')}
+            count={releaseProfileIds.length}
+          />
+
+          <TagInUse
+            label={translate('Indexer')}
+            labelPlural={translate('Indexers')}
+            count={indexerIds.length}
+          />
+
+          <TagInUse
+            label={translate('DownloadClient')}
+            labelPlural={translate('DownloadClients')}
+            count={downloadClientIds.length}
+          />
+
+          <TagInUse
+            label={translate('AutoTagging')}
+            count={autoTagIds.length}
+          />
+        </div>
+      ) : null}
+
+      {!isTagUsed && <div>{translate('NoLinks')}</div>}
+
+      <TagDetailsModal
+        label={label}
+        isTagUsed={isTagUsed}
+        movieIds={movieIds}
+        delayProfileIds={delayProfileIds}
+        importListIds={importListIds}
+        notificationIds={notificationIds}
+        releaseProfileIds={releaseProfileIds}
+        indexerIds={indexerIds}
+        downloadClientIds={downloadClientIds}
+        autoTagIds={autoTagIds}
+        isOpen={isDetailsModalOpen}
+        onModalClose={handeDetailsModalClose}
+        onDeleteTagPress={handleDeleteTagPress}
+      />
+
+      <ConfirmModal
+        isOpen={isDeleteTagModalOpen}
+        kind={kinds.DANGER}
+        title={translate('DeleteTag')}
+        message={translate('DeleteTagMessageText', { label })}
+        confirmLabel={translate('Delete')}
+        onConfirm={handleConfirmDeleteTag}
+        onCancel={handleDeleteTagModalClose}
+      />
+    </Card>
+  );
+}
+
+export default Tag;
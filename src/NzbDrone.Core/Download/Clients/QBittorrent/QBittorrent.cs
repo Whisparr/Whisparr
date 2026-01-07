1b844efb63619c15005e62665836b822b35e0f96
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-01-07 05:13:25 +0200

    New: API key support for qBittorrent

--- src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrent.cs
+++ src/NzbDrone.Core/Download/Clients/QBittorrent/QBittorrent.cs
@@ -471,9 +471,9 @@ namespace NzbDrone.Core.Download.Clients.QBittorrent
             catch (DownloadClientAuthenticationException ex)
             {
                 _logger.Error(ex, ex.Message);
-                return new NzbDroneValidationFailure("Username", "Authentication failure")
+                return new NzbDroneValidationFailure(Settings.ApiKey.IsNotNullOrWhiteSpace() ? "ApiKey" : "Username", "Authentication failure")
                 {
-                    DetailedDescription = "Please verify your username and password."
+                    DetailedDescription = "Please verify your credentials."
                 };
             }
             catch (WebException ex)

c558fb0347afb9edc6eb1b41ddf1e34f1fd186c1
JoyboySon <249372569+JoyboySon@users.noreply.github.com>
2026-01-08 16:57:26 -0800

    New: API Docs Icon
    Easier access and visibility to open api docs

--- frontend/src/Components/Page/Header/PageHeader.tsx
+++ frontend/src/Components/Page/Header/PageHeader.tsx
@@ -80,6 +80,14 @@ function PageHeader({ isSmallScreen }: PageHeaderProps) {
         <SafeForWorkButtonConnector />
 
         <IconButton
+          className={styles.translate}
+          title={translate('ApiDocumentation')}
+          name={icons.TRANSLATE}
+          to="/docs"
+          size={24}
+        />
+
+        <IconButton
           className={styles.donate}
           name={icons.HEART}
           aria-label={translate('Donate')}

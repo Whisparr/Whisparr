35283859f22a257d8e88295a0a888ebcfda322a9
Gykes <Gykes@pm.me>
2026-01-23 18:26:34 -0800

    Fixed: Retrying download for pushed releases

--- src/NzbDrone.Core/Datastore/CorruptDatabaseException.cs
+++ src/NzbDrone.Core/Datastore/CorruptDatabaseException.cs
@@ -3,7 +3,7 @@ using NzbDrone.Common.Exceptions;
 
 namespace NzbDrone.Core.Datastore
 {
-    public class CorruptDatabaseException : SonarrStartupException
+    public class CorruptDatabaseException : WhisparrStartupException
     {
         public CorruptDatabaseException(string message, params object[] args)
             : base(message, args)

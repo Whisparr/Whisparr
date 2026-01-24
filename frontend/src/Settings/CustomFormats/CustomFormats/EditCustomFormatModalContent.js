f652b89430edcd9fba142491d2c5a1cf5ee9ae61
Gykes <Gykes@pm.me>
2026-01-23 19:13:08 -0800

    Fix missing Alert import in EditCustomFormatModalContent

--- frontend/src/Settings/CustomFormats/CustomFormats/EditCustomFormatModalContent.js
+++ frontend/src/Settings/CustomFormats/CustomFormats/EditCustomFormatModalContent.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import Alert from 'Components/Alert';
 import Card from 'Components/Card';
 import FieldSet from 'Components/FieldSet';
 import Form from 'Components/Form/Form';

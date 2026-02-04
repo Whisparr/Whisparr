d078722fd3f790b408973c3dea7461997f6e1fcf
Gykes <Gykes@pm.me>
2026-02-04 00:05:59 -0800

    Fixed: Formatting empty size on disk values

--- frontend/src/Series/Details/SeriesDetails.js
+++ frontend/src/Series/Details/SeriesDetails.js
@@ -208,8 +208,8 @@ class SeriesDetails extends Component {
     } = this.props;
 
     const {
-      episodeFileCount,
-      sizeOnDisk
+      episodeFileCount = 0,
+      sizeOnDisk = 0
     } = statistics;
 
     const {
@@ -459,10 +459,9 @@ class SeriesDetails extends Component {
                             name={icons.DRIVE}
                             size={17}
                           />
+
                           <span className={styles.sizeOnDisk}>
-                            {
-                              formatBytes(sizeOnDisk || 0)
-                            }
+                            {formatBytes(sizeOnDisk)}
                           </span>
                         </div>
                       </Label>

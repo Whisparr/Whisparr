da0b5109b8d4e0a2e35a0d003c2e9db12fba4abd
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-08-25 10:24:34 -0500

    Fixed: Prevent exception for seed configuration provider with invalid indexer ID (Sonarr/Sonarr@f7b54f9d6)

--- src/NzbDrone.Core.Test/IndexerTests/SeedConfigProviderFixture.cs
+++ src/NzbDrone.Core.Test/IndexerTests/SeedConfigProviderFixture.cs
@@ -28,5 +28,24 @@ namespace NzbDrone.Core.Test.IndexerTests
 
             result.Should().BeNull();
         }
+
+        [Test]
+        public void should_not_return_config_for_invalid_indexer()
+        {
+            Mocker.GetMock<ICachedIndexerSettingsProvider>()
+                  .Setup(v => v.GetSettings(It.IsAny<int>()))
+                  .Returns<CachedIndexerSettings>(null);
+
+            var result = Subject.GetSeedConfiguration(new RemoteEpisode
+            {
+                Release = new ReleaseInfo
+                {
+                    DownloadProtocol = DownloadProtocol.Torrent,
+                    IndexerId = 1
+                }
+            });
+
+            result.Should().BeNull();
+        }
     }
 }

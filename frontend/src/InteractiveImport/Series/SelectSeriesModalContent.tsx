a1d8ca9459c1557a7c8fc56a9a7a586355f8db65
Gykes <24581046+Gykes@users.noreply.github.com>
2026-08-25 10:00:10 -0700

    Chore: Sonarr Upstream Sync - Convert Form Components to TypeScript (#1147)
    * Convert FormInputButton to TypeScript (Sonarr/Sonarr@32fa63d24)
    
    * Convert Form Components to TypeScript (Sonarr/Sonarr@682d2b4e1)
    
    * Fixed: Tooltips for detailed error messages (Sonarr/Sonarr@c589c4f85)
    
    * Fix: Adding a new root folder from edit series modal (Sonarr/Sonarr@983b079c8)
    
    * Fixed: Drop downs flickering in some cases (Sonarr/Sonarr@3b024443c)
    
    ---------
    
    Co-authored-by: Bogdan <mynameisbogdan@users.noreply.github.com>
    Co-authored-by: Mark McDowall <mark@mcdowall.ca>
    Co-authored-by: Stevie Robinson <stevie.robinson@gmail.com>

--- frontend/src/InteractiveImport/Series/SelectSeriesModalContent.tsx
+++ frontend/src/InteractiveImport/Series/SelectSeriesModalContent.tsx
@@ -21,6 +21,7 @@ import { scrollDirections } from 'Helpers/Props';
 import Series from 'Series/Series';
 import createAllSeriesSelector from 'Store/Selectors/createAllSeriesSelector';
 import dimensions from 'Styles/Variables/dimensions';
+import { InputChanged } from 'typings/inputs';
 import sortByProp from 'Utilities/Array/sortByProp';
 import translate from 'Utilities/String/translate';
 import SelectSeriesModalTableHeader from './SelectSeriesModalTableHeader';
@@ -143,7 +144,7 @@ function SelectSeriesModalContent(props: SelectSeriesModalContentProps) {
   }, [listRef, scrollerRef]);
 
   const onFilterChange = useCallback(
-    ({ value }: { value: string }) => {
+    ({ value }: InputChanged<string>) => {
       setFilter(value);
     },
     [setFilter]

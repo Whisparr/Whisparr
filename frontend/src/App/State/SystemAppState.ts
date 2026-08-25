2b672dcf787df83c06693e9aeffbc64821da2f59
Mark McDowall <mark@mcdowall.ca>
2026-08-25 18:22:13 -0500

    Convert Log FIles to TypeScript (Sonarr/Sonarr@95929dd9c)

--- frontend/src/App/State/SystemAppState.ts
+++ frontend/src/App/State/SystemAppState.ts
@@ -1,5 +1,6 @@
 import DiskSpace from 'typings/DiskSpace';
 import Health from 'typings/Health';
+import LogFile from 'typings/LogFile';
 import SystemStatus from 'typings/SystemStatus';
 import Task from 'typings/Task';
 import Update from 'typings/Update';
@@ -10,17 +11,20 @@ import LogsAppState from './LogsAppState';
 export type DiskSpaceAppState = AppSectionState<DiskSpace>;
 export type HealthAppState = AppSectionState<Health>;
 export type SystemStatusAppState = AppSectionItemState<SystemStatus>;
-export type UpdateAppState = AppSectionState<Update>;
 export type TaskAppState = AppSectionState<Task>;
+export type LogFilesAppState = AppSectionState<LogFile>;
+export type UpdateAppState = AppSectionState<Update>;
 
 interface SystemAppState {
   backups: BackupAppState;
   diskSpace: DiskSpaceAppState;
   health: HealthAppState;
-  updates: UpdateAppState;
+  logFiles: LogFilesAppState;
   logs: LogsAppState;
   status: SystemStatusAppState;
   tasks: TaskAppState;
+  updateLogFiles: LogFilesAppState;
+  updates: UpdateAppState;
 }
 
 export default SystemAppState;

1b3ceee2c18455eb3102b331882c4e60fd3d7886
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-08-27 17:24:00 -0500

    Fix finding last command run (Sonarr/Sonarr@77612810d)

--- src/NzbDrone.Core.Test/ParserTests/ReleaseGroupParserFixture.cs
+++ src/NzbDrone.Core.Test/ParserTests/ReleaseGroupParserFixture.cs
@@ -47,6 +47,9 @@ namespace NzbDrone.Core.Test.ParserTests
         [TestCase("Series Title S01 REMUX Dual Audio AVC 1080p 8-Bit-ZR-", "ZR")]
         [TestCase("Series (2008) S05E01 (1080p BDRip DDP2.0 x265) - Vialle", "Vialle")]
         [TestCase("The Series (2023) S03E01 (1080p DS4K NF Webrip DV HDR DDP5.1 x265) - Vialle.mkv", "Vialle")]
+        [TestCase("Show.Name.2009.S01.1080p.BluRay.DTS5.1.x264-D-Z0N3", "D-Z0N3")]
+        [TestCase("Series Stampede S01 MULTi 1080p BD x265 Opus AAC -Báleygr", "Báleygr")]
+        [TestCase("Series S01E01 VOSTFR 1080p WEB x265 EAC3 -Hveðrungr", "Hveðrungr")]
         public void should_parse_release_group(string title, string expected)
         {
             Parser.Parser.ParseReleaseGroup(title).Should().Be(expected);

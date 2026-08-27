03f1cd779ac56bab8cb8a16c4da7b5ce3cfe026f
康小广 <kenkangxgwe@gmail.com>
2026-08-27 13:13:30 -0500

    Follow redirects when fetching Custom Lists (Sonarr/Sonarr@7fdc4d663)

--- src/NzbDrone.Common/Http/HttpRequestBuilder.cs
+++ src/NzbDrone.Common/Http/HttpRequestBuilder.cs
@@ -393,5 +393,12 @@ namespace NzbDrone.Common.Http
 
             return this;
         }
+
+        public virtual HttpRequestBuilder AllowRedirect(bool allowAutoRedirect = true)
+        {
+            AllowAutoRedirect = allowAutoRedirect;
+
+            return this;
+        }
     }
 }

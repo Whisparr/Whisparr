69874de51887fccf19c25717a39be3a033a13d17
Stevie Robinson <stevie.robinson@gmail.com>
2026-08-27 23:20:24 -0500

    Remove the default indexer definitions for AnimeTosho (Sonarr/Sonarr@fdda9abcb)

--- src/NzbDrone.Core/Indexers/Torznab/Torznab.cs
+++ src/NzbDrone.Core/Indexers/Torznab/Torznab.cs
@@ -9,7 +9,6 @@ using NzbDrone.Common.Http;
 using NzbDrone.Core.Configuration;
 using NzbDrone.Core.Indexers.Newznab;
 using NzbDrone.Core.Parser;
-using NzbDrone.Core.ThingiProvider;
 using NzbDrone.Core.Validation;
 
 namespace NzbDrone.Core.Indexers.Torznab
@@ -42,14 +41,6 @@ namespace NzbDrone.Core.Indexers.Torznab
         public override IParseIndexerResponse GetParser()
         {
             return new TorznabRssParser();
-        }
-
-        public override IEnumerable<ProviderDefinition> DefaultDefinitions
-        {
-            get
-            {
-                yield return GetDefinition("AnimeTosho Torrents", GetSettings("https://feed.animetosho.org", apiPath: @"/nabapi", categories: Array.Empty<int>(), animeCategories: new[] { 5070 }));
-            }
         }
 
         private IndexerDefinition GetDefinition(string name, TorznabSettings settings)

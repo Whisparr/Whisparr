e214302fb53084d720d97dc1d789fb5911a5c3a7
Mark McDowall <mark@mcdowall.ca>
2026-08-29 15:54:53 -0500

    Use react-query for Log Files (Sonarr/Sonarr@ff5e73273)

--- frontend/src/Store/Actions/systemActions.js
+++ frontend/src/Store/Actions/systemActions.js
@@ -143,20 +143,6 @@ export const defaultState = {
         ]
       }
     ]
-  },
-
-  logFiles: {
-    isFetching: false,
-    isPopulated: false,
-    error: null,
-    items: []
-  },
-
-  updateLogFiles: {
-    isFetching: false,
-    isPopulated: false,
-    error: null,
-    items: []
   }
 };
 
@@ -193,9 +179,6 @@ export const SET_LOGS_FILTER = 'system/logs/setLogsFilter';
 export const SET_LOGS_TABLE_OPTION = 'system/logs/setLogsTableOption';
 export const CLEAR_LOGS_TABLE = 'system/logs/clearLogsTable';
 
-export const FETCH_LOG_FILES = 'system/logFiles/fetchLogFiles';
-export const FETCH_UPDATE_LOG_FILES = 'system/updateLogFiles/fetchUpdateLogFiles';
-
 export const RESTART = 'system/restart';
 export const SHUTDOWN = 'system/shutdown';
 
@@ -224,9 +207,6 @@ export const setLogsSort = createThunk(SET_LOGS_SORT);
 export const setLogsFilter = createThunk(SET_LOGS_FILTER);
 export const setLogsTableOption = createAction(SET_LOGS_TABLE_OPTION);
 export const clearLogsTable = createAction(CLEAR_LOGS_TABLE);
-
-export const fetchLogFiles = createThunk(FETCH_LOG_FILES);
-export const fetchUpdateLogFiles = createThunk(FETCH_UPDATE_LOG_FILES);
 
 export const restart = createThunk(RESTART);
 export const shutdown = createThunk(SHUTDOWN);
@@ -305,8 +285,6 @@ export const actionHandlers = handleThunks({
   [DELETE_BACKUP]: createRemoveItemHandler(backupsSection, '/system/backup'),
 
   [FETCH_UPDATES]: createFetchHandler('system.updates', '/update'),
-  [FETCH_LOG_FILES]: createFetchHandler('system.logFiles', '/log/file'),
-  [FETCH_UPDATE_LOG_FILES]: createFetchHandler('system.updateLogFiles', '/log/file/update'),
 
   ...createServerSideCollectionHandlers(
     'system.logs',
--- frontend/src/System/Logs/Update/UpdateLogFiles.tsx
+++ frontend/src/System/Logs/Update/UpdateLogFiles.tsx
@@ -1,46 +1,39 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import AppState from 'App/State/AppState';
 import * as commandNames from 'Commands/commandNames';
 import { executeCommand } from 'Store/Actions/commandActions';
-import { fetchUpdateLogFiles } from 'Store/Actions/systemActions';
 import createCommandExecutingSelector from 'Store/Selectors/createCommandExecutingSelector';
 import LogFiles from '../LogFiles';
+import { useUpdateLogFiles } from '../useLogFiles';
 
 function UpdateLogFiles() {
   const dispatch = useDispatch();
-  const { isFetching, items } = useSelector(
-    (state: AppState) => state.system.updateLogFiles
-  );
+  const { data = [], isFetching, refetch } = useUpdateLogFiles();
 
   const isDeleteFilesExecuting = useSelector(
     createCommandExecutingSelector(commandNames.DELETE_UPDATE_LOG_FILES)
   );
 
   const handleRefreshPress = useCallback(() => {
-    dispatch(fetchUpdateLogFiles());
-  }, [dispatch]);
+    refetch();
+  }, [refetch]);
 
   const handleDeleteFilesPress = useCallback(() => {
     dispatch(
       executeCommand({
         name: commandNames.DELETE_UPDATE_LOG_FILES,
         commandFinished: () => {
-          dispatch(fetchUpdateLogFiles());
+          refetch();
         },
       })
     );
-  }, [dispatch]);
-
-  useEffect(() => {
-    dispatch(fetchUpdateLogFiles());
-  }, [dispatch]);
+  }, [dispatch, refetch]);
 
   return (
     <LogFiles
       isDeleteFilesExecuting={isDeleteFilesExecuting}
       isFetching={isFetching}
-      items={items}
+      items={data}
       type="update"
       onRefreshPress={handleRefreshPress}
       onDeleteFilesPress={handleDeleteFilesPress}

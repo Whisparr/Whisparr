7517094ed42191484dd3ea34d4046a77350a790a
Gykes <24581046+Gykes@users.noreply.github.com>
2026-09-19 13:41:09 -0700

    Chore: Don't show Open Browser setting on unsupported setups (#1318)
    Don't show Open Browser setting on unsupported setups (Sonarr/Sonarr@5bebd3810)
    
    Co-authored-by: Mark McDowall <mark@mcdowall.ca>

--- frontend/src/Settings/General/HostSettings.tsx
+++ frontend/src/Settings/General/HostSettings.tsx
@@ -5,7 +5,7 @@ import FormInputGroup from 'Components/Form/FormInputGroup';
 import FormLabel from 'Components/Form/FormLabel';
 import useShowAdvancedSettings from 'Helpers/Hooks/useShowAdvancedSettings';
 import { inputTypes, sizes } from 'Helpers/Props';
-import { useIsWindowsService } from 'System/Status/useSystemStatus';
+import { useSystemStatusData } from 'System/Status/useSystemStatus';
 import { InputChanged } from 'typings/inputs';
 import { PendingSection } from 'typings/pending';
 import General from 'typings/Settings/General';
@@ -43,7 +43,7 @@ function HostSettings({
   onInputChange,
 }: HostSettingsProps) {
   const showAdvancedSettings = useShowAdvancedSettings();
-  const isWindowsService = useIsWindowsService();
+  const { isWindows, mode } = useSystemStatusData();
 
   return (
     <FieldSet legend={translate('Host')}>
@@ -202,7 +202,7 @@ function HostSettings({
         </>
       ) : null}
 
-      {isWindowsService ? (
+      {isWindows && mode !== 'service' ? (
         <FormGroup size={sizes.MEDIUM}>
           <FormLabel>{translate('OpenBrowserOnStart')}</FormLabel>
 
